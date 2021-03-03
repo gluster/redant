a03bf16f0418a2d79c00c70c237c596b4782e468
aujjwal-redhat <aujjwal@redhat.com>
2021-03-03 17:59:21 +0530

    Added the path
    sys.path.append added to work in all the files
    
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

--- redant_libs/redant_resources.py
+++ redant_libs/redant_resources.py
@@ -1,3 +1,7 @@
+import sys
+
+# sys.path.append("./odinControl")
+
 from rexe.rexe import Rexe as R
 from rexe.rexe import (
     set_logging_options,
@@ -11,7 +15,7 @@ class Redant_Resources:
     """
     For testing purpose this class is added
     """
-    
+
     def __init__(self, log_file_path, log_file_level):
 
         set_logging_options(log_file_level=log_file_level,log_file_path=log_file_path)
