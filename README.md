718635afd8b32ed845cb462af09facf9bb378fd9
aujjwal-redhat <aujjwal@redhat.com>
2021-03-03 16:49:56 +0530

    Added pool status lib
    Added pool status module. Modified README as well for better understanding.
    
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # RedAnt_Test
 
-Structure:
+### Structure:
 
-redant_libs: consists of the libs and ops that will help in running the test cases.
-tests: holds the test cases. Add any new test cases here.
+redant_libs: consists of the libs and ops that will help in running the test cases.<br>
+tests: holds the test cases. Add any new test cases here.<br>
+
+### To start Working:
+
+1. Clone the odinControl inside the RedAnt_Test. [This is temporary. Once odinControl is converted into a python package we won't need this step]. You can directly call the rexe from the odinControl or use a directory rexe where you can put **__init__.py** and **rexe.py** and hence call the file.
+
+2. Create a **Utilities** folder and add **conf.yaml** file [This is temporary as well just for testing. Once we are ready with the components we just need to connect them.]
+ OR
+ You can also call the conf.yaml from the odinControl. I have used it here just to indicate the independence of each component. But reusability of existing files won't hurt. :sweat_smile:
--- redant_libs/peer_ops.py
+++ redant_libs/peer_ops.py
@@ -9,6 +9,18 @@ from redant_resources import (
 import pprint   # to print the output in a better way and hence more understandable
 
 R = Rexe(conf_path="./Utilities/conf.yaml")
+
+"""
+For testing temporarily  
+
+Utilities/conf.yaml
+
+host_list: IPs
+user: user
+passwd: passwd
+
+"""
+
 RR = Redant_Resources(log_file_path='./redant.log',log_file_level='D')
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -54,8 +66,38 @@ def peer_status():
 
     return ret
 
+def pool_list(node):
+    """
+
+    runs the command gluster pool list on `node`
+    """
+    cmd = 'gluster pool list'
+
+    logger.info("Running the command "+cmd)
+    
+    ret = R.execute_command(node=node, cmd=cmd)
+
+    #TODO: remove the print
+    pp.pprint(ret)
+
+    logger.info(ret)
+
+    return ret
 
 if __name__ == "__main__":
+    
+    
     R.establish_connection()
+
+    """
+    Add libraries to test the code 
+
+    run the command: python3 redant_libs/peer_ops.py
+
+    Test the libraries
+    
+    """
+
     peer_probe('10.70.43.101','10.70.43.228')
     peer_status()
+    pool_list('10.70.43.101')
