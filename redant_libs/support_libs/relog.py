12613b3c327037b6a372060453fad27ea79246c7
srijan-sivakumar <ssivakum@redhat.com>
2021-03-09 11:50:17 +0530

    Support modules modification.
    1. Added rexe to redant.
    2. Renamed the logging module.
    3. Converted logging class methods to static.
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- redant_libs/support_libs/relog.py
+++ redant_libs/support_libs/relog.py
@@ -2,15 +2,15 @@ import logging
 import logging.handlers
 
 
-logger = logging.getLogger(__name__)
+relogger = logging.getLogger(__name__)
 class Logging:
 
-    @classmethod
+    @staticmethod
     def set_logging_options(cls, log_file_path, log_file_level):
         """
         This function is for configuring the logger
         """
-        global logger
+        global relogger
         valid_log_level = ['I', 'D', 'W', 'E', 'C']
         log_level_dict = {'I':logging.INFO, 'D':logging.DEBUG, 'W':logging.WARNING,
                 'E':logging.ERROR, 'C':logging.CRITICAL}
@@ -20,10 +20,12 @@ class Logging:
         if log_file_level not in valid_log_level:
             print("Invalid log level given, Taking Log Level as Info.")
             log_file_level = 'I'
-        logger.setLevel(log_level_dict[log_file_level])
+        relogger.setLevel(log_level_dict[log_file_level])
         log_file_handler = logging.handlers.WatchedFileHandler(log_file_path)
         log_file_handler.setFormatter(log_format)
-        logger.addHandler(log_file_handler)
+        relogger.addHandler(log_file_handler)
 
-        return logger
+    @staticmethod
+    def get_logger_handle(cls):
+        return relogger
 
