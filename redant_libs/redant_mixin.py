bce88eb6a09b9928da652d443979a6893ab5df71
srijan-sivakumar <ssivakum@redhat.com>
2021-03-12 14:08:07 +0530

    Flake and pylint fix
    Concerned files:
    1. runner_thread
    2. redant_libs/redant_mixin.py
    
    Updates: #51
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- redant_libs/redant_mixin.py
+++ redant_libs/redant_mixin.py
@@ -10,6 +10,6 @@ from redant_libs.gluster_libs.peer_ops import peer_ops
 class RedantMixin(peer_ops, Rexe, Logger):
     """
     A mixin class for redant project to encompass all ops and support
-    classes.
+    modules.
     """
     pass
