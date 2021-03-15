84a82219306b739f3e0ea32147266a95096cff1a
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-03-15 12:31:53 +0530

    Fixing Pylint and flake8 errors (#66)
    * Fixing Pylint and flake8 errors
    flake8 and pylint errors are removed and default log level
    is also removed while logging. VolumeOps module is added in
    redant_mixin.py
    
    Updates: #58
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * flake8 and pylint errors are removed and default log level
    is also removed while logging. VolumeOps module is added in
    redant_mixin.py
    
    Updates: #58
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- redant_libs/gluster_libs/volume_ops.py
+++ redant_libs/gluster_libs/volume_ops.py
@@ -1,9 +1,22 @@
-class VolumeOps:
+"""
+This file contains one class - VolumeOps which
+holds volume related APIs which will be called
+from the test case.
+"""
 
-    def volume_create(self,node: str,volname: str,
-                      bricks_list: list,force: bool=False,
+
+class VolumeOps:
+    """
+    VolumeOps class provides APIs to perform operations
+    related to volumes like create,delete,start,stop,
+    fetch information.
+    """
+
+    def volume_create(self, node: str, volname: str,
+                      bricks_list: list, force: bool = False,
                       **kwargs):
-        """Create the gluster volume with specified configuration
+        """
+        Create the gluster volume with specified configuration
         Args:
             node(str): server on which command has to be executed
             volname(str): volume name that has to be created
@@ -23,78 +36,53 @@ class VolumeOps:
                     - redundancy_count : (int)|None
                     - transport_type : tcp|rdma|tcp,rdma|None
                     - ...
-        Logging is done and exceptions are raised if required
         """
-        replica_count = arbiter_count = stripe_count = None
-        disperse_count = disperse_data_count = redundancy_count = None
-        transport_type = None
-
-        if 'replica_count' in kwargs:
-            replica_count = int(kwargs['replica_count'])
-
-        if 'arbiter_count' in kwargs:
-            arbiter_count = int(kwargs['arbiter_count'])
-
-        if 'stripe_count' in kwargs:
-            stripe_count = int(kwargs['stripe_count'])
-
-        if 'disperse_count' in kwargs:
-            disperse_count = int(kwargs['disperse_count'])
-
-        if 'disperse_data_count' in kwargs:
-            disperse_data_count = int(kwargs['disperse_data_count'])
-
-        if 'redundancy_count' in kwargs:
-            redundancy_count = int(kwargs['redundancy_count'])
-
-        if 'transport_type' in kwargs:
-            transport_type = kwargs['transport_type']
 
         replica = arbiter = stripe = disperse = disperse_data = redundancy = ''
         transport = ''
-        if replica_count is not None:
-            replica = f"replica {replica_count}"
 
-        if arbiter_count is not None:
-            arbiter = f"arbiter {arbiter_count}"
+        if 'replica_count' in kwargs:
+            replica = f"replica {kwargs['replica_count']}"
 
-        if stripe_count is not None:
-            stripe = f"stripe {stripe_count}"
+        if 'arbiter_count' in kwargs:
+            arbiter = f"arbiter {kwargs['arbiter_count']}"
 
-        if disperse_count is not None:
-            disperse = f"disperse {disperse_count}"
+        if 'stripe_count' in kwargs:
+            stripe = f"stripe {kwargs['stripe_count']}"
 
-        if disperse_data_count is not None:
-            disperse_data = f"disperse-data {disperse_data_count}"
+        if 'disperse_count' in kwargs:
+            disperse = f"disperse {kwargs['disperse_count']}"
 
-        if redundancy_count is not None:
-            redundancy = f"redundancy {redundancy_count}"
+        if 'disperse_data_count' in kwargs:
+            disperse = f"disperse-data {kwargs['disperse_data_count']}"
 
-        if transport_type is not None:
-            transport = f"transport {transport_type}"
-        
-        bricks_list_string = ' '.join(bricks_list)
+        if 'redundancy_count' in kwargs:
+            redundancy = f"redundancy {kwargs['redundancy_count']}"
+
+        if 'transport_type' in kwargs:
+            transport = f"transport {kwargs['transport_type']}"
+
         cmd = (f"gluster volume create {volname} {replica} "
                f"{arbiter} {stripe} {disperse} {disperse_data} "
-               f"{redundancy} {transport} {bricks_list_string} --xml "
+               f"{redundancy} {transport} {' '.join(bricks_list)} --xml "
                "--mode=script")
-        
+
         if force:
             cmd = cmd + " force"
-        
-        self.rlog(f"Running {cmd} on node {node}", 'I')
-        
-        ret =  self.execute_command(node=node,cmd=cmd)
-        
-        if int(ret['msg']['opRet'])!=0:
-            self.rlog(ret['msg']['opErrstr'],'E')
+
+        self.rlog(f"Running {cmd} on node {node}")
+
+        ret = self.execute_command(node=node, cmd=cmd)
+
+        if int(ret['msg']['opRet']) != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
             raise Exception(ret['msg']['opErrstr'])
-        else:    
-            self.rlog(f"Successfully ran {cmd} on {node}",'I')
 
+        self.rlog(f"Successfully ran {cmd} on {node}")
 
-    def volume_start(self,node: str,volname: str,force: bool=False):
-        """Starts the gluster volume
+    def volume_start(self, node: str, volname: str, force: bool = False):
+        """
+        Starts the gluster volume
         Args:
             node (str): Node on which cmd has to be executed.
             volname (str): volume name
@@ -102,27 +90,26 @@ class VolumeOps:
             force (bool): If this option is set to True, then start volume
                 will get executed with force option. If it is set to False,
                 then start volume will get executed without force option
-        Logging is done and exceptions are raised if required
         """
 
         if force:
             cmd = f"gluster volume start {volname} force --mode=script --xml"
         else:
             cmd = f"gluster volume start {volname} --mode=script --xml"
-            
-        self.rlog(f"Running {cmd} on node {node}", 'I')
-        
-        ret = self.execute_command(node=node,cmd=cmd)
-        
-        if int(ret['msg']['opRet'])!=0:
-            self.rlog(ret['msg']['opErrstr'],'E')
-            raise Exception(ret['msg']['opErrstr'])   
-        else:    
-            self.rlog(f"Successfully ran {cmd} on {node}",'I')
 
+        self.rlog(f"Running {cmd} on node {node}")
 
-    def volume_stop(self,node: str,volname: str,force: bool=False):
-        """Starts the gluster volume
+        ret = self.execute_command(node=node, cmd=cmd)
+
+        if int(ret['msg']['opRet']) != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
+
+        self.rlog(f"Successfully ran {cmd} on {node}")
+
+    def volume_stop(self, node: str, volname: str, force: bool = False):
+        """
+        Stops the gluster volume
         Args:
             node (str): Node on which cmd has to be executed.
             volname (str): volume name
@@ -130,72 +117,68 @@ class VolumeOps:
             force (bool): If this option is set to True, then start volume
                 will get executed with force option. If it is set to False,
                 then start volume will get executed without force option
-        Logging is done and exceptions are raised if required
         """
 
         if force:
             cmd = f"gluster volume stop {volname} force --mode=script --xml"
         else:
             cmd = f"gluster volume stop {volname} --mode=script --xml"
-        
-        self.rlog(f"Running {cmd} on node {node}", 'I')
-        
-        ret = self.execute_command(node=node,cmd=cmd)
-        
-        if int(ret['msg']['opRet'])!=0:
-            self.rlog(ret['msg']['opErrstr'],'E')
+
+        self.rlog(f"Running {cmd} on node {node}")
+
+        ret = self.execute_command(node=node, cmd=cmd)
+
+        if int(ret['msg']['opRet']) != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
             raise Exception(ret['msg']['opErrstr'])
-        else:
-            self.rlog(f"Successfully ran {cmd} on {node}",'I')
 
+        self.rlog(f"Successfully ran {cmd} on {node}")
 
-    def volume_delete(self,node: str,volname: str):
-        """Deletes the gluster volume if given volume exists in
-           gluster and deletes the directories in the bricks
-           associated with the given volume
+    def volume_delete(self, node: str, volname: str):
+        """
+        Deletes the gluster volume if given volume exists in
+        gluster.
         Args:
             node (str): Node on which cmd has to be executed.
             volname (str): volume name
-        Logging is done and exceptions are raised if required
         """
-        
-        cmd = f"gluster volume delete {volname} --mode=script --xml"
-        
-        self.rlog(f"Running {cmd} on node {node}", 'I')
-        
-        ret = self.execute_command(node=node,cmd=cmd)
-        
-        if int(ret['msg']['opRet'])!=0:
-            self.rlog(ret['msg']['opErrstr'],'E')
-            raise Exception(ret['msg']['opErrstr'])  
-        else:    
-            self.rlog(f"Successfully ran {cmd} on {node}",'I')
-        
 
-    def volume_info(self,node: str,volname: str='all') -> dict:
-        """Gives volume information
+        cmd = f"gluster volume delete {volname} --mode=script --xml"
+
+        self.rlog(f"Running {cmd} on node {node}")
+
+        ret = self.execute_command(node=node, cmd=cmd)
+
+        if int(ret['msg']['opRet']) != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
+
+        self.rlog(f"Successfully ran {cmd} on {node}")
+
+    def volume_info(self, node: str, volname: str = 'all') -> dict:
+        """
+        Gives volume information
         Args:
             node (str): Node on which cmd has to be executed.
             volname (str): volume name
         Returns:
             a dictionary with volume information.
-        Logging is done and exceptions are raised if required
         """
-        
+
         cmd = f"gluster volume info {volname} --xml"
-        
-        self.rlog(f"Running {cmd} on node {node}", 'I')
-        
-        ret = self.execute_command(node=node,cmd=cmd)
-        
-        if int(ret['msg']['opRet'])!=0:
-            self.rlog(ret['msg']['opErrstr'],'E')
+
+        self.rlog(f"Running {cmd} on node {node}")
+
+        ret = self.execute_command(node=node, cmd=cmd)
+
+        if int(ret['msg']['opRet']) != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
             raise Exception(ret['msg']['opErrstr'])
-        else:    
-            self.rlog(f"Successfully ran {cmd} on {node}",'I')
-            
+
+        self.rlog(f"Successfully ran {cmd} on {node}")
+
         volume_info = ret['msg']
-        
+
         ret_val = volume_info['volInfo']['volumes']['volume']
 
         return ret_val
--- redant_libs/redant_mixin.py
+++ redant_libs/redant_mixin.py
@@ -5,9 +5,11 @@
 from redant_libs.support_libs.rexe import Rexe
 from redant_libs.support_libs.relog import Logger
 from redant_libs.gluster_libs.peer_ops import peer_ops
+from redant_libs.gluster_libs.volume_ops import VolumeOps
+
 
 # pylint: disable=W0107
-class RedantMixin(peer_ops, Rexe, Logger):
+class RedantMixin(VolumeOps, peer_ops, Rexe, Logger):
     """
     A mixin class for redant project to encompass all ops and support
     modules.
