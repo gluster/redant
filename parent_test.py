6d4b20df0f5fc2b8732cd529ea3a71e4d01da036
aujjwal-redhat <aujjwal@redhat.com>
2021-03-15 19:58:29 +0530

    Added parent_test
    Started working on parent_test
    Created the basic functions and added the standard info.
    
    Updates: #75
    
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

---
+++ parent_test.py
@@ -0,0 +1,58 @@
+from abc import ABC
+
+
+class Parent_Test(ABC):
+
+    """
+    This class contains the standard info and methods which are needed by
+    most of the tests
+    """
+
+    _PASS: bool = True
+    _FAIL: bool = False
+
+    _TEST_RES: bool = _PASS
+
+    COMPONENT: str
+    TEST_NAME: str
+
+    def configure(self, component_arg: str, test_name_arg: str):
+        """
+        Initializes COMPONENT and TEST_NAME
+        """
+        self.COMPONENT = component_arg
+        self.TEST_NAME = test_name_arg
+
+    def core_init(self):
+        """
+        Establishes ssh connection
+        Since added in redant_mixin so
+        can call establish_connection directly
+        """
+
+        self.establish_connection()
+
+    def init(self):
+        """
+        Establishes connection by calling core_init
+        Creates volume
+        And runs the specific component in the
+        test case
+        """
+
+        self.core_init()
+        self.rlog(f"{self.TEST_NAME} from {self.COMPONENT} inits")
+
+    def run_test(self):
+        """
+        It will be overriden by Test cases
+        """
+        pass
+
+    def terminate(self):
+        """
+        Deletes the volume
+        Closes connection
+        """
+
+        self.rlog(f"{self.TEST_NAME} terminates")
