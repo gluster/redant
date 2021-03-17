16f064ade117243a0d53b874d7402d9b8d0b445d
Neeraj Pratap Singh <79842675+neesingh-rh@users.noreply.github.com>
2021-03-17 13:52:42 +0530

    Fixed the docstring (#87)
    Signed-off-by: Neeraj Pratap Singh <neesingh@redhat.com>

--- redant_libs/support_libs/io_ops.py
+++ redant_libs/support_libs/io_ops.py
@@ -1,79 +1,82 @@
+"""
+This file contains one class - io_ops which
+holds IO related APIs which will be called
+from the test case.
+"""
+
+
 class io_ops:
+    """
+    io_ops class provides APIs to perform operations
+    like creating files and directories,listing the directory
+    contents and to handle non-standard IO commands.
+    """
 
     def touch(self, file_name: str, node: str):
         """Creates a regular empty file
-        file_name: The name of the file to be created
-        node: The node in the cluster where the command is to be run
+        Args:
+            file_name (str): The name of the file to be created
+            node (str): The node in the cluster where the command is to be run
         """
 
-        try:
-            cmd = "touch {}".format(file_name)
-            self.rlog(f"Running {cmd} on node {node}")
-            ret = self.execute_command(node=node, cmd=cmd)
+        cmd = "touch {}".format(file_name)
 
-            if ret['error_code'] != 0:
-                raise Exception(ret['msg']['opErrstr'])
-            else:
-                self.rlog("Successfully ran {cmd} on {node} ")
+        self.rlog(f"Running {cmd} on node {node}")
+        ret = self.execute_command(node=node, cmd=cmd)
 
-        except Exception as error:
-            self.rlog(error, 'E')
-        return ret
+        if ret['error_code'] != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
+
+        self.rlog("Successfully ran {cmd} on {node}")
 
     def mkdir(self, dir_name: str, node: str):
         '''Creates a directory
-        dir_name: The name of the directory to be created
-        node: The node in the cluster where the command is to be run
+        Args:
+            dir_name (str): The name of the directory to be created
+            node (str): The node in the cluster where the command is to be run
         '''
 
-        try:
-            cmd = f'mkdir -p /{dir_name}'
-            self.rlog(f"Running {cmd} on node {node}")
-            ret = self.execute_command(node=node, cmd=cmd)
+        cmd = f'mkdir -p /{dir_name}'
 
-            if ret['error_code'] != 0:
-                raise Exception(ret['msg']['opErrstr'])
-            else:
-                self.rlog("Successfully ran {cmd} on {node} ")
+        self.rlog(f"Running {cmd} on node {node}")
+        ret = self.execute_command(node=node, cmd=cmd)
 
-        except Exception as error:
-            self.rlog(error, 'E')
-        return ret
+        if ret['error_code'] != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
+
+        self.rlog("Successfully ran {cmd} on {node}")
 
     def ls(self, node: str):
         '''List the directory contents
-        node: The node in the cluster where the command is to be run
+        Args:
+            node (str): The node in the cluster where the command is to be run
         '''
 
-        try:
-            cmd = 'ls'
-            self.rlog(f"Running {cmd} on node {node}")
-            ret = self.execute_command(node=node, cmd=cmd)
+        cmd = 'ls'
 
-            if ret['error_code'] != 0:
-                raise Exception(ret['msg']['opErrstr'])
-            else:
-                self.rlog("Successfully ran {cmd} on {node} ")
+        self.rlog(f"Running {cmd} on node {node}")
+        ret = self.execute_command(node=node, cmd=cmd)
 
-        except Exception as error:
-            self.rlog(error, 'E')
-        return ret
+        if ret['error_code'] != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
+
+        self.rlog("Successfully ran {cmd} on {node}")
 
     def run(self, node: str, cmd: str):
         '''Used for non-standard IO commands
-        node: The node in the cluster where the command is to be run
-        cmd: The command which is to be executed
+        Args:
+            node (str): The node in the cluster where the command is to be run
+            cmd (str): The non-standard command which is to be run
         '''
 
-        try:
-            self.rlog(f"Running {cmd} on node {node}")
-            ret = self.execute_command(node=node, cmd=cmd)
+        self.rlog(f"Running {cmd} on node {node}")
+        ret = self.execute_command(node=node, cmd=cmd)
 
-            if ret['error_code'] != 0:
-                raise Exception(ret['msg']['opErrstr'])
-            else:
-                self.rlog("Successfully ran {cmd} on {node} ")
+        if ret['error_code'] != 0:
+            self.rlog(ret['msg']['opErrstr'], 'E')
+            raise Exception(ret['msg']['opErrstr'])
 
-        except Exception as error:
-            self.rlog(error, 'E')
-        return ret
+        self.rlog("Successfully ran {cmd} on {node}")
