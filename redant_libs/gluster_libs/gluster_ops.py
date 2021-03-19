fb6579ee30102a4281188dc47e1259b4b8eec3a7
Nishith Vihar Sakinala <nsakinal@redhat.com>
2021-03-19 10:48:46 +0530

    Added glusterd status check is_glusterd_active funtion is addded which checks the staatus of glusterd on the server.
    Fixes: #91
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- redant_libs/gluster_libs/gluster_ops.py
+++ redant_libs/gluster_libs/gluster_ops.py
@@ -42,9 +42,40 @@ class GlusterOps:
         self.rlog(f"Running {cmd} on {node}")
 
         ret = self.execute_command(node=node, cmd=cmd)
-        print(ret)
+
         if int(ret['error_code']) != 0:
             self.rlog(ret['error_msg'], 'E')
             raise Exception(ret['error_msg'])
 
         self.rlog("Successfully ran {cmd} on {node}")
+
+    def is_glusterd_active(self, node: str) -> bool:
+        """
+        Checks the status of the glusterd service on the
+        specified node.
+        Args:
+            node (str): The node on which the glusterd service
+                        is to be stopped.
+        Returns:
+            1  : if glusterd active
+            0  : if glusterd not active
+           -1  : if glusterd not active and PID is alive
+        """
+        is_active = 1
+        cmd1 = "systemctl status glusterd"
+        cmd2 = "pidof glusterd"
+
+        self.rlog(f"Running {cmd1} on {node}")
+
+        ret = self.execute_command(node=node, cmd=cmd1)
+
+        if int(ret['error_code']) != 0:
+            is_active = 0
+            self.rlog(f"Running {cmd2} on {node}")
+            ret1 = self.execute_command(node=node, cmd=cmd2)
+            if ret1['error_code'] == 0:
+                is_active = -1
+                self.rlog("Successfully ran {cmd2} on {node}")
+
+        self.rlog("Successfully ran {cmd1} on {node}")
+        return is_active
