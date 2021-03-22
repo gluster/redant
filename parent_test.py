ba86997b686166914f26c458da803da814b4f0d6
aujjwal-redhat <aujjwal@redhat.com>
2021-03-22 12:57:35 +0530

    Removed abstract class and made class variables object variables
    Removed abstract concept
    Added redant_mixin
    Made PASS TEST_RES object variables
    Removed FAIL as PASS will take care of that.
    
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

--- parent_test.py
+++ parent_test.py
@@ -1,39 +1,47 @@
-from abc import ABC
+from redant_libs.redant_mixin import RedantMixin as rm
 
 
-class Parent_Test(ABC):
+class Parent_Test:
 
     """
     This class contains the standard info and methods which are needed by
     most of the tests
+
+    PASS: states the pass value
+
+    TEST_RES: states the result of the test case
+
+    COMPONENT: component which is to be tested
+    TEST_NAME: name of the test to run
     """
 
-    _PASS: bool = True
-    _FAIL: bool = False
+    PASS: bool
 
-    _TEST_RES: bool = _PASS
+    TEST_RES: bool
 
     COMPONENT: str
     TEST_NAME: str
 
-    def init(self):
+    def __init__(self, passed: bool, component: str, test_name: str):
         """
         Creates volume
         And runs the specific component in the
         test case
         """
-        self.rlog(f"{self.TEST_NAME} from {self.COMPONENT} inits")
+        self.PASS = passed
+        self.TEST_RES = self.PASS
+        self.COMPONENT = component
+        self. TEST_NAME = test_name
+        rm.rlog(f"{self.TEST_NAME} from {self.COMPONENT} inits")
+
+    def init(self):
+        pass
 
     def run_test(self):
-        """
-        It will be overriden by Test cases
-        """
         pass
 
     def terminate(self):
         """
-        Deletes the volume
-        Closes connection
+        Closes connection for now.
         """
-
-        self.rlog(f"{self.TEST_NAME} terminates")
+        rm.rlog(f"{self.TEST_NAME} terminates")
