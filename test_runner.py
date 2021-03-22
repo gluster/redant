4594d9d081eeb020f60bb4de3332e2bc8939b27f
schaffung <ssivakum@redhat.com>
2021-03-22 10:32:20 +0530

    Confusion in file naming. (#101)
    Resolving the file naming and the operations to be
    handled by the respective modules.
    
    Fixes: #100
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- test_runner.py
+++
@@ -1,6 +0,0 @@
-"""
-This is the test runner for the Redant test automation framework. The purpose
-of the test tunner is to take a given list of servers and the TCs. Taking the
-list of TCs, create threads and pass the corresponding server and client list
-to a specific thread runner.
-"""
