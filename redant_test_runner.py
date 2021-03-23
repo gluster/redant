34eb0c7c93b884d6ab7be418b6e171732dcd9850
srijan-sivakumar <ssivakum@redhat.com>
2021-03-23 17:52:51 +0530

    Thread runner object creation inside test runner
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- redant_test_runner.py
+++ redant_test_runner.py
@@ -14,6 +14,8 @@ class TestRunner:
     @classmethod
     def init(cls, test_run_dict, server_list, client_list, base_log_path="/tmp"):
         cls.test_run_dict = test_run_dict
+        cls.server_list = server_list
+        cls.client_list = client_list
 
     @classmethod
     def run_tests(cls):
@@ -21,7 +23,13 @@ class TestRunner:
         # Currently for testing the logical flow, hardcoding the calls
         # Running non-disruptive cases first.
         for test in cls.test_run_dict["nonDisruptive"]:
+            tc_class = cls.test_run_dict["nonDisruptive"][test]["testClass"]
+            runner_thread_obj = RunnerThread(tc_class, cls.client_list,
+                                             cls.server_list, "/tmp", 'I')
             print(cls.test_run_dict["nonDisruptive"][test])
         for test in cls.test_run_dict["disruptive"]:
+            tc_class = cls.test_run_dict["disruptive"][test]["testClass"]
+            runner_thread_obj = RunnerThread(tc_class, cls.client_list,
+                                             cls.server_list, "/tmp", 'I')
             print(cls.test_run_dict["disruptive"][test])
         #runner_thread_obj = RunnerThread(cls.tst_run_dict[
