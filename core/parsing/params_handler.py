b63fd2dfb76040a9b4af35a89d20c17a4593af53
aujjwal-redhat <aujjwal@redhat.com>
2021-03-29 09:27:36 +0530

    removed the duplicated code related to logging in the redant_main and also modified the code for imports to work
    Signed-off-by: aujjwal-redhat <aujjwal@redhat.com>

--- core/parsing/params_handler.py
+++ core/parsing/params_handler.py
@@ -3,7 +3,7 @@ This module contains one class - ParamsHandler,
 which contains APIs for configuration parameter
 parsing.
 """
-from parsing.test_parser import Parser
+from core.parsing.test_parser import Parser
 
 
 class ParamsHandler:
--- redant_main.py
+++ redant_main.py
@@ -6,104 +6,13 @@ This module takes care of:
 """
 
 import os
+import sys
 import argparse
 from core.parsing.params_handler import ParamsHandler
 from core.test_list_builder import TestListBuilder
 from core.test_runner import TestRunner
 from result_handler import ResultHandler
 import time
-
-def is_file_accessible(path: str, mode: str = 'r') -> bool:
-    """
-    To check if the given file is accessible or not
-    so as to prevent parsing failures.
-    Args:
-        path (str): The file path whose accessibility is
-                    to be checked.
-        mode (str): The file access mode which would needs
-                    to be validated for the given path.
-    Returns:
-        True: The file at the given path is accessible for
-              the specified mode.
-        False: The file is not accessible for the specified
-               mode.
-    """
-    try:
-        f = open(path, mode)
-        f.close()
-    except IOError:
-        return False
-    return True
-
-
-def log_dir_check(path: str, component_dict: dict, test_dict: dict):
-    """
-    Sanity check function for redant logging directory.
-    Args:
-        path (str): The directory path.
-        component_dict (dict): The dict containing component lists
-        example,
-               {
-                 "functional" : ["component1", "component2", ...],
-                 "performance" : ["component1", "component2", ...],
-                 "example" : ["component1", "component2", ...]
-               }
-        test_dict (dict): Dictionary containing list of TCs
-        example,
-                {
-                  "disruptive" : [
-                                   {
-                                     "volType" : ["volT1", "volT2",..],
-                                     "modulePath" : "module_path",
-                                     "moduleName" : "module_name",
-                                     "componentName" : "component_name",
-                                     "testClass" : "test_class",
-                                     "testType" : "test_type"
-                                   },
-                                   {
-                                     ...
-                                   }
-                                 ],
-                 "nonDisruptive" : [
-                                     {
-                                       "volType" : ["volT1", "volT2",..],
-                                       "modulePath" : "module_path",
-                                       "moduleName" : "module_name",
-                                       "componentName" : "component_name",
-                                       "testClass" : "test_class",
-                                       "testType" : "test_type"
-                                     },
-                                     {
-                                       ...
-                                     }
-                                   ]
-                }
-    Returns:
-        None
-    """
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-    # Component wise directory creation.
-    for test_type in component_dict:
-        test_type_path = path+"/"+test_type
-        if not os.path.isdir(test_type_path):
-            os.makedirs(test_type_path)
-        components = component_dict[test_type]
-        for component in components:
-            if not os.path.isdir(test_type_path+"/"+component):
-                os.makedirs(test_type_path+"/"+component)
-
-    # TC wise directory creation.
-    for test in test_dict["disruptive"]:
-        test_case_dir = path+"/"+test["modulePath"][5:-3]
-        if not os.path.isdir(test_case_dir):
-            os.makedirs(test_case_dir)
-    for test in test_dict["nonDisruptive"]:
-        test_case_dir = path+"/"+test["modulePath"][5:-3]
-        if not os.path.isdir(test_case_dir):
-            os.makedirs(test_case_dir)
-
 
 def pars_args():
     """
@@ -147,11 +56,7 @@ def main():
     start = time.time()
     args = pars_args()
 
-    if is_file_accessible(args.config_file):
-        ParamsHandler.get_config_hashmap(args.config_file)
-    else:
-        print(f"The config file at {args.config_file} is not accessible.")
-        return -1
+    ParamsHandler.get_config_hashmap(args.config_file)
 
     # Obtain the client and server dict.
     mach_conn_dict = ParamsHandler.get_nodes_info()
@@ -162,7 +67,9 @@ def main():
     test_cases_component = test_cases_tuple[1]
 
     # Creating log dirs.
-    log_dir_check(args.log_dir, test_cases_component, test_cases_dict)
+    sys.path.insert(1, ".")
+    from support.ops.support_ops.relog import Logger
+    Logger.log_dir_creation(args.log_dir, test_cases_component, test_cases_dict)
 
     # Pre test run test list builder is modified.
     test_cases_dict = TestListBuilder.pre_test_run_list_modify(test_cases_dict)
@@ -175,7 +82,7 @@ def main():
     ResultHandler.display_test_results(all_test_results)
 
     print(f"\nTotal time taken by the framework: {time.time()-start} sec")
-    return 0
+
 
 
 if __name__ == '__main__':
