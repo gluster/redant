9d7396541f2a042bc002479b0257d74fbb6ea586
Neeraj Pratap Singh <neesingh@redhat.com>
2021-04-07 20:19:12 +0530

    other functions are removed
    Signed-off-by: Neeraj Pratap Singh <neesingh@redhat.com>

--- support/ops/support_ops/io_ops.py
+++ support/ops/support_ops/io_ops.py
@@ -6,18 +6,18 @@ holds API for running all the IO commands.
 
 class io_ops:
     """
-    io_ops class provides API to handle 
+    io_ops class provides API to handle
     all the IO commands.
     """
 
     def execute_io_cmd(self, cmd: str, host: str = None):
-    '''
-    Used for all the IO commands
+        '''
+        Used for all the IO commands
 
-    Args:
-        cmd (str): The IO command which is to be run
-        host (str): The node in the cluster where the command is to be run
-    '''
+        Args:
+            cmd (str): The IO command which is to be run
+            host (str): The node in the cluster where the command is to be run
+        '''
 
         self.rlog(f"Running {cmd} on node {host}")
         ret = self.execute_command(host, cmd)
