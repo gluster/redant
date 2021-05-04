b7d5be67a4679814bde34f2bce4a44e76316f16f
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-05-04 12:22:01 +0530

    Gluster Operations shifted from parent test constructor to parent_test_run (#244)
    * Gluster Operations shifted from parent test constructor to
    parent_test_run
    Gluster Operations are shifted from constructor of parent test to
    parent_test_run under the try-except clause
    
    Fixes: #239
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * Gluster operations from terminate are shifted to parent_run_test
    The gluster operates in terminate function are shifted to
    parent_run_test under the try-except clause which handles the
    exception.
    
    Fixes: #239
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- tests/parent_test.py
+++ tests/parent_test.py
@@ -1,7 +1,7 @@
 import traceback
 import abc
-from common.mixin import RedantMixin
 from datetime import datetime
+from common.mixin import RedantMixin
 
 
 class ParentTest(metaclass=abc.ABCMeta):
@@ -24,7 +24,7 @@ class ParentTest(metaclass=abc.ABCMeta):
                     "dist-arb" : "distributed-arbiter"
                 }
     def __init__(self, mname: str, param_obj, volume_type: str,
-                 thread_flag: bool, log_path: str, log_level: str = 'I'):
+                 log_path: str, log_level: str = 'I'):
         """
         Creates volume
         And runs the specific component in the
@@ -43,22 +43,6 @@ class ParentTest(metaclass=abc.ABCMeta):
         self.client_list = param_obj.get_client_ip_list()
         self.brick_roots = param_obj.get_brick_roots()
 
-        if not thread_flag:
-            self.redant.start_glusterd()
-            self.redant.create_cluster(self.server_list)
-
-        if self.volume_type != "Generic":
-            self.vol_name = (f"{mname}-{volume_type}")
-            self.redant.volume_create(self.vol_name, self.server_list[0],
-                                      self.volume_types_info[self.conv_dict[volume_type]],
-                                      self.server_list, self.brick_roots, True)
-            self.redant.volume_start(self.vol_name, self.server_list[0])
-            self.mountpoint = (f"/mnt/{self.vol_name}")
-            self.redant.execute_io_cmd(f"mkdir -p {self.mountpoint}",
-                                       self.client_list[0])
-            self.redant.volume_mount(self.server_list[0], self.vol_name,
-                                     self.mountpoint, self.client_list[0])
-
     def _configure(self, mname: str, server_details: dict,
                    client_details: dict, log_path: str, log_level: str):
         machine_detail = {**client_details, **server_details}
@@ -71,7 +55,7 @@ class ParentTest(metaclass=abc.ABCMeta):
     def run_test(self):
         pass
 
-    def parent_run_test(self):
+    def parent_run_test(self, mname: str, volume_type: str, thread_flag: bool):
         """
         Function to handle the exception logic and invokes the run_test
         which is overridden by every TC.
@@ -82,7 +66,29 @@ class ParentTest(metaclass=abc.ABCMeta):
         ============================================================
         ''')
         try:
+            if not thread_flag:
+                self.redant.start_glusterd()
+                self.redant.create_cluster(self.server_list)
+
+            if self.volume_type != "Generic":
+                self.vol_name = (f"{mname}-{volume_type}")
+                self.redant.volume_create(self.vol_name, self.server_list[0],
+                                        self.volume_types_info[self.conv_dict[volume_type]],
+                                        self.server_list, self.brick_roots, True)
+                self.redant.volume_start(self.vol_name, self.server_list[0])
+                self.mountpoint = (f"/mnt/{self.vol_name}")
+                self.redant.execute_io_cmd(f"mkdir -p {self.mountpoint}",
+                                        self.client_list[0])
+                self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                        self.mountpoint, self.client_list[0])
             self.run_test(self.redant)
+
+            if self.volume_type != 'Generic':
+                self.redant.volume_unmount(self.mountpoint, self.client_list[0])
+                self.redant.execute_io_cmd(f"rm -rf {self.mountpoint}",
+                                        self.client_list[0])
+                self.redant.volume_stop(self.vol_name, self.server_list[0], True)
+                self.redant.volume_delete(self.vol_name, self.server_list[0])
         except Exception as error:
             tb = traceback.format_exc()
             self.redant.logger.error(error)
@@ -98,10 +104,4 @@ class ParentTest(metaclass=abc.ABCMeta):
         """
         Closes connection for now.
         """
-        if self.volume_type != 'Generic':
-            self.redant.volume_unmount(self.mountpoint, self.client_list[0])
-            self.redant.execute_io_cmd(f"rm -rf {self.mountpoint}",
-                                       self.client_list[0])
-            self.redant.volume_stop(self.vol_name, self.server_list[0], True)
-            self.redant.volume_delete(self.vol_name, self.server_list[0])
         self.redant.deconstruct_connection()
