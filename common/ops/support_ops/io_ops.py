68eb7313f947253a10b76d4afa13bba289b45292
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-05-06 08:15:13 +0530

    Basic io functions are added (#254)
    Basic io functions like creating a file or a directory
    and checking is path exists is added.
    
    Fixes: #246
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- common/ops/support_ops/io_ops.py
+++ common/ops/support_ops/io_ops.py
@@ -33,3 +33,75 @@ class IoOps(AbstractOps):
         ret = self.execute_abstract_op_node(cmd, host)
 
         return ret
+
+    def create_file(self, path: str, filename: str, node: str):
+        """
+        Creates a file in the specified specified path
+        """
+        cmd = f"touch {path}/{filename}"
+        self.execute_abstract_op_node(cmd, node)
+
+    def create_dir(self, path: str, dirname: str, node: str):
+        """
+        Creates a directory in the specified path
+        """
+        cmd = f"mkdir -p {path}/{dirname}"
+        self.execute_abstract_op_node(cmd, node)
+
+    def create_dirs(self, list_of_nodes: list, list_of_dir_paths: list):
+        """
+        Create directories on nodes.
+        Args:
+            list_of_nodes (list): Nodes on which dirs has to be created.
+            list_of_dir_paths (list): List of dirs abs path.
+        Returns:
+            bool: True of creation of all dirs on all nodes is successful.
+                False otherwise.
+        """
+        if not isinstance(list_of_nodes, list):
+            list_of_nodes = [list_of_nodes]
+
+        if isinstance(list_of_dir_paths, list):
+            list_of_dir_paths = ' '.join(list_of_dir_paths)
+
+        # Create upload dir on each node
+        cmd = f"mkdir -p {list_of_dir_paths}"
+        _rc = True
+
+        ret = self.execute_command_multinode(cmd,list_of_nodes)
+        for each_ret in ret:
+            if each_ret['error_code'] != 0:
+                self.logger.error(f"Failed to create the dirs: {list_of_dir_paths.split(' ')} "
+                                  f"on node: {each_ret['node']} - {each_ret['error_msg']}")
+                _rc = False
+
+        return _rc
+
+
+    def path_exists(self, list_of_nodes, list_of_paths):
+        """Check if paths exist on nodes.
+        Args:
+            list_of_nodes (list): List of nodes.
+            list_of_paths (list): List of abs paths to verify if path exist.
+        Returns:
+            bool: True if all paths exists on all nodes. False otherwise.
+        """
+        if not isinstance(list_of_nodes, list):
+            list_of_nodes = [list_of_nodes]
+
+        if not isinstance(list_of_paths, list):
+            list_of_paths = (list_of_paths.split(" "))
+
+        
+        _rc = True
+
+        for path in list_of_paths:
+            cmd = f"ls -l {path}"
+            ret = self.execute_command_multinode(cmd, list_of_nodes)
+        for each_ret in ret:
+            if each_ret['error_code'] != 0:
+                error_string = each_ret['error_msg'].rstrip('\n')
+                self.logger.error(f"{error_string} on node {each_ret['node']}")
+                _rc = False
+
+        return _rc
