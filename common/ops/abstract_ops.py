a4527f75b2059a17e252f995ec8160d8dafe3849
srijan-sivakumar <ssivakum@redhat.com>
2021-05-09 18:28:24 +0530

    [LintFix] Project level.
    Added lint fixes for abstract ops and along with that
    added certain pep violations as exception in the
    .pylintrc
    
    Updates: #58
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- common/ops/abstract_ops.py
+++ common/ops/abstract_ops.py
@@ -7,13 +7,15 @@ responsible for executing commands on the nodes.
 """
 from collections import OrderedDict
 
+
 class AbstractOps:
     """
     This class contains functions which is responsible for
     calling the funtions in the remote executioner for executing
     commands on the nodes
     """
-    def execute_abstract_op_node(self, cmd : str, node : str=None):
+
+    def execute_abstract_op_node(self, cmd: str, node: str = None):
         """
         Calls the function in the remote executioner to execute
         commands on the nodes. Logging is also performed along
@@ -28,26 +30,27 @@ class AbstractOps:
 
         ret = self.execute_command(cmd, node)
 
-        if(ret['error_code']!=0):
+        if(ret['error_code'] != 0):
             self.logger.error(ret['error_msg'])
-            raise Exception(ret['error_msg'])    
-        elif isinstance(ret['msg'],OrderedDict) or isinstance(ret['msg'],dict):
+            raise Exception(ret['error_msg'])
+        elif isinstance(ret['msg'], OrderedDict) or \
+                isinstance(ret['msg'], dict):
             if int(ret['msg']['opRet']) != 0:
                 self.logger.error(ret['msg']['opErrstr'])
                 raise Exception(ret['msg']['opErrstr'])
 
         return ret
-        
-    def execute_abstract_op_multinode(self, cmd : str, node : list=None):
+
+    def execute_abstract_op_multinode(self, cmd: str, node: list = None):
         """
         Calls the function in the remote executioner to execute
         commands on the nodes. Logging is also performed along
         with handling exceptions while executng the commands.
         Args:
             cmd  (str): the command to be executed by the rexe
-            node (list): the list of nodes on which the command as to be executed.
-                        If the node is None then the rexe chooses the
-                        node randomly and executes the command on it.
+            node (list): the list of nodes on which the command as to be
+                         executed. If the node is None then the rexe chooses
+                         the node randomly and executes the command on it.
         """
         self.logger.info(f"Running {cmd} on {node}")
 
@@ -57,12 +60,10 @@ class AbstractOps:
             if each_ret['error_code'] != 0:
                 self.logger.error(each_ret['msg']['opErrstr'])
                 raise Exception(each_ret['msg']['opErrstr'])
-            elif isinstance(each_ret['msg'],OrderedDict) or isinstance(each_ret['msg'],dict):
+            elif isinstance(each_ret['msg'], OrderedDict) or \
+                    isinstance(each_ret['msg'], dict):
                 if int(each_ret['msg']['opRet']) != 0:
                     self.logger.error(each_ret['msg']['opErrstr'])
                     raise Exception(each_ret['msg']['opErrstr'])
 
         return ret
-        
-        
-        
