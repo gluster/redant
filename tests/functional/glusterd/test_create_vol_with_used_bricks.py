a8ef6b1707ece4f9e0dd470625a244734dd2c1b6
Ayush Ujjwal <aujjwal@redhat.com>
2021-05-11 14:34:40 +0530

    Added test_create_vol_with_used_bricks
    Migrated the test case from the glusto-test and made changes as per the
    functionalities in redant.
    
    Updates:#292
    Fixes:#300
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_create_vol_with_used_bricks.py
+++ tests/functional/glusterd/test_create_vol_with_used_bricks.py
@@ -1,85 +1,31 @@
-#  Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-""" Description:
-      Create volume using bricks of deleted volume
 """
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
 
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.brick_ops import add_brick
-from glustolibs.gluster.brick_libs import get_all_bricks
-from glustolibs.gluster.lib_utils import form_bricks_list
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.gluster.volume_ops import (volume_stop, volume_create,
-                                           get_volume_list)
-from glustolibs.gluster.volume_libs import (cleanup_volume, setup_volume)
-from glustolibs.gluster.mount_ops import mount_volume, umount_volume
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Create volume using bricks of deleted volume
+"""
+from tests.abstract_test import AbstractTest
+
+# nonDisruptive;dist-rep
 
 
-@runs_on([['distributed-replicated'], ['glusterfs']])
-class TestCreateVolWithUsedBricks(GlusterBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.counter = 1
-        cls.get_super_method(cls, 'setUpClass')()
+class TestCreateVolWithUsedBricks(AbstractTest):
 
-        # Uploading file_dir script in all client direcotries
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to "
-                                 "clients %s" % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def tearDown(self):
-        """
-        tearDown for every test
-        """
-        # clean up all volumes
-        vol_list = get_volume_list(self.mnode)
-        if vol_list is None:
-            raise ExecutionError("Failed to get the volume list")
-
-        for volume in vol_list:
-            ret = cleanup_volume(self.mnode, volume)
-            if not ret:
-                raise ExecutionError("Unable to delete volume % s" % volume)
-            g.log.info("Volume deleted successfully : %s", volume)
-
-        # Cleaning the deleted volume bricks
-        for brick in self.brick_list:
-            node, brick_path = brick.split(r':')
-            cmd = "rm -rf " + brick_path
-            ret, _, _ = g.run(node, cmd)
-            if ret:
-                raise ExecutionError("Failed to delete the brick "
-                                     "dir's of deleted volume")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_create_vol_used_bricks(self):
+    def run_test(self, redant):
         '''
         -> Create distributed-replica Volume
         -> Add 6 bricks to the volume
@@ -88,95 +34,25 @@ class TestCreateVolWithUsedBricks(GlusterBaseClass):
         -> unmount the volume
         -> Stop and delete the volume
         -> Create another volume using bricks of deleted volume
+        -> Start the volume
+        -> Execute IO command
+        -> Mount the volume
         '''
-
-        # Create and start a volume
-        self.volume['name'] = "test_create_vol_with_fresh_bricks"
-        self.volname = "test_create_vol_with_fresh_bricks"
-        ret = setup_volume(self.mnode, self.all_servers_info, self.volume)
-        self.assertTrue(ret, "Failed to create and start volume")
-
-        # Forming brick list
-        brick_list = form_bricks_list(self.mnode, self.volname, 6,
-                                      self.servers, self.all_servers_info)
-        # Adding bricks to the volume
-        ret, _, _ = add_brick(self.mnode, self.volname, brick_list)
-        self.assertEqual(ret, 0, "Failed to add bricks to the volume %s"
-                         % self.volname)
-        g.log.info("Bricks added successfully to the volume %s", self.volname)
-
-        # Mounting the volume.
-        for mount_obj in self.mounts:
-            ret, _, _ = mount_volume(self.volname, mtype=self.mount_type,
-                                     mpoint=mount_obj.mountpoint,
-                                     mserver=self.mnode,
-                                     mclient=mount_obj.client_system)
-            self.assertEqual(ret, 0, ("Volume %s is not mounted") % (
-                self.volname))
-            g.log.info("Volume mounted successfully : %s", self.volname)
-
-        # run IOs
-        g.log.info("Starting IO on all mounts...")
-        self.all_mounts_procs = []
-        for mount_obj in self.mounts:
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d --dir-depth 2 "
-                   "--dir-length 5 --max-num-of-dirs 3 "
-                   "--num-of-files 10 %s" % (self.script_upload_path,
-                                             self.counter,
-                                             mount_obj.mountpoint))
-
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-            self.counter = self.counter + 10
-
-        # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients")
-
-        # Unmouting the volume.
-        for mount_obj in self.mounts:
-            ret, _, _ = umount_volume(mclient=mount_obj.client_system,
-                                      mpoint=mount_obj.mountpoint)
-            self.assertEqual(ret, 0, "Volume %s is not unmounted" % (
-                self.volname))
-            g.log.info("Volume unmounted successfully : %s", self.volname)
-
-        # Getting brick list
-        self.brick_list = get_all_bricks(self.mnode, self.volname)
-        if not self.brick_list:
-            raise ExecutionError("Failed to get the brick list of %s"
-                                 % self.volname)
-
-        # Stop volume
-        ret, _, _ = volume_stop(self.mnode, self.volname)
-        self.assertEqual(ret, 0, "Failed to stop the volume %s"
-                         % self.volname)
-        g.log.info("Volume %s stopped successfully", self.volname)
-
-        # Delete Volume
-        ret, _, _ = g.run(self.mnode, "gluster volume delete %s --mode=script"
-                          % self.volname)
-        self.assertEqual(ret, 0, "Failed to delete volume %s" % self.volname)
-        g.log.info("Volume deleted successfully %s", self.volname)
-
-        # Create another volume by using bricks of deleted volume
-        self.volname = "test_create_vol_used_bricks"
-        ret, _, err = volume_create(self.mnode, self.volname, brick_list[0:6],
-                                    replica_count=3)
-        self.assertNotEqual(ret, 0, "Volume creation should fail with used "
-                                    "bricks but volume creation success")
-        g.log.info("Failed to create volume with used bricks")
-
-        # Checking failed message of volume creation
-        msg = ' '.join(['volume create: test_create_vol_used_bricks: failed:',
-                        brick_list[0].split(':')[1],
-                        'is already part of a volume'])
-        self.assertIn(msg, err, "Incorrect error message for volume creation "
-                                "with used bricks")
-        g.log.info("correct error message for volume creation with "
-                   "used bricks")
+        redant.volume_unmount(self.vol_name, self.mountpoint,
+                              self.client_list[0])
+        redant.execute_io_cmd(f"rm -rf {self.mountpoint}",
+                              self.client_list[0])
+        redant.volume_stop(self.vol_name,
+                           self.server_list[0], True)
+        redant.volume_delete(self.vol_name, self.server_list[0])
+        redant.volume_create(
+            self.vol_name,
+            self.server_list[0],
+            self.vol_type_inf[self.conv_dict[self.volume_type]],
+            self.server_list, self.brick_roots, True)
+        redant.volume_start(self.vol_name, self.server_list[0])
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        redant.execute_io_cmd(f"mkdir -p {self.mountpoint}",
+                              self.client_list[0])
+        redant.volume_mount(self.server_list[0], self.vol_name,
+                            self.mountpoint, self.client_list[0])
