45abe990d975ad5d14fe91f3f5b70096eba5f1e4
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-05-11 14:45:46 +0530

    Added test_change_reserve_limit_to_wrong_values  (#299)
    * Check 'storage.reserve' with wrong values
    
    Test Steps:
    1) Create and start a distributed-replicated volume.
    2) Give different inputs to the storage.reserve volume set options
    3) Validate the command behaviour on wrong inputs
    
    Change-Id: I4bbad81cbea9b3b9e59a61fcf7f2b70eac19b216
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>
    
    * Make test compatible with Python 2
    
    - Translate function is availble on `unicode` string in Python2
    
    Change-Id: I6aa01606acc73b18d889a965f1c01f9a393c2c46
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>
    
    * Replace `translate` with `replace`
    
    - `replace` funciton to used to forgo version check
    - `unicode` is not being recognized from builtins in py2
    - `replace` seems correct alternative than fixing unicode
    
    Change-Id: Ieb9b5ad283e1a31d65bd8a9715b80f9deb0c05fe
    Signed-off-by: Leela Venkaiah G <lgangava@redhat.com>
    
    * Added test_change_reserve_limit_to_wrong_values
    
    Migrated the test case from the glusto tests and added it in the redant
    tests folder. Modified the test as per the redant standards and set of
    functionalities.
    
    Updates:#292
    Fixes:#297
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * Added try except in the test at correct place to handle all the values
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: Leela Venkaiah G <lgangava@redhat.com>

---
+++ tests/functional/glusterd/test_change_reserve_limit_to_wrong_values.py
@@ -0,0 +1,68 @@
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test changing reserve limit to wrong values
+"""
+
+import string
+from random import choice
+from tests.abstract_test import AbstractTest
+
+# nonDisruptive;dist-rep
+
+
+class TestChangeReserveLimit(AbstractTest):
+    """
+    Test to validate behaviour of 'storage.reserve' option on supplying
+    erroneous values.
+    """
+    @staticmethod
+    def get_random_string(chars, str_len=4):
+        return ''.join((choice(chars) for _ in range(str_len)))
+
+    def run_test(self, redant):
+        """
+        Test Case:
+        1) Create and start a distributed-replicated volume.
+        2) Give different inputs to the storage.reserve volume set options
+        3) Validate the command behaviour on wrong inputs
+        """
+
+        # Creation of random data for storage.reserve volume option
+        # Data has: alphabets, numbers, punctuations and their combinations
+        key = 'storage.reserve'
+        for char_type in (string.ascii_letters, string.punctuation,
+                          string.printable):
+
+            temp_val = self.get_random_string(char_type)
+            temp_val = temp_val.replace("'", "").replace("&", "")
+            value = "'{}'".format(temp_val)
+            try:
+                redant.set_volume_options(self.vol_name, {key: value},
+                                          self.server_list[0])
+            except Exception:
+                redant.logger.info("Wrong value was tested successfully")
+
+        # Passing an out of range value
+        for value in ('-1%', '-101%', '101%', '-1', '-101'):
+            try:
+                redant.set_volume_options(self.vol_name,
+                                          {key: value},
+                                          self.server_list[0])
+            except Exception:
+                redant.logger.info("Wrong value was tested successfully")
