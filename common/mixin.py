8054556ec52c0006a9cb7cd131889e6d586b5866
schaffung <ssivakum@redhat.com>
2021-05-11 14:07:21 +0530

    [Common] Guarding volds access. (#290)
    * [Common] Guarding volds access.
    
    The volds data structure should be guarded with getters
    and setters.
    
    Fixes: #282
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Added methods to support volds.
    
    Updates: #282
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Fixed lint error.
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Lint fix: part 2
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * [Common] ValueError check added.
    
    Updates: #282
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Update mixin.py
    
    Added docstring for `get_mnt_pts_list`
    
    * Added one more method for io_ops
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Added dependency

--- common/mixin.py
+++ common/mixin.py
@@ -2,6 +2,8 @@
     Module Name:
     Purpose: Refer to the redhat_mixin.md for more information
 """
+
+import copy
 from .rexe import Rexe
 from .relog import Logger
 from .ops.support_ops.io_ops import IoOps
@@ -17,3 +19,86 @@ class RedantMixin(GlusterOps, BrickOps, VolumeOps,
     A mixin class for redant project to encompass all ops and support
     modules.
     """
+
+    def __init__(self, server_config):
+        super().__init__(server_config)
+        self.volds = {}
+
+    def get_mnt_pts_dict(self, volname: str) -> dict:
+        """
+        Method to obtain the mountpath dictionary.
+        Arg:
+            volname (str)
+        Returns:
+            dictionary of nodes and their list of mountpaths.
+        """
+        return self.volds[volname]['mountpath']
+
+    def get_mnt_pts_dict_in_list(self, volname: str) -> list:
+        """
+        Method to return a modified list of mountpath which contains
+        multiple client->mountpath relation.
+        Args:
+            volname (str)
+        Returns:
+            list of client->mountpath dictionaries.
+        """
+        if volname is None:
+            raise Exception("Volname not provided.")
+
+        mnt_list = []
+        for (client, mnts) in self.volds[volname]['mountpath'].items():
+            temp_dict = {}
+            for mnt in mnts:
+                temp_dict["client"] = client
+                temp_dict["mountpath"] = mnt
+                copy_dict = copy.deepcopy(temp_dict)
+                mnt_list.append(copy_dict)
+
+        return mnt_list
+
+    def get_mnt_pts_list(self, volname: str, node: str = None) -> list:
+        """
+        Method to obtain the list of mountpaths.
+        Args:
+            volname (str)
+            node (str) client node.
+        Returns:
+            list of mountpaths belonging to a node or list of
+            all mountpoints
+        """
+        if node is None:
+            mount_point_list = []
+            for (_, mnt_pts) in self.volds[volname]['mountpath'].items():
+                mount_point_list.append(mnt_pts)
+            return mount_point_list
+        elif node not in self.volds[volname]['mountpath'].keys():
+            raise KeyError
+        else:
+            return self.volds[volname]['mountpath'][node]
+
+    def get_brick_dict(self, volname: str) -> dict:
+        """
+        Method to obtain brick dictionary
+        Args:
+            volname (str)
+        Return:
+            dictionary of nodes and their list of bricks.
+        """
+        return self.volds[volname]['brickdata']
+
+    def get_brick_list(self, volname: str, node: str) -> list:
+        """
+        Method to obtain brick list
+        Args:
+            volname (str)
+            node (str)
+        Returns:
+            List of bricks for the given node.
+        """
+        if node is None:
+            raise ValueError
+        if node not in self.volds[volname]['brickdata'].keys():
+            raise KeyError
+        else:
+            return self.volds[volname]['brickdata'][node]
