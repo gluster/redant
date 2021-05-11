d088fff620cc3d46eeb17d06f8dceb20c93a79c8
schaffung <ssivakum@redhat.com>
2021-05-11 10:21:52 +0530

    [Test] Test glusterd set reset reserve limit (#293)
    * Test set and reset of storage.reserve limit on glusterd
    
    Steps-
    1. Create a volume and start it.
    2. Set storage.reserve limit on the created volume and verify
    3. Reset storage.reserve limit on the created volume and verify
    
    Change-Id: I6592d19463696ba2c43efbb8f281024fc610d18d
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Changing the assert statement
    
    changed from
    `self.validate_vol_option('storage.reserve', '1 (DEFAULT)')`
    to
    `self.validate_vol_option('storage.reserve', '1')`
    
    Change-Id: If75820b4ab3c3b04454e232ea1eccc4ee5f7be0b
    Signed-off-by: “Milind” <mwaykole@redhat.com>
    
    * [Test] Test glusterd set reset reserve limit
    
    Migrating the first test case from glusto-tests
    to Redant.
    
    Updates: #292
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Fixed lint issue
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Update volume_ops.py
    
    Pylint return value issue.
    
    * Modified Exception
    
    Instead of raising `ValueError`, we are raising a custom Exception.
    
    Co-authored-by: “Milind” <mwaykole@redhat.com>

---
+++ tests/functional/glusterd/test_glusterd_set_reset_reserve_limit.py
@@ -0,0 +1,46 @@
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test set and reset of storage reserve limit in glusterd
+"""
+
+# nonDisruptive;rep,dist,arb,disp,dist-rep,dist-arb,dist-disp
+
+from tests.abstract_test import AbstractTest
+
+
+class TestCase(AbstractTest):
+    """ Testing set and reset of Reserve limit in GlusterD """
+
+    def run_test(self, redant):
+        """
+        Test set and reset of reserve limit on glusterd
+        1. Create a volume and start it.
+        2. Set storage.reserve limit on the created volume and verify it.
+        3. Reset storage.reserve limit on the created volume and verify it.
+        """
+        redant.set_volume_options(self.vol_name, {'storage.reserve': '50'},
+                                  self.server_list[0])
+        redant.validate_volume_option(self.vol_name,
+                                      {'storage.reserve': '50'},
+                                      self.server_list[0])
+        redant.reset_volume_option(self.vol_name, 'storage.reserve',
+                                   self.server_list[0])
+        redant.validate_volume_option(self.vol_name,
+                                      {'storage.reserve': '1 (DEFAULT)'},
+                                      self.server_list[0])
