48693fc90dec57c2e4c6c98035ee0c6db2288928
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-05-11 17:46:59 +0530

    Added test_default_log_level_of_cli (#304)
    * Add tc to check that default log level of CLI
    
    Test Steps:
    1) Create and start a volume
    2) Run volume info command
    3) Run volume status command
    4) Run volume stop command
    5) Run volume start command
    6) Check the default log level of cli.log
    
    Change-Id: I871d83500b2a3876541afa348c49b8ce32169f23
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Added test_default_log_level_of_cli
    
    Migrated the teest case from glusto-test.
    Made modifications as per te functionalities in redant.
    
    Updates:#292
    Fixes:#303
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added all the volumes and tested
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * changed the log path and added exception
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: nik-redhat <nladha@redhat.com>

---
+++ tests/functional/glusterd/test_default_log_level_of_cli.py
@@ -0,0 +1,59 @@
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+      Test to check that default log level of CLI should be INFO
+"""
+
+from tests.abstract_test import AbstractTest
+
+# nonDisruptive;rep,dist,arb,disp,dist-rep,dist-arb,dist-disp
+
+
+class TestDefaultLogLevelOfCLI(AbstractTest):
+
+    def run_test(self, redant):
+        """
+        Test Case:
+        1) Create and start a volume
+        2) Run volume info command
+        3) Run volume status command
+        4) Run volume stop command
+        5) Run volume start command
+        6) Check the default log level of cli.log
+        """
+        # Check volume info operation
+        redant.get_volume_info(self.server_list[0])
+
+        # Check volume status operation
+        redant.get_volume_status(node=self.server_list[0])
+
+        # Check volume stop operation
+        redant.volume_stop(
+            self.vol_name, self.server_list[0], True)
+
+        # Check volume start operation
+        redant.volume_start(self.vol_name, self.server_list[0])
+
+        # Check the default log level of cli.log
+        cmd = 'cat /var/log/glusterfs/cli.log | grep -F "] D [" | wc -l'
+        ret = redant.execute_command(cmd, self.server_list[0])
+
+        if int(ret['msg'][0].split("\\")[0]) == 0:
+            redant.logger.info("The default log level is INFO")
+        else:
+            raise Exception("Default log level not INFO")
