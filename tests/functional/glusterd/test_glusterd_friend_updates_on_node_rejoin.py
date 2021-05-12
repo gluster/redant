64a962e9b7405c3052026a054690c9c800a4064b
nik-redhat <nladha@redhat.com>
2021-05-12 19:40:44 +0530

    Added test_glusterd_friend_updates_on_node_rejoin
    Migrated the glusto
    [test-case](https://github.com/gluster/glusto-tests/blob/master/tests/functional/glusterd/test_glusterd_friend_updates_on_node_rejoin.py)
    
    Updates: #292
    Fixes: #316
    
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/glusterd/test_glusterd_friend_updates_on_node_rejoin.py
+++ tests/functional/glusterd/test_glusterd_friend_updates_on_node_rejoin.py
@@ -16,16 +16,14 @@
 
 from time import sleep
 from datetime import datetime, timedelta
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass
-from glustolibs.gluster.gluster_init import (restart_glusterd,
-                                             wait_for_glusterd_to_start)
+from tests.abstract_test import AbstractTest
+
+# disruptive;
 
 
-class TestGlusterdFriendUpdatesWhenPeerRejoins(GlusterBaseClass):
+class TestGlusterdFriendUpdatesWhenPeerRejoins(AbstractTest):
 
-    # pylint: disable=too-few-public-methods
-    def test_glusterd_friend_update_on_peer_rejoin(self):
+    def run_test(self, redant):
         """
         Test Steps:
         1. Restart glusterd on one of the node
@@ -34,13 +32,9 @@ class TestGlusterdFriendUpdatesWhenPeerRejoins(GlusterBaseClass):
         3. Check friend updates between rejoined node to each other node
         """
         # Restart glusterd on one of the node
-        ret = restart_glusterd(self.servers[1])
-        self.assertTrue(ret, "Failed to restart glusterd on server: %s"
-                        % self.servers[1])
+        redant.restart_glusterd(self.server_list[0])
 
-        ret = wait_for_glusterd_to_start(self.servers[1])
-        self.assertTrue(ret, "Unexpected: Glusterd not yet started on:"
-                        " server: %s" % self.servers[1])
+        redant.wait_for_glusterd_to_start(self.server_list[0])
 
         # Save the current UTC time
         # Reducing 1 second to adjust with the race conditions in logging
@@ -54,45 +48,42 @@ class TestGlusterdFriendUpdatesWhenPeerRejoins(GlusterBaseClass):
         min_updt = 4
 
         # Current cluster size
-        crnt_clstr_size = len(self.servers)
+        crnt_clstr_size = len(self.server_list)
 
         # Wait until all the updates between the cluster nodes finish
         sleep(2 * crnt_clstr_size)
 
         # Intentional, to leverage the filtering of command line
         cmd = "gluster peer status | grep 'Uuid:' | cut -d ':' -f 2"
-        ret, peer_lst, _ = g.run(self.servers[1], cmd)
-        self.assertEqual(ret, 0, "Failed to execute the peer status command")
-        peer_lst = peer_lst.splitlines()
+        ret = redant.execute_command(cmd, self.server_list[0])
+        peer_lst = ret['msg'][0].splitlines()
         peer_lst = [p_uuid.strip() for p_uuid in peer_lst]
 
         # Check if there are any friend update between other nodes
         # and the restarted node
-        for server in self.servers:
+        for server in self.server_list:
             # Don't check on the restarted node
-            if server != self.servers[1]:
+            if server != self.server_list[0]:
                 for uuid in peer_lst:
                     cmd = ("awk '/%s/,0' /var/log/glusterfs/glusterd.log |"
                            " grep '_handle_friend_update' | grep %s | wc -l"
                            % (curr_time, uuid))
-                    ret, out, _ = g.run(server, cmd)
-                    self.assertEqual(ret, 0, "Failed to get count of friend"
-                                     " updates")
-                    out = int(out)
-                    self.assertEqual(out, 0, "Unexpected: Found friend updates"
-                                     " between other nodes")
+                    ret = redant.execute_command(cmd, self.server_list[0])
+                    if int(ret['msg'][0]) != 0:
+                        raise Exception("Unexpected: Found friend updates"
+                                        " between other nodes")
 
-        g.log.info("Expected: No friend updates between other peer nodes")
+        redant.looger.info("Expected: No friend updates between other"
+                           " peer nodes")
 
         # Check friend updates between rejoined node and other nodes
         cmd = ("awk '/%s/,0' /var/log/glusterfs/glusterd.log "
                "| grep '_handle_friend_update' | wc -l" % curr_time)
-        ret, count, _ = g.run(self.servers[1], cmd)
-        self.assertEqual(ret, 0, "Failed to fetch the count of friend updates")
-        count = int(count)
+        ret = redant.execute_command(cmd, self.server_list[0])
+        count = int(ret['msg'][0])
 
         # Calculate the expected friend updates for a given cluster size
         expected_frnd_updts = min_updt * (crnt_clstr_size - min_clstr_sz + 1)
-
-        self.assertEqual(count, expected_frnd_updts, "Count of friend updates"
-                         " is not equal to the expected value")
+        if count != expected_frnd_updts:
+            raise Exception("Count of friend updates"
+                            " is not equal to the expected value")
