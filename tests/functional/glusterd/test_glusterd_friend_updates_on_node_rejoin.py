e33fea7d2036cb2d319604e12a0d292f4f3c5198
nik-redhat <nladha@redhat.com>
2021-05-12 20:21:34 +0530

    Fix typo and docstring
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/glusterd/test_glusterd_friend_updates_on_node_rejoin.py
+++ tests/functional/glusterd/test_glusterd_friend_updates_on_node_rejoin.py
@@ -1,18 +1,20 @@
-#  Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
 
 from time import sleep
 from datetime import datetime, timedelta
@@ -73,7 +75,7 @@ class TestGlusterdFriendUpdatesWhenPeerRejoins(AbstractTest):
                         raise Exception("Unexpected: Found friend updates"
                                         " between other nodes")
 
-        redant.looger.info("Expected: No friend updates between other"
+        redant.logger.info("Expected: No friend updates between other"
                            " peer nodes")
 
         # Check friend updates between rejoined node and other nodes
