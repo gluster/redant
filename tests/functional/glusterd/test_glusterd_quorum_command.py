ed8575942afc19206c25c7a549eb634d5589d1fd
schaffung <ssivakum@redhat.com>
2021-05-12 10:40:55 +0530

    [Test] test_glusterd_quorum_command migration (#312)
    * Test Quorum specific CLI commands.
    
    Steps-
    1. Create a volume and start it.
    2. Set the quorum-type to 'server' and verify it.
    3. Set the quorum-type to 'none' and verify it.
    4. Set the quorum-ratio to some value and verify it.
    
    Change-Id: I08715972c13fc455cee25f25bdda852b92a48e10
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Adding cluster options reset.
    
    The cluster options once set aren't reset and this would cause
    problem for subsequent TCs. hence reseting the options at
     teardown.
    
    Change-Id: Ifd1df2632a25ca7788a6bb4f765b3f6583ab06d6
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * [Test] test_glusterd_quorum_command migration
    
    Glusto test link : [test_glusterd_quorum_command](https://github.com/gluster/glusto-tests/blob/master/tests/functional/glusterd/test_glusterd_quorum_command.py)
    
    Fixes: #308
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Lint fixes.
    
    Updates: #308
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Redundant description removed and volume paramter added
    
    Fixes: #308
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    Co-authored-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

---
+++ tests/functional/glusterd/test_glusterd_quorum_command.py
@@ -0,0 +1,60 @@
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test quorum cli commands in glusterd
+"""
+
+# disruptive;rep,dist,disp,arb,dist-arb,dist-rep
+
+from tests.abstract_test import AbstractTest
+
+
+class TestGlusterDQuorumCLICommands(AbstractTest):
+
+    def _set_and_validate_quorum_option(self, options_dict: dict,
+                                        vol: str = 'all'):
+        """
+        Method to handle the repetitive calls for setting quorum
+        options such as quorum ratio and quorum type.
+        """
+        self.redant.set_volume_options(vol, options_dict, self.server_list[0])
+        self.redant.validate_volume_option(vol, options_dict,
+                                           self.server_list[0])
+
+    def run_test(self, redant):
+        """
+        Test quorum CLI commands on glusterd
+        1. Create a volume and start it.
+        2. Set the quorum type to 'server' and verify it.
+        3. Set the quorum type to 'none' and verify it.
+        4. Set the quorum ratio and verify it.
+        """
+        # Set server quorum type to 'server' and validate it
+        self._set_and_validate_quorum_option({'cluster.server-quorum-type':
+                                              'server'}, self.vol_name)
+
+        # Set server quorum type to 'none' and validate it
+        self._set_and_validate_quorum_option({'cluster.server-quorum-type':
+                                              'none'}, self.vol_name)
+
+        # Set server quorum ratio to 90% and validate it
+        self._set_and_validate_quorum_option({'cluster.server-quorum-ratio':
+                                              '90%'})
+
+        # Resetting all volume options.
+        redant.reset_volume_option(self.vol_name, "all", self.server_list[0])
