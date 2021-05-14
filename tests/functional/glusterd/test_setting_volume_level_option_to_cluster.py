cc4ee814f430c94e52d7ca52fc282315641ef8d4
Ayush Ujjwal <aujjwal@redhat.com>
2021-05-14 17:04:15 +0530

    Indented properly and made small changes
    Indented the docstring and the ret.set_volume_option statements
    Added condition for checking ret value in case of `is_glusterd_running`.
    Removed unused ret values.
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_setting_volume_level_option_to_cluster.py
+++ tests/functional/glusterd/test_setting_volume_level_option_to_cluster.py
@@ -1,24 +1,23 @@
 """
 Copyright (C) 2019  Red Hat, Inc. <http://www.redhat.com>
 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License along`
- with this program; if not, write to the Free Software Foundation, Inc.,
- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
- Description:
-
- In this test we are testing the volume level option to cluster
- """
+Description:
+In this test we are testing the volume level option to cluster
+"""
 
 # nonDisruptive;
 
@@ -28,12 +27,11 @@ from tests.abstract_test import AbstractTest
 class TestSettingVolumeLevelOptionToCluster(AbstractTest):
 
     def run_test(self, redant):
-        # pylint: disable=too-many-statements
         """
         Test Case:
         1) Create a cluster.
         2) Try to set volume level options to cluster level.
-           (These should fail!)
+           (This should fail!)
         eg: gluster v set all transport.listen-backlog 128
             gluster v set all performance.parallel-readdir on
         3) Check if glusterd has crashed or not.(Should not crash!)
@@ -41,9 +39,10 @@ class TestSettingVolumeLevelOptionToCluster(AbstractTest):
 
         # Set transport.listen-backlog to 128 for all volumes.(Should fail!)
         try:
-            ret = redant.set_volume_options(
-                'all', {'transport.listen-backlog': '128'},
-                self.server_list[0])
+            ret = (redant.
+                   set_volume_options('all',
+                                      {'transport.listen-backlog': '128'},
+                                      self.server_list[0]))
         except Exception as error:
             redant.logger.info(error)
             redant.logger.info("Successfully tested"
@@ -53,6 +52,9 @@ class TestSettingVolumeLevelOptionToCluster(AbstractTest):
         # Checking if glusterd is running on all the nodes.
         for each_server in self.server_list:
             ret = redant.is_glusterd_running(each_server)
+            if ret != 1:
+                raise Exception(f"Failed: Glusterd not running"
+                                f" on {each_server}")
 
         redant.logger.info("Glusterd is running on all the servers")
 
@@ -65,9 +67,9 @@ class TestSettingVolumeLevelOptionToCluster(AbstractTest):
 
         # Set performance.parallel-readdir to on for all volumes.(Should fail!)
         try:
-            ret = redant.set_volume_options(
-                'all', {'performance.parallel-readdir': 'on'},
-                self.server_list[0])
+            redant.set_volume_options('all',
+                                      {'performance.parallel-readdir': 'on'},
+                                      self.server_list[0])
         except Exception as error:
             redant.logger.info("EXPECTED: Failed to set parallel-readdir to"
                                " ON for all volumes.")
@@ -75,6 +77,10 @@ class TestSettingVolumeLevelOptionToCluster(AbstractTest):
         # Checking if glusterd is running on all the nodes.
         for each_server in self.server_list:
             ret = redant.is_glusterd_running(each_server)
+
+            if ret != 1:
+                raise Exception(f"Failed: Glusterd not running"
+                                f" on {each_server}")
 
         redant.logger.info("Glusterd is running on all the servers")
 
