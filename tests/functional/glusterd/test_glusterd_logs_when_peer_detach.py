201ca10474598408e6c8eb63c710ae27bc2a3f7c
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-05-14 16:06:17 +0530

    Migration of test_glusterd_logs_when_peer_detach (#336)
    * No Errors should generate in glusterd.log while detaching node from gluster
    
    -> Detach the node from peer
    -> Check that any error messages related to peer detach
    in glusterd log file
    -> No errors should be there in glusterd log file
    
    Change-Id: I481df5b15528fb6fd77cd1372110d7d23dd5cdef
    Signed-off-by: Rajesh Madaka <rmadaka@redhat.com>
    
    * Add py3 support in 'tests/functional/glusterd'. Part 1
    
    Change-Id: Ib414b8496ca65a48bbe42936e32a863c9c1072e4
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Add steps to add peer_probe_servers in cleanup
    
    Change-Id: I0fa6bbacda16fb97d3454a8510a937442b5755a4
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Test Migration
    test_glusterd_logs_when_peer_detach.py glusto test is migrated to
    redant
    
    Updates: #292
    Fixes: #317
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * removed rstrip in one case where it is redundant
    
    Fixes: #317
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * Flake issue fix
    
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * Exception is added and some minor changes
    
    Fixes: #317
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * Redundant return value variables removed
    
    Fixes: #317
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    Co-authored-by: Rajesh Madaka <rmadaka@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/glusterd/test_glusterd_logs_when_peer_detach.py
@@ -0,0 +1,62 @@
+"""
+  Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Description:
+        Check for error in glusterd logs when a peer is detached
+"""
+
+import random
+from tests.abstract_test import AbstractTest
+
+# disruptive;
+
+
+class GlusterdLogsWhilePeerDetach(AbstractTest):
+
+    def run_test(self, redant):
+        '''
+            1) Detach the node from peer
+            2) Check that any error messages related to peer detach
+                in glusterd log file
+            3) No errors should be there in glusterd log file
+        '''
+
+        # Getting timestamp
+        ret = redant.execute_abstract_op_node('date +%s', self.server_list[0])
+        timestamp = ret['msg'][0].rstrip("\n")
+
+        #  glusterd logs
+        redant.execute_abstract_op_node('cp /var/log/glusterfs/glusterd.log '
+                                        '/var/log/glusterfs/'
+                                        f'glusterd_{timestamp}.log',
+                                        self.server_list[0])
+
+        # Clearing the existing glusterd log file
+        redant.execute_abstract_op_node('> /var/log/glusterfs/glusterd.log',
+                                        self.server_list[0])
+
+        # Performing peer detach
+        random_server = random.choice(self.server_list[1:])
+        redant.peer_detach(random_server, self.server_list[0])
+
+        # Searching for error message in log
+        ret = redant.execute_abstract_op_node("grep ' E ' /var/log/glusterfs/"
+                                              "glusterd.log | wc -l",
+                                              self.server_list[0])
+        if int(ret['msg'][0]) != 0:
+            raise Exception("Found Error messages in glusterd "
+                            "log file after peer detach")
