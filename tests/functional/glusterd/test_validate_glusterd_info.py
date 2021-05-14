3060c44757e687246532946f0bbf41e4edab0c2f
Nishith Vihar Sakinala <nsakinal@redhat.com>
2021-05-14 18:45:29 +0530

    Test case Migration of test_validate_glusterd_info test_validate glusterd_info glusto test is migrated from glusto-tests to redant
    Updates: #292
    Fixes: #347
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- tests/functional/glusterd/test_validate_glusterd_info.py
+++ tests/functional/glusterd/test_validate_glusterd_info.py
@@ -1,27 +1,28 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass
-from glustolibs.gluster.peer_ops import get_peer_status
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+# nonDisruptive;
+from tests.abstract_test import AbstractTest
 
 
-class TestGlusterdInfo(GlusterBaseClass):
+class TestGlusterdInfo(AbstractTest):
 
-    def test_validate_glusterd_info(self):
+    def run_test(self, redant):
         """
         Steps:
             1. Check for the presence of /var/lib/glusterd/glusterd.info file
@@ -33,40 +34,43 @@ class TestGlusterdInfo(GlusterBaseClass):
                 on one node will give the UUID of the other node
         """
         uuid_list = []
-        for server in self.servers:
+        for server in self.server_list:
 
             # Getting UUID from glusterd.info
-            g.log.info("Getting the UUID from glusterd.info")
-            ret, glusterd_volinfo, _ = g.run(
-                server, "grep -i uuid /var/lib/glusterd/glusterd.info")
+            redant.logger.info("Getting the UUID from glusterd.info")
+            ret = redant.execute_abstract_op_node(
+                "grep -i uuid /var/lib/glusterd/glusterd.info", server)
+            glusterd_volinfo = " ".join(ret['msg'])
             uuid_list.append(glusterd_volinfo)
             glusterd_volinfo = (glusterd_volinfo.split("="))[1]
-            self.assertFalse(
-                ret, "Failed to run '{}' on '{}' ".format(server, server))
-            self.assertIsNotNone(
-                glusterd_volinfo, "UUID not found in 'glusterd.info' file ")
+            if not glusterd_volinfo:
+                raise Exception("UUID not found in 'glusterd.info' file ")
 
             # Getting UUID from cmd 'gluster system uuid get'
-            ret, get_uuid, _ = g.run(
-                server, "gluster system uuid get | awk {'print $2'}")
-            self.assertFalse(ret, "Unable to get the UUID ")
-            self.assertIsNotNone(get_uuid, "UUID not found")
+            ret = redant.execute_abstract_op_node(
+                "gluster system uuid get | awk {'print $2'}", server)
+            get_uuid = " ".join(ret['msg'])
+            if not get_uuid:
+                raise Exception("UUID not found")
 
             # Checking if both the uuid are same
-            self.assertEquals(
-                glusterd_volinfo, get_uuid,
-                "UUID does not match in host {}".format(server))
+            if glusterd_volinfo != get_uuid:
+                raise Exception(f"UUID does not match in host{server}")
 
             # Geting the UUID from cmd "gluster peer status"
-            for node in self.servers:
-                for i in get_peer_status(node):
-                    uuid_list.append(i["uuid"])
-                if server != node:
-                    self.assertTrue(
-                        get_uuid.replace("\n", "") in uuid_list,
-                        "uuid not matched in {}".format(node))
+            for node in self.server_list:
+                peer_status = redant.get_peer_status(node)
+                if isinstance(peer_status, list):
+                    for i in redant.get_peer_status(node):
+                        uuid_list.append(i["uuid"])
+                else:
+                    uuid_list.append(peer_status["uuid"])
 
-    def test_glusterd_config_file_check(self):
+                if server != node and get_uuid.rstrip("\n") not in uuid_list:
+                    raise Exception(f"uuid not matched in {node}")
+        self.test_glusterd_config_file_check(redant)
+
+    def test_glusterd_config_file_check(self, redant):
         """
         Steps:
             1. Check the location of glusterd socket file ( glusterd.socket )
@@ -76,21 +80,18 @@ class TestGlusterdInfo(GlusterBaseClass):
         """
 
         cmd = "ls  /var/run/ | grep -i glusterd.socket"
-        ret, out, _ = g.run(self.mnode, cmd)
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0])
+        msg = " ".join(ret['msg'])
 
         # Checking glusterd.socket file
-        self.assertFalse(
-            ret, "Failed to get glusterd.socket file on '{}'".format(
-                self.mnode))
-        self.assertEqual(
-            out.replace("\n", ""), "glusterd.socket",
-            "Failed to get expected output")
+        if msg.replace("\n", "") != "glusterd.socket":
+            raise Exception("Failed to get expected output")
 
         # Checking for glusterd.service is enabled by default
-        ret, out, _ = g.run(
-            self.mnode, "systemctl is-enabled glusterd.service")
-        self.assertFalse(
-            ret, "Failed to execute the cmd on {}".format(self.mnode))
-        self.assertEqual(
-            out.replace("\n", ""), "enabled",
-            "Output of systemctl is-enabled glusterd.service is not enabled")
+        ret = redant.execute_abstract_op_node(
+            "systemctl is-enabled glusterd.service", self.server_list[0])
+        msg = " ".join(ret['msg'])
+        if msg.replace("\n", "") != 'enabled':
+            raise Exception(
+                "Output of systemctl is-enabled glusterd.service is"
+                " not enabled")
