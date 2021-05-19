676556fdc2f2f3b650dc1839a370e7384010fdd0
Nishith Vihar Sakinala <nsakinal@redhat.com>
2021-05-19 14:51:57 +0530

    Checking of peer probe success from cmd_history.log is added
    Fixes: #355
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- tests/functional/glusterd/test_validate_peer_probe_ip_fqdn_hostname.py
+++ tests/functional/glusterd/test_validate_peer_probe_ip_fqdn_hostname.py
@@ -25,7 +25,9 @@ from tests.d_parent_test import DParentTest
 class TestPeerProbeScenarios(DParentTest):
 
     def _get_node_identifiers(self, redant):
-        """ Returns node address dict with ip, fqdn, hostname as keys """
+        """
+        Returns node address dict with ip, fqdn, hostname as keys
+        """
         node = {}
         node['ip'] = gethostbyname(self.node)
         node['fqdn'] = getfqdn(self.node)
@@ -46,48 +48,50 @@ class TestPeerProbeScenarios(DParentTest):
 
         return self._get_node_identifiers(redant)
 
-    """
     def _verify_cmd_history(self, redant, node):
-
-        #Verifies cmd_history for successful entry of peer probe of nodes
-
+        """
+        Verifies cmd_history for successful entry of peer probe of nodes
+        """
 
         # Extract the test specific cmds from cmd_history
-        start_msg = f"Starting Test : {self.id()} : {self.glustotest_run_id}"
-        end_msg = f"Ending Test: {self.id()} : {self.glustotest_run_id}"
         cmd_history_log = "/var/log/glusterfs/cmd_history.log"
-        cmd = (f"awk '/{start_msg}/ {{p=1}}; p; /{end_msg}/ {{p=0}}' "
-               f"{cmd_history_log}")
-        ret = redant.execute_abstract_op_node(self.server_list[0], cmd)
+        cmd = (f"cat {cmd_history_log}")
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0])
 
         # Verify the cmd is found from the extracted cmd log
-        peer_probe_cmd = f"peer probe {node} : SUCCESS"
+        peer_probe_cmd = f"peer probe {node} : SUCCESS\n"
         test_specific_cmd_history = " ".join(ret['msg'])
+
         if test_specific_cmd_history.count(peer_probe_cmd) != 0:
             raise Exception("Peer probe success entry not found"
                             " in cmd history")
-    """
 
     def run_test(self, redant):
         """
-        1. Add one of the node(HOST1-IP) to the other node(HOST2-IP) and
+        1. Clean the cmd_history.log file to verify peer probe success
+           for the peer being probed for that particular iteration
+        2. Add one of the node(HOST1-IP) to the other node(HOST2-IP) and
            form the cluster
            # gluster peer probe <HOST-IP>
-        2. Check the return value of the 'peer probe' command
-        3. Confirm that the cluster is formed successfully by 'peer status'
+        3. Check the return value of the 'peer probe' command
+        4. Confirm that the cluster is formed successfully by 'peer status'
            command
            # gluster peer status
-        4. Execute 'pool list' command to get the status of the cluster
+        5. Execute 'pool list' command to get the status of the cluster
            including the local node itself
            # gluster pool list
-        5. Check the cmd_history' for the status message related to
+        6. Check the cmd_history' for the status message related to
           'peer probe' command
-        6. Repeat 1-5 for FQDN and hostnames
+        7. Repeat 1-5 for FQDN and hostnames
         """
         self.by_type = ""
         self.node = None
 
         for self.by_type in ('ip', 'fqdn', 'hostname'):
+            # clearing out cmd history from /var/log/glusterfs/cmd_history.log
+            cmd = "> /var/log/glusterfs/cmd_history.log"
+            redant.execute_abstract_op_node(cmd, self.server_list[0])
+
             # Get a node to peer probe to
             host_node = self._get_new_nodes_to_peer_probe(redant)
 
@@ -98,4 +102,4 @@ class TestPeerProbeScenarios(DParentTest):
             redant.is_peer_connected(host_node['ip'], self.server_list[0])
 
             # Verify command history for successful peer probe status
-            # self._verify_cmd_history(redant, host_node[self.by_type])
+            self._verify_cmd_history(redant, host_node[self.by_type])
