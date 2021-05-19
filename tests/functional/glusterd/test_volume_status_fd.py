107d559aa7244b98ce8efe65a1172759f4841994
nik-redhat <nladha@redhat.com>
2021-05-19 15:19:42 +0530

    Migrated TC test_volume_status_fd
    Migrated the TC
    [test_volume_status_fd](https://github.com/gluster/glusto-tests/blob/b9ecce005bd7a71942188802d151e96b281bab05/tests/functional/glusterd/test_volume_status_fd.py)
    
    Updates: #292
    Fixes: #380
    
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/glusterd/test_volume_status_fd.py
+++ tests/functional/glusterd/test_volume_status_fd.py
@@ -19,76 +19,14 @@
         IOs in progress
 """
 
+# nonDisruptive;dist
 import random
-
-from glusto.core import Glusto as g
-
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import (validate_io_procs, wait_for_io_to_complete,
-                                 list_all_files_and_dirs_mounts)
-from glustolibs.gluster.mount_ops import is_mounted
+from tests.nd_parent_test import NdParentTest
 
 
-@runs_on([['distributed'], ['glusterfs']])
-class VolumeStatusFdWhenIOInProgress(GlusterBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.counter = 1
-        cls.get_super_method(cls, 'setUpClass')()
+class TestCase(NdParentTest):
 
-        # Uploading file_dir script in all client direcotries
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        """
-        setUp method for every test
-        """
-        # calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Creating Volume
-        ret = self.setup_volume_and_mount_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-        g.log.info("Volme created and mounted successfully : %s",
-                   self.volname)
-
-    def tearDown(self):
-        """
-        tearDown for every test
-        """
-        if not self.io_validation_complete:
-            g.log.info("Wait for IO to complete as IO validation did not "
-                       "succeed in test method")
-            ret = wait_for_io_to_complete(self.all_mounts_procs, self.mounts)
-            if not ret:
-                raise ExecutionError("IO failed on some of the clients")
-            g.log.info("IO is successful on all mounts")
-
-        # unmounting the volume and Cleaning up the volume
-        ret = self.unmount_volume_and_cleanup_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Failed Cleanup the Volume %s"
-                                 % self.volname)
-        g.log.info("Volume deleted successfully : %s", self.volname)
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_volume_status_fd(self):
-
+    def run_test(self, redant):
         '''
         -> Create volume
         -> Mount the volume on 2 clients
@@ -98,56 +36,36 @@ class VolumeStatusFdWhenIOInProgress(GlusterBaseClass):
         -> List all files and dirs listed
         '''
 
-        # checking volume mounted or not
-        for mount_obj in self.mounts:
-            ret = is_mounted(self.volname, mount_obj.mountpoint, self.mnode,
-                             mount_obj.client_system, self.mount_type)
-            self.assertTrue(ret, "Not mounted on %s"
-                            % mount_obj.client_system)
-            g.log.info("Mounted on %s", mount_obj.client_system)
+        # Check if the volume is mounted
+        redant.get_mounts_stat(self.mountpoint)
 
-        # run IOs
-        g.log.info("Starting IO on all mounts...")
-        self.all_mounts_procs = []
-        for mount_obj in self.mounts:
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d "
-                   "--dir-depth 2 "
-                   "--dir-length 10 "
-                   "--max-num-of-dirs 5 "
-                   "--num-of-files 15 %s" % (self.script_upload_path,
-                                             self.counter,
-                                             mount_obj.mountpoint))
-
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-            self.counter = self.counter + 10
-        self.io_validation_complete = False
+        # run IO on mountpoint
+        redant.logger.info("Starting IO on all mounts...")
+        list_of_procs = redant.run_linux_untar(self.client_list[0],
+                                               self.mountpoint)
 
         # performing  "gluster volume status volname fd" command on
         # all cluster servers randomly while io is in progress,
         # this command should not get hang while io is in progress
         count = 0
         while count < 300:
-            ret, _, _ = g.run(random.choice(self.servers),
-                              "gluster volume status %s fd" % self.volname)
-            self.assertEqual(ret, 0, ("Volume status 'fd' failed on volume %s"
-                                      % self.volname))
-            g.log.info("Volume status fd is successful for %s", self.volname)
+            cmd = f"gluster volume status {self.vol_name} fd"
+            server = random.choice(self.server_list)
+            redant.execute_abstract_op_node(cmd, server)
+            redant.logger.info(f"Successful in logging volume status"
+                               f"fd of volume {self.vol_name}")
             count += 1
 
+        # Get mountpoint list
+        mnt_list = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
-        self.io_validation_complete = True
+        ret = redant.validate_io_procs(list_of_procs, mnt_list)
+        if not ret:
+            raise Exception("IO validation failed")
 
         # List all files and dirs created
-        g.log.info("List all files and directories:")
-        ret = list_all_files_and_dirs_mounts(self.mounts)
-        self.assertTrue(ret, "Failed to list all files and dirs")
-        g.log.info("Listing all files and directories is successful")
+        redant.logger.info("List all files and directories:")
+        ret = redant.list_all_files_and_dirs_mounts(mnt_list)
+        if not ret:
+            raise Exception("Failed to list all files and dirs")
