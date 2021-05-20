e0390b322f6a312020162fc8b9b17fb87a2486e4
srijan-sivakumar <ssivakum@redhat.com>
2021-05-20 18:21:59 +0530

    [Test] Migrating test_readonly_option_on_volume
    Fixes: #403
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- tests/functional/glusterd/test_readonly_option_on_volume.py
+++ tests/functional/glusterd/test_readonly_option_on_volume.py
@@ -1,80 +1,28 @@
-#  Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
 
-""" Description:
-      Test read-only option on volumes
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
-from glusto.core import Glusto as g
 
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.gluster.volume_ops import set_volume_options
+from tests.nd_parent_test import NdParentTest
 
+# nonDisruptive;dist,rep,dist-rep,disp,dist-disp
 
-@runs_on([['distributed', 'replicated', 'distributed-replicated',
-           'dispersed', 'distributed-dispersed'], ['glusterfs']])
-class TestReadOnlyOptionOnVolume(GlusterBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.counter = 1
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Uploading file_dir script in all client direcotries
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        """
-        setUp method for every test
-        """
-        # calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Creating Volume
-        ret = self.setup_volume_and_mount_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-        g.log.info("Volme created and mounted successfully : %s",
-                   self.volname)
-
-    def tearDown(self):
-        """
-        tearDown for every test
-        """
-        # unmounting the volume and Cleaning up the volume
-        ret = self.unmount_volume_and_cleanup_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Failed Cleanup the Volume %s"
-                                 % self.volname)
-
-        # Calling GlusterBaseClass tearDown
-
-    def test_readonly_option_on_volume(self):
+class TestCase(NdParentTest):
+    def run_test(self):
         '''
         -> Create volume
         -> Mount a volume
@@ -85,72 +33,29 @@ class TestReadOnlyOptionOnVolume(GlusterBaseClass):
         '''
 
         # Setting Read-only on volume
-        ret = set_volume_options(self.mnode, self.volname,
-                                 {'read-only': 'on'})
-        self.assertTrue(ret, "gluster volume set %s read-only failed"
-                        % self.volname)
-        g.log.info("gluster volume set %s read-only executed successfully",
-                   self.volname)
+        ret = set_volume_options(self.mnode, self.volname,{'read-only': 'on'})
 
         # run IOs
-        g.log.info("Starting IO on all mounts...")
         self.all_mounts_procs = []
         for mount_obj in self.mounts:
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d "
-                   "--dir-depth 2 "
-                   "--dir-length 2 "
-                   "--max-num-of-dirs 2 "
-                   "--num-of-files 5 %s" % (self.script_upload_path,
-                                            self.counter,
-                                            mount_obj.mountpoint))
+            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files ""--dirname-start-num %d ""--dir-depth 2 ""--dir-length 2 ""--max-num-of-dirs 2 ""--num-of-files 5 %s" % (self.script_upload_path,self.counter,mount_obj.mountpoint))
 
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
+            proc = g.run_async(mount_obj.client_system, cmd,user=mount_obj.user)
             self.all_mounts_procs.append(proc)
             self.counter = self.counter + 10
 
         # Validate IO
-        self.assertFalse(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO should fail on mount points of readonly "
-            "volumes but IO succeeded"
-        )
-        g.log.info("IO failed on mount points of read only volumes "
-                   "as expected")
 
         # Setting Read only off volume
-        ret = set_volume_options(self.mnode, self.volname,
-                                 {'read-only': 'off'})
-        self.assertTrue(ret, "gluster volume set %s read-only failed"
-                        % self.volname)
-        g.log.info("gluster volume set %s read-only executed successfully",
-                   self.volname)
+        ret = set_volume_options(self.mnode, self.volname,{'read-only': 'off'})
 
         # run IOs
-        g.log.info("Starting IO on all mounts...")
         self.all_mounts_procs = []
         for mount_obj in self.mounts:
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d "
-                   "--dir-depth 2 "
-                   "--dir-length 2 "
-                   "--max-num-of-dirs 2 "
-                   "--num-of-files 5 %s" % (self.script_upload_path,
-                                            self.counter,
-                                            mount_obj.mountpoint))
+            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files ""--dirname-start-num %d ""--dir-depth 2 ""--dir-length 2 ""--max-num-of-dirs 2 ""--num-of-files 5 %s" % (self.script_upload_path,self.counter,mount_obj.mountpoint))
 
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
+            proc = g.run_async(mount_obj.client_system, cmd,user=mount_obj.user)
             self.all_mounts_procs.append(proc)
             self.counter = self.counter + 10
 
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
