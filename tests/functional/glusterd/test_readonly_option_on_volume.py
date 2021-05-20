361b6a1b90918f992ed596b382fc80d18ecec736
srijan-sivakumar <ssivakum@redhat.com>
2021-05-20 23:13:16 +0530

    Initial modifications in the test case
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- tests/functional/glusterd/test_readonly_option_on_volume.py
+++ tests/functional/glusterd/test_readonly_option_on_volume.py
@@ -14,6 +14,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    To validate read only option on mount.
 """
 
 
@@ -22,31 +25,33 @@ from tests.nd_parent_test import NdParentTest
 # nonDisruptive;dist,rep,dist-rep,disp,dist-disp
 
 class TestCase(NdParentTest):
-    def run_test(self):
-        '''
-        -> Create volume
-        -> Mount a volume
-        -> set 'read-only on' on a volume
-        -> perform some I/O's on mount point
-        -> set 'read-only off' on a volume
-        -> perform some I/O's on mount point
-        '''
+
+    def run_test(self, redant):
+        """
+        Test steps:
+        1. Create a volume
+        2. Mount the volume
+        3. set 'read-only on' on the volume
+        4. perform some I/O's on mount point
+        5. set 'read-only off' on the volume
+        6. perform some I/O's on mount point
+        """
 
         # Setting Read-only on volume
-        ret = set_volume_options(self.mnode, self.volname,{'read-only': 'on'})
+        redant.set_volume_options(self.vol_name, {'read-only': 'on'},
+                                  self.server_list[0])
 
-        # run IOs
-        self.all_mounts_procs = []
-        for mount_obj in self.mounts:
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files ""--dirname-start-num %d ""--dir-depth 2 ""--dir-length 2 ""--max-num-of-dirs 2 ""--num-of-files 5 %s" % (self.script_upload_path,self.counter,mount_obj.mountpoint))
+        # run IO
+        cmd = (f'cd /mnt/{self.volume_type};'
+               'for i in {1..100};do mkdir '
+               ' dir${i}; for j in {1..30};do echo "sample" > dir${i}/f${j};'
+               'done;done')
+        async_object = redant.execute_command_async(cmd, self.client_list[0])
 
-            proc = g.run_async(mount_obj.client_system, cmd,user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-            self.counter = self.counter + 10
-
-        # Validate IO
-
-        # Setting Read only off volume
+        # Setting read only off in volume options.
+        redant.set_volume_options(self.vol_name, {'read-only': 'off'},
+                                  self.server_list[0])
+        """ # Setting Read only off volume
         ret = set_volume_options(self.mnode, self.volname,{'read-only': 'off'})
 
         # run IOs
@@ -58,4 +63,4 @@ class TestCase(NdParentTest):
             self.all_mounts_procs.append(proc)
             self.counter = self.counter + 10
 
-        # Validate IO
+        # Validate IO"""
