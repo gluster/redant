ebeb1bff4f65a8f036a19c50cad583ac241dffc7
nik-redhat <nladha@redhat.com>
2021-05-28 16:31:57 +0530

    Migrated TC test_profile_simultaneously_on_different_nodes
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/glusterd/test_profile_simultaneously_on_different_nodes.py
+++ tests/functional/glusterd/test_profile_simultaneously_on_different_nodes.py
@@ -1,85 +1,52 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 """
-    Test Description:
-    Tests to test profile simultaneously on different nodes.
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+TC to check profile operations simultaneously on different nodes
 """
 
-from glusto.core import Glusto as g
+import traceback
+from tests.d_parent_test import DParentTest
 
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.profile_ops import profile_start
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.gluster.lib_utils import is_core_file_created
-from glustolibs.gluster.gluster_init import is_glusterd_running
-from glustolibs.gluster.volume_ops import get_volume_list
-from glustolibs.gluster.volume_libs import (cleanup_volume, setup_volume)
+# disruptive;dist,rep,dist-rep,disp,dist-disp
 
 
-@runs_on([['distributed', 'replicated', 'distributed-replicated',
-           'dispersed', 'distributed-dispersed'], ['glusterfs']])
-class TestProfileSimultaneouslyOnDifferentNodes(GlusterBaseClass):
+class TestCase(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Uploading file_dir script in all client direcotries
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        self.get_super_method(self, 'setUp')()
-        # Creating Volume and mounting volume.
-        ret = self.setup_volume_and_mount_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-        g.log.info("Volume created and mounted successfully : %s",
-                   self.volname)
-
-    def tearDown(self):
-        # Unmounting and cleaning volume.
-        ret = self.unmount_volume_and_cleanup_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Unable to delete volume % s" % self.volname)
-        g.log.info("Volume deleted successfully : %s", self.volname)
-
-        # clean up all volumes
-        vol_list = get_volume_list(self.mnode)
-        if not vol_list:
-            raise ExecutionError("Failed to get the volume list")
-        for volume in vol_list:
-            ret = cleanup_volume(self.mnode, volume)
+    def terminate(self):
+        """
+        In case the test fails midway and one of the nodes has
+        glusterd stopped then the glusterd is started on that node
+        and then the terminate function in the DParentTest is called
+        """
+        try:
+            self.redant.cleanup_volume(self.volume_name1, self.server_list[0])
+            ret = self.redant.wait_for_io_to_complete(self.list_of_procs,
+                                                      self.mnt_list)
             if not ret:
-                raise ExecutionError("Unable to delete volume % s" % volume)
-            g.log.info("Volume deleted successfully : %s", volume)
+                raise Exception("IO failed on some of the clients")
 
-        self.get_super_method(self, 'tearDown')()
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-    def test_profile_simultaneously_on_different_nodes(self):
+    def run_test(self, redant):
         """
         Test Case:
         1) Create a volume and start it.
@@ -93,93 +60,72 @@ class TestProfileSimultaneouslyOnDifferentNodes(GlusterBaseClass):
            the other node
         """
         # Timestamp of current test case of start time
-        ret, test_timestamp, _ = g.run_local('date +%s')
-        test_timestamp = test_timestamp.strip()
+        ret = redant.execute_abstract_op_node('date +%s', self.server_list[0])
+        test_timestamp = ret['msg'][0].rstrip('\n')
 
-        # Start IO on mount points.
-        self.all_mounts_procs = []
+        # Get mountpoint list
+        self.mnt_list = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
+        # run IO on mountpoint
+        self.list_of_procs = []
         counter = 1
-        for mount_obj in self.mounts:
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dir-depth 4 "
-                   "--dir-length 6 "
-                   "--dirname-start-num %d "
-                   "--max-num-of-dirs 3 "
-                   "--num-of-files 5 %s" % (
-                       self.script_upload_path,
-                       counter, mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-            counter += 1
+        for mount_obj in self.mnt_list:
+            redant.logger.info(f"Starting IO on {mount_obj['client']}:"
+                               f"{mount_obj['mountpath']}")
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      counter, 4, 6, 3, 5,
+                                                      mount_obj['client'])
+
+            self.list_of_procs.append(proc)
+            counter += 10
 
         # Start profile on volume.
-        ret, _, _ = profile_start(self.mnode, self.volname)
-        self.assertEqual(ret, 0, "Failed to start profile on volume: %s"
-                         % self.volname)
-        g.log.info("Successfully started profile on volume: %s",
-                   self.volname)
+        redant.profile_start(self.vol_name, self.server_list[0])
 
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
-        g.log.info("IO validation complete.")
+        ret = redant.validate_io_procs(self.list_of_procs, self.mnt_list)
+        if not ret:
+            raise Exception("IO validation failed")
 
         # Create and start a volume
-        self.volume['name'] = "volume_2"
-        self.volname = "volume_2"
-        ret = setup_volume(self.mnode, self.all_servers_info, self.volume)
-        self.assertTrue(ret, "Failed to create and start volume")
-        g.log.info("Successfully created and started volume_2")
+        volume_type1 = 'dist'
+        self.volume_name1 = f"{self.test_name}-{volume_type1}-1"
+        redant.setup_volume(self.volume_name1, self.server_list[0],
+                            self.vol_type_inf[self.conv_dict[volume_type1]],
+                            self.server_list, self.brick_roots)
 
         # Start profile on volume.
-        ret, _, _ = profile_start(self.mnode, self.volname)
-        self.assertEqual(ret, 0, "Failed to start profile on volume: %s"
-                         % self.volname)
-        g.log.info("Successfully started profile on volume: %s",
-                   self.volname)
+        redant.profile_start(self.volume_name1, self.server_list[0])
 
         # Run volume status on one of the node in loop
         cmd = "for i in `seq 1 100`;do gluster v status;done"
-        proc1 = g.run_async(self.servers[1], cmd)
+        proc1 = self.execute_command_async(cmd, self.server_list[1])
 
         # Check profile on one of the other node
-        cmd = "gluster v profile %s info" % self.volname
-        ret, _, _ = g.run(self.mnode, cmd)
-        self.assertEqual(ret, 0, "Failed to run profile info on volume: %s"
-                         " on node %s" % (self.volname, self.mnode))
-        g.log.info("Successfully run pofile info on volume: %s on node %s",
-                   self.volname, self.mnode)
+        cmd = f"gluster v profile {self.volume_name1} info"
+        redant.execute_abstract_op_node(cmd, self.server_list[0])
 
         # Run volume profile info on one of the other node in loop
-        cmd = """for i in `seq 1 100`;do gluster v profile %s info;
-              done""" % self.volname
-        proc2 = g.run_async(self.servers[3], cmd)
+        cmd = (f"for i in `seq 1 100`;do gluster v profile "
+               f"{self.volume_name1} info;done")
+        proc2 = self.execute_command_async(cmd, self.server_list[2])
 
-        ret1, _, _ = proc1.async_communicate()
-        ret2, _, _ = proc2.async_communicate()
+        ret = self.wait_till_async_command_ends(proc1)
+        if ret['error_code'] != 0:
+            raise Exception(f"Failed to run volume status in a loop"
+                            f" on node {self.server_list[1]}")
 
-        self.assertEqual(ret1, 0, "Failed to run volume status in a loop"
-                         " on node %s" % self.servers[1])
-        g.log.info("Successfully running volume status in a loop on node"
-                   " %s", self.servers[1])
+        ret = self.wait_till_async_command_ends(proc2)
+        if ret['error_code'] != 0:
+            raise Exception(f"Failed to run profile info in a loop"
+                            f" on node {self.server_list[2]}")
 
-        self.assertEqual(ret2, 0, "Failed to run profile info in a loop"
-                         " on node %s" % self.servers[3])
-        g.log.info("Successfully running volume status in a loop on node"
-                   " %s", self.servers[3])
-
-        # Chekcing for core files.
-        ret = is_core_file_created(self.servers, test_timestamp)
-        self.assertTrue(ret, "glusterd service should not crash")
-        g.log.info("No core file found, glusterd service running "
-                   "successfully")
+        # Checking for core files.
+        ret = redant.check_core_file_exists(self.server_list, test_timestamp)
+        if ret:
+            raise Exception("glusterd service should not have crashed")
 
         # Checking whether glusterd is running or not
-        ret = is_glusterd_running(self.servers)
-        self.assertEqual(ret, 0, "Glusterd has crashed on nodes.")
-        g.log.info("No glusterd crashes observed.")
+        ret = redant.is_glusterd_running(self.server_list)
+        if ret != 1:
+            raise Exception("Glusterd not running on servers")
