8456185fd831dddb645a13d26c566b55f6ad089f
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-05-31 11:57:12 +0530

    Migration of test_rebalance_new_node.py from glusto to redant (#530)
    * Fix up coding style issues in tests
    
    Change-Id: I14609030983d4485dbce5a4ffed1e0353e3d1bc7
    
    * Add py3 support in 'tests/functional/glusterd'. Part 2
    
    Change-Id: I1395e14d8d0aa0cc6097e51c64262fb481f36f05
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 2)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I0d2eeb978c6757d6d910ebfe21b07811bf74b80a
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize the existing content in glusterd
    
    Added wait_for_io_to_complete function to testcases
    used wait_for_glusterd function
    and wait_for_peer_connect function
    
    Change-Id: I4811848aad8cca4198cc93d8e200dfc47ae7ac9b
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * removed the rmdir which was merged in baseclass
    
    Change-Id: I04f7b7c894d48d0188379028412d9c6b48eac210
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Remove python version dependency(Part 4)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I25d30f7bdb20f0825709c4c852140e1906870ce7
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migration of test_rebalance_new_node.py from glusto to redant
    
    Fixes: #292
    Fixes: #492
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * None check added to rebalance status and wait for io complete
    is added in terminate function
    
    Fixes: #492
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/glusterd/test_rebalance_new_node.py
@@ -0,0 +1,97 @@
+"""
+  Copyright (C) 2016-2020  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Description:
+  Testing rebalance operations when io operations are performed
+  and an extra brick is added.
+"""
+
+import traceback
+from tests.d_parent_test import DParentTest
+
+# disruptive;
+
+
+class TestCase(DParentTest):
+
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
+            if not ret:
+                raise Exception("IO failed on some of the clients")
+            self.redant.cleanup_volume(self.volume_name1, self.server_list[0])
+        except Exception as e:
+            tb = traceback.format_exc()
+            self.redant.logger.error(e)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
+        Steps:
+        1) Create a distributed volume
+        2) Perform io operations
+        3) Add brick
+        4) Start Rebalance
+        5) Peer Probe new node
+        6) Get rebalance status
+        """
+        self.volume_type1 = 'dist'
+        self.volume_name1 = f"{self.test_name}-{self.volume_type1}-1"
+        conf_dict = self.vol_type_inf[self.conv_dict[self.volume_type1]]
+        redant.setup_volume(self.volume_name1, self.server_list[0], conf_dict,
+                            self.server_list[0:2], self.brick_roots, True)
+        self.mountpoint = (f"/mnt/{self.volume_name1}")
+        self.redant.execute_abstract_op_node(f"mkdir -p "
+                                             f"{self.mountpoint}",
+                                             self.client_list[0])
+        redant.volume_mount(self.server_list[0], self.volume_name1,
+                            self.mountpoint, self.client_list[0])
+
+        # run IOs
+        redant.logger.info("Starting IO on all mounts...")
+        self.all_mounts_procs = []
+        counter = 1
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.volume_name1)
+        for mount in self.mounts:
+            redant.logger.info(f"Starting IO on {mount['client']}:"
+                               f"{mount['mountpath']}")
+            proc = redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                      counter, 2, 3, 4, 10,
+                                                      mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter = counter + 10
+
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO operations failed on some"
+                            " or all of the clients")
+
+        # add a brick to the volume and start rebalance
+        redant.add_brick(self.volume_name1, self.server_list[0],
+                         self.vol_type_inf[self.conv_dict['dist']],
+                         self.server_list, self.brick_roots, True)
+
+        redant.rebalance_start(self.volume_name1, self.server_list[0])
+
+        # peer probe a new node from existing cluster
+        redant.peer_probe(self.server_list[2], self.server_list[0])
+
+        ret = redant.get_rebalance_status(self.volume_name1,
+                                          self.server_list[0])
+        if ret is None:
+            raise Exception("Rebalance status command has returned None")
