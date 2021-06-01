58e7c33829d94adf3e1da1b5c4a570ba7bca1fcb
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-01 14:51:45 +0530

    Migrated test_setting_volume_option_when_one_node_is_down_in_cluster
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/glusterd/test_setting_volume_option_when_one_node_is_down_in_cluster.py)
    
    Updates: #292
    
    Fixes: #555
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_setting_volume_option_when_one_node_is_down_in_cluster.py
+++ tests/functional/glusterd/test_setting_volume_option_when_one_node_is_down_in_cluster.py
@@ -1,24 +1,23 @@
-#  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
 
-# pylint: disable= too-many-statements
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
 
-import socket
-import random
-from time import sleep
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+This test case deals with setting the volume option
+when one node is down
 from glusto.core import Glusto as g
 from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
 from glustolibs.gluster.exceptions import ExecutionError
@@ -31,104 +30,95 @@ from glustolibs.gluster.peer_ops import (peer_probe_servers,
                                          peer_detach_servers,
                                          nodes_from_pool_list)
 
+"""
+# disruptive;dist-rep
 
-@runs_on([['distributed-replicated'], ['glusterfs']])
-class VolumeInfoSync(GlusterBaseClass):
+import socket
+import random
+from time import sleep
+from tests.d_parent_test import DParentTest
 
-    def setUp(self):
-        self.get_super_method(self, 'setUp')()
+class TestCase(DParentTest):
 
-        # check whether peers are in connected state
-        ret = self.validate_peers_are_connected()
-        if not ret:
-            raise ExecutionError("Peers are not in connected state")
+    # def setUp(self):
+    #     self.get_super_method(self, 'setUp')()
 
-        g.log.info("Peers are in connected state")
+    #     # check whether peers are in connected state
+    #     ret = self.validate_peers_are_connected()
+    #     if not ret:
+    #         raise ExecutionError("Peers are not in connected state")
 
-        # detach a node from cluster, assume last node
-        last_node = self.servers[len(self.servers) - 1]
-        ret = peer_detach_servers(self.mnode, last_node)
-        if not ret:
-            raise ExecutionError("Peer detach failed to the last node "
-                                 "%s from %s" % (last_node, self.mnode))
-        g.log.info("Peer detach SUCCESSFUL.")
+    #     g.log.info("Peers are in connected state")
 
-    def tearDown(self):
+    #     # detach a node from cluster, assume last node
+    #     last_node = self.servers[len(self.servers) - 1]
+    #     ret = peer_detach_servers(self.mnode, last_node)
+    #     if not ret:
+    #         raise ExecutionError("Peer detach failed to the last node "
+    #                              "%s from %s" % (last_node, self.mnode))
+    #     g.log.info("Peer detach SUCCESSFUL.")
 
-        ret = peer_probe_servers(self.mnode, self.servers)
-        if not ret:
-            raise ExecutionError("Failed to probe detached "
-                                 "servers %s" % self.servers)
+    # def tearDown(self):
 
-        # stopping the volume and Cleaning up the volume
-        ret = self.cleanup_volume()
-        if not ret:
-            raise ExecutionError("Failed to Cleanup the Volume %s"
-                                 % self.volname)
-        g.log.info("Volume deleted successfully : %s", self.volname)
+    #     ret = peer_probe_servers(self.mnode, self.servers)
+    #     if not ret:
+    #         raise ExecutionError("Failed to probe detached "
+    #                              "servers %s" % self.servers)
 
-        # calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
+    #     # stopping the volume and Cleaning up the volume
+    #     ret = self.cleanup_volume()
+    #     if not ret:
+    #         raise ExecutionError("Failed to Cleanup the Volume %s"
+    #                              % self.volname)
+    #     g.log.info("Volume deleted successfully : %s", self.volname)
+
+    #     # calling GlusterBaseClass tearDown
+    #     self.get_super_method(self, 'tearDown')()
 
     def test_sync_functinality(self):
 
-        # create a 2x3 volume
-        num_of_servers = len(self.servers)
-        servers_info_from_cluster = {}
-        for server in self.servers[0:num_of_servers-1]:
-            servers_info_from_cluster[server] = self.all_servers_info[server]
-
-        self.volume['servers'] = self.servers[0:num_of_servers-1]
-        self.volume['voltype']['replica_count'] = 3
-        self.volume['voltype']['dist_count'] = 2
-        ret = setup_volume(self.mnode, servers_info_from_cluster, self.volume)
-        self.assertTrue(ret, ("Failed to create "
-                              "and start volume %s" % self.volname))
-        g.log.info("Successfully created and started the volume %s",
-                   self.volname)
-
+        # # create a 2x3 volume
+        # ret = setup_volume(self., servers_info_from_cluster, self.volume)
+        # self.assertTrue(ret, ("Failed to create "
+        #                       "and start volume %s" % self.volname))
+        # g.log.info("Successfully created and started the volume %s",
+        #            self.volname)
+        num_of_servers = len(self.server_list)
         # stop glusterd on a random node of the cluster
-        random_server_index = random.randint(1, num_of_servers - 2)
-        random_server = self.servers[random_server_index]
+        random_server_index = random.randint(1, num_of_servers - 1)
+        random_server = self.server_list[random_server_index]
         cmd = "systemctl stop glusterd"
-        ret = g.run_async(random_server, cmd)
-        g.log.info("Stopping glusterd on %s", random_server)
+        redant.execute_command_async(cmd, random_server)
+        redant.logger.info(f"Stopping glusterd on {random_server}")
 
         # set a option on volume, stat-prefetch on
         self.options = {"stat-prefetch": "on"}
-        ret = set_volume_options(self.mnode, self.volname, self.options)
-        self.assertTrue(ret, ("Failed to set option stat-prefetch to on"
-                              "for the volume %s" % self.volname))
-        g.log.info("Succeeded in setting stat-prefetch option to on"
-                   "for the volume %s", self.volname)
+        redant.set_volume_options(self.vol_name,
+                                 self.options,
+                                 self.server_list[0])
 
         # start glusterd on the node where glusterd is stopped
-        ret = start_glusterd(random_server)
-        self.assertTrue(ret, "Failed to start glusterd on %s" % random_server)
+        redant.start_glusterd(random_server)
 
-        ret = wait_for_glusterd_to_start(random_server)
-        self.assertTrue(ret, "glusterd is not running on %s"
-                        % random_server)
-        g.log.info("glusterd is started and running on %s", random_server)
+        ret = redant.wait_for_glusterd_to_start(random_server)
+        if not ret:
+            print(f"glusterd is not running on {random_server}")
 
         # volume info should be synced across the cluster
-        out1 = get_volume_info(self.mnode, self.volname)
-        self.assertIsNotNone(out1, "Failed to get the volume info from %s"
-                             % self.mnode)
-        g.log.info("Getting volume info from %s is success", self.mnode)
+        out1 = redant.get_volume_info(self.server_list[0],
+                                      self.vol_name)
 
         count = 0
         while count < 60:
-            out2 = get_volume_info(random_server, self.volname)
-            self.assertIsNotNone(out2, "Failed to get the volume info from %s"
-                                 % random_server)
+            out2 = get_volume_info(random_server,
+                                   self.vol_name)
             if out1 == out2:
                 break
             sleep(2)
             count += 1
 
-        g.log.info("Getting volume info from %s is success", random_server)
-        self.assertDictEqual(out1, out2, "volume info is not synced")
+        if out1 != out2:
+            print("volume info is not synced")
 
         # stop glusterd on a random server from cluster
         random_server_index = random.randint(1, num_of_servers - 2)
