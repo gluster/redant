e64e3eb75dc8a7266d40da024b2f46f94b04e189
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-06-01 17:14:29 +0530

    Added test_create_vol_with_used_bricks (#302)
    * Create volume using bricks of deleted volume
    
    -> Create distributed-replica Volume
    -> Add 6 bricks to the volume
    -> Mount the volume
    -> Perform some I/O's on mount point
    -> unmount the volume
    -> Stop and delete the volume
    -> Create another volume using bricks of deleted volume
    
    Change-Id: I263d2f0a359ccb0409dba620363a39d92ea8d2b9
    Signed-off-by: Rajesh Madaka <rmadaka@redhat.com>
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Fix spelling mistake across the codebase
    
    Change-Id: I46fc2feffe6443af6913785d67bf310838532421
    
    * Fixed jira issue RHGSQE-29 and added quorum reset code
    
    Change-Id: Ibd50170d2c3172d7b98c2174630d31a066762f7c
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add py3 support in 'tests/functional/glusterd'. Part 1
    
    Change-Id: Ib414b8496ca65a48bbe42936e32a863c9c1072e4
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 2)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I0d2eeb978c6757d6d910ebfe21b07811bf74b80a
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize the existing content in glusterd
    
    Added wait_for_io_to_complete function to testcases
    used wait_for_glusterd function
    and wait_for_peer_connect function
    
    Change-Id: I4811848aad8cca4198cc93d8e200dfc47ae7ac9b
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Add steps to stabilise content in glusterd - part2
    
    and used wait for peer to connect and
    wait for glusterd to connect functions in testcases
    added fixes to  check file exists
    increased timeout value for failure cases
    
    Change-Id: I9d5692f635ed324ffe7dac9944ec9b8f3b933fd1
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * removed the rmdir which was merged in baseclass
    
    Change-Id: I04f7b7c894d48d0188379028412d9c6b48eac210
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Remove python version dependency(Part 4)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I25d30f7bdb20f0825709c4c852140e1906870ce7
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Added test_create_vol_with_used_bricks
    
    Migrated the test case from the glusto-test and made changes as per the
    functionalities in redant.
    
    Updates:#292
    Fixes:#300
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * first part done
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added the second part for creating volume with used bricks
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added IO
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * removed unnecessary logging; removed force as True in add brick and vol setup
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added cleanup and io validation in terminate
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * made the test case generic
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: Rajesh Madaka <rmadaka@redhat.com>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/glusterd/test_create_vol_with_used_bricks.py
@@ -0,0 +1,139 @@
+"""
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Create volume using bricks of deleted volume
+"""
+import traceback
+from tests.nd_parent_test import NdParentTest
+
+# nonDisruptive;
+
+
+class TestCase(NdParentTest):
+
+    def terminate(self):
+        """
+        The voume created in the test case is destroyed.
+        """
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
+            if not ret:
+                raise Exception("IO failed on some of the clients")
+            self.redant.cleanup_volume(self.volume_name1, self.server_list[0])
+            self.redant.cleanup_volume(self.volname, self.server_list[0])
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        '''
+        -> Create distributed-replica Volume
+        -> Add 6 bricks to the volume
+        -> Mount the volume
+        -> Perform some I/O's on mount point
+        -> unmount the volume
+        -> Stop and delete the volume
+        -> Create another volume using bricks of deleted volume
+        -> Start the volume
+        -> Execute IO command
+        -> Mount the volume
+        '''
+        if len(self.server_list) < 3:
+            raise Exception("This test case requires at least 3 servers")
+        self.volume_type1 = 'dist-rep'
+        self.volume_name1 = "test_create_vol_with_fresh_bricks"
+        conf_dict = self.vol_type_inf[self.conv_dict[self.volume_type1]]
+        redant.setup_volume(self.volume_name1, self.server_list[0],
+                            conf_dict, self.server_list,
+                            self.brick_roots)
+        mul_factor = 6
+        _, br_cmd = redant.form_brick_cmd(self.server_list,
+                                          self.brick_roots,
+                                          self.volume_name1,
+                                          mul_factor, True)
+        redant.add_brick(self.volume_name1,
+                         br_cmd[1:], self.server_list[0],
+                         replica_count=3)
+
+        mountpoint = f"/mnt/{self.volume_name1}"
+        redant.execute_abstract_op_node(f"mkdir -p {mountpoint}",
+                                        self.client_list[0])
+        redant.volume_mount(self.server_list[1], self.volume_name1,
+                            mountpoint, self.client_list[0])
+        # Run IOs
+        redant.logger.info("Starting IO on all mounts...")
+        self.all_mounts_procs = []
+        self.mounts = (redant.es.
+                       get_mnt_pts_dict_in_list(self.volume_name1))
+        counter = 1
+
+        for mount in self.mounts:
+            redant.logger.info(f"Starting IO on {mount['client']}:"
+                               f"{mount['mountpath']}")
+            proc = redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                      counter, 2, 3, 4, 10,
+                                                      mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter = counter + 10
+
+        # Validate IO
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed on some of the clients")
+
+        # Unmount the volume
+        redant.volume_unmount(self.volume_name1,
+                              mountpoint,
+                              self.client_list[0])
+        # get the list of all bricks
+        bricks_list = redant.get_all_bricks(self.volume_name1,
+                                            self.server_list[0])
+        if bricks_list is None:
+            raise Exception("Failed to get the list of all the bricks")
+
+        # Stop the volume
+        redant.volume_stop(self.volume_name1, self.server_list[0])
+
+        # Delete the volume
+        redant.volume_delete(self.volume_name1, self.server_list[0])
+
+        self.volname = "test_create_vol_used_bricks"
+
+        brick_cmd = " ".join(bricks_list[0:6])
+
+        cmd = (f"gluster volume create {self.volname}"
+               f" replica 3 {brick_cmd} --mode=script")
+
+        # creating volume with used bricks should fail
+        ret = redant.execute_abstract_op_node(cmd,
+                                              self.server_list[0],
+                                              False)
+        if ret['error_code'] == 0:
+            raise Exception("Volume creation should fail with"
+                            " used bricks.")
+
+        # Checking failed message of volume creation
+        err = ret['error_msg']
+        msg = ' '.join(['volume create: test_create_vol_used_bricks: failed:',
+                        bricks_list[0].split(':')[1],
+                        'is already part of a volume'])
+        if msg not in err:
+            raise Exception("Incorrect error message for volume creation "
+                            "with used bricks")
