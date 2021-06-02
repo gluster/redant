a905d5980fdb4faf44aa5bca4b305c1c51b0de11
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-02 18:41:51 +0530

    Modified test_gluster_vol_status_xml_dump
    * Made the test case generic
    * Added dist-arb volume in the test case
    
    Fixes: #592
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_gluster_volume_status_xml_dump.py
+++ tests/functional/glusterd/test_gluster_volume_status_xml_dump.py
@@ -1,19 +1,19 @@
 """
-  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License along
-  with this program; if not, write to the Free Software Foundation, Inc.,
-  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 Description:
@@ -25,7 +25,7 @@ from time import sleep
 from tests.nd_parent_test import NdParentTest
 
 
-# nonDisruptive;dist-arb
+# nonDisruptive;
 class TestCase(NdParentTest):
 
     def run_test(self, redant):
@@ -36,14 +36,19 @@ class TestCase(NdParentTest):
         2. Get the status of the volumes with --xml dump
             XML dump should be consistent
         """
-
+        volume_type = 'dist-arb'
+        volume_name = f"{self.test_name}-{volume_type}-1"
+        redant.volume_create(volume_name, self.server_list[0],
+                             self.vol_type_inf[self.conv_dict[volume_type]],
+                             self.server_list, self.brick_roots, True)
+        redant.volume_start(volume_name, self.server_list[0])
         volume_type1 = 'dist'
         volume_name1 = f"{self.test_name}-{volume_type1}-1"
         redant.volume_create(volume_name1, self.server_list[0],
                              self.vol_type_inf[self.conv_dict[volume_type1]],
                              self.server_list, self.brick_roots, True)
         redant.volume_start(volume_name1, self.server_list[0])
-        redant.volume_stop(self.vol_name, self.server_list[0], force=True)
+        redant.volume_stop(volume_name, self.server_list[0], force=True)
         sleep(2)
         out = redant.get_volume_status(node=self.server_list[0])
         if out is None:
@@ -61,3 +66,4 @@ class TestCase(NdParentTest):
 
         redant.volume_stop(volume_name1, self.server_list[0])
         redant.volume_delete(volume_name1, self.server_list[0])
+        redant.volume_delete(volume_name, self.server_list[0])
