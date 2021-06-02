ec7d4ddf8aa387ba85484818066b9c01a5f4aaf4
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-02 20:41:58 +0530

    Completed all the steps
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_add_brick.py
+++ tests/functional/glusterd/test_add_brick.py
@@ -28,84 +28,80 @@ class TestCase(DParentTest):
 
     def run_test(self, redant):
         """
-        1. Setup a volume
-        2. Create a 4*replica_count brick list
-        3. Add a single brick to the volume, 
+        1. Create a 4*replica_count brick list
+        2. Add a single brick to the volume,
            which should fail
-        4. Add a non-existing brick, which should
+        3. Add a non-existing brick, which should
            fail.
-        5. Add a brick from a node which is not a
+        4. Add a brick from a node which is not a
            part of the cluster.
         """
         # form bricks list to test add brick functionality
         rep_count = self.vol_type_inf[self.conv_dict['rep']]
         rep_count = rep_count['replica_count']
-        print(rep_count)
         num_of_bricks = 4 * rep_count
-        print(num_of_bricks)
 
         _, self.bricks_list = redant.form_brick_cmd(self.server_list,
-                                                 self.brick_roots,
-                                                 self.vol_name,
-                                                 num_of_bricks)
+                                                    self.brick_roots,
+                                                    self.vol_name,
+                                                    num_of_bricks)
         self.bricks_list = self.bricks_list.split(' ')
-        print("\n",self.bricks_list)
         if self.bricks_list is None:
             raise Exception("Bricks list is empty")
-        
+
         # Try to add a single brick to volume, which should fail as it is a
         # replicated volume, we should pass multiple of replica count number
         # of bricks
-        try:
-            redant.add_brick(self.vol_name,
-                             self.bricks_list[0],
-                             self.server_list[0])
-        except Exception as error:
-            print("\n1. ",error)
-
+        ret = redant.add_brick(self.vol_name,
+                               self.bricks_list[0],
+                               self.server_list[0],
+                               excep=False)
+        if ret['msg']['opRet'] == 0:
+            raise Exception("Adding a single brick to "
+                            "a replicated volume should fail")
         # add brick replica count number of bricks in which one is a
         # non existing brick (not using the brick used in the earlier test)
-        print(f"Replica count: {rep_count}\n")
         bricks_to_add = self.bricks_list[1:rep_count + 1]
         # make one of the bricks a non-existing one (randomly)
         random_index = random.randint(0, rep_count - 1)
         bricks_to_add[random_index] += "/non_existing_brick"
 
         br_cmd = " ".join(bricks_to_add)
-        print(f"\nBrick cmd:\n{br_cmd}\n")
-        try:
-            redant.add_brick(self.vol_name,
-                            br_cmd, self.server_list[0],
-                            replica_count=rep_count)
-        except Exception as error:
-            print(f"\n2. {error}\n")
+        ret = redant.add_brick(self.vol_name,
+                               br_cmd, self.server_list[0],
+                               replica_count=rep_count,
+                               excep=False)
+        if ret['msg']['opRet'] == 0:
+            raise Exception("Adding a non-existing brick to "
+                            "a volume should fail")
 
-        # # add a brick from a node which is not a part of the cluster
-        # # (not using bricks used in earlier tests)
-        # bricks_to_add = self.bricks_list[rep_count + 1:
-        #                                  (2 * rep_count) + 1]
-        # # change one (random) brick's node name to a non existent node
-        # random_index = random.randint(0, rep_count - 1)
-        # brick_to_change = bricks_to_add[random_index].split(":")
-        # brick_to_change[0] = "abc.def.ghi.jkl"
-        # bricks_to_add[random_index] = ":".join(brick_to_change)
-        # self.assertNotEqual(
-        #     add_brick(self.mnode, self.volname, bricks_to_add, **kwargs)[0], 0,
-        #     "Expected: It should fail to add brick from a node which is not "
-        #     "part of a cluster. Actual: Successfully added bricks from node "
-        #     "which is not a part of cluster to volume")
-        # g.log.info("Failed to add bricks from node which is not a part of "
-        #            "cluster to volume (as expected)")
+        # add a brick from a node which is not a part of the cluster
+        # (not using bricks used in earlier tests)
+        bricks_to_add = self.bricks_list[rep_count + 1:
+                                         (2 * rep_count) + 1]
+        # change one (random) brick's node name to a non existent node
+        random_index = random.randint(0, rep_count - 1)
+        brick_to_change = bricks_to_add[random_index].split(":")
+        brick_to_change[0] = "abc.def.ghi.jkl"
+        bricks_to_add[random_index] = ":".join(brick_to_change)
 
-        # # add correct number of valid bricks, it should succeed
-        # # (not using bricks used in earlier tests)
-        # bricks_to_add = self.bricks_list[(2 * rep_count) + 1:
-        #                                  (3 * rep_count) + 1]
-        # self.assertEqual(
-        #     add_brick(self.mnode, self.volname, bricks_to_add, **kwargs)[0], 0,
-        #     "Failed to add the bricks to the volume")
-        # g.log.info("Successfully added bricks to volume")
+        br_cmd = " ".join(bricks_to_add)
+        ret = redant.add_brick(self.vol_name,
+                               br_cmd, self.server_list[0],
+                               replica_count=rep_count,
+                               excep=False)
+        if ret['msg']['opRet'] == 0:
+            raise Exception("Adding a brick from a node which is not "
+                            "part of the cluster should fail")
 
-        # # Perform rebalance start operation
-        # self.assertEqual(rebalance_start(self.mnode, self.volname)[0], 0,
-        #                  "Rebalance start failed")
+        # add correct number of valid bricks, it should succeed
+        # (not using bricks used in earlier tests)
+        bricks_to_add = self.bricks_list[(2 * rep_count) + 1:
+                                         (3 * rep_count) + 1]
+        br_cmd = " ".join(bricks_to_add)
+        redant.add_brick(self.vol_name,
+                         br_cmd, self.server_list[0],
+                         replica_count=rep_count)
+
+        # Perform rebalance start operation
+        redant.rebalance_start(self.vol_name, self.server_list[0])
