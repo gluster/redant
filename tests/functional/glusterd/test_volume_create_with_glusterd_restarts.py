a23c9e77b9a7e0a69c159df7c948c9d46d5ae3bb
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-03 12:26:20 +0530

    started conversion
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py
+++ tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py
@@ -1,20 +1,20 @@
-#  Copyright (C) 2017-2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2017-2020 Red Hat, Inc. <http://www.redhat.com>
 
-from time import sleep
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from glusto.core import Glusto as g
 from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
 from glustolibs.gluster.exceptions import ExecutionError
@@ -25,7 +25,15 @@ from glustolibs.gluster.lib_utils import form_bricks_list
 
 
 @runs_on([['distributed'], ['glusterfs']])
-class TestVolumeCreateWithGlusterdRestarts(GlusterBaseClass):
+"""
+import traceback
+from time import sleep
+from tests.d_parent_test import DParentTest
+
+# disruptive; dist
+
+
+class TestCase(DParentTest):
 
     def tearDown(self):
 
