4ee8f17a3aad0910299b4ea922394ffa81fe8bd0
schaffung <ssivakum@redhat.com>
2021-06-03 22:00:55 +0530

    [Test] Migration of a glusto TC (#620)
    * Test remove brick after restart glusterd
    
    1. Trusted storage Pool of 4 nodes
    2. Create a distributed-replicated volumes with 4 bricks
    3. Start the volume
    4. Fuse mount the gluster volume on out of trusted nodes
    5. Create some data file
    6. Start remove-brick operation for one replica pair
    7. Restart glusterd on all nodes
    8. Try to commit the remove-brick operation while rebalance
       is in progress, it should fail
    
    Change-Id: I64901078865ef282b86c9b3ff54d065f976b9e84
    Signed-off-by: Mohit Agrawal <moagrawa@redhat.com>
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Fix spelling mistake across the codebase
    
    Change-Id: I46fc2feffe6443af6913785d67bf310838532421
    
    * Validating whether peers are connected or not before volume creation
    
    In jenkins this case is failing with peers are not connected
    while volume creation. Now having a check before creating
    the volume to make sure that peers are in cluster and in
    connected state after peer probe.
    
    Change-Id: I8aa9d2c4d1669475dd8867d42752a31604ff572f
    Signed-off-by: Bala Konda Reddy M <bmekala@redhat.com>
    
    * Add py3 support in 'tests/functional/glusterd'. Part 2
    
    Change-Id: I1395e14d8d0aa0cc6097e51c64262fb481f36f05
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Add steps to stabilize the existing content in glusterd
    
    Added wait_for_io_to_complete function to testcases
    used wait_for_glusterd function
    and wait_for_peer_connect function
    
    Change-Id: I4811848aad8cca4198cc93d8e200dfc47ae7ac9b
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Add steps to stabilise content in glusterd - part2
    
    and used wait for peer to connect and
    wait for glusterd to connect functions in testcases
    added fixes to  check file exists
    increased timeout value for failure cases
    
    Change-Id: I9d5692f635ed324ffe7dac9944ec9b8f3b933fd1
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * removed the rmdir which was merged in baseclass
    
    Change-Id: I04f7b7c894d48d0188379028412d9c6b48eac210
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Add steps to stabilise glusterd
    
    Moved steps from teardown class to teardown and removed
    unwanted teardown class and rectified the testcase
    failing with wait for io to complete by removing the step
    because after validate io the sub process terminates and
    results in failure.
    
    Change-Id: I2eaf05680b817b681aff8b48683fc9dac88896b0
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * [Test] Migration of a glusto TC
    
    test_remove_brick_after_restart_glusterd
    
    Fixes: #619
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    Co-authored-by: Mohit Agrawal <moagrawa@redhat.com>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: Bala Konda Reddy M <bmekala@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/glusterd/test_remove_brick_after_restart_glusterd.py
@@ -0,0 +1,74 @@
+"""
+Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description: TC to check that remove brick commit fails during rebalance.
+"""
+
+from tests.d_parent_test import DParentTest
+
+# disruptive;dist-rep
+
+
+class TestCase(DParentTest):
+
+    def run_test(self, redant):
+        """
+        In this test case:
+        1. Create some data file
+        2. Start remove-brick operation for one replica pair
+        3. Restart glusterd on all nodes
+        4. Try to commit the remove-brick operation while rebalance
+           is in progress, it should fail
+        """
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.all_mounts_procs = []
+        counter = 1
+        for mount in self.mounts:
+            proc = redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                      counter, 2, 5, 5, 10,
+                                                      mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter += 10
+
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO operations failed on some or all of the"
+                            " clients")
+
+        self.bricks_list = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+        if self.bricks_list is None:
+            raise Exception(f"Brick list for {self.vol_name} is None")
+
+        remove_brick_list = self.bricks_list[3:6]
+        redant.remove_brick(self.server_list[0], self.vol_name,
+                            remove_brick_list, 'start', 3)
+        redant.restart_glusterd(self.server_list)
+        if not redant.wait_for_glusterd_to_start(self.server_list):
+            raise Exception(f"Failed to start glusterd on {self.server_list}")
+
+        failure_key = True
+        try:
+            redant.remove_brick(self.server_list[0], self.vol_name,
+                                remove_brick_list, 'commit', 3)
+            failure_key = False
+        except:
+            redant.logger.info("Expected behavior, Commit had to fail.")
+            redant.remove_brick(self.server_list[0], self.vol_name,
+                                remove_brick_list, 'stop', 3)
+
+        if not failure_key:
+            raise Exception("Brick commit should've failed!")
