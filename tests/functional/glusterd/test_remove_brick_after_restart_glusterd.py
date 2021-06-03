f1f3fc55454dbd5d26418dde32b080a960923c3b
srijan-sivakumar <ssivakum@redhat.com>
2021-06-03 21:27:15 +0530

    [Test] Migration of a glusto TC
    test_remove_brick_after_restart_glusterd
    
    Fixes: #619
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- tests/functional/glusterd/test_remove_brick_after_restart_glusterd.py
+++ tests/functional/glusterd/test_remove_brick_after_restart_glusterd.py
@@ -1,189 +1,74 @@
-#  Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2018-2020  Red Hat, Inc. <http://www.redhat.com>
 
-from time import sleep
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.volume_ops import (volume_create, volume_start,
-                                           get_volume_list)
-from glustolibs.gluster.brick_libs import get_all_bricks
-from glustolibs.gluster.volume_libs import (cleanup_volume)
-from glustolibs.gluster.peer_ops import (peer_probe, peer_detach,
-                                         peer_probe_servers,
-                                         nodes_from_pool_list,
-                                         is_peer_connected)
-from glustolibs.gluster.lib_utils import form_bricks_list
-from glustolibs.gluster.brick_ops import remove_brick
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.mount_ops import mount_volume, umount_volume
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.gluster.gluster_init import restart_glusterd
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description: TC to check that remove brick commit fails during rebalance.
+"""
+
+from tests.d_parent_test import DParentTest
+
+# disruptive;dist-rep
 
 
-@runs_on([['distributed-replicated'], ['glusterfs']])
-class TestRemoveBrickAfterRestartGlusterd(GlusterBaseClass):
+class TestCase(DParentTest):
 
-    def setUp(self):
-
-        # Performing peer detach
-        for server in self.servers[1:]:
-            ret, _, _ = peer_detach(self.mnode, server)
-            if ret != 0:
-                raise ExecutionError("Peer detach failed")
-            g.log.info("Peer detach SUCCESSFUL.")
-        self.get_super_method(self, 'setUp')()
-
-    def tearDown(self):
-
-        # Cleanup and umount volume
-        g.log.info("Starting to Unmount Volume %s", self.volname)
-        ret = umount_volume(self.mounts[0].client_system,
-                            self.mounts[0].mountpoint, mtype=self.mount_type)
-        self.assertTrue(ret, ("Failed to Unmount Volume %s" % self.volname))
-
-        # Clean up all volumes and peer probe to form cluster
-        vol_list = get_volume_list(self.mnode)
-        if vol_list is not None:
-            for volume in vol_list:
-                ret = cleanup_volume(self.mnode, volume)
-                if not ret:
-                    raise ExecutionError("Failed to cleanup volume")
-                g.log.info("Volume deleted successfully : %s", volume)
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Peer probe detached servers
-        pool = nodes_from_pool_list(self.mnode)
-        for node in pool:
-            peer_detach(self.mnode, node)
-        ret = peer_probe_servers(self.mnode, self.servers)
-        if not ret:
-            raise ExecutionError("Failed to probe detached "
-                                 "servers %s" % self.servers)
-        g.log.info("Peer probe success for detached "
-                   "servers %s", self.servers)
-        self.get_super_method(self, 'tearDown')()
-
-    def test_remove_brick(self):
+    def run_test(self, redant):
         """
         In this test case:
-        1. Trusted storage Pool of 4 nodes
-        2. Create a distributed-replicated volumes with 4 bricks
-        3. Start the volume
-        4. Fuse mount the gluster volume on out of trusted nodes
-        5. Create some data file
-        6. Start remove-brick operation for one replica pair
-        7. Restart glusterd on all nodes
-        8. Try to commit the remove-brick operation while rebalance
+        1. Create some data file
+        2. Start remove-brick operation for one replica pair
+        3. Restart glusterd on all nodes
+        4. Try to commit the remove-brick operation while rebalance
            is in progress, it should fail
         """
-
-        # pylint: disable=too-many-statements
-        my_servers = self.servers[0:4]
-        my_server_info = {}
-        for server in self.servers[0:4]:
-            my_server_info[server] = self.all_servers_info[server]
-        for index in range(1, 4):
-            ret, _, _ = peer_probe(self.servers[0], self.servers[index])
-            self.assertEqual(ret, 0, ("peer probe from %s to %s is failed",
-                                      self.servers[0], self.servers[index]))
-            g.log.info("peer probe is success from %s to "
-                       "%s", self.servers[0], self.servers[index])
-
-        # Validating whether the peer are connected or not
-        # In jenkins This case is failing saying peers are not in connected
-        # state, that is reason adding a check whether peers are connected
-        # or not
-        count = 0
-        while count < 30:
-            ret = is_peer_connected(self.mnode, my_servers)
-            if ret:
-                g.log.info("Peers are in connected state")
-                break
-            sleep(3)
-            count = count + 1
-        self.assertTrue(ret, "Some peers are not in connected state")
-
-        self.volname = "testvol"
-        bricks_list = form_bricks_list(self.mnode, self.volname, 4,
-                                       my_servers,
-                                       my_server_info)
-        g.log.info("Creating a volume %s ", self.volname)
-        kwargs = {}
-        kwargs['replica_count'] = 2
-        ret = volume_create(self.mnode, self.volname,
-                            bricks_list, force=False, **kwargs)
-        self.assertEqual(ret[0], 0, ("Unable"
-                                     "to create volume %s" % self.volname))
-        g.log.info("Volume created successfully %s", self.volname)
-
-        ret, _, _ = volume_start(self.mnode, self.volname, False)
-        self.assertEqual(ret, 0, ("Failed to start the "
-                                  "volume %s", self.volname))
-        g.log.info("Get all the bricks of the volume")
-        bricks_list = get_all_bricks(self.mnode, self.volname)
-        self.assertIsNotNone(bricks_list, "Failed to get the brick list")
-        g.log.info("Successfully got the list of bricks of volume")
-
-        # Mounting a volume
-        ret, _, _ = mount_volume(self.volname, mtype=self.mount_type,
-                                 mpoint=self.mounts[0].mountpoint,
-                                 mserver=self.mnode,
-                                 mclient=self.mounts[0].client_system)
-        self.assertEqual(ret, 0, ("Volume %s is not mounted") % self.volname)
-        g.log.info("Volume mounted successfully : %s", self.volname)
-
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
         self.all_mounts_procs = []
-        # Creating files
-        command = ("cd %s/ ; "
-                   "for i in `seq 1 10` ; "
-                   "do mkdir l1_dir.$i ; "
-                   "for j in `seq 1 5` ; "
-                   "do mkdir l1_dir.$i/l2_dir.$j ; "
-                   "for k in `seq 1 10` ; "
-                   "do dd if=/dev/urandom of=l1_dir.$i/l2_dir.$j/test.$k "
-                   "bs=128k count=$k ; "
-                   "done ; "
-                   "done ; "
-                   "done ; "
-                   % (self.mounts[0].mountpoint))
+        counter = 1
+        for mount in self.mounts:
+            proc = redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                      counter, 2, 5, 5, 10,
+                                                      mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter += 10
 
-        proc = g.run_async(self.mounts[0].client_system, command,
-                           user=self.mounts[0].user)
-        self.all_mounts_procs.append(proc)
-        self.io_validation_complete = False
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO operations failed on some or all of the"
+                            " clients")
 
-        # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients")
-        self.io_validation_complete = True
+        self.bricks_list = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+        if self.bricks_list is None:
+            raise Exception(f"Brick list for {self.vol_name} is None")
 
-        remove_brick_list = bricks_list[2:4]
-        ret, _, _ = remove_brick(self.mnode, self.volname, remove_brick_list,
-                                 'start')
-        self.assertEqual(ret, 0, "Failed to start remove brick operation")
-        g.log.info("Remove bricks operation started successfully")
-        g.log.info("Restart glusterd on servers %s", self.servers)
-        ret = restart_glusterd(self.servers)
-        self.assertTrue(ret, ("Failed to restart glusterd on servers %s",
-                              self.servers))
-        g.log.info("Successfully restarted glusterd on servers %s",
-                   self.servers)
+        remove_brick_list = self.bricks_list[3:6]
+        redant.remove_brick(self.server_list[0], self.vol_name,
+                            remove_brick_list, 'start', 3)
+        redant.restart_glusterd(self.server_list)
+        if not redant.wait_for_glusterd_to_start(self.server_list):
+            raise Exception(f"Failed to start glusterd on {self.server_list}")
 
-        ret, _, _ = remove_brick(self.mnode, self.volname, remove_brick_list,
-                                 'commit')
-        self.assertNotEqual(ret, 0, "Remove brick commit ops should be fail")
-        g.log.info("Remove bricks commit operation failure is expected")
+        failure_key = True
+        try:
+            redant.remove_brick(self.server_list[0], self.vol_name,
+                                remove_brick_list, 'commit', 3)
+            failure_key = False
+        except:
+            redant.logger.info("Expected behavior, Commit had to fail.")
+            redant.remove_brick(self.server_list[0], self.vol_name,
+                                remove_brick_list, 'stop', 3)
+
+        if not failure_key:
+            raise Exception("Brick commit should've failed!")
