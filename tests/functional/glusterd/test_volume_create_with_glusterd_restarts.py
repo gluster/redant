9d4bd0e0d1a871dcd2257526bc694c035ca803cf
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-03 12:46:22 +0530

    shifted comments
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py
+++ tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py
@@ -26,36 +26,36 @@ from glustolibs.gluster.lib_utils import form_bricks_list
 
 @runs_on([['distributed'], ['glusterfs']])
 """
+# disruptive;dist
+
 import traceback
 from time import sleep
 from tests.d_parent_test import DParentTest
 
-# disruptive; dist
-
 
 class TestCase(DParentTest):
 
-    def tearDown(self):
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        self.setup_done = True
 
-        # wait till peers are in connected state
-        count = 0
-        while count < 60:
-            ret = is_peer_connected(self.mnode, self.servers)
-            if ret:
-                break
-            sleep(3)
-            count += 1
+    def terminate(self):
 
-        # clean up volumes
-        ret = cleanup_volume(self.mnode, self.volname)
-        if not ret:
-            raise ExecutionError("Unable to delete volume % s" % self.volname)
-        g.log.info("Volume deleted successfully : %s", self.volname)
+        try:
+            # wait till peers are in connected state
+            if not (self.redant.
+                    wait_for_peers_to_connect(self.server_list,
+                                              self.server_list[0]):
+                raise Exception("Failed to connect peers")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        self.get_super_method(self, 'tearDown')()
-
-    def test_volume_create_with_glusterd_restarts(self):
-        # pylint: disable=too-many-statements
+    def run_test(self, redant):
         """
         Test case:
         1) Create a cluster.
@@ -65,75 +65,80 @@ class TestCase(DParentTest):
         5) While the volume start is happening restart N4.
         6) Check if glusterd has crashed on any node.
         """
+        if len(self.server_list) < 4:
+            # raise Exception("Minimum 4 nodes required for this TC to run")
+            print("Minimum 4 nodes required for this TC to run")
 
         # Fetching all the parameters for volume_create
-        list_of_three_servers = []
-        server_info_for_three_nodes = {}
+        list_of_three_servers = self.server_list[0:3]
+        print(list_of_three_servers)
+        # for server in self.servers[0:3]:
+        #     list_of_three_servers.append(server)
+        #     server_info_for_three_nodes[server] = self.all_servers_info[server]
 
-        for server in self.servers[0:3]:
-            list_of_three_servers.append(server)
-            server_info_for_three_nodes[server] = self.all_servers_info[server]
-
-        bricks_list = form_bricks_list(self.mnode, self.volname,
-                                       3, list_of_three_servers,
-                                       server_info_for_three_nodes)
         # Restarting glusterd in a loop
-        restart_cmd = ("for i in `seq 1 5`; do "
-                       "service glusterd restart; "
-                       "systemctl reset-failed glusterd; "
-                       "sleep 3; "
-                       "done")
-        proc1 = g.run_async(self.servers[3], restart_cmd)
+        # restart_cmd = ("for i in `seq 1 5`; do "
+        #                "service glusterd restart; "
+        #                "systemctl reset-failed glusterd; "
+        #                "sleep 3; "
+        #                "done")
+        # proc1 = redant.execute_command_async(restart_cmd,
+        #                                      self.server_list[3])
 
-        # After running restart in g.async adding 10 sec sleep
-        sleep(10)
+        # # After running restart in g.async adding 10 sec sleep
+        # sleep(10)
 
         # Creating volumes using 3 servers
-        ret, _, _ = volume_create(self.mnode, self.volname,
-                                  bricks_list)
-        self.assertEqual(ret, 0, "Volume creation failed")
-        g.log.info("Volume %s created successfully", self.volname)
+        self.volume_type = "dist"
+        self.vol_name = (f"{self.test_name}-{self.volume_type}")
+        conf_hash = self.vol_type_inf[self.conv_dict[self.volume_type]]
+        redant.volume_create(self.vol_name, self.server_list[0], conf_hash,
+                             list_of_three_servers, self.brick_roots)
+        # ret, _, _ = volume_create(self.mnode, self.volname,
+        #                           bricks_list)
+        # self.assertEqual(ret, 0, "Volume creation failed")
+        # g.log.info("Volume %s created successfully", self.volname)
 
-        ret, _, _ = proc1.async_communicate()
-        self.assertEqual(ret, 0, "Glusterd restart not working.")
+        # ret, _, _ = proc1.async_communicate()
+        # self.assertEqual(ret, 0, "Glusterd restart not working.")
 
-        # Checking if peers are connected or not.
-        count = 0
-        while count < 60:
-            ret = is_peer_connected(self.mnode, self.servers)
-            if ret:
-                break
-            sleep(3)
-            count += 1
-        self.assertTrue(ret, "Peers are not in connected state.")
-        g.log.info("Peers are in connected state.")
+        # # Checking if peers are connected or not.
+        # count = 0
+        # while count < 60:
+        #     ret = is_peer_connected(self.mnode, self.servers)
+        #     if ret:
+        #         break
+        #     sleep(3)
+        #     count += 1
+        # self.assertTrue(ret, "Peers are not in connected state.")
+        # g.log.info("Peers are in connected state.")
 
-        # Restarting glusterd in a loop
-        restart_cmd = ("for i in `seq 1 5`; do "
-                       "service glusterd restart; "
-                       "systemctl reset-failed glusted; "
-                       "sleep 3; "
-                       "done")
-        proc1 = g.run_async(self.servers[3], restart_cmd)
+        # # Restarting glusterd in a loop
+        # restart_cmd = ("for i in `seq 1 5`; do "
+        #                "service glusterd restart; "
+        #                "systemctl reset-failed glusted; "
+        #                "sleep 3; "
+        #                "done")
+        # proc1 = g.run_async(self.servers[3], restart_cmd)
 
-        # After running restart in g.async adding 10 sec sleep
-        sleep(10)
+        # # After running restart in g.async adding 10 sec sleep
+        # sleep(10)
 
-        # Start the volume created.
-        ret, _, _ = volume_start(self.mnode, self.volname)
-        self.assertEqual(ret, 0, "Volume start failed")
-        g.log.info("Volume %s started successfully", self.volname)
+        # # Start the volume created.
+        # ret, _, _ = volume_start(self.mnode, self.volname)
+        # self.assertEqual(ret, 0, "Volume start failed")
+        # g.log.info("Volume %s started successfully", self.volname)
 
-        ret, _, _ = proc1.async_communicate()
-        self.assertEqual(ret, 0, "Glusterd restart not working.")
+        # ret, _, _ = proc1.async_communicate()
+        # self.assertEqual(ret, 0, "Glusterd restart not working.")
 
-        # Checking if peers are connected or not.
-        count = 0
-        while count < 60:
-            ret = is_peer_connected(self.mnode, self.servers)
-            if ret:
-                break
-            sleep(3)
-            count += 1
-        self.assertTrue(ret, "Peers are not in connected state.")
-        g.log.info("Peers are in connected state.")
+        # # Checking if peers are connected or not.
+        # count = 0
+        # while count < 60:
+        #     ret = is_peer_connected(self.mnode, self.servers)
+        #     if ret:
+        #         break
+        #     sleep(3)
+        #     count += 1
+        # self.assertTrue(ret, "Peers are not in connected state.")
+        # g.log.info("Peers are in connected state.")
