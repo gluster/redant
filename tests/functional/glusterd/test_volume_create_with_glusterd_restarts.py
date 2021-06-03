18b10b9d062450ecf17de4768198410db45789ef
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-06-03 16:35:06 +0530

    Migrate test_volume_create_with_glusterd_restarts (#603)
    * Test to check volume create with glusterd restart on one node
    
    Change-Id: Ica0771bdee1e96e9d6bb5157fb6c2125a4b419f1
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Changing systemctl to service to fix jira issue RHGSQE-197
    
    Bug https://bugzilla.redhat.com/show_bug.cgi?id=1690254 has to be fixed
    before merging this patch.
    
    Change-Id: I90e669269fafa9d0a064a64883c3e4b88080d25f
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Adding count++ to prevent test from running in infinite loop
    
    In a few testcases in glusterd count++ is missing due to which the
    testcase in an infinite loop. Fixing that and sending patch.
    
    Change-Id: I56a355f6ea3ae79231e09d7aee80031da3ebec52
    Signed-off-by: yinkui <13965432176@163.com>
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add py3 support in 'tests/functional/glusterd'. Part 2
    
    Change-Id: I1395e14d8d0aa0cc6097e51c64262fb481f36f05
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Add sleep after glusterd restart
    
    Add sleep after glusterd restart is run async
    in servers by avoiding another transaction in
    progress failure in testcase
    
    Change-Id: I514c24813dc7c102b807a582ae2b0d19069e0d34
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Add reset-failed cmd
    
    Problem:
    The testcase test_volume_create_with_glusterd_restarts
    consist of a asynchronous loop of glusterd restarts
    which fails in the lastest runs due to patch [1]
    and [2] added to glusterfs which limits the
    glusterd restarts to 6.
    
    Fix:
    Add `systemctl reset-failed glusterd` to the
    asynchronous loop.
    
    Links:
    [1] https://review.gluster.org/#/c/glusterfs/+/23751/
    [2] https://review.gluster.org/#/c/glusterfs/+/23970/
    
    Change-Id: Idd52bfeb99c0c43afa45403d71852f5f7b4514fa
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Fixing typo glusterd
    
    Change-Id: I080328dfbcde5652f9ab697f8751b87bf96e8245
    Signed-off-by: “Milind <“mwaykole@redhat.com”>
    
    * started conversion
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * shifted comments
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * lint issues
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * Migrate test_volume_create_with_glusterd_restarts
    
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py)
    
    Updates: #292
    
    Fixes: #447
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * finished the test case
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: yinkui <13965432176@163.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>
    Co-authored-by: “Milind <“mwaykole@redhat.com”>
    Co-authored-by: schaffung <ssivakum@redhat.com>

---
+++ tests/functional/glusterd/test_volume_create_with_glusterd_restarts.py
@@ -0,0 +1,126 @@
+'''
+Copyright (C) 2017-2020 Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+This test deals with volume creation with glusterd restarts.
+'''
+
+# disruptive;dist
+
+import traceback
+from time import sleep
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        self.setup_done = True
+        ret = self.redant.peer_detach_servers(self.server_list[3:],
+                                              self.server_list[0],
+                                              True)
+        if not ret:
+            raise Exception("Cluster does not have 3 servers exactly")
+
+    def terminate(self):
+        """
+        To peer probe all the servers
+        """
+        try:
+            # wait till peers are in connected state
+            if not (self.redant.
+                    wait_for_peers_to_connect(self.server_list,
+                                              self.server_list[0],
+                                              60)):
+                raise Exception("Failed to connect peers")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def _glusterd_restart_async(self):
+        # Restarting glusterd in a loop
+        restart_cmd = ("for i in `seq 1 5`; do "
+                       "service glusterd restart; "
+                       "systemctl reset-failed glusterd; "
+                       "sleep 3; "
+                       "done")
+        proc1 = self.redant.execute_command_async(restart_cmd,
+                                                  self.server_list[3])
+
+        # After running restart in async adding 10 sec sleep
+        sleep(10)
+        return proc1
+
+    def _check_process_ended(self, proc1):
+        # check if the process ended successfully
+        ret = self.redant.wait_till_async_command_ends(proc1)
+        if ret['error_code'] != 0:
+            raise Exception("Glusterd restart not working")
+
+    def run_test(self, redant):
+        """
+        Test case:
+        1) Create a cluster.
+        2) Create volume using the first three nodes say N1, N2 and N3.
+        3) While the create is happening restart the fourth node N4.
+        4) Check if glusterd has crashed on any node.
+        5) While the volume start is happening restart N4.
+        6) Check if glusterd has crashed on any node.
+        """
+        if len(self.server_list) < 4:
+            raise Exception("Minimum 4 nodes required for this TC to run")
+
+        # Fetching all the parameters for volume_create
+        list_of_three_servers = self.server_list[0:3]
+
+        # Restarting glusterd in a loop
+        proc1 = self._glusterd_restart_async()
+
+        # Creating volumes using 3 servers
+        self.volume_type = "dist"
+        self.vol_name = (f"{self.test_name}-{self.volume_type}")
+        conf_hash = self.vol_type_inf[self.conv_dict[self.volume_type]]
+        redant.volume_create(self.vol_name, self.server_list[0], conf_hash,
+                             list_of_three_servers, self.brick_roots)
+        # check if process ended
+        self._check_process_ended(proc1)
+
+        # Checking if peers are connected or not.
+        if not redant.wait_for_peers_to_connect(self.server_list,
+                                                self.server_list[0],
+                                                60):
+            raise Exception("Peers are not connected")
+
+        # Restarting glusterd in a loop
+        proc1 = self._glusterd_restart_async()
+
+        # Start the volume created.
+        redant.volume_start(self.vol_name, self.server_list[0])
+
+        # check if process ended
+        self._check_process_ended(proc1)
+
+        # Checking if peers are connected or not.
+        if not redant.wait_for_peers_to_connect(self.server_list,
+                                                self.server_list[0],
+                                                60):
+            raise Exception("Peers are not connected")
