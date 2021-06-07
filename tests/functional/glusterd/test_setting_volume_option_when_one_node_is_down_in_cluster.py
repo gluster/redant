6dc54a9a41565ac7578ea07a3cb8867e088e17e6
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-07 12:51:29 +0530

    added description
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_setting_volume_option_when_one_node_is_down_in_cluster.py
+++ tests/functional/glusterd/test_setting_volume_option_when_one_node_is_down_in_cluster.py
@@ -14,6 +14,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+This test case deals with setting volume option
+when one node is down in cluster.
 """
 # disruptive;dist-rep
 
