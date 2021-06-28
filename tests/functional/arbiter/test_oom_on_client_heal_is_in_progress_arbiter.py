48df080aae4e12b1c6aca4cdf1faf18cd7ed932e
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-28 12:20:40 +0530

    Migrated test_oom_on_client_heal_is_in_progress_arbiter
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/arbiter/test_oom_on_client_heal_is_in_progress_arbiter.py)
    
    Updates: #292
    
    Fixes: #707
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/arbiter/test_oom_on_client_heal_is_in_progress_arbiter.py
+++ tests/functional/arbiter/test_oom_on_client_heal_is_in_progress_arbiter.py
@@ -1,18 +1,22 @@
-#  Copyright (C) 2016-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2016-2020  Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    This test case deals with Arbiter self-heal tests.
 
 from glusto.core import Glusto as g
 
@@ -31,77 +35,18 @@ from glustolibs.misc.misc_libs import upload_scripts
 
 @runs_on([['arbiter'],
           ['glusterfs', 'nfs', 'cifs']])
-class ArbiterSelfHealTests(GlusterBaseClass):
-    """
-            Arbiter Self-Heal tests
-    """
-    @classmethod
-    def setUpClass(cls):
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
 
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on mounts",
-                   cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
+"""
 
-    def setUp(self):
-        """
-        setUp method for every test
-        """
-        # calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
+# disruptive;arb
+# TODO: add nfs and cifs
 
-        self.all_mounts_procs = []
-        self.io_validation_complete = False
+from tests.d_parent_test import DParentTest
 
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup Volume %s", self.volname)
-        ret = self.setup_volume_and_mount_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
 
-    def tearDown(self):
-        """
-        If test method failed before validating IO, tearDown waits for the
-        IO's to complete and checks for the IO exit status
+class TestCase(DParentTest):
 
-        Cleanup and umount volume
-        """
-        if not self.io_validation_complete:
-            g.log.info("Wait for IO to complete as IO validation did not "
-                       "succeed in test method")
-            ret = wait_for_io_to_complete(self.all_mounts_procs, self.mounts)
-            if not ret:
-                raise ExecutionError("IO failed on some of the clients")
-            g.log.info("IO is successful on all mounts")
-
-            # List all files and dirs created
-            g.log.info("List all files and directories:")
-            ret = list_all_files_and_dirs_mounts(self.mounts)
-            if not ret:
-                raise ExecutionError("Failed to list all files and dirs")
-            g.log.info("Listing all files and directories is successful")
-
-        # Cleanup and umount volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_oom_on_client_heal_in_progress(self):
+    def run_test(self, redant):
         """
         - Create a 1x(2+1) arbiter replicate volume
         - Create IO
@@ -113,88 +58,91 @@ class ArbiterSelfHealTests(GlusterBaseClass):
         """
 
         # Creating IO on client side
-        for mount_obj in self.mounts:
-            g.log.info("Generating data for %s:%s",
-                       mount_obj.client_system, mount_obj.mountpoint)
-            # Create files
-            g.log.info('Creating files...')
-            command = ("/usr/bin/env python %s create_files "
-                       "-f 1000 "
-                       "--fixed-file-size 10k "
-                       "%s" % (
-                           self.script_upload_path,
-                           mount_obj.mountpoint))
+        # for mount_obj in self.mounts:
+        #     g.log.info("Generating data for %s:%s",
+        #                mount_obj.client_system, mount_obj.mountpoint)
+        #     # Create files
+        #     g.log.info('Creating files...')
+        #     command = ("/usr/bin/env python %s create_files "
+        #                "-f 1000 "
+        #                "--fixed-file-size 10k "
+        #                "%s" % (
+        #                    self.script_upload_path,
+        #                    mount_obj.mountpoint))
 
-            proc = g.run_async(mount_obj.client_system, command,
-                               user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-        self.io_validation_complete = False
+        #     proc = g.run_async(mount_obj.client_system, command,
+        #                        user=mount_obj.user)
+        #     self.all_mounts_procs.append(proc)
+        # self.io_validation_complete = False
+        self.mnt_list = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.list_of_procs = []
+        for mount_obj in self.mnt_list:
+            redant.logger.info(f"Starting IO on {mount_obj['client']}:"
+                               f"{mount_obj['mountpath']}")
+            proc = redant.create_files(num_files=1000,
+                                       fix_fil_size="10k",
+                                       path=mount_obj['mountpath'],
+                                       node=mount_obj['client'])
+            self.list_of_procs.append(proc)
+
+        # Validate IO
+        ret = redant.validate_io_procs(self.list_of_procs, self.mnt_list)
+        if not ret:
+            raise Exception("IO validation failed")
 
         # get the bricks for the volume
-        g.log.info("Fetching bricks for the volume: %s", self.volname)
-        bricks_list = get_all_bricks(self.mnode, self.volname)
-        g.log.info("Brick list: %s", bricks_list)
+        bricks_list = redant.get_all_bricks(self.vol_name,
+                                            self.server_list[0])
 
         # Bring brick 1 offline
         bricks_to_bring_offline = [bricks_list[0]]
-        g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
-        ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s offline' %
-                        bricks_to_bring_offline)
+        redant.bring_bricks_offline(self.vol_name,
+                                    bricks_to_bring_offline)
 
-        ret = are_bricks_offline(self.mnode, self.volname,
-                                 bricks_to_bring_offline)
-        self.assertTrue(ret, 'Bricks %s are not offline'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s offline is successful',
-                   bricks_to_bring_offline)
+        if not redant.are_bricks_offline(self.vol_name,
+                                         bricks_to_bring_offline,
+                                         self.server_list[0]):
+            print(f"Bricks {bricks_to_bring_offline} are not offline")
 
-        # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
-        self.io_validation_complete = True
+        # # Bring 1-st brick online
+        # g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
+        # ret = bring_bricks_online(self.mnode, self.volname,
+        #                           bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Failed to bring bricks %s online'
+        #                 % bricks_to_bring_offline)
+        # g.log.info('Bringing bricks %s online is successful',
+        #            bricks_to_bring_offline)
 
-        # Bring 1-st brick online
-        g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        ret = bring_bricks_online(self.mnode, self.volname,
-                                  bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s online'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s online is successful',
-                   bricks_to_bring_offline)
+        # # Bring brick 3 offline
+        # bricks_to_bring_offline = [bricks_list[-1]]
+        # g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
+        # ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Failed to bring bricks %s offline' %
+        #                 bricks_to_bring_offline)
 
-        # Bring brick 3 offline
-        bricks_to_bring_offline = [bricks_list[-1]]
-        g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
-        ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s offline' %
-                        bricks_to_bring_offline)
+        # ret = are_bricks_offline(self.mnode, self.volname,
+        #                          bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Bricks %s are not offline'
+        #                 % bricks_to_bring_offline)
+        # g.log.info('Bringing bricks %s offline is successful',
+        #            bricks_to_bring_offline)
 
-        ret = are_bricks_offline(self.mnode, self.volname,
-                                 bricks_to_bring_offline)
-        self.assertTrue(ret, 'Bricks %s are not offline'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s offline is successful',
-                   bricks_to_bring_offline)
+        # # Bring brick 3 online
+        # g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
+        # ret = bring_bricks_online(self.mnode, self.volname,
+        #                           bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Failed to bring bricks %s online'
+        #                 % bricks_to_bring_offline)
+        # g.log.info('Bringing bricks %s online is successful',
+        #            bricks_to_bring_offline)
 
-        # Bring brick 3 online
-        g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        ret = bring_bricks_online(self.mnode, self.volname,
-                                  bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s online'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s online is successful',
-                   bricks_to_bring_offline)
-
-        # Get file list from mountpoint
-        g.log.info('Getting file list from mountpoints...')
-        for mount_obj in self.mounts:
-            g.log.info("Getting file list for %s:%s",
-                       mount_obj.client_system, mount_obj.mountpoint)
-            g.log.info('Getting file list...')
-            file_list = list_files(mount_obj.client_system,
-                                   mount_obj.mountpoint)
-            self.assertIsNotNone(file_list)
-        g.log.info('Getting file list from mountpoints finished successfully')
+        # # Get file list from mountpoint
+        # g.log.info('Getting file list from mountpoints...')
+        # for mount_obj in self.mounts:
+        #     g.log.info("Getting file list for %s:%s",
+        #                mount_obj.client_system, mount_obj.mountpoint)
+        #     g.log.info('Getting file list...')
+        #     file_list = list_files(mount_obj.client_system,
+        #                            mount_obj.mountpoint)
+        #     self.assertIsNotNone(file_list)
+        # g.log.info('Getting file list from mountpoints finished successfully')
