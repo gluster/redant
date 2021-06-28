760ba1229af8ccfcae9bcf3bc07f8d34c524a987
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-06-28 22:17:40 +0530

    Migrated test_split_brain (#714)
    * Creates a split brain and verifies IO failure
    
    Steps:
    1. Create a volume and mount it
    2. Disable heal and cluster-quorum-count
    3. Bring down one data and arbiter brick from one
       subvol
    4. Write IO and validate it
    5. Bring up bricks
    6. Bring down another data brick and arbiter brick
       from the same subvol
    7. Write IO and validate it
    8. Bring up bricks
    9. Check if split-brain is created
    10. Write IO -> should fail
    11. Enable heal and cluster-quorum-count
    12. Write IO -> should fail
    
    Change-Id: I229b58c1bcd70dcd87d35dc410e12f51b032b9c4
    Signed-off-by: ubansal <ubansal@redhat.com>
    
    * Remove trigger_heal and change to tearDown in arbiter cases
    
    Problem:
    -------
    test_metadata_self_heal.py - trigger_heal() function call isn't needed
    as heal is automatically triggered when shd is enabled back.
    test_split_brain.py - Test case fails in tearDownClass() as
    doCleanups() cleans up the setup before invoking tearDownClass().
    
    Solution:
    ---------
    test_metadata_self_heal.py - Removing trigger_heal() function call.
    test_split_brain.py - Change tearDownClass() to tearDown().
    
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated test_split_brain
    
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/arbiter/test_split_brain.py)
    
    Updates: #292
    
    Fixes: #712
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added io operation
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * refactored the code
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added terminate
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * removed terminate
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: ubansal <ubansal@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/arbiter/test_split_brain.py
@@ -0,0 +1,117 @@
+"""
+Copyright (C) 2020-2021 Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    This test case deals with testing split-brain issue.
+
+"""
+
+# disruptive;arb,dist-arb
+
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    def _perform_io(self, passed: bool):
+        # Write IO's
+        self.proc = (self.redant.
+                     create_files(num_files=1,
+                                  fix_fil_size="1k",
+                                  path=self.mnt_list[0]['mountpath'],
+                                  node=self.mnt_list[0]['client'],
+                                  base_file_name="test_file"))
+        # Validate IO
+        ret = self.redant.validate_io_procs(self.proc, self.mnt_list[0])
+        if not passed:
+            if ret:
+                raise Exception("IO validation passed successfully "
+                                "in split-brain")
+        else:
+            if not ret:
+                raise Exception("IO validation failed")
+
+    def run_test(self, redant):
+
+        """
+        Description: Create split-brain on files and check if IO's fail
+        - Disable self-heal and cluster-quorum-type
+        - Get the bricks from the volume
+        - Write IO and validate IO
+        - Bring 1st set of brick offline(1 Data brick and arbiter brick)
+        - Write IO and validate IO
+        - Bring 2nd set of bricks offline(1 Data brick and arbiter brick)
+        - Write IO and validate IO
+        - Check volume is in split-brain
+        - Write IO and validate IO - should fail
+        - Enable self-heal and cluster-quorum-type
+        - Write IO and validate IO - should fail
+        """
+        # Disable self-heal and cluster-quorum-type
+        options = {"self-heal-daemon": "off",
+                   "cluster.quorum-type": "none"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
+
+        # Get the bricks from the volume
+        sub_vols = redant.get_subvols(self.vol_name,
+                                      self.server_list[0])
+        self.bricks_to_bring_offline = list(sub_vols[0])
+        # Write IO's
+        self.mnt_list = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self._perform_io(True)
+
+        # Bring 1st set of brick offline(1 Data brick and arbiter brick)
+        for bricks in ((0, -1), (1, -1)):
+            down_bricks = []
+            for brick in bricks:
+                down_bricks.append(self.bricks_to_bring_offline[brick])
+
+            # bring bricks offline
+            redant.bring_bricks_offline(self.vol_name, down_bricks)
+            if not self.redant.are_bricks_offline(self.vol_name,
+                                                  down_bricks,
+                                                  self.server_list[0]):
+                raise Exception(f"Bricks {down_bricks} "
+                                f"are not offline")
+            self._perform_io(True)
+
+            # Bring bricks online
+            self.redant.bring_bricks_online(self.vol_name,
+                                            self.server_list,
+                                            down_bricks)
+
+            if not self.redant.are_bricks_online(self.vol_name,
+                                                 down_bricks,
+                                                 self.server_list[0]):
+                raise Exception(f"Bricks {down_bricks} "
+                                f"are not online")
+
+        # Check volume is in split-brain
+        if not redant.is_volume_in_split_brain(self.server_list[0],
+                                               self.vol_name):
+            raise Exception("Volume is not in split-brain state")
+
+        # Write IO's
+        self._perform_io(False)
+        # Enable self-heal and cluster-quorum-type
+        options = {"self-heal-daemon": "on",
+                   "cluster.quorum-type": "auto"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
+        # Write IO's
+        self._perform_io(False)
