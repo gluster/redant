c8f2ba3723d97adb2d63d54c2675150a5aed1ef8
Ayush Ujjwal <aujjwal@redhat.com>
2021-06-28 18:32:17 +0530

    added the rest of the operations in the test case
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/arbiter/test_resolving_meta_data_split_brain_extended_attributes.py
+++ tests/functional/arbiter/test_resolving_meta_data_split_brain_extended_attributes.py
@@ -29,6 +29,62 @@ from tests.d_parent_test import DParentTest
 
 class TestCase(DParentTest):
 
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.list_of_procs,
+                                                      self.mnt_list)
+            if not ret:
+                raise Exception("IO failed on some of the clients")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def _brick_and_io(self, bricks_to_bring_offline: list,
+                      permission: str):
+        """
+        this functions brings brick offline
+        performs permission change IO
+        brings the brick back to online
+        """
+        # bring brick offline
+        self.redant.bring_bricks_offline(self.vol_name,
+                                         bricks_to_bring_offline)
+
+        if not self.redant.are_bricks_offline(self.vol_name,
+                                              bricks_to_bring_offline,
+                                              self.server_list[0]):
+            raise Exception(f"Bricks {bricks_to_bring_offline} "
+                            f"are not offline")
+        # Modify the data
+        self.list_of_procs = []
+        for mount_obj in self.mnt_list:
+            # Modify the permissions
+            cmd = (f"cd {mount_obj['mountpath']} ; "
+                   f"chmod {permission}  test_file0.txt")
+
+            proc = self.redant.execute_command_async(cmd,
+                                                     mount_obj['client'])
+            self.list_of_procs.append(proc)
+
+        # Validate IO
+        ret = self.redant.validate_io_procs(self.list_of_procs,
+                                            self.mnt_list)
+        if not ret:
+            raise Exception("IO validation failed")
+
+        # Bring the brick online
+        self.redant.bring_bricks_online(self.vol_name,
+                                        self.server_list,
+                                        bricks_to_bring_offline)
+
+        if not self.redant.are_bricks_online(self.vol_name,
+                                             bricks_to_bring_offline,
+                                             self.server_list[0]):
+            raise Exception(f"Bricks {bricks_to_bring_offline} "
+                            f"are not online")
+
     def run_test(self, redant):
         """
         - Create a file test_file.txt
@@ -72,105 +128,28 @@ class TestCase(DParentTest):
 
         # Bring arbiter brick offline
         bricks_to_bring_offline = [brick_list_with_file[-1]]
-        redant.bring_bricks_offline(self.vol_name, bricks_to_bring_offline)
+        self._brick_and_io(bricks_to_bring_offline, "600")
 
-        if not redant.are_bricks_offline(self.vol_name,
-                                         bricks_to_bring_offline,
-                                         self.server_list[0]):
-            print(f"Bricks {bricks_to_bring_offline} are not offline")
-        # # Modify the data
-        # self.all_mounts_procs = []
-        # for mount_obj in self.mounts:
-        #     g.log.info("Modifying data for %s:%s",
-        #                mount_obj.client_system, mount_obj.mountpoint)
-        #     # Modify the permissions
-        #     g.log.info('Modifying the permissions of the file...')
-        #     command = ("cd %s ; "
-        #                "chmod 600 %s"
-        #                % (mount_obj.mountpoint, file_to_create))
+        # Bring 1-st data brick offline
+        bricks_to_bring_offline = [brick_list_with_file[0]]
+        self._brick_and_io(bricks_to_bring_offline, "644")
 
-        #     proc = g.run_async(mount_obj.client_system, command,
-        #                        user=mount_obj.user)
-        #     self.all_mounts_procs.append(proc)
-        # self.io_validation_complete = False
+        # Start healing
+        if not redant.trigger_heal(self.vol_name,
+                                   self.server_list[0]):
+            raise Exception("Heal did not trigger")
 
-        # # Validate IO
-        # self.assertTrue(
-        #     validate_io_procs(self.all_mounts_procs, self.mounts),
-        #     "IO failed on some of the clients"
-        # )
-        # self.io_validation_complete = True
+        # monitor heal completion
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name):
+            raise Exception("Heal is not yet completed")
 
-        # # Bring arbiter brick online
-        # g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        # ret = bring_bricks_online(self.mnode, self.volname,
-        #                           bricks_to_bring_offline)
-        # self.assertTrue(ret, 'Failed to bring bricks %s online' %
-        #                 bricks_to_bring_offline)
-        # g.log.info('Bringing bricks %s online is successful',
-        #            bricks_to_bring_offline)
+        # is heal complete testing
+        if not redant.is_heal_complete(self.server_list[0],
+                                       self.vol_name):
+            raise Exception("Heal not yet completed")
 
-        # # Bring 1-st data brick offline
-        # bricks_to_bring_offline = [brick_list_with_file[0]]
-        # g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
-        # ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        # self.assertTrue(ret, 'Failed to bring bricks %s offline' %
-        #                 bricks_to_bring_offline)
-
-        # ret = are_bricks_offline(self.mnode, self.volname,
-        #                          bricks_to_bring_offline)
-        # self.assertTrue(ret, 'Bricks %s are not offline'
-        #                 % bricks_to_bring_offline)
-        # g.log.info('Bringing bricks %s offline is successful',
-        #            bricks_to_bring_offline)
-
-        # # Modify the data
-        # self.all_mounts_procs = []
-        # for mount_obj in self.mounts:
-        #     g.log.info("Modifying data for %s:%s",
-        #                mount_obj.client_system, mount_obj.mountpoint)
-        #     # Modify the permissions
-        #     g.log.info('Modifying the permissions of the file...')
-        #     command = ("cd %s ; "
-        #                "chmod 644 %s"
-        #                % (mount_obj.mountpoint, file_to_create))
-
-        #     proc = g.run_async(mount_obj.client_system, command,
-        #                        user=mount_obj.user)
-        #     self.all_mounts_procs.append(proc)
-        # self.io_validation_complete = False
-
-        # # Validate IO
-        # self.assertTrue(
-        #     validate_io_procs(self.all_mounts_procs, self.mounts),
-        #     "IO failed on some of the clients"
-        # )
-        # self.io_validation_complete = True
-
-        # # Bring 1-st data brick online
-        # g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        # ret = bring_bricks_online(self.mnode, self.volname,
-        #                           bricks_to_bring_offline)
-        # self.assertTrue(ret, 'Failed to bring bricks %s online' %
-        #                 bricks_to_bring_offline)
-        # g.log.info('Bringing bricks %s online is successful',
-        #            bricks_to_bring_offline)
-
-        # # Start healing
-        # ret = trigger_heal(self.mnode, self.volname)
-        # self.assertTrue(ret, 'Heal is not started')
-        # g.log.info('Healing is started')
-
-        # # Monitor heal completion
-        # ret = monitor_heal_completion(self.mnode, self.volname)
-        # self.assertTrue(ret, 'Heal has not yet completed')
-
-        # # Check if heal is completed
-        # ret = is_heal_complete(self.mnode, self.volname)
-        # self.assertTrue(ret, 'Heal is not complete')
-        # g.log.info('Heal is completed successfully')
-
-        # # Check for split-brain
-        # ret = is_volume_in_split_brain(self.mnode, self.volname)
-        # self.assertFalse(ret, 'Volume is in split-brain state')
-        # g.log.info('Volume is not in split-brain state')
+        # Check for split-brain
+        if redant.is_volume_in_split_brain(self.server_list[0],
+                                           self.vol_name):
+            raise Exception("Volume is in split-brain state")
