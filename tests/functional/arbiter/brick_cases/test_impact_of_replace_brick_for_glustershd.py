e046af1f8a3730d5bfc7620d516afc577ba4c870
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-07-02 16:21:53 +0530

    Migrated test_impact_of_replace_brick_for_glustershd.py (#740)
    * Fixing the test-case to delete brick directories of replaced bricks, As this was not handled by teardown class
    
    Change-Id: I789adbf0909c5edd0a2eb19ed4ccebcb654700fd
    Signed-off-by: Anees Patel <anepatel@redhat.com>
    
    * Add py3 support in 'tests/functional/arbiter/brick_cases'
    
    Change-Id: Ieecf4707ee6cb7b3c58380306bf105e282986b1b
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Changing code used to create arbiter volumes
    
    As distributed-arbiter and arbiter weren't present before patch [1],
    arbiter and distributed-arbiter volumes were created by the hack show
    below where a distributed-replicated or replicated volume's configuration
    was modified to create an arbiter volume.
    ```
    @runs_on([['replicated', 'distributed-replicated'],
              ['glusterfs', 'nfs']])
    class TestSelfHeal(GlusterBaseClass):
       .................
    
       @classmethod
       def setUpClass(cls):
        ...............
    
         # Overriding the volume type to specifically test the volume
         # type Change from distributed-replicated to arbiter
         if cls.volume_type == "distributed-replicated":
             cls.volume['voltype'] = { 'type': 'distributed-replicated',
                                       'dist_count': 2,
                                       'replica_count': 3,
                                       'arbiter_count': 1,
                                       'transport': 'tcp'}
    ```
    Now this code is to be updated where we need to remove code which
    was used to override volume configuration and
    just add arbiter or distributed-arbiter in `@runs_on([],[])`
    as shown below:
    ```
    @runs_on([['replicated', 'distributed-arbiter'],
              ['glusterfs', 'nfs']])
    class TestSelfHeal(GlusterBaseClass):
    ```
    
    Links:
    [1] https://github.com/gluster/glusto-tests/commit/08b727842bc66603e3b8d1160ee4b15051b0cd20
    
    Change-Id: I4c44c2f3506bd0183fd991354fb723f8ec235a4b
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated test_impact_of_replace_brick_for_glustershd.py
    
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/arbiter/brick_cases/test_impact_of_replace_brick_for_glustershd.py)
    
    Updates: #292
    
    Fixes: #735
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * modified the command for pid
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added bricks existing check in shd
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added subvols
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added the rest of the logic
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added docstring and removed comments
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: Anees Patel <anepatel@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/arbiter/brick_cases/test_impact_of_replace_brick_for_glustershd.py
@@ -0,0 +1,119 @@
+"""
+Copyright (C) 2016-2020  Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test Cases in this module tests the self heal daemon process.
+"""
+
+
+# disruptive;arb,dist-arb
+# TODO: nfs
+
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    def run_test(self, redant):
+        """
+        1. Get the self head daemon pids.
+        2. Get all the brivks for the volume
+        3. Check if the bricks exist in the shd volfile
+        4. Get the sub volumes for the volume type
+        5. Replace a brick in all the subvols.
+        6. Check for the volume process to be online.
+        7. Get the selh heal daemon pids and compare the same
+           with the previous ones.
+        8. Get the bricks list after replace brick
+        9. Check if bricks exist in shd volfile
+        """
+        replaced_bricks = []
+        ret, pids = redant.get_self_heal_daemon_pid(self.server_list)
+        if not ret:
+            raise Exception("Either No self heal daemon process found or "
+                            "more than One self heal daemon process "
+                            f"found : {pids}")
+        glustershd_pids = pids
+        # get the bricks for the volume
+        bricks_list = redant.get_all_bricks(self.vol_name,
+                                            self.server_list[0])
+        # validate the bricks present in volume info with
+        # glustershd server volume file
+        if not (redant.
+                do_bricks_exist_in_shd_volfile(self.vol_name,
+                                               bricks_list,
+                                               self.server_list[0])):
+            raise Exception("Brick List from volume info is different "
+                            "from glustershd server volume file. "
+                            "Please check log file for details")
+        # get the subvolumes
+        subvols_list = redant.get_subvols(self.vol_name, self.server_list[0])
+
+        # replace brick from each sub-vol
+        for i in range(0, len(subvols_list)):
+            subvol_brick_list = subvols_list[i]
+            redant.logger.debug(f"sub-volume {i+1} "
+                                f"brick list : {subvol_brick_list}")
+            brick_to_replace = subvol_brick_list[-1]
+            new_brick = brick_to_replace + 'new'
+            redant.replace_brick(self.server_list[0],
+                                 self.vol_name,
+                                 brick_to_replace, new_brick)
+            replaced_bricks.append(brick_to_replace)
+
+        # Verify volume's all process are online for 60 sec
+        if not (redant.
+                wait_for_volume_process_to_be_online(self.vol_name,
+                                                     self.server_list[0],
+                                                     self.server_list,
+                                                     timeout=60)):
+            raise Exception("Volume processes not yet online")
+
+        # Verify glustershd process releases its parent process
+        if not redant.is_shd_daemonized(self.server_list):
+            raise Exception("Either No self heal daemon process found or "
+                            "more than One self heal daemon process found")
+
+        # check the self-heal daemon process
+        ret, pids = redant.get_self_heal_daemon_pid(self.server_list)
+        if not ret:
+            raise Exception("Either No self heal daemon process found or "
+                            "more than One self heal daemon process "
+                            f"found : {pids}")
+        glustershd_pids_after_replacement = pids
+
+        # Compare pids before and after replacing
+        if glustershd_pids == glustershd_pids_after_replacement:
+            raise Exception("Self heal Daemon process is same before "
+                            "and after replacing bricks")
+
+        # get the bricks for the volume after replacing
+        br_lis_aft_repl = redant.get_all_bricks(self.vol_name,
+                                                self.server_list[0])
+        if br_lis_aft_repl is None:
+            raise Exception("Empty bricks list after replacing")
+
+        # validate the bricks present in volume info
+        # with glustershd server volume file after replacing bricks
+        if not (redant.
+                do_bricks_exist_in_shd_volfile(self.vol_name,
+                                               br_lis_aft_repl,
+                                               self.server_list[0])):
+            raise Exception("Brick List from volume info is different "
+                            "from glustershd server volume file after "
+                            "replacing bricks. Please check log file "
+                            "for details")
