e09498fc86b5ce0ff982eaf2bb772db0a455b50a
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-05 10:59:35 +0530

    Migrated test_verify_df_output
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/glusterd/test_verify_df_output.py)
    
    Updates: #292
    
    Fixes: #756
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_verify_df_output.py
+++ tests/functional/glusterd/test_verify_df_output.py
@@ -1,110 +1,82 @@
-#  Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass,
-                                                   runs_on)
-from glustolibs.gluster.heal_libs import monitor_heal_completion
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.volume_libs import (replace_brick_from_volume,
-                                            shrink_volume, expand_volume)
-from glustolibs.gluster.brick_libs import get_all_bricks
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 @runs_on([['distributed-dispersed', 'distributed-replicated',
            'distributed-arbiter', 'dispersed', 'replicated',
            'arbiter'],
           ['glusterfs']])
-class VerifyDFWithReplaceBrick(GlusterBaseClass):
+"""
 
-    @classmethod
-    def setUpClass(cls):
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
+# disruptive;rep
 
-        # Upload io scripts for running IO on mounts
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        if not upload_scripts(cls.clients, [cls.script_upload_path]):
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
+from tests.d_parent_test import DParentTest
 
-    def setUp(self):
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
 
-        # Setup Volume and Mount Volume
-        if not self.setup_volume_and_mount_volume(mounts=self.mounts):
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
+class TestCase(DParentTest):
 
-    def _perform_io_and_validate(self):
-        """ Performs IO on the mount points and validates it"""
-        all_mounts_procs, count = [], 1
-        for mount_obj in self.mounts:
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d --dir-depth 2 "
-                   "--dir-length 3 --max-num-of-dirs 3 "
-                   "--num-of-files 2 %s" % (
-                       self.script_upload_path, count,
-                       mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            all_mounts_procs.append(proc)
-            count = count + 10
+    # def _perform_io_and_validate(self):
+    #     """ Performs IO on the mount points and validates it"""
+    #     all_mounts_procs, count = [], 1
+    #     for mount_obj in self.mounts:
+    #         cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
+    #                "--dirname-start-num %d --dir-depth 2 "
+    #                "--dir-length 3 --max-num-of-dirs 3 "
+    #                "--num-of-files 2 %s" % (
+    #                    self.script_upload_path, count,
+    #                    mount_obj.mountpoint))
+    #         proc = g.run_async(mount_obj.client_system, cmd,
+    #                            user=mount_obj.user)
+    #         all_mounts_procs.append(proc)
+    #         count = count + 10
 
-        # Validating IO's on mount point and waiting to complete
-        ret = validate_io_procs(all_mounts_procs, self.mounts)
-        self.assertTrue(ret, "IO failed on some of the clients")
-        g.log.info("Successfully validated IO's")
+    #     # Validating IO's on mount point and waiting to complete
+    #     ret = validate_io_procs(all_mounts_procs, self.mounts)
+    #     self.assertTrue(ret, "IO failed on some of the clients")
+    #     g.log.info("Successfully validated IO's")
 
-    def _replace_bricks_and_wait_for_heal_completion(self):
-        """ Replaces all the bricks and waits for the heal to complete"""
-        existing_bricks = get_all_bricks(self.mnode, self.volname)
-        for brick_to_replace in existing_bricks:
-            ret = replace_brick_from_volume(self.mnode, self.volname,
-                                            self.servers,
-                                            self.all_servers_info,
-                                            src_brick=brick_to_replace)
-            self.assertTrue(ret,
-                            "Replace of %s failed" % brick_to_replace)
-            g.log.info("Replace of brick %s successful for volume %s",
-                       brick_to_replace, self.volname)
+    # def _replace_bricks_and_wait_for_heal_completion(self):
+    #     """ Replaces all the bricks and waits for the heal to complete"""
+    #     existing_bricks = get_all_bricks(self.mnode, self.volname)
+    #     for brick_to_replace in existing_bricks:
+    #         ret = replace_brick_from_volume(self.mnode, self.volname,
+    #                                         self.servers,
+    #                                         self.all_servers_info,
+    #                                         src_brick=brick_to_replace)
+    #         self.assertTrue(ret,
+    #                         "Replace of %s failed" % brick_to_replace)
+    #         g.log.info("Replace of brick %s successful for volume %s",
+    #                    brick_to_replace, self.volname)
 
-            # Monitor heal completion
-            ret = monitor_heal_completion(self.mnode, self.volname)
-            self.assertTrue(ret, 'Heal has not yet completed')
-            g.log.info('Heal has completed successfully')
+    #         # Monitor heal completion
+    #         ret = monitor_heal_completion(self.mnode, self.volname)
+    #         self.assertTrue(ret, 'Heal has not yet completed')
+    #         g.log.info('Heal has completed successfully')
 
-    def _get_mount_size_from_df_h_output(self):
-        """ Extracts the mount size from the df -h output"""
+    # def _get_mount_size_from_df_h_output(self):
+    #     """ Extracts the mount size from the df -h output"""
 
-        split_cmd = " | awk '{split($0,a,\" \");print a[2]}' | sed 's/.$//'"
-        cmd = ("cd {};df -h | grep {} {}".format(self.mounts[0].mountpoint,
-                                                 self.volname, split_cmd))
-        ret, mount_size, _ = g.run(self.clients[0], cmd)
-        self.assertEqual(ret, 0, "Failed to extract mount size")
-        return float(mount_size.split("\n")[0])
+    #     split_cmd = " | awk '{split($0,a,\" \");print a[2]}' | sed 's/.$//'"
+    #     cmd = ("cd {};df -h | grep {} {}".format(self.mounts[0].mountpoint,
+    #                                              self.volname, split_cmd))
+    #     ret, mount_size, _ = g.run(self.clients[0], cmd)
+    #     self.assertEqual(ret, 0, "Failed to extract mount size")
+    #     return float(mount_size.split("\n")[0])
 
-    def test_verify_df_output_when_brick_replaced(self):
+    def run_test(self, redant):
         """
         - Take the output of df -h.
         - Replace any one brick for the volumes.
@@ -157,15 +129,3 @@ class VerifyDFWithReplaceBrick(GlusterBaseClass):
         # Verify the df -h output returns smaller value
         self.assertGreater(mount_size_after_expand, mount_size_after_shrink,
                            "The mount size has not reduced after shrinking")
-
-    def tearDown(self):
-        """
-        Cleanup and umount volume
-        """
-        # Cleanup and umount volume
-        if not self.unmount_volume_and_cleanup_volume(mounts=self.mounts):
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
