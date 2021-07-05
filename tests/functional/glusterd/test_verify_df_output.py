61cdac6dead6563bc50fa192d3d04e5cc3b336bc
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-05 11:21:25 +0530

    added logic for the first few operations
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/glusterd/test_verify_df_output.py
+++ tests/functional/glusterd/test_verify_df_output.py
@@ -28,53 +28,57 @@ from tests.d_parent_test import DParentTest
 
 class TestCase(DParentTest):
 
-    # def _perform_io_and_validate(self):
-    #     """ Performs IO on the mount points and validates it"""
-    #     all_mounts_procs, count = [], 1
-    #     for mount_obj in self.mounts:
-    #         cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-    #                "--dirname-start-num %d --dir-depth 2 "
-    #                "--dir-length 3 --max-num-of-dirs 3 "
-    #                "--num-of-files 2 %s" % (
-    #                    self.script_upload_path, count,
-    #                    mount_obj.mountpoint))
-    #         proc = g.run_async(mount_obj.client_system, cmd,
-    #                            user=mount_obj.user)
-    #         all_mounts_procs.append(proc)
-    #         count = count + 10
+    def _perform_io_and_validate(self):
+        """ Performs IO on the mount points and validates it"""
+        self.mounts = (self.redant.es.
+                       get_mnt_pts_dict_in_list(self.vol_name))
+        self.all_mounts_procs, counter = [], 1
 
-    #     # Validating IO's on mount point and waiting to complete
-    #     ret = validate_io_procs(all_mounts_procs, self.mounts)
-    #     self.assertTrue(ret, "IO failed on some of the clients")
-    #     g.log.info("Successfully validated IO's")
+        for mount in self.mounts:
+            self.redant.logger.info(f"Starting IO on {mount['client']}:"
+                                    f"{mount['mountpath']}")
+            proc = self.redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                           counter, 2, 3, 3, 2,
+                                                           mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter = counter + 10
 
-    # def _replace_bricks_and_wait_for_heal_completion(self):
-    #     """ Replaces all the bricks and waits for the heal to complete"""
-    #     existing_bricks = get_all_bricks(self.mnode, self.volname)
-    #     for brick_to_replace in existing_bricks:
-    #         ret = replace_brick_from_volume(self.mnode, self.volname,
-    #                                         self.servers,
-    #                                         self.all_servers_info,
-    #                                         src_brick=brick_to_replace)
-    #         self.assertTrue(ret,
-    #                         "Replace of %s failed" % brick_to_replace)
-    #         g.log.info("Replace of brick %s successful for volume %s",
-    #                    brick_to_replace, self.volname)
+        # Validating IO's on mount point and waiting to complete
+        ret = self.redant.validate_io_procs(self.all_mounts_procs,
+                                            self.mounts)
+        if not ret:
+            raise Exception("IO validation failed")
 
-    #         # Monitor heal completion
-    #         ret = monitor_heal_completion(self.mnode, self.volname)
-    #         self.assertTrue(ret, 'Heal has not yet completed')
-    #         g.log.info('Heal has completed successfully')
+    def _replace_bricks_and_wait_for_heal_completion(self):
+        """ Replaces all the bricks and waits for the heal to complete"""
+        existing_bricks = self.redant.get_all_bricks(self.vol_name,
+                                                     self.server_list[0])
+        if existing_bricks is None:
+            raise Exception("Failed to get the bricks list")
+        print(existing_bricks)
+        # for brick_to_replace in existing_bricks:
+        #     ret = replace_brick_from_volume(self.mnode, self.volname,
+        #                                     self.servers,
+        #                                     self.all_servers_info,
+        #                                     src_brick=brick_to_replace)
+        #     self.assertTrue(ret,
+        #                     "Replace of %s failed" % brick_to_replace)
+        #     g.log.info("Replace of brick %s successful for volume %s",
+        #                brick_to_replace, self.volname)
 
-    # def _get_mount_size_from_df_h_output(self):
-    #     """ Extracts the mount size from the df -h output"""
+        #     # Monitor heal completion
+        #     ret = monitor_heal_completion(self.mnode, self.volname)
+        #     self.assertTrue(ret, 'Heal has not yet completed')
+        #     g.log.info('Heal has completed successfully')
 
-    #     split_cmd = " | awk '{split($0,a,\" \");print a[2]}' | sed 's/.$//'"
-    #     cmd = ("cd {};df -h | grep {} {}".format(self.mounts[0].mountpoint,
-    #                                              self.volname, split_cmd))
-    #     ret, mount_size, _ = g.run(self.clients[0], cmd)
-    #     self.assertEqual(ret, 0, "Failed to extract mount size")
-    #     return float(mount_size.split("\n")[0])
+    def _get_mount_size_from_df_h_output(self):
+        """ Extracts the mount size from the df -h output"""
+
+        split_cmd = " | awk '{split($0,a,\" \");print a[2]}' | sed 's/.$//'"
+        cmd = (f"cd {self.mounts[0]['mountpath']};df -h | grep "
+               f"{self.vol_name} {split_cmd}")
+        ret = self.redant.execute_abstract_op_node(cmd, self.client_list[0])
+        return float(ret['msg'][0].split("\n")[0])
 
     def run_test(self, redant):
         """
@@ -96,36 +100,36 @@ class TestCase(DParentTest):
         # Replace all the bricks and wait till the heal completes
         self._replace_bricks_and_wait_for_heal_completion()
 
-        # Get df -h output after brick replace
-        mount_size_after_replace = self._get_mount_size_from_df_h_output()
+        # # Get df -h output after brick replace
+        # mount_size_after_replace = self._get_mount_size_from_df_h_output()
 
-        # Verify the mount point size remains the same after brick replace
-        self.assertEqual(initial_mount_size, mount_size_after_replace,
-                         "The mount sizes before and after replace bricks "
-                         "are not same")
+        # # Verify the mount point size remains the same after brick replace
+        # self.assertEqual(initial_mount_size, mount_size_after_replace,
+        #                  "The mount sizes before and after replace bricks "
+        #                  "are not same")
 
-        # Add bricks
-        ret = expand_volume(self.mnode, self.volname, self.servers,
-                            self.all_servers_info, force=True)
-        self.assertTrue(ret, "Failed to add-brick to volume")
+        # # Add bricks
+        # ret = expand_volume(self.mnode, self.volname, self.servers,
+        #                     self.all_servers_info, force=True)
+        # self.assertTrue(ret, "Failed to add-brick to volume")
 
-        # Get df -h output after volume expand
-        mount_size_after_expand = self._get_mount_size_from_df_h_output()
+        # # Get df -h output after volume expand
+        # mount_size_after_expand = self._get_mount_size_from_df_h_output()
 
-        # Verify df -h output returns greater value
-        self.assertGreater(mount_size_after_expand, initial_mount_size,
-                           "The mount size has not increased after expanding")
+        # # Verify df -h output returns greater value
+        # self.assertGreater(mount_size_after_expand, initial_mount_size,
+        #                    "The mount size has not increased after expanding")
 
-        # Remove bricks
-        ret = shrink_volume(self.mnode, self.volname, force=True)
-        self.assertTrue(ret, ("Remove brick operation failed on "
-                              "%s", self.volname))
-        g.log.info("Remove brick operation is successful on "
-                   "volume %s", self.volname)
+        # # Remove bricks
+        # ret = shrink_volume(self.mnode, self.volname, force=True)
+        # self.assertTrue(ret, ("Remove brick operation failed on "
+        #                       "%s", self.volname))
+        # g.log.info("Remove brick operation is successful on "
+        #            "volume %s", self.volname)
 
-        # Get df -h output after volume shrink
-        mount_size_after_shrink = self._get_mount_size_from_df_h_output()
+        # # Get df -h output after volume shrink
+        # mount_size_after_shrink = self._get_mount_size_from_df_h_output()
 
-        # Verify the df -h output returns smaller value
-        self.assertGreater(mount_size_after_expand, mount_size_after_shrink,
-                           "The mount size has not reduced after shrinking")
+        # # Verify the df -h output returns smaller value
+        # self.assertGreater(mount_size_after_expand, mount_size_after_shrink,
+        #                    "The mount size has not reduced after shrinking")
