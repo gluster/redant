f0290dc4c4297730e7f8848c2af495a7be3d0093
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-07 18:39:15 +0530

    added convert replicate to arbiter function
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/arbiter/brick_cases/test_replica3_to_arbiter.py
+++ tests/functional/arbiter/brick_cases/test_replica3_to_arbiter.py
@@ -43,59 +43,56 @@ class TestCase(DParentTest):
     #             has_process_stopped.append(True)
     #     return all(has_process_stopped)
 
-    # def _convert_replicated_to_arbiter_volume(self):
-    #     """
-    #     Helper module to convert replicated to arbiter volume.
-    #     """
-    #     # pylint: disable=too-many-statements
-    #     # Remove brick to reduce the replica count from replica 3
-    #     g.log.info("Removing bricks to form replica 2 volume")
-    #     ret = shrink_volume(self.mnode, self.volname, replica_num=0)
-    #     self.assertTrue(ret, "Failed to remove brick on volume %s"
-    #                     % self.volname)
-    #     g.log.info("Successfully removed brick on volume %s", self.volname)
+    def _convert_replicated_to_arbiter_volume(self):
+        """
+        Helper module to convert replicated to arbiter volume.
+        """
+        # Remove brick to reduce the replica count from replica 3
+        ret = self.redant.shrink_volume(self.server_list[0],
+                                        self.vol_name,
+                                        replica_num=0)
+        if not ret:
+            raise Exception("Failed to shrink volume")
 
-    #     # Wait for volume processes to be online
-    #     g.log.info("Wait for volume processes to be online")
-    #     ret = wait_for_volume_process_to_be_online(self.mnode, self.volname)
-    #     self.assertTrue(ret, "Volume %s process not online despite waiting "
-    #                          "for 300 seconds" % self.volname)
-    #     g.log.info("Successful in waiting for volume %s processes to be "
-    #                "online", self.volname)
+        # Wait for volume processes to be online
+        if not (self.redant.
+                wait_for_volume_process_to_be_online(self.vol_name,
+                                                     self.server_list[0],
+                                                     self.server_list)):
+            raise Exception(f"Volume {self.vol_name} process not online "
+                            "despite waiting for 300 seconds")
 
-    #     # Verifying all bricks online
-    #     g.log.info("Verifying volume's all process are online")
-    #     ret = verify_all_process_of_volume_are_online(self.mnode, self.volname)
-    #     self.assertTrue(ret, "Volume %s : All process are not online"
-    #                     % self.volname)
-    #     g.log.info("Volume %s : All process are online", self.volname)
+        # Verifying all bricks online
+        if not (self.redant.
+                verify_all_process_of_volume_are_online(self.vol_name,
+                                                        self.server_list[0])):
+            raise Exception(f"All volume processes for {self.vol_name}"
+                            " are not online")
+        # Adding the bricks to make arbiter brick
+        replica_arbiter = {'replica_count': 1, 'arbiter_count': 1}
+        ret = self.redant.expand_volume(self.server_list[0],
+                                        self.vol_name, self.server_list,
+                                        self.brick_roots, force=True,
+                                        **replica_arbiter)
+        if not ret:
+            raise Exception(f"Failed to expand the volume  {self.vol_name}")
 
-    #     # Adding the bricks to make arbiter brick
-    #     g.log.info("Adding bricks to convert to Arbiter Volume")
-    #     replica_arbiter = {'replica_count': 1, 'arbiter_count': 1}
-    #     ret = expand_volume(self.mnode, self.volname, self.servers,
-    #                         self.all_servers_info, force=True,
-    #                         **replica_arbiter)
-    #     self.assertTrue(ret, "Failed to expand the volume  %s" % self.volname)
-    #     g.log.info("Changing volume to arbiter volume is successful %s",
-    #                self.volname)
+        # Wait for volume processes to be online
+        if not (self.redant.
+                wait_for_volume_process_to_be_online(self.vol_name,
+                                                     self.server_list[0],
+                                                     self.server_list)):
+            raise Exception(f"Volume {self.vol_name} process not online "
+                            "despite waiting for 300 seconds")
 
-    #     # Wait for volume processes to be online
-    #     g.log.info("Wait for volume processes to be online")
-    #     ret = wait_for_volume_process_to_be_online(self.mnode, self.volname)
-    #     self.assertTrue(ret, "Failed to wait for volume %s processes "
-    #                          "to be online" % self.volname)
-    #     g.log.info("Successful in waiting for volume %s processes to be "
-    #                "online", self.volname)
+        # Verify volume's all process are online
+        if not (self.redant.
+                verify_all_process_of_volume_are_online(self.vol_name,
+                                                        self.server_list[0])):
+            raise Exception(f"All volume processes for {self.vol_name}"
+                            " are not online")
 
-    #     # Verify volume's all process are online
-    #     g.log.info("Verifying volume's all process are online")
-    #     ret = verify_all_process_of_volume_are_online(self.mnode, self.volname)
-    #     self.assertTrue(ret, "Volume %s : All process are not online"
-    #                     % self.volname)
-    #     g.log.info("Volume %s : All process are online", self.volname)
-
-    def test_replica_to_arbiter_volume_with_io(self):
+    def run_test(self, redant):
         """
         Description: Replica 3 to arbiter conversion with ongoing IO's
 
@@ -114,44 +111,45 @@ class TestCase(DParentTest):
         """
 
         # pylint: disable=too-many-statements
-        self.subvols = get_subvols(self.mnode, self.volname)['volume_subvols']
-        # self._convert_replicated_to_arbiter_volume()
-        # Create a dir to start untar
-        self.linux_untar_dir = "{}/{}".format(self.mounts[0].mountpoint,
-                                              "linuxuntar")
-        ret = mkdir(self.clients[0], self.linux_untar_dir)
-        self.assertTrue(ret, "Failed to create dir linuxuntar for untar")
-
-        # Start linux untar on dir linuxuntar
-        self.io_process = run_linux_untar(self.clients[0],
-                                          self.mounts[0].mountpoint,
-                                          dirs=tuple(['linuxuntar']))
-        self.is_io_running = True
-
-        # Convert relicated to arbiter volume
+        self.subvols = redant.get_subvols(self.vol_name,
+                                          self.server_list[0])
         self._convert_replicated_to_arbiter_volume()
+        # # Create a dir to start untar
+        # self.linux_untar_dir = "{}/{}".format(self.mounts[0].mountpoint,
+        #                                       "linuxuntar")
+        # ret = mkdir(self.clients[0], self.linux_untar_dir)
+        # self.assertTrue(ret, "Failed to create dir linuxuntar for untar")
 
-        # Wait for IO to complete.
-        ret = self._wait_for_untar_completion()
-        self.assertFalse(ret, "IO didn't complete or failed on client")
-        self.is_io_running = False
+        # # Start linux untar on dir linuxuntar
+        # self.io_process = run_linux_untar(self.clients[0],
+        #                                   self.mounts[0].mountpoint,
+        #                                   dirs=tuple(['linuxuntar']))
+        # self.is_io_running = True
 
-        # Start healing
-        ret = trigger_heal(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not started')
-        g.log.info('Healing is started')
+        # # Convert relicated to arbiter volume
+        # self._convert_replicated_to_arbiter_volume()
 
-        # Monitor heal completion
-        ret = monitor_heal_completion(self.mnode, self.volname,
-                                      timeout_period=3600)
-        self.assertTrue(ret, 'Heal has not yet completed')
+        # # Wait for IO to complete.
+        # ret = self._wait_for_untar_completion()
+        # self.assertFalse(ret, "IO didn't complete or failed on client")
+        # self.is_io_running = False
 
-        # Check if heal is completed
-        ret = is_heal_complete(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not complete')
-        g.log.info('Heal is completed successfully')
+        # # Start healing
+        # ret = trigger_heal(self.mnode, self.volname)
+        # self.assertTrue(ret, 'Heal is not started')
+        # g.log.info('Healing is started')
 
-        # Check for split-brain
-        ret = is_volume_in_split_brain(self.mnode, self.volname)
-        self.assertFalse(ret, 'Volume is in split-brain state')
-        g.log.info('Volume is not in split-brain state')
+        # # Monitor heal completion
+        # ret = monitor_heal_completion(self.mnode, self.volname,
+        #                               timeout_period=3600)
+        # self.assertTrue(ret, 'Heal has not yet completed')
+
+        # # Check if heal is completed
+        # ret = is_heal_complete(self.mnode, self.volname)
+        # self.assertTrue(ret, 'Heal is not complete')
+        # g.log.info('Heal is completed successfully')
+
+        # # Check for split-brain
+        # ret = is_volume_in_split_brain(self.mnode, self.volname)
+        # self.assertFalse(ret, 'Volume is in split-brain state')
+        # g.log.info('Volume is not in split-brain state')
