de6a3b2822115f70a40b1b849f6ddf61448582d3
srijan-sivakumar <ssivakum@redhat.com>
2021-07-07 09:28:31 +0530

    [Core] Result handling modification iter 2

--- core/test_runner.py
+++ core/test_runner.py
@@ -215,7 +215,7 @@ class TestRunner:
             spinner.fail(f"{mname}-{volume_type} Failed")
         else:
             result_text += " SKIP"
-            test_stats['testResult'] = None
+            test_stats['testResult'] = "SKIP"
             spinner = Halo(spinner='dots', text_color='cyan')
             spinner.info(f"{mname}-{volume_type} SKIP")
         test_stats['component'] = tc_log_path.split('/')[-4]
