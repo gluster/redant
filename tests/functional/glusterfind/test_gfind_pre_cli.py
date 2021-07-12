32bc8f41a471447a5aabcce598c9b95180347ea4
nik-redhat <nladha@redhat.com>
2021-07-12 16:15:01 +0530

    Migrated TC test_gfind_pre_cli
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/glusterfind/test_gfind_pre_cli.py
+++ tests/functional/glusterfind/test_gfind_pre_cli.py
@@ -1,86 +1,50 @@
-#  Copyright (C) 2019  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY :or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2019  Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.glusterfind_ops import (gfind_create,
-                                                gfind_list,
-                                                gfind_pre,
-                                                gfind_post,
-                                                gfind_delete)
-from glustolibs.gluster.glusterfile import (file_exists, remove_file,
-                                            check_if_pattern_in_file)
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY :or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to check glusterfind pre operation
+"""
+
+# disruptive;dist,rep,dist-rep,disp,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['replicated', 'distributed-replicated', 'dispersed',
-           'distributed', 'distributed-dispersed'],
-          ['glusterfs']])
-class TestGlusterFindPreCLI(GlusterBaseClass):
-    """
-    GlusterFindPreCLI contains tests which verifies the glusterfind pre
-    command functionality.
-    """
+class TestGlusterFindPreCLI(DParentTest):
 
-    def setUp(self):
+    def terminate(self):
         """
-        setup volume and mount volume
-        Initiate necessary variables
+        Delete glusterfind session and cleanup outfiles
         """
+        try:
+            self.redant.gfind_delete(self.server_list[0], self.vol_name,
+                                     self.session)
+            for file in self.outfiles:
+                ret = self.redant.remove_file(self.server_list[0], file, True)
+                if not ret:
+                    raise Exception(f"Failed to remove the outfile {file}")
 
-        # calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup %s", self.volname)
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume %s" % self.volname)
-        g.log.info("Successful in Setup Volume %s", self.volname)
-        self.session = 'test-session-%s' % self.volname
-        self.outfile = '/tmp/test-outfile-%s.txt' % self.volname
-
-    def tearDown(self):
-        """
-        tearDown for every test
-        Clean up and unmount the volume
-        """
-
-        ret, _, _ = gfind_delete(self.mnode, self.volname, self.session)
-        if ret != 0:
-            raise ExecutionError("Failed to delete session %s" % self.session)
-        g.log.info("Successfully deleted session %s", self.session)
-
-        g.log.info("Removing the outfile created during 'glusterfind pre'")
-        ret = remove_file(self.mnode, self.outfile, force=True)
-        if not ret:
-            raise ExecutionError("Failed to remove the outfile")
-        g.log.info("Successfully removed the outfile")
-
-        # stopping the volume
-        g.log.info("Starting to Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Cleanup Volume")
-        g.log.info("Successful in Cleanup Volume")
-
-        # calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_gfind_pre_cli(self):
+    def run_test(self, redant):
         """
         Verifying the glusterfind pre command functionality with valid
         and invalid values for the required and optional parameters.
@@ -101,104 +65,71 @@ class TestGlusterFindPreCLI(GlusterBaseClass):
                                  namespace, noencode, disablepartial,
                                  regenoutfile, outprefix, fieldsep
         """
+        self.session = f'test-session-for-post-{self.vol_name}'
+        self.outfile = f'/tmp/test-outfile-{self.vol_name}.txt'
 
-        # pylint: disable=too-many-statements
         # Creating a session for the volume
-        g.log.info("Creating a session for the volume %s", self.volname)
-        ret, _, _ = gfind_create(self.mnode, self.volname, self.session)
-        self.assertEqual(ret, 0, ("Unexpected: Creation of a session for the "
-                                  "volume %s failed" % self.volname))
-        g.log.info("Successfully created a session for the volume %s",
-                   self.volname)
+        redant.gfind_create(self.server_list[0], self.vol_name, self.session)
 
         # Perform glusterfind list to check if session exists
-        g.log.info("Performing glusterfind list to check if the session is "
-                   "created")
-        ret, _, _ = gfind_list(self.mnode, volname=self.volname,
-                               sessname=self.session)
-        self.assertEqual(ret, 0, "Failed to list the glusterfind session")
-        g.log.info("Successfully listed the glusterfind session")
+        redant.gfind_list(self.server_list[0], self.vol_name, self.session)
 
         # Starting IO on the mounts
-        mount_obj = self.mounts[0]
-        mount_dir = mount_obj.mountpoint
-        client = mount_obj.client_system
-
-        g.log.info("Creating Files on %s:%s", client, mount_dir)
-        cmd = ("cd %s ; for i in `seq 1 10` ; "
-               "do dd if=/dev/urandom of=file$i bs=1M count=1 ; "
-               "done" % mount_dir)
-        ret, _, _ = g.run(client, cmd)
-        self.assertEqual(ret, 0, "Failed to create files on mountpoint")
-        g.log.info("Files created successfully on mountpoint")
+        redant.logger.info("Creating Files on "
+                           f"{self.mountpoint}:{self.client_list[0]}")
+        cmd = (f"cd {self.mountpoint} ; for i in `seq 1 10` ; "
+               "do dd if=/dev/urandom of=file$i bs=1M count=1;done")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Check if the files exist
-        g.log.info("Checking the existence of files created during IO")
         for i in range(1, 11):
-            ret = file_exists(client, '%s/file%s' % (mount_dir, i))
-            self.assertTrue(ret, "Unexpected: File 'file%s' does not exist"
-                            % i)
-            g.log.info("Successfully validated existence of 'file%s'", i)
+            path = f"{self.mountpoint}/file{i}"
+            ret = redant.path_exists(self.client_list[0], path)
+            if not ret:
+                raise Exception("File doesn't exist")
 
         # Perform glusterfind pre for the session
-        g.log.info("Performing glusterfind pre for the session %s",
-                   self.session)
-        ret, _, _ = gfind_pre(self.mnode, self.volname, self.session,
-                              self.outfile, full=True, noencode=True,
-                              debug=True)
-        self.assertEqual(ret, 0, ("Failed to perform glusterfind pre"))
-        g.log.info("Successfully performed glusterfind pre")
+        redant.gfind_pre(self.server_list[0], self.vol_name, self.session,
+                         self.outfile, full=True, noencode=True,
+                         debug=True)
 
         # Check if the outfile exists
-        g.log.info("Checking if outfile created during glusterfind pre command"
-                   " exists")
-        ret = file_exists(self.mnode, self.outfile)
-        self.assertTrue(ret, "Unexpected: File '%s' does not exist"
-                        % self.outfile)
-        g.log.info("Successfully validated existence of '%s'", self.outfile)
+        ret = redant.path_exists(self.server_list[0], self.outfile)
+        if not ret:
+            raise Exception("File doesn't exist")
 
         # Check if all the files are listed in the outfile
         for i in range(1, 11):
-            ret = check_if_pattern_in_file(self.mnode, 'file%s' % i,
-                                           self.outfile)
-            self.assertEqual(ret, 0,
-                             ("File 'file%s' not listed in %s"
-                              % (i, self.outfile)))
-            g.log.info("File 'file%s' listed in %s", i, self.outfile)
+            pattern = f"file{i}"
+            ret = redant.check_if_pattern_in_file(self.server_list[0],
+                                                  pattern, self.outfile)
+            if ret != 0:
+                raise Exception("Pattern not found in file")
 
         # Perform glusterfind post for the session
-        g.log.info("Performing glusterfind post for the session %s",
-                   self.session)
-        ret, _, _ = gfind_post(self.mnode, self.volname, self.session)
-        self.assertEqual(ret, 0, ("Failed to perform glusterfind post"))
-        g.log.info("Successfully performed glusterfind post")
+        redant.gfind_post(self.server_list[0], self.vol_name, self.session)
 
         # Perform glusterfind pre using the invalid values for required
         # parameters
         not_volume = 'invalid-volume-name'
         not_session = 'invalid-session-name'
         not_outfile = '/tmp/not'
-        g.log.info("Performing glusterfind pre with invalid values for the "
-                   "required parameters")
-        ret, _, _ = gfind_pre(self.mnode, not_volume, not_session, not_outfile)
-        self.assertNotEqual(ret, 0, "Unexpected: glusterfind pre Successful "
+        ret = redant.gfind_pre(self.server_list[0], not_volume, not_session,
+                               not_outfile, excep=False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: glusterfind pre Successful "
                             "even with invalid values for required parameters")
-        g.log.info("Successful: glusterfind pre failed with invalid values "
-                   "for required parameters")
 
         # Perform glusterfind pre using the invalid values for optional
         # parameters
-        g.log.info("Deleting the session with invalid values for the optional "
-                   "parameters")
         invalid_options = [' --dbug', ' --noencod', ' --regenout', ' --type n',
                            ' --tagforfullfind', ' --disablepartial', ' --fll'
                            ' --outprefix none', ' --namespc']
         for opt in invalid_options:
-            ret, _, _ = g.run(self.mnode, ("glusterfind pre %s %s %s %s"
-                                           % (self.volname, self.session,
-                                              self.outfile, opt)))
-            self.assertNotEqual(ret, 0, "Unexpected: glusterfind pre "
-                                " successful for option %s which is invalid"
-                                % opt)
-        g.log.info("Successful: glusterfind pre failed with invalid value "
-                   "for optional parameters")
+            cmd = (f"glusterfind pre {self.vol_name} {self.session} "
+                   f"{self.outfile} {opt}")
+            ret = redant.execute_abstract_op_node(cmd, self.server_list[0],
+                                                  False)
+            if ret['error_code'] == 0:
+                raise Exception("Unexpected: glusterfind pre successful "
+                                f"for option {opt} which is invalid")
