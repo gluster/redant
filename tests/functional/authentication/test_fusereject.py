3c23e336ca45bbd0cc3fe2c75394f70d6531e731
nik-redhat <nladha@redhat.com>
2021-07-13 17:43:39 +0530

    Move skip check to setup
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/authentication/test_fusereject.py
+++ tests/functional/authentication/test_fusereject.py
@@ -28,9 +28,12 @@ class TestAuthRejectVol(DParentTest):
 
     @DParentTest.setup_custom_enable
     def setup_test(self):
-        """
-        Create and start the volume
-        """
+        # Check for 2 clients
+        if len(self.client_list) < 2:
+            self.TEST_RES = None
+            raise Exception("The test case require 2 clients to run the test")
+
+        # Create and start the volume
         conf_hash = self.vol_type_inf[self.volume_type]
         self.redant.setup_volume(self.vol_name, self.server_list[0],
                                  conf_hash, self.server_list,
@@ -52,11 +55,6 @@ class TestAuthRejectVol(DParentTest):
         -Check if bricks are online
         -Mounting the vol on client1
         """
-        # Check for 2 clients
-        if len(self.client_list) < 2:
-            self.TEST_RES = None
-            raise Exception("The test case require 2 clients to run the test")
-
         # Obtain hostname of clients
         ret = redant.execute_abstract_op_node("hostname", self.client_list[0])
         hostname_client1 = ret['msg'][0].rstrip('\n')
