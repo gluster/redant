09227029451d3bf92cf35eec2172376d99af838e
nik-redhat <nladha@redhat.com>
2021-07-13 17:44:28 +0530

    Move slip check to setup
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/authentication/test_auth_allow.py
+++ tests/functional/authentication/test_auth_allow.py
@@ -27,9 +27,12 @@ class TestFuseAuthAllow(DParentTest):
 
     @DParentTest.setup_custom_enable
     def setup_test(self):
-        """
-        Create and start the volume
-        """
+        # Check for 2 clients
+        if len(self.client_list) < 2:
+            self.TEST_RES = None
+            raise Exception("The test case require 2 clients to run the test")
+
+        # Create and start the volume
         conf_hash = self.vol_type_inf[self.volume_type]
         self.redant.setup_volume(self.vol_name, self.server_list[0],
                                  conf_hash, self.server_list,
@@ -54,11 +57,6 @@ class TestFuseAuthAllow(DParentTest):
         8. Repeat steps 3 to 5.
         9. Unmount the volume from client1.
         """
-        # Check for 2 clients
-        if len(self.client_list) < 2:
-            self.TEST_RES = None
-            raise Exception("The test case require 2 clients to run the test")
-
         # Setting authentication on volume for client1 using ip
         auth_dict = {'all': [self.client_list[0]]}
         if not redant.set_auth_allow(self.vol_name, self.server_list[0],
