adb5e34de16215e66d956c5342c232ac3a962a95
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-14 14:24:57 +0530

    Migrated test_heal_info_should_have_fixed_fields
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/afr/test_heal_info_should_have_fixed_fields.py)
    
    Updates: #292
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/afr/test_heal_info_should_have_fixed_fields.py
+++ tests/functional/afr/test_heal_info_should_have_fixed_fields.py
@@ -1,100 +1,47 @@
-#  Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-from glusto.core import Glusto as g
+"""
+Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
 
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass, runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.brick_libs import (select_bricks_to_bring_offline,
-                                           bring_bricks_offline,
-                                           bring_bricks_online,
-                                           are_bricks_offline,
-                                           get_all_bricks)
-from glustolibs.gluster.heal_libs import (monitor_heal_completion,
-                                          is_heal_complete,
-                                          is_volume_in_split_brain)
-from glustolibs.gluster.heal_ops import get_heal_info_summary
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import (validate_io_procs,
-                                 wait_for_io_to_complete)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Verify self-heal Triggers with self heal with heal command
+"""
+
+# disruptive;dist-rep
+# TODO: nfs, cifs
+
+import traceback
+import tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated'],
-          ['glusterfs', 'cifs', 'nfs']])
-class VerifySelfHealTriggersHealCommand(GlusterBaseClass):
-    """
-    Description:
-        Verify self-heal Triggers with self heal with heal command
-    """
-    @classmethod
-    def setUpClass(cls):
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
+class TestCase(DParentTest):
 
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on mounts",
-                   cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        self.all_mounts_procs = []
-        self.io_validation_complete = False
-
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup Volume and Mount Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-    def tearDown(self):
-        """
-        If test method failed before validating IO, tearDown waits for the
-        IO's to complete and checks for the IO exit status
-
-        Cleanup and umount volume
-        """
-        if not self.io_validation_complete:
-            g.log.info("Wait for IO to complete as IO validation did not "
-                       "succeed in test method")
-            ret = wait_for_io_to_complete(self.all_mounts_procs, self.mounts)
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
             if not ret:
-                raise ExecutionError("IO failed on some of the clients")
-            g.log.info("IO is successful on all mounts")
+                raise Exception("IO failed on some of the clients")
 
-        # Cleanup and umount volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_heal_info_should_have_fixed_fields(self):
+    def run_test(self, redant):
         """
         - Create IO
         - While IO is creating - bring down a couple of bricks
@@ -103,86 +50,89 @@ class VerifySelfHealTriggersHealCommand(GlusterBaseClass):
         - Wait for heal to complete
         - Check for fields 'Brick', 'Status', 'Number of entries' in heal info
         """
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
         # Creating files on client side
-        for mount_obj in self.mounts:
-            g.log.info("Generating data for %s:%s",
-                       mount_obj.client_system, mount_obj.mountpoint)
-            # Create files
-            g.log.info('Creating files...')
-            command = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                       "-d 2 -l 2 -f 50 %s" % (
-                           self.script_upload_path,
-                           mount_obj.mountpoint))
+        # for mount_obj in self.mounts:
+        #     g.log.info("Generating data for %s:%s",
+        #                mount_obj.client_system, mount_obj.mountpoint)
+        #     # Create files
+        #     g.log.info('Creating files...')
+        #     command = ("/usr/bin/env python %s create_deep_dirs_with_files "
+        #                "-d 2 -l 2 -f 50 %s" % (
+        #                    self.script_upload_path,
+        #                    mount_obj.mountpoint))
 
-            proc = g.run_async(mount_obj.client_system, command,
-                               user=mount_obj.user)
-            self.all_mounts_procs.append(proc)
-        self.io_validation_complete = False
+        #     proc = g.run_async(mount_obj.client_system, command,
+        #                        user=mount_obj.user)
+        #     self.all_mounts_procs.append(proc)
+        # self.io_validation_complete = False
 
-        # Select bricks to bring offline
-        bricks_to_bring_offline_dict = (select_bricks_to_bring_offline(
-            self.mnode, self.volname))
-        bricks_to_bring_offline = bricks_to_bring_offline_dict['volume_bricks']
+        # # Select bricks to bring offline
+        # bricks_to_bring_offline_dict = (select_bricks_to_bring_offline(
+        #     self.mnode, self.volname))
+        # bricks_to_bring_offline = bricks_to_bring_offline_dict['volume_bricks']
 
-        # Bring brick offline
-        g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
-        ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s offline' %
-                        bricks_to_bring_offline)
+        # # Bring brick offline
+        # g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
+        # ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Failed to bring bricks %s offline' %
+        #                 bricks_to_bring_offline)
 
-        ret = are_bricks_offline(self.mnode, self.volname,
-                                 bricks_to_bring_offline)
-        self.assertTrue(ret, 'Bricks %s are not offline'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s offline is successful',
-                   bricks_to_bring_offline)
+        # ret = are_bricks_offline(self.mnode, self.volname,
+        #                          bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Bricks %s are not offline'
+        #                 % bricks_to_bring_offline)
+        # g.log.info('Bringing bricks %s offline is successful',
+        #            bricks_to_bring_offline)
 
-        # Validate IO
-        self.assertTrue(
-            validate_io_procs(self.all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
-        self.io_validation_complete = True
+        # # Validate IO
+        # self.assertTrue(
+        #     validate_io_procs(self.all_mounts_procs, self.mounts),
+        #     "IO failed on some of the clients"
+        # )
+        # self.io_validation_complete = True
 
-        # Bring brick online
-        g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        ret = bring_bricks_online(self.mnode, self.volname,
-                                  bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s online' %
-                        bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s online is successful',
-                   bricks_to_bring_offline)
+        # # Bring brick online
+        # g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
+        # ret = bring_bricks_online(self.mnode, self.volname,
+        #                           bricks_to_bring_offline)
+        # self.assertTrue(ret, 'Failed to bring bricks %s online' %
+        #                 bricks_to_bring_offline)
+        # g.log.info('Bringing bricks %s online is successful',
+        #            bricks_to_bring_offline)
 
-        # Monitor heal completion
-        ret = monitor_heal_completion(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal has not yet completed')
+        # # Monitor heal completion
+        # ret = monitor_heal_completion(self.mnode, self.volname)
+        # self.assertTrue(ret, 'Heal has not yet completed')
 
-        # Check if heal is completed
-        ret = is_heal_complete(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not complete')
-        g.log.info('Heal is completed successfully')
+        # # Check if heal is completed
+        # ret = is_heal_complete(self.mnode, self.volname)
+        # self.assertTrue(ret, 'Heal is not complete')
+        # g.log.info('Heal is completed successfully')
 
-        # Check for split-brain
-        ret = is_volume_in_split_brain(self.mnode, self.volname)
-        self.assertFalse(ret, 'Volume is in split-brain state')
-        g.log.info('Volume is not in split-brain state')
+        # # Check for split-brain
+        # ret = is_volume_in_split_brain(self.mnode, self.volname)
+        # self.assertFalse(ret, 'Volume is in split-brain state')
+        # g.log.info('Volume is not in split-brain state')
 
-        # Get heal info
-        g.log.info('Getting heal info...')
-        heal_info_dicts = get_heal_info_summary(self.mnode, self.volname)
-        self.assertFalse(ret, 'Failed to get heal info')
-        g.log.info(heal_info_dicts)
+        # # Get heal info
+        # g.log.info('Getting heal info...')
+        # heal_info_dicts = get_heal_info_summary(self.mnode, self.volname)
+        # self.assertFalse(ret, 'Failed to get heal info')
+        # g.log.info(heal_info_dicts)
 
-        bricks_list = get_all_bricks(self.mnode, self.volname)
-        self.assertIsNotNone(bricks_list, 'Brick list is None')
+        # bricks_list = get_all_bricks(self.mnode, self.volname)
+        # self.assertIsNotNone(bricks_list, 'Brick list is None')
 
-        # Check all fields in heal info dict
-        g.log.info('Checking for all the fields in heal info...')
-        for brick in bricks_list:
-            g.log.info('Checking fields for %s', brick)
-            self.assertEqual(heal_info_dicts[brick]['status'], 'Connected',
-                             'Status is not Connected for brick %s' % brick)
-            self.assertEqual(heal_info_dicts[brick]['numberOfEntries'], '0',
-                             'numberOfEntries is not 0 for brick %s' % brick)
+        # # Check all fields in heal info dict
+        # g.log.info('Checking for all the fields in heal info...')
+        # for brick in bricks_list:
+        #     g.log.info('Checking fields for %s', brick)
+        #     self.assertEqual(heal_info_dicts[brick]['status'], 'Connected',
+        #                      'Status is not Connected for brick %s' % brick)
+        #     self.assertEqual(heal_info_dicts[brick]['numberOfEntries'], '0',
+        #                      'numberOfEntries is not 0 for brick %s' % brick)
 
-        g.log.info('Successfully checked for all the fields in heal info')
+        # g.log.info('Successfully checked for all the fields in heal info')
