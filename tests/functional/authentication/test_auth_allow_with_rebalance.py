16cf81f200cf999d89116e6bf9dce09557661144
nik-redhat <nladha@redhat.com>
2021-07-15 10:42:28 +0530

    Migrated TC test_auth_allow_with_rebalance
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/authentication/test_auth_allow_with_rebalance.py
+++ tests/functional/authentication/test_auth_allow_with_rebalance.py
@@ -1,101 +1,72 @@
-#  Copyright (C) 2021  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-""" Description:
-        Test cases in this module tests the authentication allow feature
 """
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass,
-                                                   runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.auth_ops import set_auth_allow
-from glustolibs.gluster.volume_libs import expand_volume
-from glustolibs.gluster.rebalance_ops import (rebalance_start,
-                                              wait_for_rebalance_to_complete)
+ Copyright (C) 2021  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test cases in this module tests the authentication allow feature
+"""
+
+# disruptive;dist-rep,dist-disp
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated', 'distributed-dispersed'], ['glusterfs']])
-class FuseAuthAllow(GlusterBaseClass):
-    """
-    Tests to verify auth.allow feature on fuse mount.
-    """
-    @classmethod
-    def setUpClass(cls):
+class TestFuseAuthAllow(DParentTest):
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
         """
-        Create and start volume
+        Create and start the volume
         """
-        cls.get_super_method(cls, 'setUpClass')()
-        # Create and start volume
-        ret = cls.setup_volume()
-        if not ret:
-            raise ExecutionError("Failed to setup "
-                                 "and start volume %s" % cls.volname)
-
-    def _authenticated_mount(self, mount_obj):
-        """
-        Mount volume on authenticated client
-
-        Args:
-            mount_obj(obj): Object of GlusterMount class
-        """
-        # Mount volume
-        ret = mount_obj.mount()
-        self.assertTrue(ret, ("Failed to mount %s on client %s" %
-                              (mount_obj.volname,
-                               mount_obj.client_system)))
-        g.log.info("Successfully mounted %s on client %s", mount_obj.volname,
-                   mount_obj.client_system)
-
-        # Verify mount
-        ret = mount_obj.is_mounted()
-        self.assertTrue(ret, ("%s is not mounted on client %s"
-                              % (mount_obj.volname, mount_obj.client_system)))
-        g.log.info("Verified: %s is mounted on client %s",
-                   mount_obj.volname, mount_obj.client_system)
+        conf_hash = self.vol_type_inf[self.volume_type]
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots, force=True)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        for client in self.client_list:
+            self.redant.execute_abstract_op_node("mkdir -p "
+                                                 f"{self.mountpoint}",
+                                                 client)
 
     def _add_brick_rebalance(self):
-        """Create files,Perform Add brick and wait for rebalance to complete"""
+        """
+        Create files,Perform Add brick and wait for rebalance to complete
+        """
         # Create files on mount point using dd command
-        cmd = ('cd %s;for i in {1..100000};'
-               'do dd if=/dev/urandom bs=1024 count=1 of=file$i;done;'
-               % (self.mounts[0].mountpoint))
-        ret, _, _ = g.run(self.mounts[0].client_system, cmd)
-        self.assertEqual(ret, 0, "Failed to createfiles on mountpoint")
-        g.log.info("Successfully created files on mountpoint")
+        cmd = (f"cd {self.mountpoint};for i in "
+               "{1..10000};do dd if=/dev/urandom bs=1024 count=1 "
+               "of=file$i;done;")
+        self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Add brick to volume
-        ret = expand_volume(self.mnode, self.volname, self.servers,
-                            self.all_servers_info)
-        self.assertTrue(ret, "Failed to add brick on volume %s"
-                        % self.volname)
+        if not self.redant.expand_volume(self.server_list[0], self.vol_name,
+                                         self.server_list, self.brick_roots):
+            raise Exception("Failed to expand volume")
 
         # Trigger rebalance and wait for it to complete
-        ret, _, _ = rebalance_start(self.mnode, self.volname,
+        self.redant.rebalance_start(self.vol_name, self.server_list[0],
                                     force=True)
-        self.assertEqual(ret, 0, "Failed to start rebalance on the volume %s"
-                         % self.volname)
 
         # Wait for rebalance to complete
-        ret = wait_for_rebalance_to_complete(self.mnode, self.volname,
-                                             timeout=1200)
-        self.assertTrue(ret, "Rebalance is not yet complete on the volume "
-                             "%s" % self.volname)
-        g.log.info("Rebalance successfully completed")
+        if not self.redant.wait_for_rebalance_to_complete(self.vol_name,
+                                                          self.server_list[0],
+                                                          timeout=1200):
+            raise Exception("Rebalance is not yet complete on the volume "
+                            f"{self.vol_name}")
 
-    def test_auth_allow_with_rebalance(self):
+    def run_test(self, redant):
         """
         Validating the FUSE authentication volume options with rebalance
         Steps:
@@ -109,47 +80,35 @@ class FuseAuthAllow(GlusterBaseClass):
         8. Repeat steps from 3 to 7
         """
         # Setting authentication on volume for client1 using ip
-        auth_dict = {'all': [self.mounts[0].client_system]}
-        ret = set_auth_allow(self.volname, self.mnode, auth_dict)
-        self.assertTrue(ret, "Failed to set authentication")
+        auth_dict = {'all': [self.client_list[0]]}
+        if not redant.set_auth_allow(self.vol_name, self.server_list[0],
+                                     auth_dict):
+            raise Exception("Failed to set authentication")
 
         # Mounting volume on client1
-        self._authenticated_mount(self.mounts[0])
+        redant.authenticated_mount(self.vol_name, self.server_list[0],
+                                   self.mountpoint, self.client_list[0])
 
         # Create files,perform add-brick,trigger rebalance
         self._add_brick_rebalance()
 
         # Unmount volume from client1
-        ret = self.mounts[0].unmount()
-        self.assertTrue(ret, ("Failed to unmount volume %s from client %s"
-                              % (self.volname, self.mounts[0].client_system)))
+        redant.volume_unmount(self.vol_name, self.mountpoint,
+                              self.client_list[0])
 
         # Obtain hostname of client1
-        ret, hostname_client1, _ = g.run(self.mounts[0].client_system,
-                                         "hostname")
-        self.assertEqual(ret, 0, ("Failed to obtain hostname of client %s"
-                                  % self.mounts[0].client_system))
-        g.log.info("Obtained hostname of client. IP- %s, hostname- %s",
-                   self.mounts[0].client_system, hostname_client1.strip())
+        ret = redant.execute_abstract_op_node("hostname", self.client_list[0])
+        hostname_client1 = ret['msg'][0].rstrip('\n')
 
         # Setting authentication on volume for client1 using hostname
-        auth_dict = {'all': [hostname_client1.strip()]}
-        ret = set_auth_allow(self.volname, self.mnode, auth_dict)
-        self.assertTrue(ret, "Failed to set authentication")
+        auth_dict = {'all': [hostname_client1]}
+        if not redant.set_auth_allow(self.vol_name, self.server_list[0],
+                                     auth_dict):
+            raise Exception("Failed to set authentication")
 
         # Mounting volume on client1
-        self._authenticated_mount(self.mounts[0])
+        self.authenticated_mount(self.vol_name, self.server_list[0],
+                                 self.mountpoint, self.client_list[0])
 
         # Create files,perform add-brick and trigger rebalance
         self._add_brick_rebalance()
-
-    def tearDown(self):
-        """
-        Cleanup volume
-        """
-        ret = self.cleanup_volume()
-        if not ret:
-            raise ExecutionError("Failed to cleanup volume.")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
