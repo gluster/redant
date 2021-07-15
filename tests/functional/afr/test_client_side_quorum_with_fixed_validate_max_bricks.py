498a60c426b1bd2c10e2424edd12f1c3b52371cd
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-07-15 17:43:24 +0530

    Migrated test_client_side_quorum_with_fixed_validate_max_bricks (#830)
    * Delete quorum method from old file. Added fixes for flake8
    
    Change-Id: I2acf835a4cf7301c64c4c8a9423f78672cdf9aa4
    Signed-off-by: Vitalii Koriakov <vkoriako@redhat.com>
    
    * Fix AFR test case tearDown and library import
    
    The test case 'test_client_side_quorum_with_fixed_validate_max_bricks'
    does not have a tearDown part where the volume options which have been
    set inside the test case have not been reset to default.
    
    The library function 'set_volume_options' was being imported from a
    wrong library. This fix includes this change along with the tearDown
    steps.
    
    Change-Id: Ic57494e7a7e8a25303b7979f98cc2dfbc9a7d7b6
    Signed-off-by: Vinayak Papnoi <vpapnoi@redhat.com>
    
    * Add py3 support for tests in 'tests/functional/afr'
    
    Change-Id: Ic14be81f1cd42c470d2bb5c15505fc1bc168a393
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path/script_abs_path(Part 4)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I1df0324dac2da5aad4064cc72ef77dcb5bf67e4f
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated test_client_side_quorum_with_fixed_validate_max_bricks
    
    Migrated the [test
    case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/afr/test_client_side_quorum_with_fixed_validate_max_bricks.py)
    
    Updates: #292
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * removed unwanted logs and modified the cond
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * removed unwanted logs and modified the cond
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: Vitalii Koriakov <vkoriako@redhat.com>
    Co-authored-by: Vinayak Papnoi <vpapnoi@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/afr/test_client_side_quorum_with_fixed_validate_max_bricks.py
@@ -0,0 +1,62 @@
+"""
+Copyright (C) 2016-2019  Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test Cases in this module tests the client side quorum.
+"""
+# disruptive;rep,dist-rep
+# TODO: nfs, cifs
+
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Test Script with Client Side Quorum with fixed should validate
+        maximum number of bricks to accept
+
+        * set cluster quorum to fixed
+        * set cluster.quorum-count to higher number which is greater than
+          number of replicas in a sub-voulme
+        * Above step should fail
+        """
+        # set cluster.quorum-type to fixed
+        options = {"cluster.quorum-type": "fixed"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
+        # get the subvolumes
+        subvols_list = redant.get_subvols(self.vol_name, self.server_list[0])
+
+        # get the number of bricks in replica set
+        num_bricks_in_subvol = len(subvols_list[0])
+
+        # set cluster.quorum-count to higher value than the number
+        # of bricks in replica set
+        start_range = num_bricks_in_subvol + 1
+        end_range = num_bricks_in_subvol + 30
+        for i in range(start_range, end_range):
+            options = {"cluster.quorum-count": f"{i}"}
+            ret = redant.set_volume_options(self.vol_name, options,
+                                            self.server_list[0],
+                                            excep=False)
+            if ret['msg']['opRet'] == '0':
+                raise Exception(f"Unexpected: Able to set {options} "
+                                f"for volume {self.vol_name}, quorum-count"
+                                " should not be greater than number of"
+                                " bricks in replica set")
