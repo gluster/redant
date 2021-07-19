0bd96fa3831b74ffe84977cbc0787978f7880162
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-19 11:38:09 +0530

    added the rest of the logic
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/afr/test_shd_should_not_crash_executed_heal_info.py
+++ tests/functional/afr/test_shd_should_not_crash_executed_heal_info.py
@@ -21,9 +21,23 @@ Description:
 
 # disruptive;rep
 
+import traceback
 from tests.d_parent_test import DParentTest
 
+
 class TestCase(DParentTest):
+
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
+            if not ret:
+                raise Exception("IO failed on some clients")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
     def run_test(self, redant):
         """
@@ -49,150 +63,108 @@ class TestCase(DParentTest):
         redant.set_volume_options(self.vol_name, options,
                                   self.server_list[0])
 
-        # # Creating files on client side
-        # for mount_obj in self.mounts:
-        #     g.log.info("Generating data for %s:%s",
-        #                mount_obj.client_system, mount_obj.mountpoint)
-        #     # Create files
-        #     g.log.info('Creating files...')
-        #     command = ("/usr/bin/env python %s create_files -f 10 "
-        #                "--fixed-file-size 1M %s" % (
-        #                    self.script_upload_path,
-        #                    mount_obj.mountpoint))
+        # Creating files on client side
+        for mount_obj in self.mounts:
+            # Create files
+            redant.logger.info(f"Starting IO on {mount_obj['client']}:"
+                               f"{mount_obj['mountpath']}")
+            proc = redant.create_files(num_files=10,
+                                       fix_fil_size="1M",
+                                       path=mount_obj['mountpath'],
+                                       node=mount_obj['client'])
 
-        #     proc = g.run_async(mount_obj.client_system, command,
-        #                        user=mount_obj.user)
-        #     self.all_mounts_procs.append(proc)
-        # self.io_validation_complete = False
+            self.all_mounts_procs.append(proc)
 
-        # # Validate IO
-        # self.assertTrue(
-        #     validate_io_procs(self.all_mounts_procs, self.mounts),
-        #     "IO failed on some of the clients"
-        # )
-        # self.io_validation_complete = True
+        # Validate IO
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("IO validation failed")
 
-        # # Bring brick0 offline
-        # g.log.info('Bringing bricks %s offline', bricks_list[0])
-        # ret = bring_bricks_offline(self.volname, bricks_list[0])
-        # self.assertTrue(ret, 'Failed to bring bricks %s offline'
-        #                 % bricks_list[0])
+        # Bring brick0 offline
+        redant.bring_bricks_offline(self.vol_name, bricks_list[0])
 
-        # ret = are_bricks_offline(self.mnode, self.volname,
-        #                          [bricks_list[0]])
-        # self.assertTrue(ret, 'Bricks %s are not offline'
-        #                 % bricks_list[0])
-        # g.log.info('Bringing bricks %s offline is successful',
-        #            bricks_list[0])
+        if not redant.are_bricks_offline(self.vol_name,
+                                         [bricks_list[0]],
+                                         self.server_list[0]):
+            raise Exception(f"Bricks {bricks_list[0]} are not offline")
 
-        # # Creating files on client side
-        # number_of_files_one_brick_off = '1000'
-        # self.all_mounts_procs = []
-        # for mount_obj in self.mounts:
-        #     g.log.info("Generating data for %s:%s",
-        #                mount_obj.client_system, mount_obj.mountpoint)
-        #     # Create files
-        #     g.log.info('Creating files...')
-        #     command = ("/usr/bin/env python %s create_files "
-        #                "-f %s "
-        #                "--fixed-file-size 1k "
-        #                "--base-file-name new_file "
-        #                "%s"
-        #                % (self.script_upload_path,
-        #                   number_of_files_one_brick_off,
-        #                   mount_obj.mountpoint))
+        # Creating files on client side
+        number_of_files_one_brick_off = 1000
+        self.all_mounts_procs = []
+        for mount_obj in self.mounts:
+            # Create files
+            redant.logger.info(f"Starting IO on {mount_obj['client']}:"
+                               f"{mount_obj['mountpath']}")
+            proc = redant.create_files(num_files=number_of_files_one_brick_off,
+                                       fix_fil_size="1k",
+                                       path=mount_obj['mountpath'],
+                                       node=mount_obj['client'],
+                                       base_file_name="new_file")
 
-        #     proc = g.run_async(mount_obj.client_system, command,
-        #                        user=mount_obj.user)
-        #     self.all_mounts_procs.append(proc)
-        # self.io_validation_complete = False
+            self.all_mounts_procs.append(proc)
 
-        # # Validate IO
-        # self.assertTrue(
-        #     validate_io_procs(self.all_mounts_procs, self.mounts),
-        #     "IO failed on some of the clients"
-        # )
-        # self.io_validation_complete = True
+        # Validate IO
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("IO validation failed")
 
-        # # Get heal info
-        # g.log.info("Getting heal info...")
-        # heal_info_data = get_heal_info_summary(self.mnode, self.volname)
-        # self.assertIsNotNone(heal_info_data, 'Failed to get heal info.')
-        # g.log.info('Success in getting heal info')
+        # Get heal info
+        heal_info_data = redant.get_heal_info_summary(self.server_list[0],
+                                                      self.vol_name)
+        if heal_info_data is None:
+            raise Exception("Failed to get heal info summary")
 
-        # # Check quantity of file pending heal
-        # for brick in bricks_list[1:]:
-        #     self.assertEqual(heal_info_data[brick]['numberOfEntries'],
-        #                      str(int(number_of_files_one_brick_off)+1),
-        #                      'Number of files pending heal is not correct')
+        # Check quantity of file pending heal
+        for brick in bricks_list[1:]:
+            if heal_info_data[brick]['numberOfEntries'] != '1001':
+                raise Exception("Number of files pending heal is incorrect")
 
-        # # Setting options
-        # g.log.info('Setting options...')
-        # options = {"performance.enable-least-priority": "enable"}
-        # ret = set_volume_options(self.mnode, self.volname, options)
-        # self.assertTrue(ret, 'Failed to set options %s' % options)
-        # g.log.info("Successfully set %s for volume %s",
-        #            options, self.volname)
+        # Setting options
+        options = {"performance.enable-least-priority": "enable"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
 
-        # # Bring brick1 offline
-        # g.log.info('Bringing bricks %s offline', bricks_list[1])
-        # ret = bring_bricks_offline(self.volname, bricks_list[1])
-        # self.assertTrue(ret, 'Failed to bring bricks %s offline'
-        #                 % bricks_list[1])
+        # Bring brick1 offline
+        redant.bring_bricks_offline(self.vol_name, bricks_list[1])
 
-        # ret = are_bricks_offline(self.mnode, self.volname,
-        #                          [bricks_list[1]])
-        # self.assertTrue(ret, 'Bricks %s are not offline'
-        #                 % bricks_list[1])
-        # g.log.info('Bringing bricks %s offline is successful',
-        #            bricks_list[1])
+        if not redant.are_bricks_offline(self.vol_name,
+                                         [bricks_list[1]],
+                                         self.server_list[0]):
+            raise Exception(f"Brick {bricks_list[1]} is not offline")
 
-        # # Setting options
-        # g.log.info('Setting options...')
-        # options = {"quorum-type": "fixed"}
-        # ret = set_volume_options(self.mnode, self.volname, options)
-        # self.assertTrue(ret, 'Failed to set options %s' % options)
-        # g.log.info("Successfully set %s for volume %s",
-        #            options, self.volname)
+        # Setting options
+        options = {"quorum-type": "fixed"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
 
-        # # Creating files on client side
-        # number_of_files_two_brick_off = '100'
-        # self.all_mounts_procs = []
-        # for mount_obj in self.mounts:
-        #     g.log.info("Generating data for %s:%s",
-        #                mount_obj.client_system, mount_obj.mountpoint)
-        #     # Create files
-        #     g.log.info('Creating files...')
-        #     command = ("/usr/bin/env python %s create_files "
-        #                "-f %s "
-        #                "--fixed-file-size 1k "
-        #                "--base-file-name new_new_file "
-        #                "%s"
-        #                % (self.script_upload_path,
-        #                   number_of_files_two_brick_off,
-        #                   mount_obj.mountpoint))
+        # Creating files on client side
+        number_of_files_two_brick_off = 100
+        self.all_mounts_procs = []
+        for mount_obj in self.mounts:
+            redant.logger.info(f"Starting IO on {mount_obj['client']}:"
+                               f"{mount_obj['mountpath']}")
+            proc = redant.create_files(num_files=number_of_files_two_brick_off,
+                                       fix_fil_size="1k",
+                                       path=mount_obj['mountpath'],
+                                       node=mount_obj['client'],
+                                       base_file_name="new_new_file")
 
-        #     proc = g.run_async(mount_obj.client_system, command,
-        #                        user=mount_obj.user)
-        #     self.all_mounts_procs.append(proc)
-        # self.io_validation_complete = False
+            self.all_mounts_procs.append(proc)
 
-        # # Validate IO
-        # self.assertTrue(
-        #     validate_io_procs(self.all_mounts_procs, self.mounts),
-        #     "IO failed on some of the clients"
-        # )
-        # self.io_validation_complete = True
+        # Validate IO
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("IO validation failed")
 
-        # # Get heal info
-        # g.log.info("Getting heal info...")
-        # heal_info_data = get_heal_info_summary(self.mnode, self.volname)
-        # self.assertIsNotNone(heal_info_data, 'Failed to get heal info.')
-        # g.log.info('Success in getting heal info')
+        # Get heal info
+        heal_info_data = redant.get_heal_info_summary(self.server_list[0],
+                                                      self.vol_name)
+        if heal_info_data is None:
+            raise Exception("Failed to get heal info summary")
 
-        # # Check quantity of file pending heal
-        # number_of_files_to_check = str(int(number_of_files_one_brick_off) +
-        #                                int(number_of_files_two_brick_off) + 1)
-        # self.assertEqual(heal_info_data[bricks_list[-1]]['numberOfEntries'],
-        #                  number_of_files_to_check,
-        #                  'Number of files pending heal is not correct')
+        # Check quantity of file pending heal
+        number_of_files_to_check = (number_of_files_one_brick_off
+                                    + number_of_files_two_brick_off + 1)
+        if (heal_info_data[bricks_list[-1]]['numberOfEntries']
+           != str(number_of_files_to_check)):
+            raise Exception('Number of files pending heal is not correct')
