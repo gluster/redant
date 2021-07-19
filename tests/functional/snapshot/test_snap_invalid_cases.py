1bc0d3fd004b3505cb37dcfb0973b755b11b7db6
srijan-sivakumar <ssivakum@redhat.com>
2021-07-19 16:41:26 +0530

    [TestMigration] Migrating test_snap_invlid_cases
    [Test](https://github.com/gluster/glusto-tests/blob/master/tests/functional/snapshot/test_snap_invalid_cases.py)
    
    Updates: #292
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- tests/functional/snapshot/test_snap_invalid_cases.py
+++ tests/functional/snapshot/test_snap_invalid_cases.py
@@ -1,137 +1,87 @@
-#  Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 """
+  Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 Description:
 
 Test Cases in this module tests the
 snapshot Status and Info for Invalid cases.
 
 """
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass
-from glustolibs.gluster.gluster_base_class import runs_on
-from glustolibs.gluster.snap_ops import (snap_create,
-                                         snap_status, snap_info)
+
+# disruptive;rep
+# disruptive;rep,dist,disp,dist-rep,dist-disp
+
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated', 'replicated', 'distributed',
-           'distributed-dispersed', 'dispersed'],
-          ['glusterfs', 'nfs', 'cifs']])
-class SnapshotstatusInvalidcases(GlusterBaseClass):
-    """
-    1. Create volumes
-    2. create multiple snapshots
-    3. Show status of non existing snapshots
-    4. Show info of non existing snapshots
-    5. Show status of snaps of non-existing volumes
-    6. Show info of snaps of non-existing volume
-    7. status of snapshots with invalid command
-    8. Info of snapshots with invalid command
-    """
+class TestCase(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.get_super_method(cls, 'setUpClass')()
-        cls.snap5 = "snap5"
-        cls.snap1 = "snap1"
-        cls.volname1 = "volume1"
+    def run_test(self, redant):
+        """
+        1. Create volumes
+        2. create multiple snapshots
+        3. Show status of non existing snapshots
+        4. Show info of non existing snapshots
+        5. Show status of snaps of non-existing volumes
+        6. Show info of snaps of non-existing volume
+        7. status of snapshots with invalid command
+        8. Info of snapshots with invalid command
+        """
 
-    def setUp(self):
-
-        # SettingUp volume and Mounting the volume
-        self.get_super_method(self, 'setUp')()
-        g.log.info("Starting to SetUp Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to setup volume %s" % self.volname)
-        g.log.info("Volume %s has been setup successfully", self.volname)
-
-    def test_snap_invalid_case(self):
+        self.snap5 = "snap5"
+        self.snap1 = "snap1"
+        self.volname1 = "volume1"
 
         # Creating snapshot:
-        g.log.info("Starting to Create snapshot")
         for count in range(1, 3):
-            self.snap = "snap%s" % count
-            ret = snap_create(self.mnode, self.volname, self.snap)
-            self.assertTrue(ret, ("Failed to create snapshot for volume %s"
-                                  % self.volname))
-        g.log.info("Snapshot %s created successfully"
-                   " for volume %s", self.snap, self.volname)
+            self.snap_name = f"snap{count}"
+            redant.snap_create(self.vol_name, self.snap_name,
+                               self.server_list[0])
 
         # Check snapshot info for non-existing snapshot
-        g.log.info("Checking snapshot info")
-        ret, _, _ = snap_info(self.mnode, self.snap5)
-        self.assertEqual(ret, 1, "Unexpected: Successful in "
-                         "getting information for"
-                         "non-existing %s snapshot" % self.snap5)
-        g.log.info("Expected result: failed to get information"
-                   " for non-existing %s snapshot", self.snap5)
+        ret = redant.get_snap_info_by_snapname(self.snap5,
+                                               self.server_list[0],
+                                               excep=False)
+        if ret is not None:
+            raise Exception(f"Snap info for {self.snap5}, a non existant snap"
+                            " didn't fail.")
 
         # Check snapshot status for non-existing snapshot
-        g.log.info("Checking snapshot status")
-        ret, _, _ = snap_status(self.mnode, self.snap5)
-        self.assertEqual(ret, 1, "Unexpected: Successful in getting "
-                         "status for non-existing "
-                         "%s snapshot" % self.snap5)
-        g.log.info("Expected result: failed to get status"
-                   " for non-existing %s snapshot", self.snap5)
+        ret = redant.get_snap_status_by_snapname(self.snap5,
+                                                 self.server_list[0])
+        if ret is not None:
+            raise Exception(f"Snap status for {self.snap5}, a non existant "
+                            "snap didn't fail")
 
         # Check snapshot info for non-existing volume
-        g.log.info("Checking snapshot info")
-        ret, _, _ = snap_info(self.mnode, self.volname1)
-        self.assertEqual(ret, 1, "Unexpected: Successful in getting "
-                         "information for"
-                         "non-existing %s volume" % self.volname1)
-        g.log.info("Expected result: failed to get information"
-                   " for non-existing %s volume", self.volname1)
-
-        # Check snapshot status for non-existing volume
-        g.log.info("Checking snapshot status")
-        ret, _, _ = snap_info(self.mnode, self.volname1)
-        self.assertEqual(ret, 1, "Unexpected: Successful in getting "
-                         "status for non-existing "
-                         "%s volume" % self.volname1)
-        g.log.info("Expected result: Need to fail to get status"
-                   " for non-existing %s volume", self.volname1)
+        ret = redant.get_snap_info_by_volname(self.volname1,
+                                              self.server_list[0], excep=False)
+        if ret is not None:
+            raise Exception(f"Snap info for {self.volname1}, a non existant"
+                            " volume didn't fail.")
 
         # Invalid command
-        g.log.info("Passing invalid status command")
         cmd = "gluster snapshot snap1 status"
-        ret, _, _ = g.run(self.mnode, cmd)
-        self.assertEqual(ret, 1, "Unexpected: Snapshot status"
-                         " command Successful even with Invalid"
-                         " command")
-        g.log.info("Expected result: snapshot status command failed")
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception(f"The command {cmd} should've failed.")
 
         # Invalid command
-        g.log.info("Passing invalid info command")
         cmd = "gluster snapshot snap1 info"
-        ret, _, _ = g.run(self.mnode, cmd)
-        self.assertEqual(ret, 1, "Unexpected: Snapshot info "
-                         "command Successful even with Invalid "
-                         " command")
-        g.log.info("Expected result: snapshot information command Failed")
-
-    def tearDown(self):
-
-        # Unmount and cleanup original volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception(f"The command {cmd} should've failed.")
