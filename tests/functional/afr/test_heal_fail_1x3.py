de8d5828f58597e09e42b78d77ee5dc4e174cd1c
Ayush Ujjwal <77244483+aujjwal-redhat@users.noreply.github.com>
2021-07-22 11:40:43 +0530

    Migrated test_heal_fail_1x3 (#819)
    * Added an afr test case: test gfid heal on 1x3 vol
    
    Description: Test case which checks for gfid self heal
    of a file on 1x3 replicated volume
    
    Change-Id: I3bad7c16435bd99fa3f5b812c65970bebdbd18ac
    Signed-off-by: Kartik_Burmee <kburmee@redhat.com>
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Add py3 support for tests in 'tests/functional/afr'
    
    Change-Id: Ic14be81f1cd42c470d2bb5c15505fc1bc168a393
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path/script_abs_path(Part 4)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I1df0324dac2da5aad4064cc72ef77dcb5bf67e4f
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize afr testcases
    
    Added steps to reset volume and resolved teardown class
    cleanup failures.
    
    Change-Id: I06b0ed8810c9b064fd2ee7c0bfd261928d8c07db
    
    * Remove python version dependency(Part 1)
    
    sys library was added to all the testcases to fetch
    the `sys.version_info.major` which fetches the version
    of python with which glusto and glusto-tests is installed
    and runs the I/O script i.e file_dir_ops.py with that
    version of python but this creates a problem as older jobs
    running on older platforms won't run the way they use to,
    like if the older platform had python2 by default and
    we are running it tests from a slave which
    has python3 it'll fails and visa-versa.
    
    The problem is introduced due the below code:
    ```
    cmd = ("/usr/bin/env python%d %s create_deep_dirs_with_files "
           "--dirname-start-num 10 --dir-depth 1 --dir-length 1 "
           "--max-num-of-dirs 1 --num-of-files 5 %s" % (
           sys.version_info.major, self.script_upload_path,
           self.mounts[0].mountpoint))
    ```
    
    The solution to this problem is to change `python%d`
    to `python` which would enable the code to run with
    whatever version of python is avaliable on that client
    this would enable us to run any version of framework
    with both the older and latest platforms.
    
    Change-Id: I7c8200a7578f03c482f0c6a91832b8c0fdb33e77
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated test_heal_fail_1x3
    
    Migrated the [test case](https://github.com/gluster/glusto-tests/blob/HEAD/tests/functional/afr/test_heal_fail_1x3.py)
    
    Updates: #292
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * lint fix
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    * added arequal logic
    
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>
    
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/afr/test_heal_fail_1x3.py
@@ -0,0 +1,134 @@
+"""
+Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    This test case verifies the gfid self-heal on a 1x3
+    replicate volume.
+"""
+
+# disruptive;rep
+# TODO: nfs, cifs
+
+import traceback
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
+            if not ret:
+                raise Exception("IO failed on some of the clients")
+
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
+        1. file created at mount point
+        2. 2 bricks brought down
+        3. file deleted
+        4. created a new file from the mount point
+        5. all bricks brought online
+        6. check if gfid worked correctly
+        """
+
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
+        options = {"cluster.quorum-type": "fixed"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
+
+        for mount_obj in self.mounts:
+            proc = redant.create_files('10k', mount_obj['mountpath'],
+                                       mount_obj['client'],
+                                       base_file_name='test_file')
+            self.all_mounts_procs.append(proc)
+        # Validate I/O
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("Failed to validate IO")
+
+        # getting list of all bricks
+        all_bricks = redant.get_all_bricks(self.vol_name,
+                                           self.server_list[0])
+        if all_bricks is None:
+            raise Exception("Failed to get all the bricks")
+
+        redant.bring_bricks_offline(self.vol_name, all_bricks[:2])
+        if not redant.are_bricks_offline(self.vol_name, all_bricks[:2],
+                                         self.server_list[0]):
+            raise Exception(f"Bricks {all_bricks[:2]} not all offline.")
+
+        command = f"rm -f {self.mounts[0]['mountpath']}/test_file0.txt"
+        redant.logger.info("Deleting the file on the mountpoint")
+        redant.execute_abstract_op_node(command,
+                                        self.mounts[0]['client'])
+
+        self.all_mounts_procs = []
+        for mount_obj in self.mounts:
+            proc = redant.create_files('1M', mount_obj['mountpath'],
+                                       mount_obj['client'],
+                                       base_file_name='test_file')
+            self.all_mounts_procs.append(proc)
+        # Validate I/O
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("Failed to validate IO")
+
+        redant.bring_bricks_online(self.vol_name, self.server_list,
+                                   all_bricks[:2])
+        if not redant.are_bricks_online(self.vol_name, all_bricks[:2],
+                                        self.server_list[0]):
+            raise Exception(f"Bricks {all_bricks[:2]} not online.")
+
+        redant.get_file_stat(self.mounts[0]['client'],
+                             f"{self.mounts[0]['mountpath']}/test_file0.txt")
+
+        # check if heal is complete
+        if not redant.is_heal_complete(self.server_list[0],
+                                       self.vol_name):
+            raise Exception("Heal not yet finished")
+
+        redant.logger.info("checking if the areequal checksum of all "
+                           "the bricks in the subvol match")
+        checksum_list = []
+        for brick in all_bricks:
+            node, brick_path = brick.split(':')
+            command = f"arequal-checksum {brick_path}/.glusterfs/landfill"
+            ret = redant.execute_abstract_op_node(command, node)
+
+            checksum_list.append(ret['msg'])
+
+            # checking file size of healed file on each brick to verify
+            # correctness of choice for sink and source
+            stat_dict = (redant.
+                         get_file_stat(node,
+                                       f"{brick_path}/test_file0.txt")['msg'])
+            if stat_dict['st_size'] != 1048576:
+                raise Exception("File size of healed file is different"
+                                " than expected.")
+
+        if not all(val == checksum_list[0] for val in checksum_list):
+            raise Exception("the arequal checksum of all bricks is"
+                            "not same")
