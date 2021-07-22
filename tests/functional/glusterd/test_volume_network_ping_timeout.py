2d84fb8b7a9c8bf893cf7d913bd11c666bcc18b2
nishith-vihar <77044911+nishith-vihar@users.noreply.github.com>
2021-07-22 18:27:41 +0530

    Migration of test_volume_network_ping_timeout.py (#551)
    * Test case for Verifying checksum of mountpoint
    
    before and after changing network ping timeout
    
    Desc:
    
    Change-Id: I8f3636cd899e536d2401a8cd93b98bf66ceea0f7
    Signed-off-by: Rajesh Madaka <rmadaka@redhat.com>
    
    * Fix spelling mistake across the codebase
    
    Change-Id: I46fc2feffe6443af6913785d67bf310838532421
    
    * Add py3 support in 'tests/functional/glusterd'. Part 2
    
    Change-Id: I1395e14d8d0aa0cc6097e51c64262fb481f36f05
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 2)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I0d2eeb978c6757d6d910ebfe21b07811bf74b80a
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 5)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I5319ce497ca3359e0e7dbd9ece481bada1ee2205
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migration of test_volume_network_ping_timeout.py
    
    Updates: #292
    Fixes: #539
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>
    
    * Rebased and changed TC logic
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Correcting merge issues
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    Co-authored-by: Rajesh Madaka <rmadaka@redhat.com>
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: srijan-sivakumar <ssivakum@redhat.com>

---
+++ tests/functional/glusterd/test_volume_network_ping_timeout.py
@@ -0,0 +1,98 @@
+"""
+  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+  Description:
+        Test Cases in this module related to Glusterd network ping timeout
+        of the volume.
+"""
+
+import re
+from tests.nd_parent_test import NdParentTest
+
+
+# nonDisruptive;dist,rep,dist-rep,disp,dist-disp
+class TestCase(NdParentTest):
+
+    def run_test(self, redant):
+        """
+        1) Create Volume
+        2) Mount the Volume
+        3) Create some files on mount point
+        4) calculate the checksum of Mount point
+        5) Check the default network ping timeout of the volume.
+        6) Change network ping timeout to some other value
+        7) calculate checksum again
+        8) checksum should be same without remounting the volume.
+        """
+
+        # run IOs
+        redant.logger.info("Starting IO on all mounts...")
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        for mount in self.mounts:
+            redant.logger.info(f"Starting IO on {mount['client']}:"
+                               f"{mount['mountpath']}")
+            proc = redant.create_files(num_files=1, fix_fil_size="1k",
+                                       path=mount['mountpath'],
+                                       node=mount['client'],
+                                       base_file_name="test_file")
+            self.all_mounts_procs.append(proc)
+
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO operations failed on some"
+                            " or all of the clients")
+
+        # Checksum calculation of mount point before
+        # changing network.ping-timeout
+        before_checksum = redant.collect_mounts_arequal(self.mounts)
+
+        # List all files and dirs created
+        redant.logger.info("List all files and directories:")
+        ret = redant.list_all_files_and_dirs_mounts(self.mounts)
+        if not ret:
+            raise Exception("Failed to list all files and dirs")
+
+        # performing gluster volume get volname all and
+        # getting network ping time out value
+        volume_options = redant.get_volume_options(self.vol_name,
+                                                   node=self.server_list[0])
+        ret = False
+        if re.search(r'\b42\b', volume_options['network.ping-timeout']):
+            ret = True
+        if not ret:
+            raise Exception("network ping time out value is not correct")
+
+        # Changing network ping time out value to specific volume
+        networking_ops = {'network.ping-timeout': '12'}
+        ret = redant.set_volume_options(self.vol_name, networking_ops,
+                                        self.server_list[0])
+
+        # Checksum calculation of mount point after
+        # changing network.ping-timeout
+        after_checksum = redant.collect_mounts_arequal(self.mounts)
+
+        # comparing list of checksums of mountpoints before and after
+        # network.ping-timeout change
+        if before_checksum != after_checksum:
+            raise Exception("Checksum not same before and after "
+                            "network.ping-timeout change")
+
+        # List all files and dirs created
+        redant.logger.info("List all files and directories:")
+        ret = redant.list_all_files_and_dirs_mounts(self.mounts)
+        if not ret:
+            raise Exception("Failed to list all files and dirs")
