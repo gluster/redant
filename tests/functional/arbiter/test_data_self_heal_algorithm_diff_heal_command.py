6b6c78f0a6527f18da5effa38a260f555c51903b
nik-redhat <nladha@redhat.com>
2021-07-23 16:49:13 +0530

    Migrated TC test_data_self_heal_algorithm_diff_heal_command
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/arbiter/test_data_self_heal_algorithm_diff_heal_command.py
+++ tests/functional/arbiter/test_data_self_heal_algorithm_diff_heal_command.py
@@ -1,104 +1,41 @@
-#  Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
 
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass, runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.volume_ops import set_volume_options
-from glustolibs.gluster.volume_libs import expand_volume
-from glustolibs.gluster.brick_libs import (select_bricks_to_bring_offline,
-                                           bring_bricks_offline,
-                                           bring_bricks_online,
-                                           are_bricks_offline)
-from glustolibs.gluster.heal_libs import (
-    monitor_heal_completion,
-    is_heal_complete,
-    is_volume_in_split_brain,
-    is_shd_daemonized)
-from glustolibs.gluster.rebalance_ops import (rebalance_start,
-                                              wait_for_rebalance_to_complete)
-from glustolibs.gluster.heal_ops import trigger_heal
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import (collect_mounts_arequal, validate_io_procs)
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to check data-self-heal-algorithm diff option when all other
+    data-self-heal options are turned off
+"""
+
+# disruptive;arb,dist-arb
+# TODO: NFS,CIFS
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['arbiter', 'distributed-arbiter'],
-          ['glusterfs', 'cifs', 'nfs']])
-class TestSelfHeal(GlusterBaseClass):
-    """
-    Description:
-        Arbiter Test cases related to
-        healing in default configuration of the volume
-    """
+class TestSelfHeal(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on mounts",
-                   cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup Volume and Mount Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-    def tearDown(self):
+    def run_test(self, redant):
         """
-        Cleanup and umount volume
-        """
-
-        # Cleanup and umount volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_data_self_heal_algorithm_diff_heal_command(self):
-        """
-        Test Volume Option - 'cluster.data-self-heal-algorithm' : 'diff'
-
         Description:
         - set the volume option
-        "metadata-self-heal": "off"
-        "entry-self-heal": "off"
-        "data-self-heal": "off"
-        "data-self-heal-algorithm": "diff"
-        "self-heal-daemon": "off"
+            "metadata-self-heal": "off"
+            "entry-self-heal": "off"
+            "data-self-heal": "off"
+            "data-self-heal-algorithm": "diff"
+            "self-heal-daemon": "off"
         - create IO
         - calculate arequal
         - bring down all bricks processes from selected set
@@ -112,164 +49,107 @@ class TestSelfHeal(GlusterBaseClass):
         - check if heal is completed
         - check for split-brain
         - calculate arequal and compare with arequal before bringing bricks
-        offline and after bringing bricks online
+          offline and after bringing bricks online
         """
-        # pylint: disable=too-many-branches,too-many-statements
         # Setting options
-        g.log.info('Setting options...')
         options = {"metadata-self-heal": "off",
                    "entry-self-heal": "off",
                    "data-self-heal": "off",
                    "data-self-heal-algorithm": "diff"}
-        ret = set_volume_options(self.mnode, self.volname, options)
-        self.assertTrue(ret, 'Failed to set options')
-        g.log.info("Options "
-                   "'metadata-self-heal', "
-                   "'entry-self-heal', "
-                   "'data-self-heal', "
-                   "'self-heal-daemon' "
-                   "are set to 'off',"
-                   "'data-self-heal-algorithm' "
-                   "is set to 'diff' successfully")
+        redant.set_volume_options(self.vol_name, options, self.server_list[0],
+                                  multi_option=True)
 
         # Creating files on client side
-        all_mounts_procs = []
-        g.log.info("Generating data for %s:%s",
-                   self.mounts[0].client_system, self.mounts[0].mountpoint)
-        # Creating files
-        command = "/usr/bin/env python %s create_files -f 100 %s" % (
-            self.script_upload_path,
-            self.mounts[0].mountpoint)
+        mount_dict = []
+        cmd = ("python3 /tmp/file_dir_ops.py create_files -f 100 "
+               f"{self.mountpoint}")
 
-        proc = g.run_async(self.mounts[0].client_system, command,
-                           user=self.mounts[0].user)
-        all_mounts_procs.append(proc)
+        proc = redant.execute_command_async(cmd, self.client_list[0])
+        mount_dict.append({
+            "client": self.client_list[0],
+            "mountpath": self.mountpoint
+        })
 
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
+        ret = self.redant.validate_io_procs(proc, mount_dict)
+        if not ret:
+            raise Exception("IO validation failed")
 
         # Setting options
-        g.log.info('Setting options...')
         options = {"self-heal-daemon": "off"}
-        ret = set_volume_options(self.mnode, self.volname, options)
-        self.assertTrue(ret, 'Failed to set options')
-        g.log.info("Option 'self-heal-daemon' is set to 'off' successfully")
+        redant.set_volume_options(self.vol_name, options, self.server_list[0])
 
         # Select bricks to bring offline
-        bricks_to_bring_offline_dict = (select_bricks_to_bring_offline(
-            self.mnode, self.volname))
-        bricks_to_bring_offline = bricks_to_bring_offline_dict['volume_bricks']
+        offline_brick_list = (redant.select_volume_bricks_to_bring_offline(
+                              self.vol_name, self.server_list[0]))
 
         # Bring brick offline
-        g.log.info('Bringing bricks %s offline...', bricks_to_bring_offline)
-        ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s offline' %
-                        bricks_to_bring_offline)
+        if not redant.bring_bricks_offline(self.vol_name, offline_brick_list):
+            raise Exception("Failed to bring bricks offline")
 
-        ret = are_bricks_offline(self.mnode, self.volname,
-                                 bricks_to_bring_offline)
-        self.assertTrue(ret, 'Bricks %s are not offline'
-                        % bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s offline is successful',
-                   bricks_to_bring_offline)
+        if not redant.are_bricks_offline(self.vol_name, offline_brick_list,
+                                         self.server_list[0]):
+            raise Exception(f"Bricks {offline_brick_list} are not offline")
 
         # Modify the data
-        all_mounts_procs = []
-        g.log.info("Modifying data for %s:%s",
-                   self.mounts[0].client_system, self.mounts[0].mountpoint)
-        command = ("/usr/bin/env python %s create_files -f 100 "
-                   "--fixed-file-size 1M %s" % (
-                       self.script_upload_path,
-                       self.mounts[0].mountpoint))
+        cmd = ("python3 /tmp/file_dir_ops.py create_files -f 100 "
+               f" --fixed-file-size 1M {self.mountpoint}")
 
-        proc = g.run_async(self.mounts[0].client_system, command,
-                           user=self.mounts[0].user)
-        all_mounts_procs.append(proc)
+        proc = redant.execute_command_async(cmd, self.client_list[0])
 
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
+        ret = self.redant.validate_io_procs(proc, mount_dict)
+        if not ret:
+            raise Exception("IO validation failed")
 
         # Get arequal before getting bricks online
-        g.log.info('Getting arequal before getting bricks online...')
-        ret, result_before_online = collect_mounts_arequal(self.mounts)
-        self.assertTrue(ret, 'Failed to get arequal')
-        g.log.info('Getting arequal before getting bricks online '
-                   'is successful')
+        result_before_online = redant.collect_mounts_arequal(mount_dict)
 
         # Bring brick online
-        g.log.info('Bringing bricks %s online...', bricks_to_bring_offline)
-        ret = bring_bricks_online(self.mnode, self.volname,
-                                  bricks_to_bring_offline)
-        self.assertTrue(ret, 'Failed to bring bricks %s online' %
-                        bricks_to_bring_offline)
-        g.log.info('Bringing bricks %s online is successful',
-                   bricks_to_bring_offline)
+        if not redant.bring_bricks_online(self.vol_name, self.server_list,
+                                          offline_brick_list):
+            raise Exception("Failed to bring bricks online")
 
         # Expand volume by adding bricks to the volume
-        g.log.info("Start adding bricks to volume...")
-        ret = expand_volume(self.mnode, self.volname, self.servers,
-                            self.all_servers_info)
-        self.assertTrue(ret, ("Failed to expand the volume when IO in "
-                              "progress on volume %s", self.volname))
-        g.log.info("Expanding volume is successful on volume %s", self.volname)
+        if not redant.expand_volume(self.server_list[0], self.vol_name,
+                                    self.server_list, self.brick_roots):
+            raise Exception("Failed to expand the volume when files present"
+                            " on mountpoint")
 
         # Do rebalance
-        ret, _, _ = rebalance_start(self.mnode, self.volname)
-        self.assertEqual(ret, 0, 'Failed to start rebalance')
-        g.log.info('Rebalance is started')
+        redant.rebalance_start(self.vol_name, self.server_list[0])
 
-        ret = wait_for_rebalance_to_complete(self.mnode, self.volname)
-        self.assertTrue(ret, 'Rebalance is not completed')
-        g.log.info('Rebalance is completed successfully')
+        if not redant.wait_for_rebalance_to_complete(self.vol_name,
+                                                     self.server_list[0]):
+            raise Exception("Rebalance is not completed")
 
         # Setting options
-        g.log.info('Setting options...')
         options = {"self-heal-daemon": "on"}
-        ret = set_volume_options(self.mnode, self.volname, options)
-        self.assertTrue(ret, 'Failed to set options')
-        g.log.info("Option 'self-heal-daemon' is set to 'on' successfully")
+        redant.set_volume_options(self.vol_name, options, self.server_list[0])
 
         # Wait for self-heal-daemons to be online
-        g.log.info("Waiting for self-heal-daemons to be online")
-        ret = is_shd_daemonized(self.all_servers)
-        self.assertTrue(ret, "Either No self heal daemon process found")
-        g.log.info("All self-heal-daemons are online")
+        if not redant.is_shd_daemonized(self.server_list):
+            raise Exception("Failed to wait for self-heal deamon to be "
+                            "online")
 
         # Start healing
-        ret = trigger_heal(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not started')
-        g.log.info('Healing is started')
+        if not redant.trigger_heal(self.vol_name, self.server_list[0]):
+            raise Exception("Heal is not started")
 
         # Monitor heal completion
-        ret = monitor_heal_completion(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal has not yet completed')
-
-        # Check if heal is completed
-        ret = is_heal_complete(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not complete')
-        g.log.info('Heal is completed successfully')
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name):
+            raise Exception("Heal has not yet completed")
 
         # Check for split-brain
-        ret = is_volume_in_split_brain(self.mnode, self.volname)
-        self.assertFalse(ret, 'Volume is in split-brain state')
-        g.log.info('Volume is not in split-brain state')
+        if redant.is_volume_in_split_brain(self.server_list[0],
+                                           self.vol_name):
+            raise Exception("Volume is in split-brain state")
 
         # Get arequal after getting bricks online
-        g.log.info('Getting arequal after getting bricks online...')
-        ret, result_after_online = collect_mounts_arequal(self.mounts)
-        self.assertTrue(ret, 'Failed to get arequal')
-        g.log.info('Getting arequal after getting bricks online '
-                   'is successful')
+        result_after_online = redant.collect_mounts_arequal(mount_dict)
 
         # Checking arequals before bringing bricks offline
         # and after bringing bricks online
-        self.assertEqual(sorted(result_before_online),
-                         sorted(result_after_online),
-                         'Checksums are not equal')
-        g.log.info('Checksums are equal')
+        if result_before_online != result_after_online:
+            raise Exception("Checksums are not equal")
