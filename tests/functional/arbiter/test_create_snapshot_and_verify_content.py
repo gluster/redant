875a2352e3292a43b6d6fc5e6730ad1d93406a3b
Nikhil Ladha <nladha@redhat.com>
2021-07-23 09:51:45 +0530

    Migrate TC test_create_snapshot_and_verify_content (#859)
    * Creating snapshot on arbiter volume and verifying the contents
    
    Change-Id: I21f3d8acce35acb852e36c2747d67bc8ed235e9d
    Signed-off-by: Vitalii Koriakov <vkoriako@redhat.com>
    
    * Add py3 support in 'tests/functional/arbiter'
    
    Change-Id: I5a60646b984557ed024cb4b3a8088ce7dfb7622c
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 3)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: Icf32bb20b7eaf2eabb07b59be813997a28872565
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize the existing content in Arbiter
    
    Change-Id: I2ba0c81dad41bdac704007bd1780b8a98cb50358
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Changing code used to create arbiter volumes
    
    As distributed-arbiter and arbiter weren't present before patch [1],
    arbiter and distributed-arbiter volumes were created by the hack show
    below where a distributed-replicated or replicated volume's configuration
    was modified to create an arbiter volume.
    ```
    @runs_on([['replicated', 'distributed-replicated'],
              ['glusterfs', 'nfs']])
    class TestSelfHeal(GlusterBaseClass):
       .................
    
       @classmethod
       def setUpClass(cls):
        ...............
    
         # Overriding the volume type to specifically test the volume
         # type Change from distributed-replicated to arbiter
         if cls.volume_type == "distributed-replicated":
             cls.volume['voltype'] = { 'type': 'distributed-replicated',
                                       'dist_count': 2,
                                       'replica_count': 3,
                                       'arbiter_count': 1,
                                       'transport': 'tcp'}
    ```
    Now this code is to be updated where we need to remove code which
    was used to override volume configuration and
    just add arbiter or distributed-arbiter in `@runs_on([],[])`
    as shown below:
    ```
    @runs_on([['replicated', 'distributed-arbiter'],
              ['glusterfs', 'nfs']])
    class TestSelfHeal(GlusterBaseClass):
    ```
    
    Links:
    [1] https://github.com/gluster/glusto-tests/commit/08b727842bc66603e3b8d1160ee4b15051b0cd20
    
    Change-Id: I4c44c2f3506bd0183fd991354fb723f8ec235a4b
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 2)
    
    Please refer to commit message of patch [1].
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: Ic0b3b1333ac7b1ae02f701943d49510e6d46c259
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrate TC test_create_snapshot_and_verify_content
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Use mount dict
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Update param type
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Add type for path
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Vitalii Koriakov <vkoriako@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/arbiter/test_create_snapshot_and_verify_content.py
@@ -0,0 +1,91 @@
+"""
+ Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to verify content on creating snapshots
+"""
+
+# disruptive;arb,dist-arb
+# TODO: nfs
+from tests.d_parent_test import DParentTest
+
+
+class TestArbiterSelfHeal(DParentTest):
+
+    def run_test(self, redant):
+        """
+        - Create an arbiter volume
+        - Create IO
+        - Calculate arequal of the mount point
+        - Take a snapshot of the volume
+        - Create new data on mount point
+        - Restore the snapshot
+        - Calculate arequal of the mount point
+        - Compare arequals
+        """
+        # Creating files on client side
+        proc = redant.create_deep_dirs_with_files(self.mountpoint, 1, 2, 2,
+                                                  2, 20, self.client_list[0])
+        mount_dict = {
+            "client": self.client_list[0],
+            "mountpath": self.mountpoint
+        }
+        ret = redant.validate_io_procs(proc, mount_dict)
+        if not ret:
+            raise Exception("IO validation failed")
+
+        # Get arequal before snapshot
+        arequal_before_snapshot = redant.collect_mounts_arequal(mount_dict)
+
+        # Create snapshot
+        snapshot_name = 'testsnap'
+        redant.snap_create(self.vol_name, snapshot_name, self.server_list[0])
+
+        # Add files on client side
+        proc = redant.create_deep_dirs_with_files(self.mountpoint, 1, 2, 2,
+                                                  2, 20, self.client_list[0])
+        ret = redant.validate_io_procs(proc, mount_dict)
+        if not ret:
+            raise Exception("IO validation failed")
+
+        # Stop the volume
+        redant.volume_stop(self.vol_name, self.server_list[0])
+
+        # Revert snapshot
+        redant.snap_restore(snapshot_name, self.server_list[0])
+
+        # Start the volume
+        redant.volume_start(self.vol_name, self.server_list[0])
+
+        # Wait for volume processes to be online
+        if not (redant.wait_for_volume_process_to_be_online(self.vol_name,
+                self.server_list[0], self.server_list)):
+            raise Exception("Failed to wait for volume processes to "
+                            "be online")
+
+        # Verify volume's all process are online
+        if not (redant.verify_all_process_of_volume_are_online(self.vol_name,
+                self.server_list[0])):
+            raise Exception("All process of volume are not online")
+
+        # Get arequal after restoring snapshot
+        arequal_after_restoring = redant.collect_mounts_arequal(mount_dict)
+
+        # Checking arequals before creating and after restoring snapshot
+        if arequal_before_snapshot != arequal_after_restoring:
+            raise Exception("Arequal before creating snapshot "
+                            "and after restoring snapshot are not equal")
