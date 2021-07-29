f661f62907275cf52e5d43d193cf75c037e327e2
Ayush Ujjwal <aujjwal@redhat.com>
2021-07-29 16:30:49 +0530

    removed trigger heal
    Signed-off-by: Ayush Ujjwal <aujjwal@redhat.com>

--- tests/functional/arbiter/test_handling_data_split_brain_of_files_heal_command.py
+++ tests/functional/arbiter/test_handling_data_split_brain_of_files_heal_command.py
@@ -193,16 +193,14 @@ class TestCase(DParentTest):
                                 self.mountpoint, client)
 
         # Start heal from mount point
-        # for mount_obj in self.mounts:
-        #     command = ("python3 /tmp/file_dir_ops.py "
-        #                f"read {mount_obj['mountpath']}")
-        #     redant.execute_abstract_op_node(command,
-        #                                     mount_obj['client'])
+        for mount_obj in self.mounts:
+            command = ("python3 /tmp/file_dir_ops.py "
+                       f"read {mount_obj['mountpath']}")
+            redant.execute_abstract_op_node(command,
+                                            mount_obj['client'])
         options = {"self-heal-daemon": "on"}
         redant.set_volume_options(self.vol_name, options,
                                   self.server_list[0])
-        if not redant.trigger_heal(self.vol_name, self.server_list[0]):
-            raise Exception("Failed to trigger heal.")
 
         # Monitor heal completion
         if not redant.monitor_heal_completion(self.server_list[0],
