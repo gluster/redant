c076b9a0419d223f1269eaec861b122ed7e2569d
Nikhil Ladha <nladha@redhat.com>
2021-08-01 19:34:47 +0530

    Migrated TC test_manual_heal_should_trigger_heal (#908)
    * Manual heal command should trigger heal of the file
    
    Change-Id: Ie685a2e60c19bc096c54034a6b2f7d4380441f3d
    Signed-off-by: Vitalii Koriakov <vkoriako@redhat.com>
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Modified the test per BZ Bug 1579758
    
    Change-Id: I8d15f482dbde12670975e32af685570a0eaa50b6
    Signed-off-by: Anees <anepatel@redhat.com>
    
    * Add py3 support for tests in 'tests/functional/afr'
    
    Change-Id: Ic14be81f1cd42c470d2bb5c15505fc1bc168a393
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path/script_abs_path(Part 4)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: I1df0324dac2da5aad4064cc72ef77dcb5bf67e4f
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 1)
    
    sys library was added to all the testcases to fetch
    the `sys.version_info.major` which fetches the version
    of python with which glusto and glusto-tests is installed
    and runs the I/O script i.e file_dir_ops.py with that
    version of python but this creates a problem as older jobs
    running on older platforms won't run the way they use to,
    like if the older platform had python2 by default and
    we are running it tests from a slave which
    has python3 it'll fails and visa-versa.
    
    The problem is introduced due the below code:
    ```
    cmd = ("/usr/bin/env python%d %s create_deep_dirs_with_files "
           "--dirname-start-num 10 --dir-depth 1 --dir-length 1 "
           "--max-num-of-dirs 1 --num-of-files 5 %s" % (
           sys.version_info.major, self.script_upload_path,
           self.mounts[0].mountpoint))
    ```
    
    The solution to this problem is to change `python%d`
    to `python` which would enable the code to run with
    whatever version of python is avaliable on that client
    this would enable us to run any version of framework
    with both the older and latest platforms.
    
    Change-Id: I7c8200a7578f03c482f0c6a91832b8c0fdb33e77
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_manual_heal_should_trigger_heal
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix replica count
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Vitalii Koriakov <vkoriako@redhat.com>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/afr/test_manual_heal_should_trigger_heal.py
@@ -0,0 +1,131 @@
+"""
+ Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Tc to test if heal is triggered on executing heal manually
+"""
+
+# disruptive;dist
+from tests.d_parent_test import DParentTest
+
+
+class TestSelfHeal(DParentTest):
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        conf_hash = self.vol_type_inf['dist']
+        conf_hash['dist_count'] = 1
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        for client in self.client_list:
+            self.redant.execute_abstract_op_node(f"mkdir -p "
+                                                 f"{self.mountpoint}",
+                                                 client)
+            self.redant.volume_mount(self.server_list[0],
+                                     self.vol_name,
+                                     self.mountpoint, client)
+
+    def run_test(self, redant):
+        """
+        - create a single brick volume
+        - add some files and directories
+        - get arequal from mountpoint
+        - add-brick such that this brick makes the volume a replica vol 1x2
+        - start heal
+        - make sure heal is completed
+        - get arequals from all bricks and compare with arequal from mountpoint
+        """
+        # Start IO on mounts
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.all_mounts_procs = []
+        for mount_obj in self.mounts:
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      0, 1, 1, 1, 10,
+                                                      mount_obj['client'])
+            self.all_mounts_procs.append(proc)
+
+        # Validate IO
+        ret = redant.validate_io_procs(self.all_mounts_procs, self.mounts)
+        if not ret:
+            raise Exception("IO failed on some of the clients")
+
+        # Get arequal for mount before adding bricks
+        arequals = redant.collect_mounts_arequal(self.mounts)
+        mount_point_total = arequals[0][-1].split(':')[-1]
+
+        # Form brick list to add
+        _, bricks_to_add = redant.form_brick_cmd(self.server_list,
+                                                 self.brick_roots,
+                                                 self.vol_name, 1,
+                                                 True)
+
+        # Add bricks
+        redant.add_brick(self.vol_name, bricks_to_add, self.server_list[0],
+                         force=True, replica_count=2)
+
+        # Make sure the newly added bricks are available in the volume
+        # get the bricks for the volume
+        bricks_list = redant.get_all_bricks(self.vol_name,
+                                            self.server_list[0])
+        if bricks_list is None:
+            raise Exception("Failed to get the brick list")
+
+        add_brick_list = bricks_to_add.split(" ")
+        for brick in add_brick_list:
+            if brick not in bricks_list:
+                raise Exception(f"Brick {brick} is not in brick list")
+
+        # Make sure volume change from distribute to replicate volume
+        vol_info_dict = redant.get_volume_type_info(self.server_list[0],
+                                                    self.vol_name)
+        vol_type = vol_info_dict['volume_type_info']['typeStr']
+        if vol_type != "Replicate":
+            raise Exception("Volume type is not converted to Replicate "
+                            "after adding bricks")
+
+        # Start healing
+        if not redant.trigger_heal(self.vol_name, self.server_list[0]):
+            raise Exception("Heal is not started")
+
+        # Monitor heal completion
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name):
+            raise Exception("Heal has not yet completed")
+
+        # Check if heal is completed
+        if not redant.is_heal_complete(self.server_list[0],
+                                       self.vol_name):
+            raise Exception("Heal is not yet complete")
+
+        # Check for split-brain
+        if redant.is_volume_in_split_brain(self.server_list[0],
+                                           self.vol_name):
+            raise Exception("Volume is in split-brain state")
+
+        # Get arequal on bricks and compare with mount_point_total
+        # It should be the same
+        arequals = redant.collect_bricks_arequal(bricks_list)
+        for arequal in arequals:
+            brick_total = arequal[-1].split(':')[-1]
+            if brick_total != mount_point_total:
+                raise Exception("Arequals for mountpoint and brick is not "
+                                "equal")
