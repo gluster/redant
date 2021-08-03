0acf59bcca0f3a2d824fb7bc1ad68d5228ed75af
srijan-sivakumar <ssivakum@redhat.com>
2021-08-03 13:27:02 +0530

    [Common] Clearing out files and dirs
    Clearing out the old *.socket files and other dirs
    from /var/run/gluster dir.
    
    Fixes: #874
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- common/ops/support_ops/machine_ops.py
+++ common/ops/support_ops/machine_ops.py
@@ -145,7 +145,11 @@ class MachineOps(AbstractOps):
         # directory on the servers.
         cmd = ("rm -rf /var/lib/glusterd/vols/*; rm -rf /var/lib/glusterd"
                "/peers/*; rm -rf /var/lib/glusterd/snaps/*; "
-               "rm -rf /var/lib/glusterd/glusterfind/.keys/*")
+               "rm -rf /var/lib/glusterd/glusterfind/.keys/*; "
+               "rm -rf /var/run/gluster/*.socket; "
+               "rm -rf /var/run/gluster/vols/*; "
+               "rm -rf /var/run/gluster/shd/*; "
+               "rm -rf /var/run/gluster/snaps/*")
         for node in server_list:
             self.execute_abstract_op_node(cmd, node, False)
 
