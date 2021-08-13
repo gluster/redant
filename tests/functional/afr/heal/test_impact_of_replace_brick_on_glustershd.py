e5e07da87a493de2fb5f26227b32db1282c93848
Sheetal Pamecha <spamecha@redhat.com>
2021-08-13 11:57:44 +0530

    Migrated TC tests/functional/afr/heal/test_impact_of_replace_brick_on_glustershd.py
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/afr/heal/test_impact_of_replace_brick_on_glustershd.py
+++ tests/functional/afr/heal/test_impact_of_replace_brick_on_glustershd.py
@@ -1,101 +1,76 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
+    Test Script to check impact of replace brick on shd and verify
+    the glustershd server vol file has only entries for replicate volumes.
+"""
+
+# disruptive;
 
 from random import choice
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass
-from glustolibs.gluster.volume_libs import (
-    log_volume_info_and_status, wait_for_volume_process_to_be_online,
-    setup_volume, cleanup_volume)
-from glustolibs.gluster.lib_utils import get_servers_bricks_dict
-from glustolibs.gluster.brick_libs import get_all_bricks
-from glustolibs.gluster.brick_ops import replace_brick
-from glustolibs.gluster.heal_libs import (get_self_heal_daemon_pid,
-                                          do_bricks_exist_in_shd_volfile,
-                                          is_shd_daemonized)
-from glustolibs.gluster.volume_ops import get_volume_list
+from tests.d_parent_test import DParentTest
 
 
-class SelfHealDaemonProcessTestsWithMultipleVolumes(GlusterBaseClass):
+class TestCase(DParentTest):
     """
-    SelfHealDaemonProcessTestsWithMultipleVolumes contains tests which
-    verifies the self-heal daemon process on multiple volumes running.
+    This test verifies the self-heal daemon process on
+    multiple volumes running.
     """
-    def setUp(self):
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
         """
-        setup volume and initialize necessary variables
-        which is used in tests
+        Override the volume create, start and mount in parent_run_test
         """
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volume for all the volume types
-        self.volume_configs = []
-        for volume_type in self.default_volume_type_config:
-            self.volume_configs.append(
-                {'name': 'testvol_%s' % volume_type,
-                 'servers': self.servers,
-                 'voltype': self.default_volume_type_config[volume_type]})
-
-        for volume_config in self.volume_configs[1:]:
-            ret = setup_volume(mnode=self.mnode,
-                               all_servers_info=self.all_servers_info,
-                               volume_config=volume_config,
-                               multi_vol=True)
-            volname = volume_config['name']
-            if not ret:
-                raise ExecutionError("Failed to setup Volume"
-                                     " %s" % volname)
-            g.log.info("Successful in setting volume %s", volname)
+        default_volume_type_config = {
+            'rep': {
+                'dist_count': 1,
+                'replica_count': 2,
+                'transport': 'tcp'},
+            'disp': {
+                'disperse_count': 6,
+                'redundancy_count': 2,
+                'transport': 'tcp'},
+            'dist-rep': {
+                'dist_count': 2,
+                'replica_count': 3,
+                'transport': 'tcp'}
+        }
+        # Create and start multiple volumes
+        for vol_type, conf in default_volume_type_config.items():
+            vol_name = f"testvol-{vol_type}"
+            self.redant.setup_volume(vol_name, self.server_list[0],
+                                     conf, self.server_list,
+                                     self.brick_roots, force=True)
 
             # Verify volume's all process are online for 60 sec
-            ret = wait_for_volume_process_to_be_online(self.mnode, volname, 60)
-            if not ret:
-                raise ExecutionError("Volume %s : All process are not online"
-                                     % volname)
-            g.log.info("Successfully Verified volume %s processes are online",
-                       volname)
+            if not (self.redant.wait_for_volume_process_to_be_online(
+                    vol_name, self.server_list[0], self.server_list,
+                    60)):
+                raise Exception("Failed to wait for volume processes "
+                                "to be online")
 
         # Verfiy glustershd process releases its parent process
-        ret = is_shd_daemonized(self.servers)
-        if not ret:
-            raise ExecutionError("Self Heal Daemon process was still"
-                                 " holding parent process.")
-        g.log.info("Self Heal Daemon processes are online")
+        if not self.redant.is_shd_daemonized(self.server_list):
+            raise Exception("Self Heal Daemon process was still"
+                            " holding parent process.")
+        self.GLUSTERSHD = "/var/lib/glusterd/glustershd/glustershd-server.vol"
 
-        self.glustershd = "/var/lib/glusterd/glustershd/glustershd-server.vol"
-
-    def tearDown(self):
-        """
-        Clean up the volume and umount volume from client
-        """
-
-        # Cleanup volume
-        volume_list = get_volume_list(self.mnode)
-        for volume in volume_list:
-            ret = cleanup_volume(self.mnode, volume)
-            if not ret:
-                raise ExecutionError("Failed to cleanup Volume %s" % volume)
-        g.log.info("Successfully Cleaned up all Volumes")
-
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_impact_of_replace_brick_on_glustershd(self):
+    def run_test(self, redant):
         """
         Test Script to verify the glustershd server vol file
         has only entries for replicate volumes
@@ -104,83 +79,74 @@ class SelfHealDaemonProcessTestsWithMultipleVolumes(GlusterBaseClass):
         3.Do replace brick on the replicate volume
         4.Confirm that the brick is replaced
         5.Check the glustershd processes - Only 1 glustershd should be listed
-                                           and pid should be different
+                                           and pid should not be same
         6.glustershd server vol should be updated with new bricks
         """
-        # Check the self-heal daemon process
-        ret, glustershd_pids = get_self_heal_daemon_pid(self.servers)
-        self.assertTrue(ret, ("Either no self heal daemon process found or "
-                              "more than one self heal daemon process "
-                              "found : %s" % glustershd_pids))
-        g.log.info("Successful in getting single self heal daemon process"
-                   " on all nodes %s", self.servers)
 
-        volume_list = get_volume_list(self.mnode)
+        # check the self-heal daemon process
+        ret, glustershd_pids = (redant.
+                                get_self_heal_daemon_pid(self.server_list))
+        if not ret:
+            raise Exception("Either No self heal daemon process found or "
+                            "more than One self heal daemon process "
+                            f"found : {glustershd_pids}")
+
+        volume_list = redant.get_volume_list(self.server_list[0])
+
         for volume in volume_list:
-
             # Log Volume Info and Status before replacing brick
-            ret = log_volume_info_and_status(self.mnode, volume)
-            self.assertTrue(ret, ("Logging volume info and status "
-                                  "failed on volume %s", volume))
-            g.log.info("Successful in logging volume info and status "
-                       "of volume %s", volume)
+            if not (redant.log_volume_info_and_status(self.server_list[0],
+                                                      volume)):
+                raise Exception("Logging volume info and status failed "
+                                f"on volume {volume}")
 
             # Selecting a random source brick to replace
-            src_brick = choice(get_all_bricks(self.mnode, volume))
-            src_node, original_brick = src_brick.split(":")
-
-            # Creating a random destination brick in such a way
-            # that the brick is select from the same node but always
-            # picks a different from the original brick
-            list_of_bricks = [
-                brick for brick in get_servers_bricks_dict(
-                    src_node, self.all_servers_info)[src_node]
-                if brick not in original_brick]
-            dst_brick = ('{}:{}/{}_replaced'.format(
-                src_node, choice(list_of_bricks),
-                original_brick.split('/')[::-1][0]))
+            src_brick = choice(redant.get_all_bricks(
+                               volume, self.server_list[0]))
+            node = src_brick.split(':')[0]
+            _, dest_brick = redant.form_brick_cmd(node,
+                                                  self.brick_roots,
+                                                  volume, 1,
+                                                  True)
 
             # Replace brick for the volume
-            ret, _, _ = replace_brick(self.mnode, volume,
-                                      src_brick, dst_brick)
-            self.assertFalse(ret, "Failed to replace brick "
-                             "from the volume %s" % volume)
-            g.log.info("Successfully replaced faulty brick from "
-                       "the volume %s", volume)
+            if not redant.replace_brick(self.server_list[0], volume,
+                                        src_brick, dest_brick):
+                raise Exception(f"Failed to replace brick: {src_brick}")
 
             # Verify all volume process are online
-            ret = wait_for_volume_process_to_be_online(self.mnode, volume)
-            self.assertTrue(ret, "Volume %s : All process are not online"
-                            % volume)
-            g.log.info("Volume %s : All process are online", volume)
+            if not (redant.wait_for_volume_process_to_be_online(
+                    volume, self.server_list[0],
+                    self.server_list)):
+                raise Exception("Volume processes not yet online")
 
             # Check the self-heal daemon process after replacing brick
-            ret, pid_after_replace = get_self_heal_daemon_pid(self.servers)
-            self.assertTrue(ret, "Either no self heal daemon process "
-                            "found or more than one self heal "
-                            "daemon process found : %s" % pid_after_replace)
-            g.log.info("Successful in getting Single self heal "
-                       " daemon process on all nodes %s", self.servers)
+            ret, pid_after_replace = (redant.get_self_heal_daemon_pid(
+                                      self.server_list))
+            if not ret:
+                raise Exception("Either No self heal daemon process "
+                                "found or more than One self heal "
+                                "daemon process "
+                                f"found : {pid_after_replace}")
 
             # Compare the glustershd pids
-            self.assertNotEqual(glustershd_pids, pid_after_replace,
-                                "Self heal daemon process should be different "
-                                "after replacing bricks in %s volume"
-                                % volume)
-            g.log.info("EXPECTED: Self heal daemon process should be different"
-                       " after replacing bricks in replicate volume")
+            if glustershd_pids != pid_after_replace:
+                raise Exception("Self heal Daemon process is different before "
+                                "and after replacing bricks")
 
             # Get the bricks for the volume
-            bricks_list = get_all_bricks(self.mnode, volume)
-            g.log.info("Brick List : %s", bricks_list)
+            brick_list = redant.get_all_bricks(volume,
+                                               self.server_list[0])
+            if brick_list is None:
+                raise Exception("Empty bricks list after replacing")
 
             # Validate the bricks present in volume info with
             # glustershd server volume file
-            ret = do_bricks_exist_in_shd_volfile(self.mnode, volume,
-                                                 bricks_list)
-            self.assertTrue(ret, ("Brick List from volume info is "
-                                  "different from glustershd server "
-                                  "volume file. Please check log file "
-                                  "for details"))
-            g.log.info("Bricks in volume %s exists in glustershd server "
-                       "volume file", volume)
+            if not (redant.
+                    do_bricks_exist_in_shd_volfile(volume,
+                                                   brick_list,
+                                                   self.server_list[0])):
+                raise Exception("Brick List from volume info is different "
+                                "from glustershd server volume file after "
+                                "replacing bricks. Please check log file "
+                                "for details")
