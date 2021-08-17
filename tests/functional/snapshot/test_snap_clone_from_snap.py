a2097d412499dc61543925cffea90d93089528c7
srijan-sivakumar <ssivakum@redhat.com>
2021-08-17 12:30:47 +0530

    [Test] Migrating a snap case
    Updates: #292
    
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>

--- tests/functional/snapshot/test_snap_clone_from_snap.py
+++ tests/functional/snapshot/test_snap_clone_from_snap.py
@@ -1,71 +1,47 @@
-#  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
 """
+  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 Description:
 
 Test Cases in this module tests the
 Creation of clone volume from snapshot.
 """
 
-from glusto.core import Glusto as g
+import traceback
+from tests.d_parent_test import DParentTest
 
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass
-from glustolibs.gluster.gluster_base_class import runs_on
-from glustolibs.gluster.volume_ops import (volume_start,
-                                           get_volume_info, volume_list)
-from glustolibs.gluster.volume_libs import cleanup_volume
-from glustolibs.gluster.snap_ops import (snap_create,
-                                         snap_activate,
-                                         snap_clone)
-from glustolibs.misc.misc_libs import upload_scripts
+# disruptive;rep,dist,disp,dist-rep,dist-disp
 
 
-@runs_on([['replicated', 'distributed-replicated', 'dispersed',
-           'distributed-dispersed', 'distributed'],
-          ['glusterfs', 'nfs', 'cifs']])
-class SnapshotCloneValidate(GlusterBaseClass):
+class TestCase(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.get_super_method(cls, 'setUpClass')()
-        cls.snap = "snap0"
+    def terminate(self):
+        try:
+            ret = self.redant.wait_for_io_to_complete(self.all_mounts_procs,
+                                                      self.mounts)
+            if not ret:
+                raise Exception("IO failed on some of the clients")
+        except Exception as e:
+            tb = traceback.format_exc()
+            self.redant.logger.error(e)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts "
-                                 "to clients ")
-        g.log.info("Successfully uploaded IO scripts to clients %s")
-
-    def setUp(self):
-        self.get_super_method(self, 'setUp')()
-
-        g.log.info("Starting to SetUp Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to setup volume %s" % self.volname)
-        g.log.info("Volume %s has been setup successfully", self.volname)
-
-    def test_snap_clone_validate(self):
+    def run_test(self, redant):
 
         """
         CloneSnapTest contains tests which verifies Clone volume
@@ -86,81 +62,48 @@ class SnapshotCloneValidate(GlusterBaseClass):
         """
 
         # write files on all mounts
-        g.log.info("Starting IO on all mounts...")
-        g.log.info("mounts: %s", self.mounts)
-        all_mounts_procs = []
-        for mount_obj in self.mounts:
-            cmd = ("/usr/bin/env python %s create_files "
-                   "-f 10 --base-file-name file %s" % (
-                       self.script_upload_path,
-                       mount_obj.mountpoint))
-            proc = g.run(self.clients[0], cmd)
-            all_mounts_procs.append(proc)
-        g.log.info("Successfully Performed I/O on all mount points")
+        counter = 1
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
+        for mount in self.mounts:
+            redant.logger.info(f"Starting IO on {mount['client']}:"
+                               f"{mount['mountpath']}")
+            proc = redant.create_deep_dirs_with_files(mount['mountpath'],
+                                                      counter, 2, 3, 4, 10,
+                                                      mount['client'])
+            self.all_mounts_procs.append(proc)
+            counter += 10
+
+        # Wait till IO finishes.
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed.")
 
         # Creating snapshot:
-        g.log.info("Starting to Create snapshot")
-        ret, _, _ = snap_create(self.mnode, self.volname, self.snap)
-        self.assertEqual(ret, 0, ("Failed to create snapshot for volume %s"
-                                  % self.volname))
-        g.log.info("Snapshot snap1 created successfully for volume %s",
-                   self.volname)
+        snap_name = f"{self.vol_name}-snap"
+        redant.snap_create(self.vol_name, snap_name, self.server_list[0])
 
         # Activating snapshot
-        g.log.info("Starting to Activate Snapshot")
-        ret, _, _ = snap_activate(self.mnode, self.snap)
-        self.assertEqual(ret, 0, ("Failed to Activate snapshot %s"
-                                  % self.snap))
-        g.log.info("Snapshot %s activated successfully", self.snap)
+        redant.snap_activate(snap_name, self.server_list[0])
 
         # Creating and starting a Clone of snapshot:
-        g.log.info("Starting to Clone Snapshot")
         for count in range(1, 11):
-            self.clone = "clone%s" % count
-            ret, _, _ = snap_clone(self.mnode, self.snap, self.clone)
-            self.assertEqual(ret, 0, "Failed to clone %s" % self.clone)
-            g.log.info("%s created successfully", self.clone)
+            clone_name = f"{self.vol_name}-clone{count}"
+            redant.snap_clone(snap_name, clone_name, self.server_list[0])
 
         # Start clone volumes
-        g.log.info("starting to Validate clone volumes are started")
         for count in range(1, 11):
-            self.clone = "clone%s" % count
-            ret, _, _ = volume_start(self.mnode, self.clone)
-            self.assertEqual(ret, 0, ("Failed to start %s" % self.clone))
-            g.log.info("%s started successfully", self.clone)
+            clone_name = f"{self.vol_name}-clone{count}"
+            redant.volume_start(clone_name, self.server_list[0])
 
         # Validate Volume Started
-        g.log.info("Validating volume started")
         for count in range(1, 11):
-            self.clone = "clone%s" % count
-            vol_info = get_volume_info(self.mnode, self.clone)
-            if vol_info[self.clone]['statusStr'] != 'Started':
-                raise ExecutionError("Volume %s failed to start" % self.clone)
-            g.log.info("Volume %s is in Started state", self.clone)
+            clone_name = f"{self.vol_name}-clone{count}"
+            if not redant.is_volume_started(clone_name, self.server_list[1]):
+                raise Exception(f"{clone_name} is not started.")
 
         # validate with list information
         # with 10 clone volume and 1 existing volume
-        g.log.info("Validating with list information")
-        ret, out, _ = volume_list(self.mnode)
-        vlist = out.strip().split('\n')
-        self.assertEqual(len(vlist), 11, "Failed to validate volume list")
-        g.log.info("Successfully validated volumes in list")
-
-    def tearDown(self):
-
-        # Cleanup and umount volume
-        g.log.info("Starting to cleanup volume %s", self.clone)
-        for count in range(1, 11):
-            self.clone = "clone%s" % count
-            ret = cleanup_volume(self.mnode, self.clone)
-            if not ret:
-                raise ExecutionError("Failed to Cleanup cloned volume")
-            g.log.info("successfully Unmount Volume and Cleanup Volume")
-
-        # Unmount and cleanup original volume
-        g.log.info("Starting to unmount and cleanup volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount"
-                                 "the volume & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
+        ret = redant.get_volume_list(self.server_list[0])
+        if len(ret) != 11:
+            raise Exception(f"Expected 11 volumes. Got {ret}")
