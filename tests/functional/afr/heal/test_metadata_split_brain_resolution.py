2469b62b5ffb971b2b938617dbb5d21ac63f77b4
Sheetal Pamecha <spamecha@redhat.com>
2021-08-19 10:46:12 +0530

    Migrated TC tests/functional/afr/heal/test_metadata_split_brain_resolution.py (#962)
    * afr: Test metadata split-brain resolution using heal CLI
    
    Change-Id: I634d11cb582521b03f0bb481172e2f4f68d1c2ce
    Signed-off-by: karthik-us <ksubrahm@redhat.com>
    
    * Add py3 support for tests in 'tests/functional/afr/heal'
    
    Change-Id: Id4df838565ec3f9ad765cf223bb5115e43dac1c5
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 3)
    
    Please refer to the commit message of the below patch:
    https://review.gluster.org/#/c/glusto-tests/+/23902/
    
    Change-Id: Icf32bb20b7eaf2eabb07b59be813997a28872565
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize afr testcases
    
    Added steps to reset volume and resolved teardown class
    cleanup failures.
    
    Change-Id: I06b0ed8810c9b064fd2ee7c0bfd261928d8c07db
    
    * Migrated TC tests/functional/afr/heal/test_metadata_split_brain_resolution.py
    
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>
    
    * Add test steps
    
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>
    
    * Use IO func in TC
    
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>
    
    Co-authored-by: karthik-us <ksubrahm@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/afr/heal/test_metadata_split_brain_resolution.py
@@ -0,0 +1,198 @@
+"""
+  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+        Test cases in this module tests whether heal command for resolving
+        split-brains will resolve all the files in metadata split-brains by
+        using one of the method (bigger-file/latest-mtime/source-brick).
+"""
+
+# disruptive;rep
+
+from tests.d_parent_test import DParentTest
+
+
+class TestCase(DParentTest):
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create
+        """
+        conf_hash = self.vol_type_inf['rep']
+        conf_hash['replica_count'] = 2
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots, force=True)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        for client in self.client_list:
+            self.redant.execute_abstract_op_node(f"mkdir -p "
+                                                 f"{self.mountpoint}",
+                                                 client)
+            self.redant.volume_mount(self.server_list[0],
+                                     self.vol_name,
+                                     self.mountpoint, client)
+
+    def _verify_brick_arequals(self):
+        """
+        Collects arequal for all bricks and compare
+        """
+        all_bricks = (self.redant.get_online_bricks_list(
+                      self.vol_name, self.server_list[0]))
+        arequal = self.redant.collect_bricks_arequal(all_bricks)
+        if len(set(tuple(x) for x in arequal)) != 1:
+            raise Exception("Arequal is not same on all the bricks "
+                            "in the subvol")
+
+    def run_test(self, redant):
+        """
+        Test steps:
+        1.Turn off the data, metadata and entry self heal options
+        2.Create IO
+        3.Calculate arequal of the bricks
+        4.Turn off the option self heal daemon
+        5.Bring down "brick1" process
+        6.Change the permissions of the directories and files
+        7.Bring back the brick "brick1" process
+        8.Bring down "brick1" process
+        9.Change the permissions of the directories and files
+        10.Bring back the brick "brick1" process
+        11.Turn on the option self heal daemon
+        12.Check if there any files in split-brain
+        13.Trigger heal and wait for it to complete
+        14.Do lookup on the files from mount
+        15.Check if files are not in split-brain
+        16.Calculate arequal of the bricks
+        17.Change metadata of same files
+        18.Do lookup on the files from mount
+        19.Calculate arequal of the bricks
+        """
+        # Setting options
+        options = {"metadata-self-heal": "off",
+                   "entry-self-heal": "off",
+                   "data-self-heal": "off"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0],
+                                  True)
+
+        # Creating files and directories on client side
+        cmd = (f"mkdir {self.mountpoint}/test_metadata_sb && "
+               f"cd {self.mountpoint}/test_metadata_sb && "
+               "for i in `seq 1 3`; do mkdir dir_$i; "
+               "for j in `seq 1 5`; do dd if=/dev/urandom "
+               "of=dir_$i/file_$j bs=1K count=1; done; "
+               "dd if=/dev/urandom of=file_$i bs=1K count=1; done;")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Check arequals for all the bricks
+        self._verify_brick_arequals()
+
+        # Set option self-heal-daemon to OFF
+        options = {"self-heal-daemon": "off"}
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0])
+
+        # Bring brick1 offline
+        bricks_list = redant.get_all_bricks(self.vol_name,
+                                            self.server_list[0])
+
+        redant.bring_bricks_offline(self.vol_name, bricks_list[0])
+        if not redant.are_bricks_offline(self.vol_name, bricks_list[0],
+                                         self.server_list[0]):
+            raise Exception(f"Brick {bricks_list[0]} is not offline")
+
+        # Change metadata of some files & directories
+        cmd = (f"cd {self.mountpoint}/test_metadata_sb && "
+               "for i in `seq 1 2`; do chmod -R 0555 dir_$i file_$i;"
+               "done;")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Bring brick1 online and check the status
+        redant.bring_bricks_online(self.vol_name, self.server_list,
+                                   bricks_list[0])
+        if not redant.are_bricks_online(self.vol_name, bricks_list[0],
+                                        self.server_list[0]):
+            raise Exception(f"Brick {bricks_list[0]} is not online")
+
+        # Bring brick2 offline
+        redant.bring_bricks_offline(self.vol_name, bricks_list[1])
+        if not redant.are_bricks_offline(self.vol_name, bricks_list[1],
+                                         self.server_list[1]):
+            raise Exception(f"Brick {bricks_list[1]} is not offline")
+
+        # Change metadata of same files & directories as before
+        cmd = (f"cd {self.mountpoint}/test_metadata_sb && "
+               "for i in `seq 1 2` ; do chmod -R 0777 dir_$i file_$i;"
+               "done;")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Bring brick2 online and check the status
+        redant.bring_bricks_online(self.vol_name, self.server_list,
+                                   bricks_list[1])
+        if not redant.are_bricks_online(self.vol_name, bricks_list[1],
+                                        self.server_list[1]):
+            raise Exception(f"Brick {bricks_list[1]} is not online")
+
+        # Set option self-heal-daemon to ON
+        options = {"self-heal-daemon": "on"}
+        redant.set_volume_options(self.vol_name, options, self.server_list[0])
+
+        # Check if there any files in split-brain
+        if not redant.is_volume_in_split_brain(self.server_list[0],
+                                               self.vol_name):
+            raise Exception("Failed to create split-brain state")
+
+        node, _ = bricks_list[1].split(':')
+        cmd = ("gluster v heal " + self.vol_name + " split-brain "
+               "source-brick " + bricks_list[1])
+        redant.execute_abstract_op_node(cmd, node)
+
+        # triggering heal
+        if not redant.trigger_heal(self.vol_name,
+                                   self.server_list[0]):
+            raise Exception("Start heal failed")
+
+        # waiting for heal to complete
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name):
+            raise Exception("Heal not yet completed")
+
+        # Do lookup on the files from mount
+        cmd = (f"ls -lR {self.mountpoint}/test_metadata_sb")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Checking if files are still in split-brain
+        if redant.is_volume_in_split_brain(self.server_list[0],
+                                           self.vol_name):
+            raise Exception("File still in split-brain")
+
+        # Check arequals for all the bricks
+        self._verify_brick_arequals()
+
+        # Change metadata of same files & directories as before
+        cmd = (f"cd {self.mountpoint}/test_metadata_sb && "
+               "for i in `seq 1 2` ; do chmod -R 0555 dir_$i file_$i;"
+               "done")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Do lookup on the mount
+        self.mounts = {"client": self.client_list[0],
+                       "mountpath": self.mountpoint}
+        redant.get_mounts_stat(self.mounts)
+
+        # Check arequals for all the bricks
+        self._verify_brick_arequals()
