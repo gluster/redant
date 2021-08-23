50b946b0692a6b30279533f05fe9783d2ee5a9c7
Sheetal Pamecha <spamecha@redhat.com>
2021-08-23 09:30:02 +0530

    Migrated TC test_server_side_healing_happens_only_when_glustershd_running.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/afr/heal/test_server_side_healing_happens_only_when_glustershd_running.py
+++ tests/functional/afr/heal/test_server_side_healing_happens_only_when_glustershd_running.py
@@ -1,88 +1,34 @@
-#  Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2021 Red Hat, Inc. <http://www.redhat.com>
 
-""" Description:
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
         Test Cases in this module tests the self heal daemon process.
 """
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.brick_libs import (
-    bring_bricks_offline, bring_bricks_online,
-    select_volume_bricks_to_bring_offline, get_online_bricks_list)
-from glustolibs.gluster.heal_libs import (
-    get_self_heal_daemon_pid, is_shd_daemonized,
-    monitor_heal_completion, bring_self_heal_daemon_process_offline,
-    disable_granular_heal)
-from glustolibs.gluster.heal_ops import (get_heal_info_summary,
-                                         trigger_heal_full)
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.gluster.volume_ops import (set_volume_options,
-                                           get_volume_options)
-from glustolibs.gluster.mount_ops import mount_volume, umount_volume
+# disruptive;rep
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['replicated'], ['glusterfs']])
-class SelfHealDaemonProcessTestsWithSingleVolume(GlusterBaseClass):
+class TestCase(DParentTest):
     """
-    SelfHealDaemonProcessTestsWithSingleVolume contains tests which
     verifies the self-heal daemon process on a single volume
     """
 
-    def setUp(self):
-
-        # Calling GlusterBaseClass setUpClass
-        self.get_super_method(self, 'setUp')()
-
-        # Upload script
-        self.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                   "file_dir_ops.py")
-        ret = upload_scripts(self.clients, [self.script_upload_path])
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients")
-
-        # Setup Volume and Mount Volume
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-        # Verify glustershd process releases its parent process
-        ret = is_shd_daemonized(self.servers)
-        if not ret:
-            raise ExecutionError("Self Heal Daemon process was still"
-                                 " holding parent process.")
-        g.log.info("Self Heal Daemon processes are online")
-
-    def tearDown(self):
-        """
-        Clean up the volume and umount volume from client
-        """
-        # Stopping the volume
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Unmount Volume and Cleanup Volume")
-        g.log.info("Successful in Unmount Volume and Cleanup Volume")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_server_side_healing_happens_only_when_glustershd_running(self):
+    def run_test(self, redant):
         """
         Test Script which verifies that the server side healing must happen
         only if the heal daemon is running on the node where source brick
@@ -100,151 +46,131 @@ class SelfHealDaemonProcessTestsWithSingleVolume(GlusterBaseClass):
          * trigger client side heal
          * heal should complete successfully
         """
-        # pylint: disable=too-many-locals,too-many-statements,too-many-lines
+
+        # Verfiy glustershd process releases its parent process
+        if not redant.is_shd_daemonized(self.server_list):
+            raise Exception("Self Heal Daemon process was still"
+                            " holding parent process.")
 
         # Disable granular heal if not disabled already
-        granular = get_volume_options(self.mnode, self.volname,
-                                      'granular-entry-heal')
+        granular = redant.get_volume_options(self.vol_name,
+                                             'granular-entry-heal',
+                                             self.server_list[0])
         if granular['cluster.granular-entry-heal'] == 'on':
-            ret = disable_granular_heal(self.mnode, self.volname)
-            self.assertTrue(ret,
-                            "Unable to set granular-entry-heal to on")
+            if not redant.disable_granular_heal(self.vol_name,
+                                                self.server_list[0]):
+                raise Exception("Failed to disable granular-entry-heal")
 
         # Setting Volume options
         options = {"metadata-self-heal": "on",
                    "entry-self-heal": "on",
                    "data-self-heal": "on"}
-        ret = set_volume_options(self.mnode, self.volname, options)
-        self.assertTrue(ret, 'Failed to set options %s' % options)
-        g.log.info("Successfully set %s for volume %s",
-                   options, self.volname)
+        redant.set_volume_options(self.vol_name, options,
+                                  self.server_list[0], True)
 
         # Check the self-heal daemon process
-        ret, pids = get_self_heal_daemon_pid(self.servers)
-        self.assertTrue(ret, ("Either No self heal daemon process found or "
-                              "more than One self heal daemon process "
-                              "found : %s" % pids))
-        g.log.info("Successful in verifying self heal daemon process"
-                   " on all nodes %s", self.servers)
+        ret, pids = redant.get_self_heal_daemon_pid(self.server_list)
+        if not ret:
+            raise Exception("Either No self heal daemon process found or "
+                            "more than One self heal daemon process "
+                            f"found : {pids}")
 
         # Select the bricks to bring offline
-        bricks_to_bring_offline = (select_volume_bricks_to_bring_offline
-                                   (self.mnode, self.volname))
-        g.log.info("Brick List to bring offline : %s", bricks_to_bring_offline)
+        offline_brick_list = (redant.select_volume_bricks_to_bring_offline(
+                              self.vol_name, self.server_list[0]))
 
         # Bring down the selected bricks
-        ret = bring_bricks_offline(self.volname, bricks_to_bring_offline)
-        self.assertTrue(ret, "Failed to bring down the bricks")
-        g.log.info("Brought down the brick process "
-                   "for %s", bricks_to_bring_offline)
+        if not redant.bring_bricks_offline(self.vol_name,
+                                           offline_brick_list):
+            raise Exception("Failed to bring bricks offline")
 
         # Write files on all mounts
-        all_mounts_procs, num_files_to_write = [], 100
-        for mount_obj in self.mounts:
-            cmd = ("/usr/bin/env python %s create_files "
-                   "-f %d --base-file-name file %s" % (self.script_upload_path,
-                                                       num_files_to_write,
-                                                       mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
+        self.mnt_list = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        all_mounts_procs = []
+        for mount_obj in self.mnt_list:
+            proc = redant.create_files(num_files=100,
+                                       fix_fil_size="1k",
+                                       path=mount_obj['mountpath'],
+                                       node=mount_obj['client'],
+                                       base_file_name="file")
             all_mounts_procs.append(proc)
 
         # Validate IO
-        ret = validate_io_procs(all_mounts_procs, self.mounts)
-        self.assertTrue(ret, "IO failed on some of the clients")
-        g.log.info("IO is successful on all mounts")
+        if not redant.validate_io_procs(all_mounts_procs, self.mnt_list):
+            raise Exception("IO validation failed")
 
         # Get online bricks list
-        online_bricks = get_online_bricks_list(self.mnode, self.volname)
-        g.log.info("Online Bricks for volume %s : %s",
-                   self.volname, online_bricks)
+        online_bricks = redant.get_online_bricks_list(self.vol_name,
+                                                      self.server_list[0])
 
         # Get the nodes where bricks are running
         bring_offline_glustershd_nodes = []
         for brick in online_bricks:
             bring_offline_glustershd_nodes.append(brick.split(":")[0])
-        g.log.info("self heal deamon on nodes %s to be killed",
-                   bring_offline_glustershd_nodes)
 
         # Kill the self heal daemon process on nodes
-        ret = bring_self_heal_daemon_process_offline(
-            bring_offline_glustershd_nodes)
-        self.assertTrue(ret, ("Unable to bring self heal daemon process"
-                              " offline for nodes %s"
-                              % bring_offline_glustershd_nodes))
-        g.log.info("Sucessfully brought down self heal process for "
-                   "nodes %s", bring_offline_glustershd_nodes)
+        if not (redant.bring_self_heal_daemon_process_offline(
+                bring_offline_glustershd_nodes)):
+            raise Exception("Unable to bring self heal daemon process "
+                            "offline for nodes: "
+                            f"{bring_offline_glustershd_nodes}")
 
         # Check the heal info
-        heal_info = get_heal_info_summary(self.mnode, self.volname)
-        g.log.info("Successfully got heal info %s for the volume %s",
-                   heal_info, self.volname)
+        heal_info1 = (redant.get_heal_info_summary(
+                      self.server_list[0], self.vol_name))
+        if heal_info1 is None:
+            raise Exception("Failed to get the heal info summary.")
 
         # Bring bricks online
-        ret = bring_bricks_online(self.mnode, self.volname,
-                                  bricks_to_bring_offline, 'glusterd_restart')
-        self.assertTrue(ret, ("Failed to bring bricks: %s online"
-                              % bricks_to_bring_offline))
+        if not (redant.bring_bricks_online(self.vol_name, self.server_list,
+                offline_brick_list, True)):
+            raise Exception("Failed to bring bricks: "
+                            f"{offline_brick_list} online")
 
         # Issue heal
-        ret = trigger_heal_full(self.mnode, self.volname)
-        self.assertFalse(ret, ("Able to trigger heal on volume %s where "
-                               "self heal daemon is not running"
-                               % self.volname))
-        g.log.info("Expected : Unable to trigger heal on volume %s where "
-                   "self heal daemon is not running", self.volname)
+        if not redant.trigger_heal_full(self.vol_name,
+                                        self.server_list[0]):
+            raise Exception("Starting full heal failed")
 
         # Wait for 130 sec to heal
-        ret = monitor_heal_completion(self.mnode, self.volname, 130)
-        self.assertFalse(ret, ("Heal Completed on volume %s" % self.volname))
-        g.log.info("Expected : Heal pending on volume %s", self.volname)
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name, 130):
+            raise Exception("Heal has completed")
 
         # Check the heal info
-        heal_info_after_triggering_heal = get_heal_info_summary(self.mnode,
-                                                                self.volname)
-        g.log.info("Successfully got heal info for the volume %s",
-                   self.volname)
+        heal_info = redant.get_heal_info_summary(self.server_list[0],
+                                                 self.vol_name)
+        if heal_info is None:
+            raise Exception("Failed to get the heal info summary.")
 
         # Compare with heal pending with the files wrote
         for node in online_bricks:
-            self.assertGreaterEqual(
-                int(heal_info_after_triggering_heal[node]['numberOfEntries']),
-                num_files_to_write,
-                ("Some of the files are healed from source bricks %s where "
-                 "self heal daemon is not running" % node))
-        g.log.info("EXPECTED: No files are healed from source bricks where "
-                   "self heal daemon is not running")
+            num_files_to_heal = int(heal_info[node]['numberOfEntries'])
+            if num_files_to_heal >= 100:
+                raise Exception("Some of the files are healed from "
+                                "source bricks {node} where self heal"
+                                "daemon is not running")
 
         # Unmount and Mount volume again as volume options were set
         # after mounting the volume
-        for mount_obj in self.mounts:
-            ret, _, _ = umount_volume(mount_obj.client_system,
-                                      mount_obj.mountpoint)
-            self.assertEqual(ret, 0, "Failed to unmount %s"
-                             % mount_obj.client_system)
-            ret, _, _ = mount_volume(self.volname,
-                                     mtype='glusterfs',
-                                     mpoint=mount_obj.mountpoint,
-                                     mserver=self.mnode,
-                                     mclient=mount_obj.client_system)
-            self.assertEqual(ret, 0, "Failed to mount %s"
-                             % mount_obj.client_system)
+        for client in self.client_list:
+            redant.volume_unmount(self.vol_name, self.mountpoint,
+                                  client)
+            redant.volume_mount(self.server_list[0], self.vol_name,
+                                self.mountpoint, client)
 
         all_mounts_procs = []
-        for mount_obj in self.mounts:
-            cmd = ("cd %s;for i in `seq 1 5`; do ls -l;cat *; stat *; sleep 5;"
-                   " done " % (mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
+        for mount_obj in self.mnt_list:
+            cmd = ("cd {mount_obj['mountpoint']}; for i in `seq 1 5`; "
+                   "do ls -l; done")
+            proc = redant.execute_command_async(cmd, mount_obj['client'])
             all_mounts_procs.append(proc)
 
         # Validate IO
-        ret = validate_io_procs(all_mounts_procs, self.mounts)
-        self.assertTrue(ret, "Reads failed on some of the clients")
-        g.log.info("Reads successful on all mounts")
+        if not redant.validate_io_procs(all_mounts_procs, self.mnt_list):
+            raise Exception("IO validation failed")
 
         # Wait for heal to complete
-        ret = monitor_heal_completion(self.mnode, self.volname)
-        self.assertTrue(ret, "Unable to heal the pending entries")
-        g.log.info("Successfully healed the pending entries for volume %s",
-                   self.volname)
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name):
+            raise Exception("Heal has not yet completed")
