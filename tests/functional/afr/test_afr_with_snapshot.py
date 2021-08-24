0b6ab11af05021704b0d82749abab88f4bc5239a
Nikhil Ladha <nladha@redhat.com>
2021-08-24 14:14:46 +0530

    Migrated TC test_afr_with_snapshot (#977)
    * Test entry transaction crash consistency with fops
    
    Testcase 1: Test entry transaction crash consistency : create
    - Create IO
    - Calculate arequal before creating snapshot
    - Create snapshot
    - Modify the data
    - Stop the volume
    - Restore snapshot
    - Start the volume
    - Get arequal after restoring snapshot
    - Compare arequals
    
    Testcase 2: Test entry transaction crash consistency : delete
    - Create IO of 50 files
    - Delete 20 files
    - Calculate arequal before creating snapshot
    - Create snapshot
    - Delete 20 files more
    - Stop the volume
    - Restore snapshot
    - Start the volume
    - Get arequal after restoring snapshot
    - Compare arequals
    
    Testcase 3: Test entry transaction crash consistency : rename
    - Create IO of 50 files
    - Rename 20 files
    - Calculate arequal before creating snapshot
    - Create snapshot
    - Rename 20 files more
    - Stop the volume
    - Restore snapshot
    - Start the volume
    - Get arequal after restoring snapshot
    - Compare arequals
    
    Change-Id: I7cb9182f91ae50c47d5ae9b3f8031413b2bbfbbf
    Co-authored-by: Vitalii Koriakov <vkoriako@redhat.com>
    Signed-off-by: Vitalii Koriakov <vkoriako@redhat.com>
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 1)
    
    sys library was added to all the testcases to fetch
    the `sys.version_info.major` which fetches the version
    of python with which glusto and glusto-tests is installed
    and runs the I/O script i.e file_dir_ops.py with that
    version of python but this creates a problem as older jobs
    running on older platforms won't run the way they use to,
    like if the older platform had python2 by default and
    we are running it tests from a slave which
    has python3 it'll fails and visa-versa.
    
    The problem is introduced due the below code:
    ```
    cmd = ("/usr/bin/env python%d %s create_deep_dirs_with_files "
           "--dirname-start-num 10 --dir-depth 1 --dir-length 1 "
           "--max-num-of-dirs 1 --num-of-files 5 %s" % (
           sys.version_info.major, self.script_upload_path,
           self.mounts[0].mountpoint))
    ```
    
    The solution to this problem is to change `python%d`
    to `python` which would enable the code to run with
    whatever version of python is avaliable on that client
    this would enable us to run any version of framework
    with both the older and latest platforms.
    
    Change-Id: I7c8200a7578f03c482f0c6a91832b8c0fdb33e77
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_afr_with_snapshoot
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Break the TC into multiple TCs
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Vitalii Koriakov <vkoriako@redhat.com>

---
+++ tests/functional/afr/test_afr_with_snapshot.py
@@ -0,0 +1,99 @@
+"""
+ Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test cases related to afr snapshot.
+"""
+
+# disruptive;rep,dist-rep
+# TODO: NFS, CIFS
+from time import sleep
+from tests.d_parent_test import DParentTest
+
+
+class TestAFRSnapshot(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Test entry transaction crash consistency : create
+
+        Description:
+        - Create IO
+        - Calculate arequal before creating snapshot
+        - Create snapshot
+        - Modify the data
+        - Stop the volume
+        - Restore snapshot
+        - Start the volume
+        - Get arequal after restoring snapshot
+        - Compare arequals
+        """
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        # Creating files on client side
+        count = 1
+        for mount_obj in self.mounts:
+            cmd = (f"python3 /tmp/file_dir_ops.py create_files -f 200 "
+                   f"--base-file-name file_{count} {mount_obj['mountpath']}")
+            proc = redant.execute_command_async(cmd, mount_obj['client'])
+            self.all_mounts_procs.append(proc)
+            count = count + 10
+
+        # Wait for IO to complete
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed to complete on some of the clients")
+
+        # Get arequal before creating snapshot
+        result_before_snapshot = redant.collect_mounts_arequal(self.mounts)
+
+        # Create snapshot
+        snapshot_name = ("entry_transaction_crash_consistency_create-"
+                         f"{self.vol_name}-fuse")
+        redant.snap_create(self.vol_name, snapshot_name, self.server_list[0])
+
+        # Modify the data
+        self.all_mounts_procs = []
+        for mount_obj in self.mounts:
+            cmd = ("python3 /tmp/file_dir_ops.py append "
+                   f"{mount_obj['mountpath']}")
+            proc = redant.execute_command_async(cmd, mount_obj['client'])
+            self.all_mounts_procs.append(proc)
+
+        # Wait for IO to complete
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed to complete on some of the clients")
+
+        # Restore snapshot
+        ret = redant.snap_restore_complete(self.vol_name, snapshot_name,
+                                           self.server_list[0])
+        if not ret:
+            raise Exception(f"Failed to restore snapshot {snapshot_name}")
+
+        # Check if heal is completed
+        if not redant.is_heal_complete(self.server_list[0], self.vol_name):
+            raise Exception('Heal is not complete')
+
+        # Wait for volume graph to get loaded.
+        sleep(10)
+
+        # Get arequal after restoring snapshot
+        result_after_restoring = redant.collect_mounts_arequal(self.mounts)
+
+        # Checking arequal before creating snapshot
+        # and after restoring snapshot
+        if result_before_snapshot != result_after_restoring:
+            raise Exception('Checksums are not equal')
---
+++ tests/functional/afr/test_afr_with_snapshot_rename.py
@@ -0,0 +1,113 @@
+"""
+ Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test cases related to afr snapshot.
+    Third test for test_afr_with_snapshot.
+"""
+
+# disruptive;rep,dist-rep
+# TODO: NFS, CIFS
+from time import sleep
+from tests.d_parent_test import DParentTest
+
+
+class TestAFRSnapshot(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Test entry transaction crash consistency : rename
+
+        Description:
+        - Create IO of 50 files
+        - Rename 20 files
+        - Calculate arequal before creating snapshot
+        - Create snapshot
+        - Rename 20 files more
+        - Stop the volume
+        - Restore snapshot
+        - Start the volume
+        - Get arequal after restoring snapshot
+        - Compare arequals
+        """
+        self.all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        # Creating files on client side
+        count = 1
+        for mount_obj in self.mounts:
+            cmd = (f"python3 /tmp/file_dir_ops.py create_files -f 50 "
+                   f"--base-file-name file_{count} {mount_obj['mountpath']}")
+            proc = redant.execute_command_async(cmd, mount_obj['client'])
+            self.all_mounts_procs.append(proc)
+            count = count + 10
+
+        # Wait for IO to complete
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed to complete on some of the clients")
+
+        # Rename files
+        self.all_mounts_procs = []
+        cmd = ("python3 /tmp/file_dir_ops.py mv -s FirstRename"
+               f" {self.mountpoint}")
+        proc = redant.execute_command_async(cmd, self.client_list[0])
+        self.all_mounts_procs.append(proc)
+
+        # Wait for IO to complete
+        if not redant.validate_io_procs(self.all_mounts_procs,
+                                        self.mounts[0]):
+            raise Exception("IO failed to complete on the client")
+
+        # Get arequal before creating snapshot
+        result_before_snapshot = redant.collect_mounts_arequal(self.mounts[0])
+
+        # Create snapshot
+        snapshot_name = ("entry_transaction_crash_consistency_rename-"
+                         f"{self.vol_name}-fuse")
+        redant.snap_create(self.vol_name, snapshot_name, self.server_list[0])
+
+        # Rename files
+        self.all_mounts_procs = []
+        cmd = ("python3 /tmp/file_dir_ops.py mv -s SecondRename "
+               f" {self.mountpoint}")
+        proc = redant.execute_command_async(cmd, self.client_list[0])
+        self.all_mounts_procs.append(proc)
+
+        # Wait for IO to complete
+        if not redant.validate_io_procs(self.all_mounts_procs,
+                                        self.mounts[0]):
+            raise Exception("IO failed to complete on the client")
+
+        # Restore snapshot
+        ret = redant.snap_restore_complete(self.vol_name, snapshot_name,
+                                           self.server_list[0])
+        if not ret:
+            raise Exception(f"Failed to restore snapshot {snapshot_name}")
+
+        # Check if heal is completed
+        if not redant.is_heal_complete(self.server_list[0], self.vol_name):
+            raise Exception('Heal is not complete')
+
+        # Wait for volume graph to get loaded.
+        sleep(10)
+
+        # Get arequal after restoring snapshot
+        result_after_restoring = redant.collect_mounts_arequal(self.mounts[0])
+
+        # Checking arequal before creating snapshot
+        # and after restoring snapshot
+        if result_before_snapshot != result_after_restoring:
+            raise Exception('Checksums are not equal')
