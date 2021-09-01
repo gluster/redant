3dd1b21b512068290e4882ff4d56b1c5d6bfc4c5
Nikhil Ladha <nladha@redhat.com>
2021-09-01 10:16:57 +0530

    Migrated TC test_snap_info_glusterd_restart (#1009)
    * Snapshot: Information of snapshots after restarting glusterd
    
    Test Cases in this module tests the snapshot information after
    glusterd is restarted.
    
    Change-Id: I7c5e761d8a8cd261841d064dbd94093e1c5b6edd
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    
    * Add py3 support in 'tests/functional/snapshot'
    
    Change-Id: Ie408d7972452123b63eb5cc17c61bc319a99e304
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Fix name used for snapshot creation
    
    There can be a case where due to a failed tearDown of a previous
    test case, the snapshot creation might fail because of redundant
    snapshot of the same name present.
    
    This fix includes the changed snapshot names along with tearDown
    for deleting the snapshots created in the test case
    "test_snap_info_glusterd_restart.py".
    
    Changing the names of the test case function to something
    relevant to the test case.
    Making use of the method "wait_for_glusterd_to_start" in places
    where glusterd is restarted.
    
    Change-Id: I6cb50dc84d306194b6bd363daf7ae0ebd6bb12ee
    Signed-off-by: Vinayak Papnoi <vpapnoi@redhat.com>
    
    * Migrated TC test_snap_info_glusterd_restart
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix snap info
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: srivickynesh <sselvan@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Vinayak Papnoi <vpapnoi@redhat.com>

---
+++ tests/functional/snapshot/test_snap_info_glusterd_restart.py
@@ -0,0 +1,94 @@
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to test the snapshot information after glusterd is restarted.
+"""
+
+# disruptive;rep,dist,dist-rep,disp,dist-disp
+from tests.d_parent_test import DParentTest
+
+
+class TestSnapshotInfoGlusterdRestart(DParentTest):
+
+    def _snapshot_info(self):
+        """
+        This section checks the snapshot information:
+        * Using snapname
+        * Using volname
+        * Without using snapname or volname
+        """
+        # Check snapshot info using snapname
+        for snap in self.snapshots:
+            ret = self.redant.get_snap_info_by_snapname(snap,
+                                                        self.server_list[0])
+            if not ret:
+                raise Exception("Failed to get snap info")
+
+        # Check snapshot info using volname
+        ret = self.redant.get_snap_info_by_volname(self.vol_name,
+                                                   self.server_list[0])
+        if not ret:
+            raise Exception("Failed to get snap info")
+
+        # Validate snapshot information without using snapname or volname
+        ret = self.redant.get_snap_info(self.server_list[0])
+        if not ret:
+            raise Exception("Failed to get snap info")
+
+        for i, snap in enumerate(self.snapshots):
+            if snap not in ret.keys():
+                raise Exception("Failed to validate snap information")
+
+    def run_test(self, redant):
+        """
+        * Create multiple snapshots
+        * Check snapshot info
+          - Without using snapname or volname
+          - Using snapname
+          - Using volname
+        * Restart glusterd on all servers
+        * Repeat the snapshot info step for all the three scenarios
+          mentioned above
+        """
+        self.snapshots = [(f"snap-test-snap-info-gd-restart-{i}")
+                          for i in range(0, 2)]
+
+        # Create snapshots with description
+        for snap in self.snapshots:
+            redant.snap_create(self.vol_name, f"{snap}",
+                               self.server_list[0],
+                               description="$p3C!@l C#@R@cT#R$")
+
+        # Perform the snapshot info tests before glusterd restart
+        self._snapshot_info()
+
+        # Restart Glusterd on all servers
+        redant.restart_glusterd(self.server_list)
+
+        # Wait for glusterd to be online and validate glusterd running on all
+        # server nodes
+        if not redant.wait_for_glusterd_to_start(self.server_list):
+            raise Exception(f"Glusterd is not running on {self.server_list}")
+
+        # Check if peers are connected
+        if not redant.wait_for_peers_to_connect(self.server_list,
+                                                self.server_list[0]):
+            raise Exception("Peers are not yet in connected state")
+
+        # perform the snapshot info tests after glusterd restart
+        self._snapshot_info()
