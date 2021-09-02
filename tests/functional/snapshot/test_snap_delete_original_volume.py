a02b12906867721ed832086334902353cd9545b6
Nikhil Ladha <nladha@redhat.com>
2021-09-02 10:10:07 +0530

    Migrated TC test_snap_delete_original_volume (#1011)
    * Snapshot: deleting orginal volume and check clone volume
    
    Test Cases in this module tests the
    Creation of clone from snapshot of volume
    and delete snapshot and original volume.
    Validate cloned volume is not affected.
    
    Change-Id: Ic61defe96fed6adeab21b3715878ac8093156645
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Add py3 support in 'tests/functional/snapshot'
    
    Change-Id: Ie408d7972452123b63eb5cc17c61bc319a99e304
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 1)
    
    Removing script_local_path as both script_local_path and
    cls.script_upload_path hold the same values which makes
    each script slow. This will help decrease the execution
    time of the test suite.
    
    PoC:
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    b = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.063s
    user 0m0.039s
    sys  0m0.019s
    
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.013s
    user 0m0.009s
    sys  0m0.003s
    
    Code changes needed:
    
    From:
    script_local_path = ("/usr/share/glustolibs/io/scripts/"
                         "file_dir_ops.py")
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, script_local_path)
    
    To:
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, cls.script_upload_path)
    
    Change-Id: I7908b3b418bbc929b7cc3ff81e3675310eecdbeb
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add function get_unique_lv_list_from_all_servers
    
    Added library to cleanup lv created after snapshot clone and made
    modifications with cleanup.
    
    Change-Id: I71a437bf99eac1170895510475ddb30748771672
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Add sleep in snapshot failures
    
    Add sleep in test_snap_delete_original_volume.py
    after cloning a volume added sleep and then starting
    the volume. Changed io to write in one mount point
    otherwise seeing issues with validate io.
    removed baseclass cleanup because original volume is
    already cleaned up in testcase.
    
    Change-Id: I7bf9686384e238e1afe8491013a3058865343eee
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Migrated TC test_snap_delete_original_volume
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: srivickynesh <sselvan@redhat.com>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/snapshot/test_snap_delete_original_volume.py
@@ -0,0 +1,113 @@
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test Cases in this module tests the creation of clone from snapshot of
+    volume and delete snapshot and original volume.
+    Validate cloned volume is not affected.
+"""
+
+# disruptive;rep,dist,disp,dist-rep,dist-disp
+from time import sleep
+from tests.d_parent_test import DParentTest
+
+
+class TestSnapshotSelfheal(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Steps:
+        1. Create and mount distributed-replicated volume
+        2. Perform I/O on mountpoints
+        3. Create snapshot
+        4. activate snapshot created in step3
+        5. clone created snapshot in step3
+        6. delete original volume
+        7. Validate clone volume
+        """
+        self.clone = "clone1"
+        self.mpoint = "/mnt/clone1"
+        self.snap = "snap1"
+
+        # Perform I/O
+        all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
+        # Create files
+        for mount_obj in self.mounts:
+            proc = redant.create_files('1k', mount_obj['mountpath'],
+                                       mount_obj['client'], 100)
+            all_mounts_procs.append(proc)
+
+        # Validate IO
+        if not self.redant.validate_io_procs(all_mounts_procs, self.mounts):
+            raise Exception("IO failed on some of the clients")
+
+        # Creating snapshot
+        redant.snap_create(self.vol_name, self.snap, self.server_list[0])
+
+        # Activating snapshot
+        redant.snap_activate(self.snap, self.server_list[0])
+
+        # snapshot list
+        ret = redant.snap_list(self.server_list[0])
+        if self.snap not in ret['msg']['snapList']['snapshot']:
+            raise Exception(f"Failed to validate snapshot {self.snap}"
+                            " in snap list")
+
+        # Creating a Clone of snapshot:
+        redant.snap_clone(self.snap, self.clone, self.server_list[0])
+
+        # After cloning a volume wait for 5 second to start the volume
+        sleep(5)
+
+        # Validate clone volumes are started
+        redant.volume_start(self.clone, self.server_list[0])
+
+        for mount_obj in self.mounts:
+            # Unmount Volume
+            redant.volume_unmount(self.vol_name, mount_obj['mountpath'],
+                                  mount_obj['client'])
+
+        # Delete original volume
+        ret = redant.cleanup_volume(self.server_list[0], self.vol_name)
+        if not ret:
+            raise Exception(f"Failed to delete the volume: {self.vol_name}")
+
+        # get volume info
+        vol_info = redant.get_volume_info(self.server_list[0], self.clone)
+        if not vol_info:
+            raise Exception("Failed to get volume info "
+                            f"for cloned volume {self.clone}")
+        if vol_info[self.clone]['statusStr'] != 'Started':
+            raise Exception("Unexpected: cloned volume is not started "
+                            f"{self.clone}")
+
+        # Volume status
+        ret = redant.get_volume_status(self.clone, self.server_list[0])
+        if not ret:
+            raise Exception(f"Failed to get volume status for {self.clone}")
+
+        if self.clone not in ret.keys():
+            raise Exception("Failed to get volume status for volume "
+                            f"{self.clone}")
+
+        # Volume list validate
+        ret = redant.get_volume_list(self.server_list[0])
+        if self.clone not in ret:
+            raise Exception("Failed to validate volume list for volume "
+                            f" {self.clone}")
