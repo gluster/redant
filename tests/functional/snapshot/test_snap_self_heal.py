a221ba166ee21fb36d5649b6dbd6e9aa95ee2d9e
Nikhil Ladha <nladha@redhat.com>
2021-09-02 14:10:09 +0530

    Migrated TC test_snap_self_heal (#1019)
    * Snapshot: Create clone and check for self heal
    
    Create a clone and check for self heal operation on
    the cloned volume.
    
    Change-Id: Icf61f996fcd503a6c0d0bf936900858b715a4742
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    
    * Add py3 support in 'tests/functional/snapshot'
    
    Change-Id: Ie408d7972452123b63eb5cc17c61bc319a99e304
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 1)
    
    Removing script_local_path as both script_local_path and
    cls.script_upload_path hold the same values which makes
    each script slow. This will help decrease the execution
    time of the test suite.
    
    PoC:
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    b = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.063s
    user 0m0.039s
    sys  0m0.019s
    
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.013s
    user 0m0.009s
    sys  0m0.003s
    
    Code changes needed:
    
    From:
    script_local_path = ("/usr/share/glustolibs/io/scripts/"
                         "file_dir_ops.py")
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, script_local_path)
    
    To:
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, cls.script_upload_path)
    
    Change-Id: I7908b3b418bbc929b7cc3ff81e3675310eecdbeb
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 5)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I5319ce497ca3359e0e7dbd9ece481bada1ee2205
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove hot and cold bricks list on regular volumes
    
    For the non-tiered volume types, In few test cases while bringing
    bricks offline, collecting both hot_tier_bricks and cold_tier_bricks
    and it is not needed to collect hot and cold tier bricks.
    
    Removing the hot and cold tiered bricks and collecting only bricks
    of the particular volume as mentioned below.
    
    Removing below section
    ```
    bricks_to_bring_offline_dict = (select_bricks_to_bring_offline(
                    self.mnode, self.volname))
    bricks_to_bring_offline = list(filter(None, (
    bricks_to_bring_offline_dict['hot_tier_bricks'] +
    bricks_to_bring_offline_dict['cold_tier_bricks'] +
    bricks_to_bring_offline_dict['volume_bricks'])))
    ```
    
    Modifying as below for bringing bricks offline.
    ```
    bricks_to_bring_offline = bricks_to_bring_offline_dict['volume_bricks']
    ```
    
    Change-Id: Icb1dc4a79cf311b686d839f2c9390371e42142f7
    Signed-off-by: Bala Konda Reddy M <bala12352@gmail.com>
    
    * Migrated TC test_snap_self_heal
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix volname
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: srivickynesh <sselvan@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>
    Co-authored-by: Bala Konda Reddy M <bala12352@gmail.com>

---
+++ tests/functional/snapshot/test_snap_self_heal.py
@@ -0,0 +1,169 @@
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test Cases in this module tests the creation of clone from snapshot
+    of volume.
+"""
+
+# disruptive;dist-rep
+from tests.d_parent_test import DParentTest
+
+
+class TestSnapshotSelfheal(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Steps:
+        1. create a volume
+        2. mount volume
+        3. create snapshot of that volume
+        4. Activate snapshot
+        5. Clone snapshot and Mount
+        6. Perform I/O
+        7. Bring Down Few bricks from volume without
+           affecting the volume or cluster.
+        8. Perform I/O
+        9. Bring back down bricks to online
+        10. Validate heal is complete with areequal
+        """
+        self.snap = "snap1"
+        self.clone = "clone1"
+        self.mount1 = "/mnt/clone1"
+
+        # Creating snapshot:
+        redant.snap_create(self.vol_name, self.snap, self.server_list[0])
+
+        # Activating snapshot
+        redant.snap_activate(self.snap, self.server_list[0])
+
+        # snapshot list
+        redant.snap_list(self.server_list[0])
+
+        # Creating a Clone volume from snapshot:
+        redant.snap_clone(self.snap, self.clone, self.server_list[0])
+
+        #  start clone volumes
+        redant.volume_start(self.clone, self.server_list[0])
+
+        # Mounting a clone volume
+        redant.execute_abstract_op_node(f"mkdir -p {self.mount1}",
+                                        self.client_list[0])
+        redant.volume_mount(self.server_list[0], self.clone, self.mount1,
+                            self.client_list[0])
+
+        # Checking cloned volume mounted or not
+        if not redant.is_mounted(self.clone, self.mount1, self.client_list[0],
+                                 self.server_list[0]):
+            raise Exception("Cloned Volume not mounted on mount point: "
+                            f"{self.mount1}")
+
+        # write files on all mounts
+        proc = redant.create_files('1k', self.mount1, self.client_list[0], 10,
+                                   'file')
+
+        # Validate IO
+        self.mounts = {
+            "client": self.client_list[0],
+            "mountpath": self.mount1
+        }
+        if not redant.validate_io_procs(proc, self.mounts):
+            raise Exception("IO failed on some of the clients")
+
+        # get the bricks from the volume
+        bricks_list = redant.get_all_bricks(self.clone, self.server_list[0])
+        if not bricks_list:
+            raise Exception("Failed to get the brick list")
+
+        # Select bricks to bring offline
+        offline_bricks = (redant.select_volume_bricks_to_bring_offline(
+                          self.clone, self.server_list[0]))
+
+        if not redant.bring_bricks_offline(self.clone, offline_bricks):
+            raise Exception("Failed to bring the bricks offline")
+
+        # Offline Bricks list
+        offline_bricks_list = (redant.get_offline_bricks_list(self.clone,
+                               self.server_list[0]))
+        if not offline_bricks_list:
+            raise Exception("Failed to get offline bricklist"
+                            f"for volume {self.clone}")
+
+        for bricks in offline_bricks_list:
+            if bricks not in offline_bricks:
+                raise Exception("Failed to validate offline bricks list")
+
+        # Online Bricks list
+        online_bricks = redant.get_online_bricks_list(self.clone,
+                                                      self.server_list[0])
+        if not online_bricks:
+            raise Exception("Failed to get online bricklist"
+                            f"for volume {self.clone}")
+
+        # write files mountpoint
+        proc = redant.create_files('1k', self.mount1, self.client_list[0], 10)
+
+        # Validate IO
+        if not redant.validate_io_procs(proc, self.mounts):
+            raise Exception("IO failed on some of the clients")
+
+        # Bring all bricks online
+        redant.bring_bricks_online(self.clone, self.server_list,
+                                   offline_bricks)
+
+        # Validate Bricks are online
+        if not redant.are_bricks_online(self.clone, bricks_list,
+                                        self.server_list[0]):
+            raise Exception("Failed to bring all the bricks online")
+
+        # Wait for volume processes to be online
+        if not (redant.wait_for_volume_process_to_be_online(self.clone,
+                self.server_list[0], self.server_list)):
+            raise Exception("Few volume processess are offline for the "
+                            f"volume: {self.clone}")
+
+        # Verify volume's all process are online
+        if not (redant.verify_all_process_of_volume_are_online(self.clone,
+                self.server_list[0])):
+            raise Exception("Few process after volume start are offline for"
+                            f" volume: {self.clone}")
+
+        # wait for the heal process to complete
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.clone):
+            raise Exception("Heal has not yet completed")
+
+        # Check arequal
+        # get the subvolumes
+        subvols = redant.get_subvols(self.clone, self.server_list[0])
+        if not subvols:
+            raise Exception("Failed to get the subvols for the volume")
+        num_subvols = len(subvols)
+
+        # Get arequals and compare
+        for i in range(0, num_subvols):
+            # Get arequal for first brick
+            arequal = self.redant.collect_bricks_arequal(subvols[i][0])
+            first_brick_total = arequal[0][-1].split(':')[-1]
+
+            # Get arequal for every brick and compare with first brick
+            arequals = self.redant.collect_bricks_arequal(subvols[i])
+            for arequal in arequals:
+                brick_total = arequal[-1].split(':')[-1]
+                if first_brick_total != brick_total:
+                    raise Exception('Arequals for subvol and brick are '
+                                    'not equal')
