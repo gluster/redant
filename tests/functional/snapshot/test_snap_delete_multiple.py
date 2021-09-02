7a8f6ec05dc811e79511b8fa66515fabfaf214e0
Nikhil Ladha <nladha@redhat.com>
2021-09-02 10:07:31 +0530

    Migrated TC test_snap_delete_multiple (#1010)
    * Snapshot: Delete Multiple snapshots of volumes simultaneously
    
    Delete multiple snapshots of the original and cloned volumes
    simultaneously.
    
    Change-Id: Ic5165b896d2adf4cb51ad4252b7caad7bb6f0892
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    Signed-off-by: Sri <sselvan@redhat.com>
    
    * Add py3 support in 'tests/functional/snapshot'
    
    Change-Id: Ie408d7972452123b63eb5cc17c61bc319a99e304
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 1)
    
    Removing script_local_path as both script_local_path and
    cls.script_upload_path hold the same values which makes
    each script slow. This will help decrease the execution
    time of the test suite.
    
    PoC:
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    b = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.063s
    user 0m0.039s
    sys  0m0.019s
    
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.013s
    user 0m0.009s
    sys  0m0.003s
    
    Code changes needed:
    
    From:
    script_local_path = ("/usr/share/glustolibs/io/scripts/"
                         "file_dir_ops.py")
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, script_local_path)
    
    To:
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, cls.script_upload_path)
    
    Change-Id: I7908b3b418bbc929b7cc3ff81e3675310eecdbeb
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add steps to stabilize contents in snapshot - part2
    
    used library functions to wait for glusterd to start
    and wait for peer to connect and made modifications in teardown part
    to rectified statements to correct values
    
    Change-Id: I40b4362ae1491acf75681c7623c16c53213bb1b9
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Remove python version dependency(Part 5)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I5319ce497ca3359e0e7dbd9ece481bada1ee2205
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Fix I/O of test_snap_delete_multiple
    
    Problem:
    test_snap_delete_multiple fails on I/O validation across all automation
    runs constantly with the below trace back:
    Traceback (most recent call last):
      File "/usr/share/glustolibs/io/scripts/file_dir_ops.py", line 1246, in <module>
        rc = args.func(args)
      File "/usr/share/glustolibs/io/scripts/file_dir_ops.py", line 374, in create_files
        base_file_name, file_types)
      File "/usr/share/glustolibs/io/scripts/file_dir_ops.py", line 341, in _create_files
        ret = pool.map(lambda file_tuple: _create_file(*file_tuple), files)
      File "/usr/lib64/python2.7/multiprocessing/pool.py", line 250, in map
        return self.map_async(func, iterable, chunksize).get()
      File "/usr/lib64/python2.7/multiprocessing/pool.py", line 554, in get
        raise self._value
    IOError: [Errno 17] File exists: '/mnt/testvol_distributed_glusterfs/testfile42.txt'
    
    Fix:
    Change the I/O to use --base-file-name parameter when running the I/O
    scripts.
    
    Change-Id: Ic5a8222f4fafeac4ac9aadc9c4d23327711ed9f0
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_snap_delete_multiple
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix object name
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Make mounts dict list
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Mart the TC as excluded
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: srivickynesh <sselvan@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/snapshot/test_snap_delete_multiple.py
@@ -0,0 +1,174 @@
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TCs in this module tests the creation of clone from snapshot of volume.
+"""
+
+# disruptive;rep,dist,disp,dist-rep,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
+
+
+class TestSnapshotCloneDeleteMultiple(DParentTest):
+
+    def terminate(self):
+        """
+        Disable activate-on-create option in snapshot config
+        """
+        try:
+            option = {'activate-on-create': 'disable'}
+            self.redant.set_snap_config(option, self.server_list[0])
+
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def _io_operation(self, name):
+        """
+        Perform IO and validate it
+        """
+        # Perform I/O
+        all_mounts_procs = []
+        for mount_obj in self.mounts:
+            # Create files
+            proc = self.redant.create_files('1k', mount_obj['mountpath'],
+                                            mount_obj['client'], 100, name)
+            all_mounts_procs.append(proc)
+
+        # Validate IO
+        if not self.redant.validate_io_procs(all_mounts_procs, self.mounts):
+            raise Exception("IO failed on some of the clients")
+
+    def _create_and_clone_snap(self, value, snap, clone, counter):
+        """
+        Create snaps, clone them and start the cloned snap volume
+        """
+        # Creating snapshots
+        for snap_count in value:
+            self.redant.snap_create(self.vol_name, f"snap{snap_count}",
+                                    self.server_list[0])
+
+        # Validate snapshot list
+        ret = self.redant.snap_list(self.server_list[0])
+        if int(ret['msg']['snapList']['count']) != counter:
+            raise Exception("Failed to validate snapshots with"
+                            "expected number of snapshots")
+
+        if counter == 40:
+            return 0
+
+        # Creating a Clone of snapshot:
+        self.redant.snap_clone(snap, clone, self.server_list[0])
+
+        # Start cloned volumes
+        self.redant.volume_start(clone, self.server_list[0])
+
+        # log Cloned Volume information
+        if not self.redant.log_volume_info_and_status(self.server_list[0],
+                                                      clone):
+            raise Exception(f"Failed to Log Info, Status of Volume {clone}")
+
+        return counter+10
+
+    def _mount_clone_and_io(self, clone, mpoint):
+        """
+        Mount snapshot clones on client and validate
+        """
+        # Create mountpoint on client
+        self.redant.execute_abstract_op_node(f"mkdir -p {mpoint}",
+                                             self.client_list[0])
+
+        # Mounting a volume
+        self.redant.volume_mount(self.server_list[0], clone, mpoint,
+                                 self.client_list[0])
+
+        # Checking volume mounted or not
+        if not self.redant.is_mounted(clone, mpoint, self.client_list[0],
+                                      self.server_list[0]):
+            raise Exception(f"Volume not mounted on mount point: {mpoint}")
+
+    def run_test(self, redant):
+        """
+        Steps:
+        1. create and mount volume
+        2. Create 20 snapshots
+        3. Clone one of the snapshot
+        4. mount the clone volume
+        5. Perform I/O on mounts
+        6. Create 10 more snapshots
+        7. create Clone volume from latest snapshot
+           and Mount it
+        8. Perform I/O
+        9. Create 10 more snapshot
+        10. Validate total number of
+            snapshots created.
+        11. Delete all created snapshots.
+        """
+        self.snap1 = "snap1"
+        self.snap2 = "snap21"
+        self.clone1 = "clone1"
+        self.clone2 = "clone2"
+        self.mpoint1 = "/mnt/clone1"
+        self.mpoint2 = "/mnt/clone2"
+
+        # Enable Activate on create
+        option = {'activate-on-create': 'enable'}
+        redant.set_snap_config(option, self.server_list[0])
+
+        value1 = list(range(0, 20))
+        value2 = list(range(20, 30))
+        value3 = list(range(30, 40))
+        # Clone and start clone1
+        ret1 = self._create_and_clone_snap(value1, self.snap1, self.clone1,
+                                           counter=20)
+        if ret1 != 30:
+            raise Exception("Failed to create and clone snap")
+
+        # Mount clone1
+        self._mount_clone_and_io(self.clone1, self.mpoint1)
+
+        # Perform IO on mountpoint of clone1
+        self.mounts = [{
+            "client": self.client_list[0],
+            "mountpath": self.mpoint1
+        }]
+        self._io_operation("first")
+
+        # Clone and start clone2
+        ret3 = self._create_and_clone_snap(value2, self.snap2, self.clone2,
+                                           ret1)
+        if ret3 != 40:
+            raise Exception("Failed to create and clone snap")
+
+        # Mount clone2
+        self._mount_clone_and_io(self.clone2, self.mpoint2)
+
+        # Perform IO on mountpoint of clone1
+        self.mounts = [{
+            "client": self.client_list[0],
+            "mountpath": self.mpoint2
+        }]
+        self._io_operation("second")
+
+        # Create 10 more snapshots
+        ret1 = self._create_and_clone_snap(value3, self.snap2, self.clone2,
+                                           ret3)
+        if ret1 != 0:
+            raise Exception("Failed to create snapshots")
