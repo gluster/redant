e2164eb31f85f0b549f77b2949e8cb0d24b285c4
nik-redhat <nladha@redhat.com>
2021-09-03 17:16:33 +0530

    Mirgated the TC test_snap_delete_existing_scheduler
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/snapshot/test_snap_delete_existing_scheduler.py
+++ tests/functional/snapshot/test_snap_delete_existing_scheduler.py
@@ -1,134 +1,87 @@
-#  Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to validate snapshot scheduler behavior when existing schedule
+    is deleted.
+"""
+
+# disruptive;rep,dist,dist-rep,disp,dist-disp
+import traceback
 from time import sleep
-
-from glusto.core import Glusto as g
-
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.gluster.volume_ops import get_volume_info
-from glustolibs.gluster.shared_storage_ops import (enable_shared_storage,
-                                                   is_shared_volume_mounted,
-                                                   disable_shared_storage)
-from glustolibs.gluster.snap_scheduler import (scheduler_enable,
-                                               scheduler_init,
-                                               scheduler_status,
-                                               scheduler_disable,
-                                               scheduler_add_jobs,
-                                               scheduler_delete,
-                                               scheduler_list)
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['replicated', 'distributed-replicated', 'dispersed',
-           'distributed-dispersed', 'distributed'],
-          ['glusterfs']])
-class SnapshotDeleteExistingScheduler(GlusterBaseClass):
+class TestSnapshotDeleteExistingScheduler(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.get_super_method(cls, 'setUpClass')()
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts "
-                                 "to clients %s" % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-
-        # SettingUp volume and Mounting the volume
-        self.get_super_method(self, 'setUp')()
-        g.log.info("Starting to SetUp and Mount Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to setup volume %s" % self.volname)
-        g.log.info("Volume %s has been setup successfully", self.volname)
-
-        # Enable Shared storage
-        g.log.info("Starting to enable shared storage")
-        ret = enable_shared_storage(self.mnode)
-        if not ret:
-            raise ExecutionError("Unexpected: Failed to enable shared storage")
-        g.log.info("Successfully enabled shared storage as expected")
-
-        # Validate shared storage mounted
-        g.log.info("Starting to validate shared storage mounted")
-        count = 0
-        while count < 5:
-            ret = is_shared_volume_mounted(self.mnode)
-            if ret:
-                break
-            sleep(2)
-            count += 1
-        if not ret:
-            raise ExecutionError("Failed to mount shared volume")
-        g.log.info("Successfully mounted shared volume")
-
-        # Validate shared storage is enabled
-        g.log.info("Starting to validate shared storage volume")
-        volinfo = get_volume_info(self.mnode, "gluster_shared_storage")
-        if ((volinfo['gluster_shared_storage']['options']
-             ['cluster.enable-shared-storage']) != 'enable'):
-            raise ExecutionError("Unexpected: shared storage is disabled")
-        g.log.info("Shared storage enabled successfully as expected")
-
-    def tearDown(self):
-
-        # Disable snap scheduler
-        g.log.info("Starting to disable snapshot scheduler on all nodes")
-        ret, _, _ = scheduler_disable(self.mnode)
-        if ret != 0:
-            raise ExecutionError("Failed to disable snap scheduler "
-                                 "on all nodes")
-        g.log.info("Successfully disabled snapshot scheduler on all nodes")
-
-        # Check if shared storage is enabled
-        # Disable if true
-        g.log.info("Checking if shared storage is mounted")
-        ret = is_shared_volume_mounted(self.mnode)
-        if ret:
-            g.log.info("Disabling shared storage")
-            ret = disable_shared_storage(self.mnode)
-            if not ret:
-                raise ExecutionError("Failed to disable shared storage")
-            g.log.info("Successfully disabled shared storage")
-
-        # Unmount and cleanup-volume
-        g.log.info("Starting to Unmount and cleanup-volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Unmount and Cleanup Volume")
-        g.log.info("Successful in Unmount Volume and Cleanup Volume")
-
-    def test_snap_delete_existing_scheduler(self):
-        # pylint: disable=too-many-statements
+    def terminate(self):
         """
-        Description:
+        Wait for IO to finish if the TC fails midway.
+        Disable snap_scheduler and shared_storage
+        """
+        try:
+            if self.is_io_running:
+                io_ret = (self.redant.wait_for_io_to_complete(
+                          self.all_mounts_procs, self.mounts))
 
-        Validating snapshot scheduler behavior when existing schedule
-        is deleted.
+            # Disable snap scheduler
+            self.redant.scheduler_disable(self.server_list[0], False)
 
+            # Check snapshot scheduler status
+            for server in self.server_list:
+                count = 0
+                while count < 40:
+                    ret = self.redant.scheduler_status(server, False)
+                    status = ret['msg'][0].split(':')
+                    if len(status) == 3:
+                        status = status[2].strip()
+                        if ret['error_code'] == 0 \
+                           and status == "Disabled":
+                            break
+                    sleep(2)
+                    count += 1
+                if ret['error_code'] != 0:
+                    snap_stat = False
+
+            # Check if shared storage is enabled
+            # Disable if true
+            ret = self.redant.is_shared_volume_mounted(self.server_list)
+            if ret:
+                ret = self.redant.disable_shared_storage(self.server_list[0])
+                if not ret:
+                    shared_stor = False
+
+            if not io_ret:
+                raise Exception("Failed to wait for IO to complete")
+
+            if not snap_stat:
+                raise Exception("Failed to check status of scheduler")
+
+            if not shared_stor:
+                raise Exception("Failed to disable shared storage")
+
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
         Steps:
         * Enable shared volume
         * Create a volume
@@ -142,84 +95,78 @@ class SnapshotDeleteExistingScheduler(GlusterBaseClass):
         * Validate IO is successful
         * Perform snap_scheduler list
         """
+        self.is_io_running = False
+
+        # Enable shared storage
+        ret = redant.enable_shared_storage(self.server_list[0])
+        if not ret:
+            raise Exception("Failed to enable shared storage")
+
+        # Validate shared storage volume is mounted
+        ret = redant.is_shared_volume_mounted(self.server_list[0], timeout=10)
+        if not ret:
+            raise Exception("Failed to validate if shared volume is mounted")
 
         # Initialise snap scheduler
-        g.log.info("Initialising snap_scheduler on all servers")
         count = 0
-        while count < 80:
-            ret = scheduler_init(self.servers)
+        sleep(2)
+        while count < 40:
+            ret = redant.scheduler_init(self.server_list)
             if ret:
                 break
             sleep(2)
             count += 1
-        self.assertTrue(ret, "Failed to initialise scheduler on all servers")
-        g.log.info("Successfully initialised scheduler on all servers")
+        if not ret:
+            raise Exception("Failed to initialize scheduler on all nodes")
 
         # Enable snap scheduler
-        g.log.info("Enabling snap_scheduler")
-        ret, _, _ = scheduler_enable(self.mnode)
-        self.assertEqual(ret, 0, "Failed to enable scheduler on node %s"
-                         % self.mnode)
-        g.log.info("Successfully enabled scheduler on node %s", self.mnode)
+        redant.scheduler_enable(self.server_list[0])
 
         # Validate snapshot scheduler status
-        g.log.info("Validating status of snap_scheduler")
-        for server in self.servers:
+        for server in self.server_list:
             count = 0
             while count < 40:
-                ret, status, _ = scheduler_status(server)
-                if status.strip().split(":")[2] == ' Enabled':
-                    break
+                ret = redant.scheduler_status(server, False)
+                status = ret['msg'][0].split(':')
+                if len(status) == 3:
+                    status = status[2].strip()
+                    if ret['error_code'] == 0 and status == "Enabled":
+                        break
                 sleep(2)
-                count += 2
-        self.assertEqual(status.strip().split(":")[2], ' Enabled',
-                         "Failed to validate status of scheduler")
-        g.log.info("Successfully validated scheduler status")
+                count += 1
+            if ret['error_code'] != 0:
+                raise Exception("Failed to check status of scheduler"
+                                f" on node {server}")
 
         # Write files on all mounts
-        g.log.info("Starting IO on all mounts...")
-        all_mounts_procs = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.all_mounts_procs = []
         for mount_obj in self.mounts:
-            cmd = ("/usr/bin/env python %s create_files "
-                   "-f 10 --base-file-name file %s" % (
-                       self.script_upload_path,
-                       mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            all_mounts_procs.append(proc)
+            proc = redant.create_files('1k', {mount_obj['mountpath']},
+                                       mount_obj['client'], 10, 'file')
+            self.all_mounts_procs.append(proc)
+            self.is_io_running = True
 
         # Add a job to schedule snapshot every 30 mins
-        g.log.info("Starting to add new job")
         self.scheduler = r"*/30 * * * *"
         self.job_name = "Job1"
-        ret, _, _ = scheduler_add_jobs(self.mnode, self.job_name,
-                                       self.scheduler, self.volname)
-        self.assertEqual(ret, 0, "Failed to add job")
-        g.log.info("Successfully added Job on volume %s", self.volname)
+        redant.scheduler_add_jobs(self.server_list[0], self.job_name,
+                                  self.scheduler, self.vol_name)
 
         # Perform snap_scheduler list
-        g.log.info("Starting to list all scheduler jobs")
-        ret, _, _ = scheduler_list(self.mnode)
-        self.assertEqual(ret, 0, "Failed to list scheduler jobs")
-        g.log.info("Successfully listed all jobs")
+        redant.scheduler_list(self.server_list[0])
 
         # Delete scheduled job
-        g.log.info("Starting to delete scheduled jobs")
-        ret, _, _ = scheduler_delete(self.mnode, self.job_name)
-        self.assertEqual(ret, 0, "Failed to delete scheduled job")
-        g.log.info("Successfully deleted scheduled job %s", self.job_name)
+        redant.scheduler_delete(self.server_list[0], self.job_name)
 
         # Validate IO
-        self.assertTrue(
-            validate_io_procs(all_mounts_procs, self.mounts),
-            "IO failed on some of the clients"
-        )
+        if not redant.validate_io_procs(self.all_mounts_procs, self.mounts):
+            raise Exception("IO failed.")
+        self.is_io_running = False
 
         # Perform snap_scheduler list (no active jobs should be present)
-        g.log.info("Starting to list all scheduler jobs")
-        ret, out, _ = scheduler_list(self.mnode)
-        self.assertEqual(ret, 0, "Failed to list scheduler jobs")
-        ret1 = out.strip().split(":")
-        self.assertEqual(ret1[1], " No snapshots scheduled", "Unexpected: "
-                         "Jobs are getting listed even after being deleted")
-        g.log.info("Expected: No snapshots Jobs scheduled")
+        ret = redant.scheduler_list(self.server_list[0])
+        out = ret['msg'][0].split(":")[1].strip()
+        if out != "No snapshots scheduled":
+            raise Exception("Unexpected: Jobs are getting listed even after "
+                            "being deleted")
