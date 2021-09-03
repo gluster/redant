84af967ef71fb4296601424914cdff022b232e5f
Nikhil Ladha <nladha@redhat.com>
2021-09-03 11:52:54 +0530

    Migrated TC test_validate_snapshot_max_limit (#1023)
    * test : snapshot max-hard-limit and max-soft-limit
    
    The purpose of this test is to validate snapshot
    hard and soft max-limt options.
    
    Change-Id: I63e661549977251104c120d7b25422ff57fdadeb
    Signed-off-by: Sunny Kumar <sunkumar@redhat.com>
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    
    * snapshot:: configuring snapshot max-hard-limit and max-soft-limit case
    
    The purpose of this test is to validate snapshot
    hard and soft max-limt options.
    
    Change-Id: I000b840421620094287ba393367cad7c8df8e23a
    Signed-off-by: srivickynesh <sselvan@redhat.com>
    
    * Fix spelling mistake across the codebase
    
    Change-Id: I46fc2feffe6443af6913785d67bf310838532421
    
    * Add py3 support in 'tests/functional/snapshot'
    
    Change-Id: Ie408d7972452123b63eb5cc17c61bc319a99e304
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Fix bunch of py3 incompatibilities
    
    Change-Id: I2e85670e50e3dab8727295c34aa6ec4f1326c19d
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 1)
    
    Removing script_local_path as both script_local_path and
    cls.script_upload_path hold the same values which makes
    each script slow. This will help decrease the execution
    time of the test suite.
    
    PoC:
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    b = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.063s
    user 0m0.039s
    sys  0m0.019s
    
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.013s
    user 0m0.009s
    sys  0m0.003s
    
    Code changes needed:
    
    From:
    script_local_path = ("/usr/share/glustolibs/io/scripts/"
                         "file_dir_ops.py")
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, script_local_path)
    
    To:
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, cls.script_upload_path)
    
    Change-Id: I7908b3b418bbc929b7cc3ff81e3675310eecdbeb
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 5)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I5319ce497ca3359e0e7dbd9ece481bada1ee2205
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_validate_snapshot_max_limit
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Mark TC as excluded
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix error string
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Sunny Kumar <sunkumar@redhat.com>
    Co-authored-by: srivickynesh <sselvan@redhat.com>
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/snapshot/test_validate_snapshot_max_limit.py
@@ -0,0 +1,152 @@
+"""
+ Copyright (C) 2017-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    The purpose of this test is to validate snapshot
+    hard and soft max-limt options.
+"""
+
+# disruptive;rep,dist-rep,disp,dist,dist-disp
+# TODO: NFS,CIFS
+from tests.d_parent_test import DParentTest
+
+
+class TestSnapCreateMax(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Steps :
+        1. Create and start a volume
+        2. Mount the volume on a client
+        3. Perform some heavy IO
+        4. Varify IO
+        5. modify max snap limit to default to 10.
+        6. modify soft-limit to 50%
+        6. Create 5 snapshots
+        7. Varify 5 created successfully
+        8. Create 6th snapshot -  check for warning
+           -- it should not fail.
+        9. modify soft-limit to 100%
+        10. Create 7th snapshot -  check for warning
+           -- it should not show warning.
+        11. Reach the snap-max-hard-limit by creating more snapshots
+        12. Create 11th snapshot - check for failure
+           -- it shoul fail.
+        13. varify the no. of snpas it should be 10.
+        14. modify max snap limit to 20
+        15. create 10 more snaps
+        16. varify the no. of snpas it should be 20
+        """
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
+        # Start IO on all mounts.
+        all_mounts_procs = []
+        count = 1
+        for mount_obj in self.mounts:
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      count, 2, 5, 5, 5,
+                                                      mount_obj['client'])
+            all_mounts_procs.append(proc)
+            count = count + 10
+
+        # Validate IO
+        if not redant.validate_io_procs(all_mounts_procs, self.mounts):
+            raise Exception("IO failed.")
+
+        # Get stat of all the files/dirs created.
+        ret = redant.get_mounts_stat(self.mounts)
+        if not ret:
+            raise Exception("Stat failed on some of the clients")
+
+        # set config snap-max-hard-limit for 10 snpas
+        max_hard_limit = {'snap-max-hard-limit': '10'}
+        redant.set_snap_config(max_hard_limit, self.server_list[0],
+                               self.vol_name)
+
+        # set config snap-max-soft-limit to 50%
+        max_soft_limit = {'snap-max-soft-limit': '50'}
+        redant.set_snap_config(max_soft_limit, self.server_list[0])
+
+        # Create 5 snaps
+        for i in range(1, 6):
+            redant.snap_create(self.vol_name, f"snapy{i}",
+                               self.server_list[0])
+
+        # Check for no. of snaps using snap_list it should be 5
+        snap_list = redant.get_snap_list(self.server_list[0], self.vol_name)
+        if len(snap_list) != 5:
+            raise Exception(f"Expected 5 snapshots. Found {len(snap_list)}"
+                            " snapshots")
+
+        # Validate all 5 snap names present in snap_list
+        for i in range(1, 6):
+            if f"snapy{i}" not in snap_list:
+                raise Exception(f"snapy{i} snap not found in {snap_list}")
+
+        # create 6th snapshot
+        redant.snap_create(self.vol_name, "snapy6", self.server_list[0])
+
+        # set config snap-max-soft-limit to 100%
+        max_soft_limit = {'snap-max-soft-limit': '100'}
+        redant.set_snap_config(max_soft_limit, self.server_list[0])
+
+        # create 7th snapshot
+        redant.snap_create(self.vol_name, "snapy7", self.server_list[0])
+
+        # Create 3 snaps
+        for i in range(8, 11, 1):
+            redant.snap_create(self.vol_name, f"snapy{i}",
+                               self.server_list[0])
+
+        # Check for no. of snaps using snap_list it should be 10
+        snap_list = redant.get_snap_list(self.server_list[0], self.vol_name)
+        if len(snap_list) != 10:
+            raise Exception(f"Expected 10 snapshots. Found {len(snap_list)}"
+                            " snapshots")
+
+        # Validate all 10 snap names created
+        for i in range(1, 11, 1):
+            if f"snapy{i}" not in snap_list:
+                raise Exception(f"snapy{i} snap not found in {snap_list}")
+
+        # create 11th snapshot
+        ret = redant.snap_create(self.vol_name, "snapy11",
+                                 self.server_list[0], excep=False)
+        if ret['msg']['opRet'] == '0':
+            raise Exception("Unexpected: Successfully created 11th snapshot")
+
+        # Check for no. of snaps using snap_list it should be 10
+        snap_list = redant.get_snap_list(self.server_list[0], self.vol_name)
+        if len(snap_list) != 10:
+            raise Exception(f"Expected 10 snapshots. Found {len(snap_list)}"
+                            " snapshots")
+
+        # modify config snap-max-hard-limit for 20 snpas
+        max_hard_limit = {'snap-max-hard-limit': '20'}
+        redant.set_snap_config(max_hard_limit, self.server_list[0],
+                               self.vol_name)
+
+        # Create 10 snaps
+        for i in range(11, 21, 1):
+            redant.snap_create(self.vol_name, f"snapy{i}",
+                               self.server_list[0])
+
+        # Check for no. of snaps using snap_list it should be 20
+        snap_list = redant.get_snap_list(self.server_list[0], self.vol_name)
+        if len(snap_list) != 20:
+            raise Exception(f"Expected 20 snapshots. Found {len(snap_list)}"
+                            " snapshots")
