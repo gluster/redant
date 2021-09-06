119429b0b0b63686be1a2ebcd00b0d0868b28832
Nikhil Ladha <nladha@redhat.com>
2021-09-06 15:50:35 +0530

    Migrated TC test_subdir_mount (#1035)
    * Test to verify Fuse sub directory mount operation
    
    Change-Id: I6cdb3122af071f9f7bcfaeba8427e5c4aad8a4ec
    
    * Add py3 support in 'tests/functional/fuse_subdir'
    
    Change-Id: I56194047ac6105cddb43d3f413b03bcdbbcde587
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Migrated TC test_subdir_mount
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Jilju Joy <jijoy@localhost.localdomain>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>

---
+++ tests/functional/fuse_subdir/test_subdir_mount.py
@@ -0,0 +1,70 @@
+"""
+ Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test Cases in this module tests the Fuse sub directory feature
+"""
+
+# disruptive;dist,rep,dist-rep,disp,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
+
+
+class TestFuseSubDirMount(DParentTest):
+
+    def terminate(self):
+        """
+        Unmount the subdirs mounted in the TC, if the TC fails midway
+        """
+        try:
+            if self.is_mounted:
+                cmd = f"umount {self.mountpoint}"
+                self.redant.execute_abstract_op_node(cmd, self.client_list[0],
+                                                     False)
+
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
+        Steps:
+        1. Create a sub-directory on the mounted volume.
+        2. Unmount volume.
+        3. Mount sub-directory on client. This should pass.
+        """
+        self.is_mounted = False
+
+        # Creating a sub directory on mounted volume
+        redant.create_dir(self.mountpoint, "d1", self.client_list[0])
+
+        # Unmount volume
+        redant.volume_unmount(self.vol_name, self.mountpoint,
+                              self.client_list[0])
+
+        # Mounting sub directory on client.
+        volname = f"{self.vol_name}/d1"
+        redant.authenticated_mount(volname, self.server_list[0],
+                                   self.mountpoint, self.client_list[0])
+        self.is_mounted = True
+
+        # Unmount sub directories
+        cmd = f"umount {self.mountpoint}"
+        redant.execute_abstract_op_node(cmd, self.client_list[0], False)
+        self.is_mounted = False
