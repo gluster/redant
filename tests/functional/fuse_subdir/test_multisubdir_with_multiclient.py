b614f3c2eb436a4f8bfb0a839722d3910a7cbfb6
nik-redhat <nladha@redhat.com>
2021-09-06 17:25:36 +0530

    Migrated TC test_multisubdir_with_multiclient
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/fuse_subdir/test_multisubdir_with_multiclient.py
+++ tests/functional/fuse_subdir/test_multisubdir_with_multiclient.py
@@ -1,51 +1,68 @@
-#  Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2017-2018  Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass,
-                                                   runs_on)
-from glustolibs.gluster.mount_ops import mount_volume, umount_volume
-from glustolibs.gluster.exceptions import ExecutionError
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-@runs_on([['distributed-replicated', 'replicated', 'distributed',
-           'dispersed', 'distributed-dispersed'],
-          ['glusterfs']])
-class MultipleDirMappingClient(GlusterBaseClass):
-    """
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
     Test case validates multiple dirs mapping to multiple client
     having access to mount respective dirs,with auth allow functionality
-    """
-    @classmethod
-    def setUpClass(cls):
-        """
-        setup volume and mount volume
-        calling GlusterBaseClass setUpClass
-        """
-        cls.get_super_method(cls, 'setUpClass')()
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup and Mount Volume %s",
-                   cls.volname)
-        ret = cls.setup_volume_and_mount_volume(mounts=cls.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume "
-                                 "and Mount_Volume %s" % cls.volname)
-        g.log.info("Successful in Setup and Mount Volume %s", cls.volname)
+"""
 
-    def test_subdir_mounts(self):
+# disruptive;dist,rep,dist-rep,disp,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
+
+
+class TestMultipleDirMappingClient(DParentTest):
+
+    def terminate(self):
+        """
+        Unmount the subdirs mounted in the TC
+        """
+        try:
+            if self.is_mounted:
+                cmd = f"umount {self.mountpoint}"
+                for client in self.client_list[0:2]:
+                    self.redant.execute_abstract_op_node(cmd, client, False)
+
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        # Check server requirements
+        self.redant.check_hardware_requirements(clients=self.client_list,
+                                                clients_count=2)
+
+        # Create and start the volume
+        conf_hash = self.vol_type_inf[self.volume_type]
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots, force=True)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        for client in self.client_list:
+            self.redant.execute_abstract_op_node("mkdir -p "
+                                                 f"{self.mountpoint}",
+                                                 client)
+            self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                     self.mountpoint, client)
+
+    def run_test(self, redant):
         """
         Mount the volume on client
         Create 2 subdir on client subdir1,subdir2
@@ -53,89 +70,39 @@ class MultipleDirMappingClient(GlusterBaseClass):
         Mount subdir1 on client1.Try Mounting subdir1 on client2
         Mount subdir2 on client2.Try Mounting subdir2 on client1
         """
-        # Create 2 subdirectories
-        cmd = ("mkdir %s/dir1 && mkdir %s/dir2"
-               % (self.mounts[0].mountpoint,
-                  self.mounts[0].mountpoint))
-        ret, _, _ = g.run(self.mounts[0].client_system, cmd)
-        self.assertEqual(ret, 0, "Failed to create directories on mountpoint")
-        g.log.info("Directories created successfully on mountpoint")
+        self.is_mounted = False
 
-        # unmount volume
-        ret = self.unmount_volume(self.mounts)
-        self.assertTrue(ret, "Volumes UnMount failed")
-        g.log.info("Volumes UnMounted successfully")
+        # Creating two sub directories
+        redant.create_dir(self.mountpoint, "dir1", self.client_list[0])
+        redant.create_dir(self.mountpoint, "dir2", self.client_list[0])
+
+        # Unmounting volumes
+        for client in self.client_list:
+            redant.volume_unmount(self.vol_name, self.mountpoint, client)
 
         # Set auth allow permission on subdirs
-        cmd = ("gluster volume set %s auth.allow '/dir1(%s),/dir2(%s)'"
-               % (self.volname, self.clients[0], self.clients[1]))
-        g.run(self.mnode, cmd)
-
-        # Sometimes the mount command is returning exit code as 0 in case of
-        # mount failures as well
-        # Hence not asserting while running mount command in test case.
-        # Instead asserting on basis on performing grep on mount point
-        # BZ 1590711
-
-        self.mpoint = "/mnt/Mount_Point1"
+        auth_dict = {'/dir1': [self.client_list[0]],
+                     '/dir2': [self.client_list[1]]}
+        if not redant.set_auth_allow(self.vol_name, self.server_list[0],
+                                     auth_dict):
+            raise Exception("Failed to set authentication")
 
         # Test Subdir2 mount on client 1
-        _, _, _ = mount_volume("%s/dir2" % self.volname, self.mount_type,
-                               self.mpoint, self.mnode, self.clients[0])
-        cmd = ("mount | grep %s") % self.mpoint
-        ret, _, _ = g.run(self.clients[0], cmd)
-        if ret == 0:
-            raise ExecutionError("%s/dir2 mount should fail,"
-                                 "But dir2 mounted successfully on"
-                                 "unauthorized client" % self.volname)
-        g.log.info("%s/dir2 is not mounted on unauthorized client",
-                   self.volname)
+        volname = f"{self.vol_name}/dir2"
+        redant.unauthenticated_mount(volname, self.server_list[0],
+                                     self.mountpoint, self.client_list[0])
+
         # Test Subdir1 mount on client 1
-        _, _, _ = mount_volume("%s/dir1" % self.volname, self.mount_type,
-                               self.mpoint, self.mnode, self.clients[0])
-        cmd = ("mount | grep %s") % self.mpoint
-        ret, _, _ = g.run(self.clients[0], cmd)
-        self.assertEqual(ret, 0, ("%s/dir1 mount failed"
-                                  % self.volname))
-        g.log.info("%s/dir1 is mounted Successfully", self.volname)
+        volname = f"{self.vol_name}/dir1"
+        redant.authenticated_mount(volname, self.server_list[0],
+                                   self.mountpoint, self.client_list[0])
+
         # Test Subdir1 mount on client 2
-        _, _, _ = mount_volume("%s/dir1" % self.volname, self.mount_type,
-                               self.mpoint, self.mnode, self.clients[1])
-        cmd = ("mount | grep %s") % self.mpoint
-        ret, _, _ = g.run(self.clients[1], cmd)
-        if ret == 0:
-            raise ExecutionError("%s/dir1 mount should fail,"
-                                 "But dir1 mounted successfully on"
-                                 "unauthorized client" % self.volname)
-        g.log.info("%s/dir1 is not mounted on unauthorized client",
-                   self.volname)
+        volname = f"{self.vol_name}/dir1"
+        redant.unauthenticated_mount(volname, self.server_list[0],
+                                     self.mountpoint, self.client_list[1])
         # Test Subdir2 mount on client 2
-        _, _, _ = mount_volume("%s/dir2" % self.volname, self.mount_type,
-                               self.mpoint, self.mnode, self.clients[1])
-        cmd = ("mount | grep %s") % self.mpoint
-        ret, _, _ = g.run(self.clients[1], cmd)
-        self.assertEqual(ret, 0, ("%s/dir2 mount failed" % self.volname))
-        g.log.info("%s/dir2 is mounted Successfully", self.volname)
-
-    def tearDown(self):
-        """
-        Clean up the volume and umount volume from client
-        """
-        # Unmount Volume from client
-        g.log.info("Starting to Unmount volume")
-        for client in self.clients:
-            ret, _, _ = umount_volume(client, self.mpoint,
-                                      self.mount_type)
-            if ret == 1:
-                raise ExecutionError("Unmounting the mount point %s failed"
-                                     % self.mpoint)
-            g.log.info("Unmount Volume Successful")
-            cmd = ("rm -rf %s") % self.mpoint
-            ret, _, _ = g.run(client, cmd)
-            g.log.info("Mount point %s deleted successfully", self.mpoint)
-
-        # cleanup-volume
-        ret = self.cleanup_volume()
-        if not ret:
-            raise ExecutionError("Failed to Cleanup Volume")
-        g.log.info("Cleanup volume %s Completed Successfully", self.volname)
+        volname = f"{self.vol_name}/dir2"
+        redant.authenticated_mount(volname, self.server_list[0],
+                                   self.mountpoint, self.client_list[1])
+        self.is_mounted = True
