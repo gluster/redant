6435aa3e4fbe8c91ce338615484428bc12b50b78
nik-redhat <nladha@redhat.com>
2021-09-07 20:03:53 +0530

    Migrated TC test_ec_verify_full_heal
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/disperse/test_ec_verify_full_heal.py
+++ tests/functional/disperse/test_ec_verify_full_heal.py
@@ -1,108 +1,31 @@
-#  Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
 
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass, runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.volume_libs import get_subvols
-from glustolibs.gluster.heal_libs import (monitor_heal_completion,
-                                          is_heal_complete,
-                                          is_volume_in_split_brain)
-from glustolibs.gluster.heal_ops import trigger_heal_full
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.io.utils import (collect_mounts_arequal,
-                                 validate_io_procs,
-                                 list_all_files_and_dirs_mounts,
-                                 wait_for_io_to_complete)
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to verify full-heal
+"""
+
+# disruptive;disp,dist-disp
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['dispersed', 'distributed-dispersed'],
-          ['glusterfs']])
-class TestHealFull(GlusterBaseClass):
+class TestHealFull(DParentTest):
 
-    @classmethod
-    def setUpClass(cls):
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on mounts",
-                   cls.clients)
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts to clients %s"
-                                 % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volumes
-        if self.volume_type == "distributed-replicated":
-            self.volume_configs = []
-
-        self.all_mounts_procs = []
-        self.io_validation_complete = False
-
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup Volume and Mount Volume")
-        ret = self.setup_volume_and_mount_volume(mounts=self.mounts,
-                                                 volume_create_force=False)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-    def tearDown(self):
-        """
-        If test method failed before validating IO, tearDown waits for the
-        IO's to complete and checks for the IO exit status
-
-        Cleanup and umount volume
-        """
-        if not self.io_validation_complete:
-            g.log.info("Wait for IO to complete as IO validation did not "
-                       "succeed in test method")
-            ret = wait_for_io_to_complete(self.all_mounts_procs, self.mounts)
-            if not ret:
-                raise ExecutionError("IO failed on some of the clients")
-            g.log.info("IO is successful on all mounts")
-
-            # List all files and dirs created
-            g.log.info("List all files and directories:")
-            ret = list_all_files_and_dirs_mounts(self.mounts)
-            if not ret:
-                raise ExecutionError("Failed to list all files and dirs")
-            g.log.info("Listing all files and directories is successful")
-
-        # Cleanup and umount volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_heal_full_after_deleting_files(self):
+    def run_test(self, redant):
         """
         - Create IO
         - Calculate arequal from mount
@@ -112,76 +35,60 @@ class TestHealFull(GlusterBaseClass):
         - Check for split-brain
         - Calculate arequal checksum and compare it
         """
-        # pylint: disable=too-many-locals,too-many-statements
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.all_mounts_procs = []
+        count = 1
         # Creating files on client side
         for mount_obj in self.mounts:
-            g.log.info("Generating data for %s:%s",
-                       mount_obj.client_system, mount_obj.mountpoint)
             # Create dirs with file
-            g.log.info('Creating dirs with file...')
-            command = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                       "-d 2 -l 2 -n 2 -f 20 %s"
-                       % (self.script_upload_path,
-                          mount_obj.mountpoint))
-
-            proc = g.run_async(mount_obj.client_system, command,
-                               user=mount_obj.user)
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      count, 2, 2, 2, 20,
+                                                      mount_obj['client'])
             self.all_mounts_procs.append(proc)
-        self.io_validation_complete = False
+            count = count + 10
 
         # Validate IO
-        g.log.info("Wait for IO to complete and validate IO ...")
-        ret = validate_io_procs(self.all_mounts_procs, self.mounts)
-        self.assertTrue(ret, "IO failed on some of the clients")
-        self.io_validation_complete = True
-        g.log.info("IO is successful on all mounts")
+        ret = redant.validate_io_procs(self.all_mounts_procs,
+                                       self.mounts)
+        if not ret:
+            raise Exception("IO failed on some of the clients")
 
         # Get areequal before deleting the files from brick
-        g.log.info('Getting areequal before getting bricks offline...')
-        ret, result_before_killing_procs = collect_mounts_arequal(self.mounts)
-        self.assertTrue(ret, 'Failed to get arequal')
-        g.log.info('Getting areequal before getting bricks offline '
-                   'is successful')
+        result_before_klng_procs = redant.collect_mounts_arequal(self.mounts)
 
-        subvols = get_subvols(self.mnode, self.volname)['volume_subvols']
+        subvols = redant.get_subvols(self.vol_name, self.vol_name)
+        if not subvols:
+            raise Exception("Failed to get the subvols list")
 
         # Delete data from backend from the erasure node
         for subvol in subvols:
             erasure = subvol[-1]
-            g.log.info('Clearing ec brick %s', erasure)
             node, brick_path = erasure.split(':')
-            ret, _, err = g.run(node, 'cd %s/ ; rm -rf *' % brick_path)
-            g.log.error('Clearing ec brick %s is unsuccessful', erasure)
-            self.assertFalse(ret, err)
-        g.log.info('Clearing data from brick is successful')
+            redant.execute_abstract_op_node(f"cd {brick_path}/ ; rm -rf *",
+                                            node)
 
         # Trigger heal full
-        ret = trigger_heal_full(self.mnode, self.volname)
+        if not redant.trigger_heal_full(self.vol_name, self.server_list[0]):
+            raise Exception("Failed to trigger full-heal")
 
         # Monitor heal completion
-        ret = monitor_heal_completion(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal has not yet completed')
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name,):
+            raise Exception("Heal not completed")
 
         # Check if heal is completed
-        ret = is_heal_complete(self.mnode, self.volname)
-        self.assertTrue(ret, 'Heal is not complete')
-        g.log.info('Heal is completed successfully')
+        if not redant.is_heal_complete(self.server_list[0], self.vol_name):
+            raise Exception('Heal is not complete')
 
         # Check for split-brain
-        ret = is_volume_in_split_brain(self.mnode, self.volname)
-        self.assertFalse(ret, 'Volume is in split-brain state')
-        g.log.info('Volume is not in split-brain state')
+        if redant.is_volume_in_split_brain(self.server_list[0],
+                                           self.vol_name):
+            raise Exception("Volume is in split-brain state")
 
         # Get areequal after healing
-        g.log.info('Getting areequal after getting bricks online...')
-        ret, result_after_healing = collect_mounts_arequal(self.mounts)
-        self.assertTrue(ret, 'Failed to get arequal')
-        g.log.info('Getting areequal after getting bricks online '
-                   'is successful')
+        result_after_healing = redant.collect_mounts_arequal(self.mounts)
 
         # Comparing areequals
-        self.assertEqual(result_before_killing_procs, result_after_healing,
-                         'Areequals areequals before before killing arbiter '
-                         'processes and after healing are not equal')
-        g.log.info('Areequals areequals before before killing arbiter '
-                   'processes and after healing equal')
+        if result_before_klng_procs != result_after_healing:
+            raise Exception('Arequals before before clearing data '
+                            'and after healing are not equal')
