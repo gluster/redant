70b7ec83713b130e51a4787ee74941127956e9ef
Nikhil Ladha <nladha@redhat.com>
2021-09-08 11:16:54 +0530

    Migrated TC test_ec_verify_full_heal (#1047)
    * functional/disperse: verifies full heal functionality
    
    This test case validates that full heal crawls whole
    filesystem instead of just scanning indices/xattrop.
    
    RHG3-9691
    Signed-off-by: Sunil Kumar Acharya <sheggodu@redhat.com>
    Change-Id: I7064b86750c37f8619960574a93b0da470b225ad
    Signed-off-by: Karan Sandha <ksandha@redhat.com>
    
    * Add py3 support in 'tests/functional/disperse'
    
    Change-Id: I308f95d16ac18ec80c5c78aac9152d9ae41449bb
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove variable script_local_path(Part 1)
    
    Removing script_local_path as both script_local_path and
    cls.script_upload_path hold the same values which makes
    each script slow. This will help decrease the execution
    time of the test suite.
    
    PoC:
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    b = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.063s
    user 0m0.039s
    sys  0m0.019s
    
    $cat test.py
    a = ("/usr/share/glustolibs/io/scripts/"
         "file_dir_ops.py")
    $time python test.py
    
    real 0m0.013s
    user 0m0.009s
    sys  0m0.003s
    
    Code changes needed:
    
    From:
    script_local_path = ("/usr/share/glustolibs/io/scripts/"
                         "file_dir_ops.py")
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, script_local_path)
    
    To:
    cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
                              "file_dir_ops.py")
    ret = upload_scripts(cls.clients, cls.script_upload_path)
    
    Change-Id: I7908b3b418bbc929b7cc3ff81e3675310eecdbeb
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Remove python version dependency(Part 2)
    
    Please refer to commit message of patch [1].
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: Ic0b3b1333ac7b1ae02f701943d49510e6d46c259
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_ec_verify_full_heal
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix function call
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Sunil Kumar Acharya <sheggodu@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/disperse/test_ec_verify_full_heal.py
@@ -0,0 +1,88 @@
+"""
+ Copyright (C) 2015-2020  Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to verify full-heal
+"""
+
+# disruptive;disp,dist-disp
+from tests.d_parent_test import DParentTest
+
+
+class TestHealFull(DParentTest):
+
+    def run_test(self, redant):
+        """
+        - Create IO
+        - Calculate arequal from mount
+        - Delete data from backend from the EC volume
+        - Trigger heal full
+        - Check if heal is completed
+        - Calculate arequal checksum and compare it
+        """
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        self.all_mounts_procs = []
+        count = 1
+        # Creating files on client side
+        for mount_obj in self.mounts:
+            # Create dirs with file
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      count, 2, 2, 2, 20,
+                                                      mount_obj['client'])
+            self.all_mounts_procs.append(proc)
+            count = count + 10
+
+        # Validate IO
+        ret = redant.validate_io_procs(self.all_mounts_procs,
+                                       self.mounts)
+        if not ret:
+            raise Exception("IO failed on some of the clients")
+
+        # Get areequal before deleting the files from brick
+        result_before_klng_procs = redant.collect_mounts_arequal(self.mounts)
+
+        subvols = redant.get_subvols(self.vol_name, self.server_list[0])
+        if not subvols:
+            raise Exception("Failed to get the subvols list")
+
+        # Delete data from backend from the erasure node
+        for subvol in subvols:
+            erasure = subvol[-1]
+            node, brick_path = erasure.split(':')
+            redant.execute_abstract_op_node(f"cd {brick_path}/ ; rm -rf *",
+                                            node)
+
+        # Trigger heal full
+        if not redant.trigger_heal_full(self.vol_name, self.server_list[0]):
+            raise Exception("Failed to trigger full-heal")
+
+        # Monitor heal completion
+        if not redant.monitor_heal_completion(self.server_list[0],
+                                              self.vol_name,):
+            raise Exception("Heal not completed")
+
+        # Check if heal is completed
+        if not redant.is_heal_complete(self.server_list[0], self.vol_name):
+            raise Exception('Heal is not complete')
+
+        # Get areequal after healing
+        result_after_healing = redant.collect_mounts_arequal(self.mounts)
+
+        # Comparing areequals
+        if result_before_klng_procs != result_after_healing:
+            raise Exception('Arequals before before clearing data '
+                            'and after healing are not equal')
