ea9ff48e1f65c982d8f08b351582f70e7a925e0b
nik-redhat <nladha@redhat.com>
2021-09-08 22:39:17 +0530

    Migrated TC test_ec_read_from_hardlink
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/disperse/test_ec_read_from_hardlink.py
+++ tests/functional/disperse/test_ec_read_from_hardlink.py
@@ -14,51 +14,21 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass, runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.mount_ops import mount_volume, umount_volume
-from glustolibs.gluster.volume_ops import (set_volume_options,
-                                           volume_reset)
-from glustolibs.gluster.glusterfile import create_link_file
+# disruptive;disp,dist-disp
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['dispersed', 'distributed-dispersed'],
-          ['glusterfs', 'nfs']])
-class TestEcReadFromHardlink(GlusterBaseClass):
+class TestEcReadFromHardlink(DParentTest):
 
-    def setUp(self):
-        self.get_super_method(self, 'setUp')()
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        # Create and start the volume
+        conf_hash = self.vol_type_inf[self.volume_type]
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots, force=True)
 
-        # Setup volume
-        if not self.setup_volume():
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-    def tearDown(self):
-        self.get_super_method(self, 'tearDown')()
-
-        # Unmount the volume
-        ret = umount_volume(mclient=self.mounts[0].client_system,
-                            mpoint=self.mounts[0].mountpoint)
-        if not ret:
-            raise ExecutionError("Unable to umount the volume")
-        g.log.info("Unmounting of the volume %s succeeded", self.volname)
-
-        # The reason for volume reset is, metadata-cache is enabled
-        # by group, can't disable the group in glusterfs.
-        ret, _, _ = volume_reset(self.mnode, self.volname)
-        if ret:
-            raise ExecutionError("Unable to reset the volume {}".
-                                 format(self.volname))
-        g.log.info("Volume: %s reset successful ", self.volname)
-
-        # Cleanup the volume
-        if not self.cleanup_volume():
-            raise ExecutionError("Unable to perform volume clenaup")
-        g.log.info("Volume cleanup is successfull")
-
-    def test_ec_read_from_hardlink(self):
+    def run_test(self, redant):
         """
         Test steps:
         1. Enable metadata-cache(md-cache) options on the volume
@@ -66,46 +36,37 @@ class TestEcReadFromHardlink(GlusterBaseClass):
         3. Read data from the hardlink.
         4. Read data from the actual file.
         """
-        options = {'group': 'metadata-cache'}
         # Set metadata-cache options as group
-        ret = set_volume_options(self.mnode, self.volname, options)
-        self.assertTrue(ret, "Unable to set the volume options {}".
-                        format(options))
-        g.log.info("Able to set the %s options", options)
+        options = {'group': 'metadata-cache'}
+        redant.set_volume_options(self.vol_name, options, self.server_list[0])
 
         # Mounting the volume on one client
-        ret, _, _ = mount_volume(self.volname, mtype=self.mount_type,
-                                 mpoint=self.mounts[0].mountpoint,
-                                 mserver=self.mnode,
-                                 mclient=self.mounts[0].client_system)
-        self.assertEqual(ret, 0, ("Volume {} is not mounted").
-                         format(self.volname))
-        g.log.info("Volume mounted successfully : %s", self.volname)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        redant.execute_abstract_op_node(f"mkdir -p {self.mountpoint}",
+                                        self.client_list[0])
+        redant.volume_mount(self.server_list[0], self.vol_name,
+                            self.mountpoint, self.client_list[0])
 
-        file_name = self.mounts[0].mountpoint + "/test1"
+        file_name = f"{self.mountpoint}/test1"
         content = "testfile"
-        hard_link = self.mounts[0].mountpoint + "/test1_hlink"
-        cmd = 'echo "{content}" > {file}'.format(file=file_name,
-                                                 content=content)
+        hard_link = f"{self.mountpoint}/test1_hlink"
+        cmd = f'echo "{content}" > {file_name}'
 
         # Creating a file with data
-        ret, _, _ = g.run(self.mounts[0].client_system, cmd)
-        self.assertEqual(ret, 0, "Sucessful in creating a file with data")
-        g.log.info("file created successfully on %s",
-                   self.mounts[0].mountpoint)
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Creating a hardlink for the file created
-        ret = create_link_file(self.mounts[0].client_system,
-                               file_name, hard_link)
-        self.assertTrue(ret, "Link file creation failed")
-        g.log.info("Link file creation for %s is successful", file_name)
+        ret = redant.create_link_file(self.client_list[0], file_name,
+                                      hard_link)
+        if not ret:
+            raise Exception("Link file creation failed")
 
         # Reading from the file as well as the hardlink
         for each in (file_name, hard_link):
-            ret, out, _ = g.run(self.mounts[0].client_system,
-                                "cat {}".format(each))
-            self.assertEqual(ret, 0, "Unable to read the {}".format(each))
-            self.assertEqual(content, out.strip('\n'), "The content {} and"
-                             " data in file {} is not same".
-                             format(content, each))
-            g.log.info("Read of %s file is successful", each)
+            ret = redant.execute_abstract_op_node(f"cat {each}",
+                                                  self.client_list[0], False)
+            if ret['error_code'] != 0:
+                raise Exception(f"Unable to read the {each}")
+            if ret['msg'][0].strip() != content:
+                raise Exception(f"The content {content} and data in file "
+                                f"{each} is not same")
