dda30cf5f2ae1f19e01fa1abc4887cdb2cb2fa89
nik-redhat <nladha@redhat.com>
2021-09-20 17:05:52 +0530

    Migrated TC test_add_brick_rebalance_with_self_heal_in_progress
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/dht/test_add_brick_rebalance_with_self_heal_in_progress.py
+++ tests/functional/dht/test_add_brick_rebalance_with_self_heal_in_progress.py
@@ -1,61 +1,48 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along`
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to check rebalance after add-brick when self-heal is in progress
+"""
+
+# disruptive;dist-rep,dist-arb
 from random import choice
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.brick_libs import get_all_bricks, bring_bricks_online
-from glustolibs.gluster.heal_libs import monitor_heal_completion
-from glustolibs.gluster.rebalance_ops import (
-    rebalance_start, wait_for_rebalance_to_complete)
-from glustolibs.gluster.volume_libs import expand_volume
-from glustolibs.io.utils import (collect_mounts_arequal, validate_io_procs,
-                                 wait_for_io_to_complete)
-from glustolibs.misc.misc_libs import kill_process
+import traceback
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated', 'distributed-arbiter'], ['glusterfs']])
-class TestAddBrickRebalanceWithSelfHeal(GlusterBaseClass):
+class TestAddBrickRebalanceWithSelfHeal(DParentTest):
 
-    def setUp(self):
+    def terminate(self):
+        """
+        Wait for IO to complete if the TC fails midway
+        """
+        try:
+            if self.is_copy_running:
+                if not (self.redant.wait_for_io_to_complete(
+                        self.list_of_io_processes, self.mounts)):
+                    raise Exception("Failed to wait for IO to complete")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volume
-        if not self.setup_volume_and_mount_volume([self.mounts[0]]):
-            raise ExecutionError("Failed to Setup and mount volume")
-
-        self.is_io_running = False
-
-    def tearDown(self):
-
-        # If I/O processes are running wait for it to complete
-        if self.is_io_running:
-            if not wait_for_io_to_complete(self.list_of_io_processes,
-                                           [self.mounts[0]]):
-                raise ExecutionError("Failed to wait for I/O to complete")
-
-        if not self.unmount_volume_and_cleanup_volume([self.mounts[0]]):
-            raise ExecutionError("Failed to Cleanup Volume")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_add_brick_rebalance_with_self_heal_in_progress(self):
+    def run_test(self, redant):
         """
         Test case:
         1. Create a volume, start it and mount it.
@@ -71,66 +58,70 @@ class TestAddBrickRebalanceWithSelfHeal(GlusterBaseClass):
         9. Collect arequal checksum on mount point and compare
            it with the one taken in step 4.
         """
+        self.is_copy_running = False
+        self.list_of_io_processes = []
         # Start I/O from mount point and wait for it to complete
-        cmd = ("cd %s; for i in {1..1000} ; do "
-               "dd if=/dev/urandom of=file$i bs=10M count=1; done"
-               % self.mounts[0].mountpoint)
-        self.list_of_io_processes = [
-            g.run_async(self.mounts[0].client_system, cmd)]
+        cmd = (f"cd {self.mountpoint}; for i in {{1..1000}} ; do "
+               "dd if=/dev/urandom of=file$i bs=10M count=1; done")
+        proc = redant.execute_command_async(cmd, self.client_list[0])
+        self.list_of_io_processes.append(proc)
         self.is_copy_running = True
 
         # Get a list of all the bricks to kill brick
-        brick_list = get_all_bricks(self.mnode, self.volname)
-        self.assertIsNotNone(brick_list, "Empty present brick list")
+        brick_list = redant.get_all_bricks(self.vol_name, self.server_list[0])
+        if not brick_list:
+            raise Exception("Empty brick list")
 
         # Kill brick process of a brick which is being removed
         brick = choice(brick_list)
         node, _ = brick.split(":")
-        ret = kill_process(node, process_names="glusterfsd")
-        self.assertTrue(ret, "Failed to kill brick process of brick %s"
-                        % brick)
+        ret = redant.kill_process(node, process_names="glusterfsd")
+        if not ret:
+            raise Exception(f"Failed to kill brick process of brick {brick}")
 
         # Validate if I/O was successful or not.
-        ret = validate_io_procs(self.list_of_io_processes, self.mounts)
-        self.assertTrue(ret, "IO failed on some of the clients")
+        self.mounts = {
+            "client": self.client_list[0],
+            "mountpath": self.mountpoint
+        }
+        ret = redant.validate_io_procs(self.list_of_io_processes, self.mounts)
+        if not ret:
+            raise Exception("IO failed")
         self.is_copy_running = False
 
         # Collect arequal checksum before ops
-        arequal_checksum_before = collect_mounts_arequal(self.mounts[0])
+        arequal_checksum_before = redant.collect_mounts_arequal(self.mounts)
 
         # Bring back the brick online by starting volume with force
-        ret = bring_bricks_online(self.mnode, self.volname, brick_list,
-                                  bring_bricks_online_methods=[
-                                      'volume_start_force'])
-        self.assertTrue(ret, "Error in bringing back brick online")
-        g.log.info('All bricks are online now')
+        ret = redant.bring_bricks_online(self.vol_name, self.server_list,
+                                         brick_list)
+        if not ret:
+            raise Exception("Error in bringing back brick online")
 
         # Add brick to volume
-        ret = expand_volume(self.mnode, self.volname, self.servers,
-                            self.all_servers_info)
-        self.assertTrue(ret, "Failed to add brick on volume %s"
-                        % self.volname)
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots)
+        if not ret:
+            raise Exception(f"Failed to add brick on volume {self.vol_name}")
 
         # Trigger rebalance and wait for it to complete
-        ret, _, _ = rebalance_start(self.mnode, self.volname,
-                                    force=True)
-        self.assertEqual(ret, 0, "Failed to start rebalance on the volume %s"
-                         % self.volname)
+        redant.rebalance_start(self.vol_name, self.server_list[0],
+                               force=True)
 
         # Wait for rebalance to complete
-        ret = wait_for_rebalance_to_complete(self.mnode, self.volname,
-                                             timeout=1200)
-        self.assertTrue(ret, "Rebalance is not yet complete on the volume "
-                             "%s" % self.volname)
-        g.log.info("Rebalance successfully completed")
+        ret = redant.wait_for_rebalance_to_complete(self.vol_name,
+                                                    self.server_list[0],
+                                                    timeout=1200)
+        if not ret:
+            raise Exception("Rebalance is not yet complete on the volume "
+                            f"{self.vol_name}")
 
         # Wait for heal to complete
-        ret = monitor_heal_completion(self.mnode, self.volname)
-        self.assertTrue(ret, "heal has not yet completed")
-        g.log.info("Self heal completed")
+        if not self.redant.monitor_heal_completion(self.server_list[0],
+                                                   self.vol_name):
+            raise Exception("Heal is not yet completed")
 
         # Check for data loss by comparing arequal before and after ops
-        arequal_checksum_after = collect_mounts_arequal(self.mounts[0])
-        self.assertEqual(arequal_checksum_before, arequal_checksum_after,
-                         "arequal checksum is NOT MATCHNG")
-        g.log.info("arequal checksum is SAME")
+        arequal_checksum_after = redant.collect_mounts_arequal(self.mounts)
+        if arequal_checksum_before != arequal_checksum_after:
+            raise Exception("arequal checksum is NOT MATCHNG")
