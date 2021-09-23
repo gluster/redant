2fb7a33dcd96456c354b08cfc6bd51cee28ea849
nik-redhat <nladha@redhat.com>
2021-09-23 18:30:47 +0530

    Migrated TC test_dht_copy_dir
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/dht/test_dht_copy_dir.py
+++ tests/functional/dht/test_dht_copy_dir.py
@@ -1,227 +1,167 @@
-#  Copyright (C) 2020  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2020  Red Hat, Inc. <http://www.redhat.com>
 
-# pylint: disable=pointless-string-statement,too-many-locals
-# pylint: disable=too-many-branches,too-many-statements,too-many-function-args
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass,
-                                                   runs_on)
-from glustolibs.gluster.brick_libs import get_all_bricks
-from glustolibs.gluster.dht_test_utils import validate_files_in_dir
-import glustolibs.gluster.constants as const
-from glustolibs.gluster.glusterdir import mkdir
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to check copy of a dir when dest_dir present or not
+"""
+
+# disruptive;rep,dist,dist-rep,arb,dist-arb,disp,dist-disp
+from common.ops.gluster_ops.constants import \
+    (TEST_FILE_EXISTS_ON_HASHED_BRICKS, TEST_LAYOUT_IS_COMPLETE)
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['replicated', 'distributed', 'distributed-replicated',
-           'dispersed', 'distributed-dispersed', 'arbiter',
-           'distributed-arbiter'],
-          ['glusterfs']])
-class DhtCopyTest(GlusterBaseClass):
+class TestDhtCopy(DParentTest):
 
-    destination_exists = False
-
-    def copy_dir(self):
+    def _copy_dir(self):
         """
-        Description:
-        This test creates a parent directory and subdirectories
-        at mount point. After that it creates a copy of parent
-        directory at mount point, first when destination
-        directory is not there, and second sub-test creates a
-        copy after creating destination directory for copying.
-        In the first test, contents will be copied from one
-        directory to another but in the second test case, entire
-        directory will be copied to another directory along with
-        the contents.Then it checks for correctness of layout
-        and content of source and copied directory at all
-        sub-vols.
+        Copy direcotry and check correctness of layout
         """
+        # Create multiple directories
+        self.redant.create_dir(self.mountpoint, "root_dir/test_dir{1..3}",
+                               self.client_list[0])
 
-        g.log.info("creating multiple,multilevel directories")
-        m_point = self.mounts[0].mountpoint
-        fqpath = m_point + '/root_dir/test_dir{1..3}'
-        client_ip = self.clients[0]
-        flag = mkdir(client_ip, fqpath, True)
-        self.assertTrue(flag, "Directory creation: failed")
+        cmd = f"ls {self.mountpoint}/root_dir"
+        ret = self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        command = 'ls ' + m_point + '/root_dir'
-        ret, out, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "can't list the created directories")
-
-        list_of_created_dirs = out.split('\n')
+        list_of_created_dirs = "".join(ret['msg']).split('\n')
         flag = True
         for x_count in range(3):
-            dir_name = 'test_dir%d' % (x_count + 1)
+            dir_name = f'test_dir{x_count + 1}'
             if dir_name not in list_of_created_dirs:
                 flag = False
-        self.assertTrue(flag, "ls command didn't list all the directories")
-        g.log.info("creation of multiple,multilevel directories created")
+        if not flag:
+            raise Exception("ls command didn't list all the directories")
 
-        g.log.info("creating files at different directory levels")
-        command = 'touch ' + m_point + '/root_dir/test_file{1..5}'
-        ret, _, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "files not created")
+        # Create files at different directory levels
+        cmd = f"touch {self.mountpoint}/root_dir/test_file\{{1..5\}}"
+        self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        command = 'ls ' + m_point + '/root_dir'
-        ret, out, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "can't list the created directories")
+        cmd = f"ls {self.mountpoint}/root_dir"
+        ret = self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        list_of_files_and_dirs = out.split('\n')
+        list_of_files_and_dirs = "".join(ret['msg']).split('\n')
         flag = True
         for x_count in range(3):
-            dir_name = 'test_dir%d' % (x_count + 1)
+            dir_name = f'test_dir{x_count + 1}'
             if dir_name not in list_of_files_and_dirs:
                 flag = False
         for x_count in range(5):
-            file_name = 'test_file%d' % (x_count + 1)
+            file_name = f'test_file{x_count + 1}'
             if file_name not in list_of_files_and_dirs:
                 flag = False
-        self.assertTrue(
-            flag, "ls command didn't list all the directories and files")
-        g.log.info("creation of files at multiple levels successful")
+        if not flag:
+            raise Exception("ls command didn't list all the directories")
 
         if not self.destination_exists:
             destination_dir = 'root_dir_1'
         else:
-            fqpath = m_point + '/new_dir'
-            flag = mkdir(client_ip, fqpath, True)
-            self.assertTrue(flag, "new_dir not created")
+            self.redant.create_dir(self.mountpoint, "new_dir",
+                                   self.client_list[0])
             destination_dir = 'new_dir/root_dir'
 
-        g.log.info("performing layout checks for root_dir")
-        flag = validate_files_in_dir(self.clients[0],
-                                     m_point + '/root_dir',
-                                     const.TEST_FILE_EXISTS_ON_HASHED_BRICKS)
-        self.assertTrue(flag, "root directory not present on every brick")
+        # Performing layout checks for root_dir
+        flag = (self.redant.validate_files_in_dir(
+                self.client_list[0], f"{self.mountpoint}/root_dir",
+                test_type=TEST_FILE_EXISTS_ON_HASHED_BRICKS))
+        if not ret:
+            raise Exception("root directory not present on every brick")
 
-        flag = validate_files_in_dir(self.clients[0],
-                                     m_point + '/root_dir',
-                                     test_type=(
-                                         const.TEST_LAYOUT_IS_COMPLETE))
-        self.assertTrue(flag, "layout of every directory is complete")
-        g.log.info("every directory is present on every brick and layout "
-                   "of each brick is correct")
+        ret = (self.redant.validate_files_in_dir(
+               self.client_list[0], f"{self.mountpoint}/root_dir",
+               test_type=TEST_LAYOUT_IS_COMPLETE))
+        if not ret:
+            raise Exception("layout of every directory is not complete")
 
-        g.log.info("copying root_dir at the mount point")
-        command = "cp -r " + m_point + '/root_dir ' + m_point \
-            + '/' + destination_dir
-        ret, out, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "directory was not copied")
+        # Copying root_dir at the mount point
+        cmd = (f"cp -r {self.mountpoint}/root_dir "
+               f"{self.mountpoint}/{destination_dir}")
+        self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        g.log.info("performing layout checks for copied directory")
+        # Performing layout checks for copied directory
+        ret = (self.redant.validate_files_in_dir(
+               self.client_list[0], f"{self.mountpoint}/{destination_dir}",
+               test_type=TEST_FILE_EXISTS_ON_HASHED_BRICKS))
+        if not ret:
+            raise Exception("directories not present on every brick")
 
-        flag = validate_files_in_dir(self.clients[0],
-                                     m_point + '/' + destination_dir,
-                                     const.TEST_FILE_EXISTS_ON_HASHED_BRICKS)
-        self.assertTrue(flag, "directories not present on every brick")
+        ret = (self.redant.validate_files_in_dir(
+               self.client_list[0], f"{self.mountpoint}/{destination_dir}",
+               test_type=TEST_LAYOUT_IS_COMPLETE))
+        if not ret:
+            raise Exception("layout of every directory is not complete")
 
-        flag = validate_files_in_dir(self.clients[0],
-                                     m_point + '/' + destination_dir,
-                                     test_type=(
-                                         const.TEST_LAYOUT_IS_COMPLETE))
-        self.assertTrue(flag, "layout of every directory is complete")
-        g.log.info("verified: layouts correct")
+        # Listing the copied directory
+        cmd = f"ls -A1 {self.mountpoint}/{destination_dir}"
+        self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        g.log.info("listing the copied directory")
-        command = 'ls -A1 ' + m_point + '/' + destination_dir
-        ret, out, _ = g.run(client_ip, command)
-        self.assertIsNotNone(out, "copied directory not listed")
+        # Copied directory listed
+        cmd = f"ls -A1 {self.mountpoint}/root_dir"
+        ret1 = self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        g.log.info("copied directory listed")
-        command = 'ls -A1 ' + m_point + '/root_dir'
-        ret, out1, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "details of root_dir not listed")
+        cmd = f"ls -A1 {self.mountpoint}/{destination_dir}"
+        ret2 = self.redant.execute_abstract_op_node(cmd, self.client_list[0])
+        if ret1['msg'] != ret2['msg']:
+            raise Exception("contents and attributes of original and "
+                            "copied directory not same")
 
-        command = 'ls -A1 ' + m_point + '/' + destination_dir
-        ret, out2, _ = g.run(client_ip, command)
-        self.assertEqual(ret, 0, "details of copied dir not listed")
-        self.assertEqual(out1, out2,
-                         "contents and attributes of original and "
-                         "copied directory not same")
-        g.log.info("the contents and attributes of copied directory "
-                   "are same")
+        # Listing the copied directory on all the subvolumes
+        brick_list = self.redant.get_all_bricks(self.vol_name,
+                                                self.server_list[0])
+        if not brick_list:
+            raise Exception("Failed to ge the brick list")
 
-        g.log.info("listing the copied directory on all the subvolumes")
-        brick_list = get_all_bricks(self.mnode, self.volname)
         for brick in brick_list:
+            host_addr, brick_path = brick.split(':')
 
-            brick_tuple = brick.partition(':')
-            brick_path = brick_tuple[2]
-            host_addr = brick_tuple[0]
+            cmd = f"ls -A1 {brick_path}/{destination_dir}"
+            self.redant.execute_abstract_op_node(cmd, host_addr)
 
-            command = 'ls -A1 ' + brick_path + '/' + destination_dir
-            ret, out, _ = g.run(host_addr, command)
-            self.assertIsNotNone(out,
-                                 ("copied directory not listed on brick "
-                                  "%s", brick))
+            cmd = f"ls -l --time-style=\'+\' {brick_path}/root_dir/ | grep ^d"
+            ret1 = self.redant.execute_abstract_op_node(cmd, host_addr)
 
-            g.log.info("copied directory listed on brick %s", brick)
-            command = 'ls -l --time-style=\'+\' ' + brick_path \
-                + '/root_dir/' + ' | grep ^d'
-            ret, out1, _ = g.run(host_addr, command)
-            self.assertEqual(ret, 0, "details of root_dir not listed on "
-                                     "brick %s" % brick)
+            cmd = (f"ls -l --time-style=\'+\' {brick_path}/{destination_dir}"
+                   " | grep ^d")
+            ret2 = self.redant.execute_abstract_op_node(cmd, host_addr)
+            if ret1['msg'] != ret2['msg']:
+                raise Exception("contents and attributes of original and "
+                                "copied directory not same on brick "
+                                f"{brick}")
 
-            command = 'ls -l --time-style=\'+\' ' + brick_path + '/' \
-                + destination_dir + '| grep ^d'
-            ret, out2, _ = g.run(host_addr, command)
-            self.assertEqual(ret, 0, "details of copied dir not listed on "
-                                     "brick %s" % brick)
-            self.assertEqual(out1, out2,
-                             "contents and attributes of original and "
-                             "copied directory not same on brick "
-                             "%s" % brick)
-            g.log.info("the contents and attributes of copied directory "
-                       "are same on brick %s", brick)
-        g.log.info("the copied directory is present on all the subvolumes")
-
-    @classmethod
-    def setUpClass(cls):
-
-        # Calling GlusterBaseClass setUpClass
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Setup Volume and Mount Volume
-        g.log.info("Starting to Setup Volume and Mount Volume")
-        ret = cls.setup_volume_and_mount_volume(cls.mounts)
-        if not ret:
-            raise ExecutionError("Failed to Setup_Volume and Mount_Volume")
-        g.log.info("Successful in Setup Volume and Mount Volume")
-
-    def tearDown(self):
-
-        # Unmount and cleanup original volume
-        g.log.info("Starting to Unmount Volume and Cleanup Volume")
-        ret = self.unmount_volume_and_cleanup_volume(mounts=self.mounts)
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_copy_directory(self):
-
+    def run_test(self, redant):
+        """
+        Steps:
+        - Create a parent directory and subdirectories at mount point.
+        - After that create a copy of parent directory at mount point
+        - First when destination directory is not there, and then do again
+          after creating destination directory for copying.
+        - In the first test, contents will be copied from one directory
+          to another.
+        - In the second test case, entire directory will be copied to another
+          directory along with the contents.
+        - Then it checks for correctness of layout and content of source and
+          copied directory at all sub-vols.
+        """
         # Checking when destination directory for copying directory doesn't
         # exist
         self.destination_exists = False
-        self.copy_dir()
+        self._copy_dir()
 
         # Checking by creating destination directory first and then copying
         # created directory
         self.destination_exists = True
-        self.copy_dir()
+        self._copy_dir()
