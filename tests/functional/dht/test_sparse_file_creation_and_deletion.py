94ed638267dd84d27d7d9979c71520ec585b64b6
Sheetal Pamecha <spamecha@redhat.com>
2021-09-24 18:20:10 +0530

    Migrate TC test_sparse_file_creation_and_deletion.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/dht/test_sparse_file_creation_and_deletion.py
+++ tests/functional/dht/test_sparse_file_creation_and_deletion.py
@@ -1,74 +1,59 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+# disruptive;dist-rep,dist-arb,dist-disp,dist
 from time import sleep
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.lib_utils import get_size_of_mountpoint
+from copy import deepcopy
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated', 'distributed-arbiter',
-           'distributed-dispersed', 'distributed'], ['glusterfs']])
-class TestSparseFileCreationAndDeletion(GlusterBaseClass):
+class TestSparseFileCreationAndDeletion(DParentTest):
 
-    def setUp(self):
-
-        self.get_super_method(self, 'setUp')()
-
-        # Changing dist_count to 5
-        self.volume['voltype']['dist_count'] = 5
-
-        # Creating Volume and mounting the volume
-        ret = self.setup_volume_and_mount_volume([self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-
-        # Assign a variable for the first_client
-        self.first_client = self.mounts[0].client_system
-
-    def tearDown(self):
-
-        # Unmounting and cleaning volume
-        ret = self.unmount_volume_and_cleanup_volume([self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Unable to delete volume %s" % self.volname)
-
-        self.get_super_method(self, 'tearDown')()
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        conf_hash = deepcopy(self.vol_type_inf[self.volume_type])
+        conf_hash['dist_count'] = 5
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots, True)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        self.redant.execute_abstract_op_node(f"mkdir -p {self.mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.mountpoint, self.client_list[0])
 
     def _create_two_sparse_files(self):
         """Create 2 sparse files from /dev/zero and /dev/null"""
 
         # Create a tuple to hold both the file names
         self.sparse_file_tuple = (
-            "{}/sparse_file_zero".format(self.mounts[0].mountpoint),
-            "{}/sparse_file_null".format(self.mounts[0].mountpoint)
-            )
+            f"{self.mountpoint}/sparse_file_zero",
+            f"{self.mountpoint}/sparse_file_null")
 
         # Create 2 spares file where one is created from /dev/zero and
         # another is created from /dev/null
-        for filename, input_file in ((self.sparse_file_tuple[0], "/dev/zero"),
-                                     (self.sparse_file_tuple[1], "/dev/null")):
-            cmd = ("dd if={} of={} bs=1M seek=5120 count=1000"
-                   .format(input_file, filename))
-            ret, _, _ = g.run(self.first_client, cmd)
-            self.assertEqual(ret, 0, 'Failed to create %s ' % filename)
-
-        g.log.info("Successfully created sparse_file_zero and"
-                   " sparse_file_null")
+        for filename, io_file in ((self.sparse_file_tuple[0], "/dev/zero"),
+                                  (self.sparse_file_tuple[1], "/dev/null")):
+            cmd = (f"dd if={io_file} of={filename} bs=1M seek=5120 count=1000")
+            self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
     def _check_du_and_ls_of_sparse_file(self):
         """Check du and ls -lks on spare files"""
@@ -76,37 +61,30 @@ class TestSparseFileCreationAndDeletion(GlusterBaseClass):
         for filename in self.sparse_file_tuple:
 
             # Fetch output of ls -lks for the sparse file
-            cmd = "ls -lks {}".format(filename)
-            ret, out, _ = g.run(self.first_client, cmd)
-            self.assertEqual(ret, 0, "Failed to get ls -lks for file %s "
-                             % filename)
-            ls_value = out.split(" ")[5]
+            cmd = f"ls -lks {filename}"
+            ret = self.redant.execute_abstract_op_node(cmd,
+                                                       self.client_list[0])
+            ls_value = ret['msg'][0].split(" ")[5]
 
             # Fetch output of du for the sparse file
-            cmd = "du --block-size=1 {}".format(filename)
-            ret, out, _ = g.run(self.first_client, cmd)
-            self.assertEqual(ret, 0, "Failed to get du for file %s "
-                             % filename)
-            du_value = out.split("\t")[0]
+            cmd = f"du --block-size=1 {filename}"
+            ret = self.redant.execute_abstract_op_node(cmd,
+                                                       self.client_list[0])
+            du_value = ret['msg'][0].split("\t")[0]
 
             # Compare du and ls -lks value
-            self. assertNotEqual(ls_value, du_value,
-                                 "Unexpected: Sparse file size coming up same "
-                                 "for du and ls -lks")
-
-        g.log.info("Successfully checked sparse file size using ls and du")
+            if int(ls_value) == int(du_value):
+                raise Exception("Unexpected: Sparse file size coming up same "
+                                "for du and ls -lks")
 
     def _delete_two_sparse_files(self):
         """Delete sparse files"""
 
         for filename in self.sparse_file_tuple:
-            cmd = "rm -rf {}".format(filename)
-            ret, _, _ = g.run(self.first_client, cmd)
-            self.assertEqual(ret, 0, 'Failed to delete %s ' % filename)
+            cmd = f"rm -rf {filename}"
+            self.redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        g.log.info("Successfully remove both sparse files")
-
-    def test_sparse_file_creation_and_deletion(self):
+    def run_test(self, redant):
         """
         Test case:
         1. Create volume with 5 sub-volumes, start and mount it.
@@ -118,10 +96,10 @@ class TestSparseFileCreationAndDeletion(GlusterBaseClass):
         6. Remove the files using rm -rf.
         """
         # Check df -h for avaliable size
-        available_space_at_start = get_size_of_mountpoint(
-            self.first_client, self.mounts[0].mountpoint)
-        self.assertIsNotNone(available_space_at_start,
-                             "Failed to get available space on mount point")
+        available_space_at_start = (redant.get_size_of_mountpoint(
+                                    self.mountpoint, self.client_list[0]))
+        if not available_space_at_start:
+            raise Exception("Failed to get available space on mount point")
 
         # Create 2 sparse file one from /dev/null and one from /dev/zero
         self._create_two_sparse_files()
@@ -131,13 +109,15 @@ class TestSparseFileCreationAndDeletion(GlusterBaseClass):
         self._check_du_and_ls_of_sparse_file()
 
         # Check df -h for avaliable size(It should be less than step 2)
-        available_space_now = get_size_of_mountpoint(
-            self.first_client, self.mounts[0].mountpoint)
-        self.assertIsNotNone(available_space_now,
-                             "Failed to get avaliable space on mount point")
+        available_space_now = (redant.get_size_of_mountpoint(
+                               self.mountpoint, self.client_list[0]))
+        if not available_space_now:
+            raise Exception("Failed to get avaliable space on mount point")
+
         ret = (int(available_space_at_start) > int(available_space_now))
-        self.assertTrue(ret, "Available space at start not less than "
-                        "available space now")
+        if not ret:
+            raise Exception("Available space at start less than "
+                            "available space now")
 
         # Remove the files using rm -rf
         self._delete_two_sparse_files()
@@ -147,10 +127,10 @@ class TestSparseFileCreationAndDeletion(GlusterBaseClass):
         sleep(180)
 
         # Check df -h after removing sparse files
-        available_space_now = get_size_of_mountpoint(
-            self.first_client, self.mounts[0].mountpoint)
-        self.assertIsNotNone(available_space_now,
-                             "Failed to get avaliable space on mount point")
+        available_space_now = (redant.get_size_of_mountpoint(
+                               self.mountpoint, self.client_list[0]))
+
         ret = int(available_space_at_start) - int(available_space_now) < 1500
-        self.assertTrue(ret, "Available space at start and available space now"
-                        " is not equal")
+        if not ret:
+            raise Exception("Available space at start and available space now"
+                            " is not equal")
