0ba7f1156e75faab70ac9639350e608dda1d24e2
Nikhil Ladha <nladha@redhat.com>
2021-09-28 14:09:04 +0530

    Migrated TC test_directory_healing (#1122)
    * glusto/dht: test case for directory healing
    
    The test case tests directory healing.
    
    If a mkdir happens successfully when a subvol is down, the
    directory should heal and should have zeroed layout once the
    subvol is up.
    
    Change-Id: Ia2f5747a1008112a5dcebda8a953ee3d2de9f75f
    Signed-off-by: Susant Palai <spalai@redhat.com>
    
    * Add py3 support in 'tests/functional/dht'
    
    Change-Id: I44fe85519c8fd381064670e54dac8736107b0928
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Fix to make TC compatible with DHT lib
    
    The TC was failing with "AssertionError: ('hash range is not
    there %s', False)" even though the bricks were healed and the
    directory was created on non-hashed bricks. This was due to the
    conflict between the TC and the DHT library changes (added to
    fix the issues caused by DHT pass-through functionality). The
    code is now modified according to the library changes and hence
    the TC works fine.
    
    Change-Id: I501e7db89643822fbc711e631ceacda79e4c4ea4
    Signed-off-by: sayaleeraut <saraut@redhat.com>
    
    * Moving test steps from teardown class to teardown
    
    Move cases from teardown class to teardown in dht
    
    Change-Id: Id0cf120c6229715521ae19fd4bb00cad553d701f
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Migrated TC test_directory_healing
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Update volume types
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Susant Palai <spalai@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: sayaleeraut <saraut@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/dht/test_directory_healing.py
@@ -0,0 +1,111 @@
+"""
+ Copyright (C) 2018-2020 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test cases in this module tests directory healing
+"""
+
+# disruptive;dist-rep,dist-disp,dist-arb
+# TODO: NFS
+from tests.d_parent_test import DParentTest
+
+
+class TestDirHeal(DParentTest):
+
+    def run_test(self, redant):
+        """
+        - Create and start a volume
+        - Mount it on a client
+        - Bring down a subvol
+        - Create a directory so that it does not hash to down subvol
+        - Bring up the subvol
+        - Check if directory is healed
+        """
+
+        # directory that needs to be created
+        parent_dir = f"{self.mountpoint}/parent"
+        target_dir = f"{self.mountpoint}/parent/child"
+
+        # create parent dir
+        redant.create_dir(self.mountpoint, "parent", self.client_list[0])
+
+        # find non-hashed subvol for child
+        hashed, non_hashed = [], []
+        hash_num = redant.calculate_hash(self.server_list[0], "child")
+        bricklist = redant.get_all_bricks(self.vol_name, self.server_list[0])
+        if not bricklist:
+            raise Exception("Failed to get the brick list")
+
+        for brick in bricklist:
+            ret = redant.check_hashrange(f"{brick}/parent")
+            if not ret:
+                raise Exception("Failed to get the hash range")
+
+            hash_range_low = ret[0]
+            hash_range_high = ret[1]
+            if hash_range_low <= hash_num <= hash_range_high:
+                hashed.append(brick)
+
+        non_hashed = [brick for brick in bricklist if brick not in hashed]
+
+        # bring non_hashed offline
+        ret = redant.bring_bricks_offline(self.vol_name, non_hashed)
+        if not ret:
+            raise Exception(f'Error in bringing down brick {brick}')
+
+        # create child directory
+        redant.create_dir(self.mountpoint, "parent/child",
+                          self.client_list[0])
+
+        # Check that the dir is not created on the down brick
+        for brick in non_hashed:
+            non_hashed_host, dir_path = brick.split(":")
+            brickpath = f"{dir_path}/parent/child"
+            ret = redant.execute_abstract_op_node(f"stat {brickpath}",
+                                                  non_hashed_host, False)
+            if ret['error_code'] == 0:
+                raise Exception(f"Expected {brickpath} to be not present "
+                                f"on {non_hashed_host}")
+
+        # bring up the subvol
+        ret = redant.bring_bricks_online(self.vol_name, self.server_list,
+                                         non_hashed)
+        if not ret:
+            raise Exception("Error in bringing back subvol online")
+
+        redant.execute_abstract_op_node(f"ls {target_dir}",
+                                        self.client_list[0])
+
+        # check if the directory is created on non_hashed
+        for brick in non_hashed:
+            non_hashed_host, dir_path = brick.split(":")
+            absolutedirpath = f"{dir_path}/parent/child"
+            ret = redant.get_file_stat(non_hashed_host, absolutedirpath)
+            if ret['error_code'] != 0:
+                raise Exception("Unexpected: Stat of path failed")
+
+        # check if directory is healed => i.e. layout is zeroed out
+        for brick in non_hashed:
+            brick_path = f"{brick}/parent/child"
+            ret = redant.check_hashrange(brick_path)
+            if not ret:
+                raise Exception("Failed to check hash range")
+
+            hash_range_low = ret[0]
+            hash_range_high = ret[1]
+            if hash_range_low and hash_range_high:
+                self.logger.error("Directory is not healed")
