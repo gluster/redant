fdf586ee93ffa58cdcb75d3cfcb77de53f3bd1d2
nik-redhat <nladha@redhat.com>
2021-09-28 15:50:18 +0530

    Fix add-brick
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/dht/test_negative_exercise_add_brick_command.py
+++ tests/functional/dht/test_negative_exercise_add_brick_command.py
@@ -29,7 +29,7 @@ class TestExerciseAddbrickCommand(DParentTest):
         """Test add-brick command without volume"""
         # Form bricks list for add-brick
         brick_cmd = redant.form_brick_cmd_to_add_brick(self.server_list[0],
-                                                       self.volname,
+                                                       self.vol_name,
                                                        self.server_list,
                                                        self.brick_roots)
         cmd = f"gluster volume add-brick {brick_cmd}"
@@ -57,7 +57,7 @@ class TestExerciseAddbrickCommand(DParentTest):
         # Case 3: test_add_nested_brick
         """Test add nested bricks to the volume"""
         # Get sub-vols for forming a nested bricks list
-        subvols = redant.get_subvols(self.vol_name, self.server_list[0])
+        subvols = redant.get_subvols(self.vol_name, self.server_list[0])[0]
         nested_bricks_list = [f"{x}/nested" for x in subvols]
         cmd = (f"gluster volume add-brick {self.vol_name} "
                f"{' '.join(nested_bricks_list)}")
@@ -74,7 +74,7 @@ class TestExerciseAddbrickCommand(DParentTest):
         """Test add-bricks to an non existent volume"""
         # Form bricks list for add-brick
         brick_cmd = redant.form_brick_cmd_to_add_brick(self.server_list[0],
-                                                       self.volname,
+                                                       self.vol_name,
                                                        self.server_list,
                                                        self.brick_roots)
         cmd = f"gluster volume add-brick novolume {brick_cmd}"
@@ -102,9 +102,9 @@ class TestExerciseAddbrickCommand(DParentTest):
         brick_str = " ".join(bricks_list)
         ret = redant.add_brick(self.vol_name, brick_str, self.server_list[0],
                                excep=False)
-        if ret['error_code'] == 0:
+        if ret['msg']['opRet'] == '0':
             raise Exception("Unexpected: add-brick is successful")
         err_msg = "Pre-validation failed on localhost"
-        if err_msg not in ret['error_msg']:
+        if err_msg not in ret['msg']['opErrstr']:
             raise Exception("Unexpected: add-brick successful without "
                             "any error")
