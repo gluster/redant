1bb8804630274325731d9c59627909b54ad1e252
Nikhil Ladha <nladha@redhat.com>
2021-09-28 14:16:10 +0530

    Migrated TC test_induc_holes_in_layout_by_removebrick_force_then_lookup (#1125)
    * TC: Induce holes in layout by removebrick force then lookup
    
    Objective:
    When induced holes in the layout by remove brick force, lookup sent
    on the directory from client should fix the directory layout without
    any holes or overlaps.
    
    Change-Id: If4af10929e8e7d4da93cea80c515e37acf53d34e
    Signed-off-by: Prasad Desala <tdesala@redhat.com>
    
    * access self.mnod so we must get layout in mount_point/dirpath, and change the log.
    
    Change-Id: I361b2c59108b19480906f6dfd49b023ed1eb05cd
    Signed-off-by: yinkui <13965432176@163.com>
    
    * Add py3 support in 'tests/functional/dht'
    
    Change-Id: I44fe85519c8fd381064670e54dac8736107b0928
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Add code to override the volume config
    
    Changing the distribute count to 4 for the volume type
    distributed-replicated or distributed-dispersed, as earlier with
    distribute count 2, after remove-brick, the dist-rep & dist-disp
    volumes were converted to pure rep or pure dispersed, which caused
    "layout not complete" error as with the DHT pass-through feature
    layout is not set on bricks if volume type is pure replicated/pure
    dispersed on gluster version 6.0
    Adding distributed-arbiter volume type and have added code to
    override its configuration as well.
    
    Change-Id: Ic7a3404ed49d24f956de33f7bd5ca8ea61297e5b
    Signed-off-by: sayaleeraut <saraut@redhat.com>
    
    * Moving test steps from teardown class to teardown
    
    Move cases from teardown class to teardown in dht
    
    Change-Id: Id0cf120c6229715521ae19fd4bb00cad553d701f
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Migrated TC test_induc_holes_in_layout_by_removebrick_force_then_lookup
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Prasad Desala <tdesala@redhat.com>
    Co-authored-by: yinkui <13965432176@163.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: sayaleeraut <saraut@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>

---
+++ tests/functional/dht/test_induce_holes_in_layout_by_removebrick_force_then_lookup.py
@@ -0,0 +1,121 @@
+"""
+ Copyright (C) 2018-2020 Red Hat, Inc. http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    TC to induce holes in layout by remove brick force then lookup to
+    fix layout
+"""
+
+# disruptive;dist,dist-rep,dist-arb,dist-disp
+from common.ops.gluster_ops.constants import (TEST_LAYOUT_IS_COMPLETE,
+                                              FILETYPE_DIRS)
+from copy import deepcopy
+from tests.d_parent_test import DParentTest
+
+
+class TestRebalanceValidation(DParentTest):
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        # Minimum of 2 clients are needed
+        self.redant.check_hardware_requirements(clients=self.client_list,
+                                                clients_count=2)
+
+        conf_hash = deepcopy(self.vol_type_inf[self.volume_type])
+        if conf_hash['dist_count'] <= 2:
+            conf_hash['dist_count'] = 4
+
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        self.redant.execute_abstract_op_node(f"mkdir -p {self.mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.mountpoint, self.client_list[0])
+
+    def run_test(self, redant):
+        """
+        Steps :
+        1) Create a volume and mount it using FUSE.
+        2) Create a directory "testdir" on mount point.
+        3) Check if the layout is complete.
+        4) Log volume info and status before remove-brick operation.
+        5) Form a list of bricks to be removed.
+        6) Start remove-brick operation using 'force'.
+        7) Let remove-brick complete and check layout.
+        8) Mount the volume on a new mount.
+        9) Send a lookup on mount point.
+        10) Check if the layout is complete.
+        """
+        redant.create_dir(self.mountpoint, "testdir", self.client_list[0])
+
+        # DHT Layout validation
+        ret = redant.validate_files_in_dir(self.client_list[0],
+                                           self.mountpoint,
+                                           test_type=TEST_LAYOUT_IS_COMPLETE,
+                                           file_type=FILETYPE_DIRS)
+        if not ret:
+            raise Exception("LAYOUT_IS_COMPLETE: FAILED")
+
+        # Log Volume Info and Status before shrinking the volume.
+        if not redant.log_volume_info_and_status(self.server_list[0],
+                                                 self.vol_name):
+            raise Exception("Logging volume info and status failed "
+                            f"on volume {self.vol_name}")
+
+        # Form bricks list for Shrinking volume
+        bricks_list_to_remove = (redant.form_bricks_list_to_remove_brick(
+                                 self.server_list[0], self.vol_name,
+                                 subvol_num=1))
+
+        if bricks_list_to_remove is None:
+            raise Exception("Failed to form bricks list to remove-brick. "
+                            f"Hence unable to shrink volume {self.vol_name}")
+
+        # Shrink volume
+        redant.remove_brick(self.server_list[0], self.vol_name,
+                            bricks_list_to_remove, "force")
+
+        # Check the layout
+        ret = redant.is_layout_complete(self.server_list[0], self.vol_name,
+                                        dirpath='/testdir')
+        if ret:
+            raise Exception(f"Unexpected: Volume {self.vol_name} layout is"
+                            " complete")
+
+        # Mount the volume on a new mount point
+        redant.execute_abstract_op_node(f"mkdir  -p {self.mountpoint}",
+                                        self.client_list[1])
+
+        redant.volume_mount(self.server_list[0], self.vol_name,
+                            self.mountpoint, self.client_list[1])
+
+        # Send a look up on the directory
+        cmd = f'ls {self.mountpoint}/testdir'
+        redant.execute_abstract_op_node(cmd, self.client_list[1])
+
+        # DHT Layout validation
+        ret = redant.validate_files_in_dir(self.client_list[1],
+                                           f"{self.mountpoint}/testdir",
+                                           test_type=TEST_LAYOUT_IS_COMPLETE,
+                                           file_type=FILETYPE_DIRS)
+        if not ret:
+            raise Exception("LAYOUT_IS_COMPLETE: FAILED")
