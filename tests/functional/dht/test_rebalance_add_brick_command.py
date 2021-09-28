0a355438aad34d4513970b92474dd3807baa3174
nik-redhat <nladha@redhat.com>
2021-09-28 21:21:57 +0530

    Migrated TC test_rebalance_add_brick_command
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/dht/test_rebalance_add_brick_command.py
+++ tests/functional/dht/test_rebalance_add_brick_command.py
@@ -1,115 +1,133 @@
-#  Copyright (C) 2017-2021 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+ Copyright (C) 2017-2021 Red Hat, Inc. <http://www.redhat.com>
 
-"""Positive test - Exercise Add-brick command"""
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
 
-from glusto.core import Glusto as g
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-from glustolibs.gluster.brick_libs import get_all_bricks
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.volume_libs import (cleanup_volume, expand_volume,
-                                            volume_stop)
-from glustolibs.gluster.volume_ops import get_volume_list
-from glustolibs.io.utils import validate_io_procs
-from glustolibs.misc.misc_libs import upload_scripts
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Positive test - Exercise Add-brick command
+"""
+
+# disruptive;dist,rep,disp,dist-rep,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['dispersed', 'replicated', 'distributed', 'distributed-dispersed',
-           'distributed-replicated'],
-          ['glusterfs']])
-class ExerciseAddbrickCommand(GlusterBaseClass):
-    """Positive test - Exercise Add-brick command"""
+class TestExerciseAddbrickCommand(DParentTest):
 
-    def setUp(self):
-        """Setup Volume"""
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 self.vol_type_inf[self.volume_type],
+                                 self.server_list, self.brick_roots)
 
-        # Setup Volume Volume
-        g.log.info("Starting to Setup Volume")
-        ret = self.setup_volume()
+    def terminate(self):
+        """
+        Wait for IO toc omplete, if the TC fails early
+        """
+        try:
+            if self.is_io_running:
+                if not self.redant.wait_for_io_to_complete(self.io_process,
+                                                           self.mounts):
+                    raise Exception("Failed to wait for IO to complete")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
+        Steps:
+        - Create and start a volume
+        - Expand volume, it should be a success
+        - Expand volume again, it should be a success
+        """
+        self.is_io_running = False
+
+        # Add brick to running volume
+        vol_bricks_before = redant.get_all_bricks(self.vol_name,
+                                                  self.server_list[0])
+
+        force = False
+        if self.volume_type == "dist-disp" or self.volume_type == "disp":
+            force = True
+
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
         if not ret:
-            raise ExecutionError("Failed to Setup Volume")
+            raise Exception("Failed to expand volume")
 
-    def test_add_brick_running_volume(self):
-        """Add brick to running volume
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
+
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
+
+        vol_bricks_before = vol_bricks_after
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
+
+        # Reset volume config
+        ret = redant.shrink_volume(self.server_list[0], self.vol_name,
+                                   subvol_num=2, force=True)
+        if not ret:
+            raise Exception("Shrinking volume to old config failed")
+
+        # Case 2: def test_add_bricks_stopped_volume
         """
-        vol_bricks = {
-            'before': get_all_bricks(self.mnode, self.volname),
-            'after': []}
-
-        g.log.debug("Expanding volume %s", self.volname)
-        ret = expand_volume(mnode=self.mnode,
-                            volname=self.volname,
-                            servers=self.servers,
-                            all_servers_info=self.all_servers_info)
-
-        self.assertTrue(ret, 'Unable to expand volume %s' % self.volname)
-        g.log.info("Add brick successfully %s", self.volname)
-
-        vol_bricks['after'] = get_all_bricks(self.mnode, self.volname)
-
-        self.assertGreater(len(vol_bricks['after']), len(vol_bricks['before']),
-                           "Expected new volume size to be greater than old")
-
-        g.log.debug("Expanding volume %s", self.volname)
-        ret = expand_volume(mnode=self.mnode,
-                            volname=self.volname,
-                            servers=self.servers,
-                            all_servers_info=self.all_servers_info)
-        self.assertTrue(ret, 'Unable to expand volume %s' % self.volname)
-
-        g.log.info("Success on expanding volume %s", self.volname)
-
-        vol_bricks['before'] = vol_bricks['after']
-        vol_bricks['after'] = get_all_bricks(self.mnode, self.volname)
-        self.assertGreater(len(vol_bricks['after']), len(vol_bricks['before']),
-                           "Expected new volume size to be greater than old")
-
-        g.log.info('Success in add bricks to running volume')
-
-    def test_add_bricks_stopped_volume(self):
-        """Add bricks to stopped volume
+        Steps:
+        - Create and start a volume
+        - Stop the volume
+        - Expand volume again, it should be a success
         """
-        ret, _, _ = volume_stop(self.mnode, self.volname)
-        self.assertEqual(ret, 0, "Can't stop volume")
+        redant.volume_stop(self.vol_name, self.server_list[0])
 
-        g.log.info('Volume %s stopped successfully', self.volname)
+        vol_bricks_before = redant.get_all_bricks(self.vol_name,
+                                                  self.server_list[0])
 
-        vol_bricks = {
-            'before': get_all_bricks(self.mnode, self.volname),
-            'after': []
-        }
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
 
-        g.log.debug("Adding bricks to volume %s", self.volname)
-        ret = expand_volume(mnode=self.mnode,
-                            volname=self.volname,
-                            servers=self.servers,
-                            all_servers_info=self.all_servers_info)
-        self.assertTrue(ret, 'Unable to expand volume %s' % self.volname)
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
 
-        g.log.info('Added bricks to stopped volume %s', self.volname)
+        # Reset volume config
+        ret = redant.shrink_volume(self.server_list[0], self.vol_name,
+                                   subvol_num=1, force=True)
+        if not ret:
+            raise Exception("Shrinking volume to old config failed")
 
-        vol_bricks['after'] = get_all_bricks(self.mnode, self.volname)
-        self.assertGreater(len(vol_bricks['after']), len(vol_bricks['before']),
-                           "Expected new volume size to be greater than old")
-        g.log.info('Success in add bricks to stopped volume')
-
-    def test_add_bricks_io_mount_point(self):
+        # Case 3: def test_add_bricks_io_mount_point
         """
         Test Case Steps:
         1. Create, start and mount volume.
@@ -118,72 +136,37 @@ class ExerciseAddbrickCommand(GlusterBaseClass):
         4. Wait for rebalance to complete.
         5. Check for I/O failures if any.
         """
-
         # Mount volume
-        ret = self.mount_volume(self.mounts)
-        self.assertTrue(ret, 'Mount volume: FAIL')
-        g.log.info('Mounted Volume %s: Success', self.volname)
-
-        # Upload io scripts for running IO on mounts
-        g.log.info("Upload io scripts to clients %s for running IO on "
-                   "mounts", self.clients)
-        script_location = "/usr/share/glustolibs/io/scripts/file_dir_ops.py"
-        ret = upload_scripts(self.clients, script_location)
-        if not ret:
-            clients = ", ".join(self.clients)
-            raise ExecutionError("Failed to upload IO scripts to clients %s" %
-                                 clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   self.clients)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        self.redant.execute_abstract_op_node(f"mkdir -p {self.mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.mountpoint, self.client_list[0])
 
         # Start IO on mounts
-        io_process = []
+        self.io_process = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
         for index, mount_obj in enumerate(self.mounts, start=1):
-            g.log.info("Starting IO on %s:%s", mount_obj.client_system,
-                       mount_obj.mountpoint)
-            cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-                   "--dirname-start-num %d "
-                   "--dir-depth 2 "
-                   "--dir-length 2 "
-                   "--max-num-of-dirs 2 "
-                   "--num-of-files 10 %s" % (
-                       script_location, index + 10,
-                       mount_obj.mountpoint))
-            proc = g.run_async(mount_obj.client_system, cmd,
-                               user=mount_obj.user)
-            io_process.append(proc)
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      index + 10, 2, 2, 2, 10,
+                                                      mount_obj['client'])
+            self.io_process.append(proc)
+            self.is_io_running = True
 
         # Expand volume
-        ret = expand_volume(mnode=self.mnode,
-                            volname=self.volname,
-                            servers=self.servers,
-                            all_servers_info=self.all_servers_info)
-        self.assertTrue(ret, "Expand volume %s: Fail" % self.volname)
-        g.log.info("Volume %s expanded: Success", self.volname)
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
 
         # Validate IO on current mount point
-        self.assertTrue(validate_io_procs(io_process, self.mounts),
-                        'IO Failed on clients')
-        g.log.info('I/O successful on all the clients')
+        ret = redant.validate_io_procs(self.io_process, self.mounts)
+        if not ret:
+            raise Exception('IO Failed on clients')
+        self.is_io_running = False
 
-        self.assertTrue(self.unmount_volume(self.mounts),
-                        'Unmount end points: Fail')
-        g.log.info("Unmount mount points: Success")
-
-        g.log.info('Add brick during IO operations successfully')
-
-    def tearDown(self):
-        """tear Down callback"""
-        # clean up all volumes
-        vol_list = get_volume_list(self.mnode)
-        if vol_list is None:
-            raise ExecutionError("Failed to get the volume list")
-
-        for volume in vol_list:
-            ret = cleanup_volume(self.mnode, volume)
-            if not ret:
-                raise ExecutionError("Unable to delete volume % s" % volume)
-            g.log.info("Volume deleted successfully : %s", volume)
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
+        redant.volume_unmount(self.vol_name, self.mountpoint,
+                              self.client_list[0])
+        redant.execute_abstract_op_node(f"rm -rf {self.mountpoint}",
+                                        self.client_list[0])
