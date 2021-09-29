6626b1b3aa37eeedf0c3754c303dba72f3430bc0
Nikhil Ladha <nladha@redhat.com>
2021-09-29 13:32:36 +0530

    Migrated TC test_rebalance_add_brick_command (#1134)
    * Add test rebalance positive test - add brick command
    
    Changes:
    - Updated test case add brics to volume with IO operations.
    
    Change-Id: I61e83c0d58d65783da4e54bc2a8a32a35b515c07
    
    * Shorten all the logs around verify_io_procs
    
    No functional change, just make the tests a bit more readable.
    It could be moved to a decorator later on, wrapping tests.
    
    Change-Id: I484bb8b46907ee8f33dfcf4c960737a21819cd6a
    Signed-off-by: Yaniv Kaul <ykaul@redhat.com>
    
    * Add py3 support in 'tests/functional/dht'
    
    Change-Id: I44fe85519c8fd381064670e54dac8736107b0928
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Remove python version dependency(Part 4)
    
    Please refer to commit message of patch [1].
    
    [1] https://review.gluster.org/#/c/glusto-tests/+/24140/
    
    Change-Id: I25d30f7bdb20f0825709c4c852140e1906870ce7
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Fix tier1 test case failures
    
    Problems:
    ---------
    test_self_heal_with_link_files.py fails as granular entry heal
    creates a directory which causes aqreual-checksum mismatch.
    
    test_brick_full_add_brick_rebalance.py fails with
    `fallocate: fallocate failed: No space left on device` which
    happens as the volume gets filled quicker than expected.
    
    test_rebalance_add_brick_command.py fails with disperse volumes
    as it runs `expand_volume()` till the bricks is exhausted.
    
    test_snapshot_restore.py `if not ret` causes teardown failures
    even when the command was successful.
    
    Fixes:
    ------
    test_self_heal_with_link_files.py - Update to support granualar entry heal.
    test_brick_full_add_brick_rebalance.py - Modify code to not fail on I/O.
    test_rebalance_add_brick_command.py - Modify code to not run expand_volume
    4 times.
    test_snapshot_restore.py - Change to `if ret` to fix tearDown failure
    
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Add docstring for test and remove unnecessary logging
    
    * Fix pylint issues
    
    * Migrated TC test_rebalance_add_brick_command
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix remove brick
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Miroslav Asenov <masenov@redhat.com>
    Co-authored-by: Yaniv Kaul <ykaul@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/dht/test_rebalance_add_brick_command.py
@@ -0,0 +1,174 @@
+"""
+ Copyright (C) 2017-2021 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Positive test - Exercise Add-brick command
+"""
+
+# disruptive;dist,rep,disp,dist-rep,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
+
+
+class TestExerciseAddbrickCommand(DParentTest):
+
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 self.vol_type_inf[self.volume_type],
+                                 self.server_list, self.brick_roots)
+
+    def terminate(self):
+        """
+        Wait for IO toc omplete, if the TC fails early
+        """
+        try:
+            if self.is_io_running:
+                if not self.redant.wait_for_io_to_complete(self.io_process,
+                                                           self.mounts):
+                    raise Exception("Failed to wait for IO to complete")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
+
+    def run_test(self, redant):
+        """
+        Steps:
+        - Create and start a volume
+        - Expand volume, it should be a success
+        - Expand volume again, it should be a success
+        """
+        self.is_io_running = False
+
+        # Add brick to running volume
+        vol_bricks_before = redant.get_all_bricks(self.vol_name,
+                                                  self.server_list[0])
+
+        force = False
+        if self.volume_type == "dist-disp" or self.volume_type == "disp":
+            force = True
+
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed to expand volume")
+
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
+
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
+
+        vol_bricks_before = vol_bricks_after
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
+
+        # Reset volume config
+        ret = redant.shrink_volume(self.server_list[0], self.vol_name,
+                                   subvol_num=2, force=True)
+        if not ret:
+            raise Exception("Shrinking volume to old config failed")
+
+        # Case 2: def test_add_bricks_stopped_volume
+        """
+        Steps:
+        - Create and start a volume
+        - Stop the volume
+        - Expand volume again, it should be a success
+        """
+        redant.volume_stop(self.vol_name, self.server_list[0])
+
+        vol_bricks_before = redant.get_all_bricks(self.vol_name,
+                                                  self.server_list[0])
+
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
+
+        vol_bricks_after = redant.get_all_bricks(self.vol_name,
+                                                 self.server_list[0])
+        if len(vol_bricks_after) <= len(vol_bricks_before):
+            raise Exception("Expected new volume size to be greater than old")
+
+        # Reset volume config
+        redant.volume_start(self.vol_name, self.server_list[0], force=True)
+
+        ret = redant.shrink_volume(self.server_list[0], self.vol_name,
+                                   subvol_num=1, force=True)
+        if not ret:
+            raise Exception("Shrinking volume to old config failed")
+
+        # Case 3: def test_add_bricks_io_mount_point
+        """
+        Test Case Steps:
+        1. Create, start and mount volume.
+        2. Start I/O on volume.
+        3. Add brick and start rebalance on the volume.
+        4. Wait for rebalance to complete.
+        5. Check for I/O failures if any.
+        """
+        # Mount volume
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        self.redant.execute_abstract_op_node(f"mkdir -p {self.mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.mountpoint, self.client_list[0])
+
+        # Start IO on mounts
+        self.io_process = []
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+        for index, mount_obj in enumerate(self.mounts, start=1):
+            proc = redant.create_deep_dirs_with_files(mount_obj['mountpath'],
+                                                      index + 10, 2, 2, 2, 10,
+                                                      mount_obj['client'])
+            self.io_process.append(proc)
+            self.is_io_running = True
+
+        # Expand volume
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   force=force)
+        if not ret:
+            raise Exception("Failed ot expand volume")
+
+        # Validate IO on current mount point
+        ret = redant.validate_io_procs(self.io_process, self.mounts)
+        if not ret:
+            raise Exception('IO Failed on clients')
+        self.is_io_running = False
+
+        redant.volume_unmount(self.vol_name, self.mountpoint,
+                              self.client_list[0])
+        redant.execute_abstract_op_node(f"rm -rf {self.mountpoint}",
+                                        self.client_list[0])
