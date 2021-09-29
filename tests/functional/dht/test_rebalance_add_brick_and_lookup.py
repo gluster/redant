c42ccd6564646831faea24a4107b9a169cc2d984
Nikhil Ladha <nladha@redhat.com>
2021-09-29 11:49:00 +0530

    Migrated TC test_rebalance_add_brick_and_lookup (#1133)
    * Rebalance with add brick and lookup on mount
    
    Steps-
    1. Create a distributed-replicated volume, start and mount it.
    2. Create deep dirs (200) and create some 100 files on the
    deepest directory.
    3. Expand volume.
    4. Start rebalance.
    5. Once rebalance is completed, do a lookup on mount and log
    the time taken.
    
    Change-Id: I3a55d2670cc6bda7670f97f0cd6208dc9e36a5d6
    Signed-off-by: srijan-sivakumar <ssivakum@redhat.com>
    
    * Fix test_rebalance_with_add_brick_and_lookup
    
    Problem:
    The current approach compares the lookup time with a
    predetermined time of 4.4 secs (It took	4 seconds in
    RHGS 3.5.2 and keeping a buffer of 10%). But this value
    is machine is specific and vairies accordingly with
    machines. For e.g, below are the time taken when executed
    the above tests	locally
    
    ```
    Glusterfs 49
    
    Lookup took : 19 seconds (distributed)
    Lookup took : 14 seconds (distributed-replicated)
    Lookup took : 14 seconds (distributed-dispersed)
    Lookup took : 14 seconds (distributed-arbiter)
    
    Glusterfs 56.2
    
    Lookup took : 15 seconds (distributed)
    Lookup took : 10 seconds (distributed-replicated)
    Lookup took : 13 seconds (distributed-dispersed)
    Lookup took : 11 seconds (distributed-arbiter)
    
    Glusterfs 59
    
    Lookup took : 15 seconds (distributed)
    Lookup took : 10 seconds (distributed-replicated)
    Lookup took : 13 seconds (distributed-dispersed)
    Lookup took : 10 seconds (distributed-arbiter)
    
    ```
    
    Solution:
    The ideal way is to uninstall gluster rpms and install an
    older version and run the lookup and finally compare the
    results. But this approach is not feasible with our setup.
    Hence removing the time	comparison check until an efficient
    approach is found
    
    Signed-off-by: Pranav <prprakas@redhat.com>
    
    * Migrated TC test_rebalance_add_brick_and_lookup
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: srijan-sivakumar <ssivakum@redhat.com>
    Co-authored-by: Pranav <prprakas@redhat.com>

---
+++ tests/functional/dht/test_rebalance_add_brick_and_lookup.py
@@ -0,0 +1,71 @@
+"""
+ Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along`
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-131 USA.
+
+ Description:
+    Rebalance with add brick and log time taken for lookup
+"""
+
+# disruptive;dist,dist-rep,dist-arb,dist-disp
+from time import time
+from tests.d_parent_test import DParentTest
+
+
+class TestRebalanceWithAddBrickAndLookup(DParentTest):
+
+    def run_test(self, redant):
+        """
+        Steps:
+        - Create a Distributed-Replicated volume.
+        - Create deep dirs(200) and 100 files on the deepest directory.
+        - Expand volume.
+        - Initiate rebalance
+        - Once rebalance is completed, do a lookup on mount and time it.
+        """
+        # Create Deep dirs.
+        cmd = (f"cd {self.mountpoint}/; for i in {{1..200}};do mkdir "
+               "dir${i}; cd dir${i}; if [ ${i} -eq 100 ]; then for j in"
+               " {1..100}; do touch file${j}; done; fi; done;")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+
+        # Expand the volume.
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots)
+        if not ret:
+            raise Exception(f"Failed to expand the volume {self.vol_name}")
+
+        # Start Rebalance.
+        redant.rebalance_start(self.vol_name, self.server_list[0])
+
+        # Wait for rebalance to complete
+        ret = redant.wait_for_rebalance_to_complete(self.vol_name,
+                                                    self.server_list[0],
+                                                    timeout=500)
+        if not ret:
+            raise Exception("Rebalance is not yet complete on the volume "
+                            f"{self.vol_name}")
+
+        # Do a lookup on the mountpoint and note the time taken to run.
+        # The time used for comparison is taken as a benchmark on using a
+        # RHGS 3.5.2 for this TC. For 3.5.2, the time takes came out to be
+        # 4 seconds. Now the condition for subtest to pass is for the lookup
+        # should not be more than 10% of this value, i.e. 4.4 seconds.
+        cmd = f"ls -R {self.mountpoint}/"
+        start_time = time()
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
+        end_time = time()
+        time_taken = end_time - start_time
+        redant.logger.info(f"Lookup took : {time_taken} seconds")
