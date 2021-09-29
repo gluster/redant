b8b2c2fd5ce16a318e3eb0eea82cf92c26473692
Nikhil Ladha <nladha@redhat.com>
2021-09-29 11:50:06 +0530

    Migrated TC test_negative_exercise_add_brick_command (#1130)
    * Adding testcase: Negative test - Exercise Add-brick command
    
    This testcase covers below scenarios,
    1) Add-brick without volname
    2) Adding a duplicate brick
    3) Adding a brick which is already part of another volume
    4) Adding a nested brick i.e brick inside another brick
    5) Add a brick to a non existent volume
    6) Add a brick from the peer which is not in cluster
    
    Change-Id: I2d68715facabaa172db94afc7e1b64f95fb069a7
    Signed-off-by: Prasad Desala <tdesala@redhat.com>
    
    * Fix up coding style issues in tests
    
    Change-Id: I14609030983d4485dbce5a4ffed1e0353e3d1bc7
    
    * TC: test_negative_exercise_add_brick
    
    Fixed the validation code in test_add_brick_without_volname
    
    Change-Id: I5449b6472cc2b6644b833be47233d344a9dd8d1b
    Signed-off-by: Prasad Desala <tdesala@redhat.com>
    
    * Add py3 support in 'tests/functional/dht'
    
    Change-Id: I44fe85519c8fd381064670e54dac8736107b0928
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Migrated TC test_negative_exercise_add_brick_command
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix add-brick
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Prasad Desala <tdesala@redhat.com>
    Co-authored-by: Nigel Babu <nigelb@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>

---
+++ tests/functional/dht/test_negative_exercise_add_brick_command.py
@@ -0,0 +1,110 @@
+"""
+ Copyright (C) 2017-2018 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Negative test - Exercise Add-brick command
+"""
+
+# disruptive;rep,dist,dist-rep,disp,dist-disp
+from tests.d_parent_test import DParentTest
+
+
+class TestExerciseAddbrickCommand(DParentTest):
+
+    def run_test(self, redant):
+        """Test add-brick command without volume"""
+        # Form bricks list for add-brick
+        brick_cmd = redant.form_brick_cmd_to_add_brick(self.server_list[0],
+                                                       self.vol_name,
+                                                       self.server_list,
+                                                       self.brick_roots)
+        cmd = f"gluster volume add-brick {brick_cmd}"
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: Add-brick is successfull")
+
+        # Case 2: test_add_duplicate_brick
+        """
+        Test add-bricks to the volume which are already part of the volume
+        """
+        # Get sub-vols for adding the same bricks to the volume
+        subvols = redant.get_subvols(self.vol_name, self.server_list[0])
+        cmd = (f"gluster volume add-brick {self.vol_name} "
+               f"{' '.join(subvols[0])}")
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: Add-brick is successfull")
+        err_msg = ("Brick may be containing or be contained by an existing"
+                   " brick")
+        if err_msg not in ret['error_msg']:
+            raise Exception("Unexpected: add-brick is successful without any"
+                            " error")
+
+        # Case 3: test_add_nested_brick
+        """Test add nested bricks to the volume"""
+        # Get sub-vols for forming a nested bricks list
+        subvols = redant.get_subvols(self.vol_name, self.server_list[0])[0]
+        nested_bricks_list = [f"{x}/nested" for x in subvols]
+        cmd = (f"gluster volume add-brick {self.vol_name} "
+               f"{' '.join(nested_bricks_list)}")
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: Add-brick is successfull")
+        err_msg = ("Brick may be containing or be contained by an existing"
+                   " brick")
+        if err_msg not in ret['error_msg']:
+            raise Exception("Unexpected: add-brick is successful without any"
+                            " error")
+
+        # Case 4: test_add_brick_non_existent_volume
+        """Test add-bricks to an non existent volume"""
+        # Form bricks list for add-brick
+        brick_cmd = redant.form_brick_cmd_to_add_brick(self.server_list[0],
+                                                       self.vol_name,
+                                                       self.server_list,
+                                                       self.brick_roots)
+        cmd = f"gluster volume add-brick novolume {brick_cmd}"
+        ret = redant.execute_abstract_op_node(cmd, self.server_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: Add-brick is successfull")
+        err_msg = "does not exist"
+        if err_msg not in ret['error_msg']:
+            raise Exception("Unexpected: add-brick is successful without any"
+                            " error")
+
+        # Case 5: test_add_brick_peer_not_in_cluster
+        """
+        Test add bricks to the volume from the host which is not
+        in the cluster.
+        """
+        # Form bricks list for add-brick
+        bricks_list = redant.get_subvols(self.vol_name,
+                                         self.server_list[0])[0]
+        for (i, item) in enumerate(bricks_list):
+            server, _ = item.split(":")
+            item.replace(server, "abc.def.ghi.jkl")
+            bricks_list[i] = item.replace(server, "abc.def.ghi.jkl")
+
+        brick_str = " ".join(bricks_list)
+        ret = redant.add_brick(self.vol_name, brick_str, self.server_list[0],
+                               excep=False)
+        if ret['msg']['opRet'] == '0':
+            raise Exception("Unexpected: add-brick is successful")
+        err_msg = "Pre-validation failed on localhost"
+        if err_msg not in ret['msg']['opErrstr']:
+            raise Exception("Unexpected: add-brick successful without "
+                            "any error")
