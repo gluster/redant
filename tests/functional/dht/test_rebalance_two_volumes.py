677a3cfb9d8c6e1431ed3b441211163bb5322b5a
Sheetal Pamecha <spamecha@redhat.com>
2021-09-30 17:20:09 +0530

    Migrate TC test_rebalance_two_volumes.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/dht/test_rebalance_two_volumes.py
+++ tests/functional/dht/test_rebalance_two_volumes.py
@@ -1,73 +1,39 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.rebalance_ops import (
-    rebalance_start, wait_for_rebalance_to_complete)
-from glustolibs.gluster.volume_libs import expand_volume
-from glustolibs.io.utils import collect_mounts_arequal
-from glustolibs.gluster.mount_ops import mount_volume
-from glustolibs.gluster.volume_ops import (volume_create, volume_start,
-                                           volume_stop, volume_delete)
-from glustolibs.gluster.lib_utils import form_bricks_list
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along`
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+# disruptive;dist,dist-rep
+
+import traceback
+from copy import deepcopy
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed', 'distributed-replicated'], ['glusterfs']])
-class TestRebalanceTwoVolumes(GlusterBaseClass):
+class TestRebalanceTwoVolumes(DParentTest):
 
-    def setUp(self):
+    def terminate(self):
+        try:
+            self.redant.cleanup_volume(self.server_list, self.second_vol_name)
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volume
-        if not self.setup_volume_and_mount_volume([self.mounts[0]]):
-            raise ExecutionError("Failed to Setup and mount volume")
-
-        self.first_client = self.mounts[0].client_system
-
-        self.second_vol_name = "second_volume"
-        self.second_mountpoint = "/mnt/{}".format(self.second_vol_name)
-        self.is_second_volume_created = False
-
-    def tearDown(self):
-
-        # Unmount and clean volume
-        if not self.unmount_volume_and_cleanup_volume([self.mounts[0]]):
-            raise ExecutionError("Failed to Cleanup Volume")
-
-        if self.is_second_volume_created:
-            # Stop the 2nd volume
-            ret, _, _ = volume_stop(self.mnode, self.second_vol_name)
-            self.assertEqual(ret, 0, ("volume stop failed for %s"
-                                      % self.second_vol_name))
-            g.log.info("Volume %s stopped", self.second_vol_name)
-
-            # Delete the 2nd volume
-            ret = volume_delete(self.mnode, self.second_vol_name)
-            self.assertTrue(ret, ("Failed to cleanup the Volume "
-                                  "%s", self.second_vol_name))
-            g.log.info("Volume deleted successfully : %s",
-                       self.second_vol_name)
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_rebalance_two_volumes(self):
+    def run_test(self, redant):
         """
         Test case:
         1. Create a volume, start it and mount it
@@ -80,84 +46,68 @@ class TestRebalanceTwoVolumes(GlusterBaseClass):
         8. Collect arequal checksum on mount point post-rebalance
            and compare with value from step 4
         """
+        # Create a 2nd volume, start and mount it
+        self.second_vol_name = "second_volume"
+        self.second_mountpoint = f"/mnt/{self.second_vol_name}"
+        conf_dict = deepcopy(self.vol_type_inf[self.volume_type])
+        if self.volume_type == "dist":
+            factor = 3
+        elif self.volume_type == "dist-rep":
+            factor = 6
+        brick_dict, brick_cmd = redant.form_brick_cmd(self.server_list,
+                                                      self.brick_roots,
+                                                      self.second_vol_name,
+                                                      factor)
+        redant.volume_create_with_custom_bricks(self.second_vol_name,
+                                                self.server_list[0],
+                                                conf_dict, brick_cmd,
+                                                brick_dict, force=True)
+        redant.volume_start(self.second_vol_name, self.server_list[0])
+        self.redant.execute_abstract_op_node("mkdir -p "
+                                             f"{self.second_mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.second_mountpoint, self.client_list[0])
 
-        # Get brick list
-        bricks_list = form_bricks_list(self.mnode, self.volname, 3,
-                                       self.servers, self.all_servers_info)
-        self.assertIsNotNone(bricks_list, "Bricks list is None")
-
-        # Create 2nd volume
-        ret, _, _ = volume_create(self.mnode, self.second_vol_name,
-                                  bricks_list)
-        self.assertEqual(ret, 0, ("Failed to create volume %s") % (
-            self.second_vol_name))
-        g.log.info("Volume %s created successfully", self.second_vol_name)
-
-        # Start 2nd volume
-        ret, _, _ = volume_start(self.mnode, self.second_vol_name)
-        self.assertEqual(ret, 0, ("Failed to start volume %s") % (
-            self.second_vol_name))
-        g.log.info("Started volume %s", self.second_vol_name)
-
-        self.is_second_volume_created = True
-
-        # Mount 2nd volume
-        for mount_obj in self.mounts:
-            ret, _, _ = mount_volume(self.second_vol_name,
-                                     mtype=self.mount_type,
-                                     mpoint=self.second_mountpoint,
-                                     mserver=self.mnode,
-                                     mclient=mount_obj.client_system)
-            self.assertEqual(ret, 0, ("Failed to mount volume %s") % (
-                self.second_vol_name))
-            g.log.info("Volume mounted successfully : %s",
-                       self.second_vol_name)
-
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
         # Start I/O from mount point for volume 1 and wait for it to complete
-        cmd = ("cd %s; for i in {1..1000} ; do "
-               "dd if=/dev/urandom of=file$i bs=10M count=1; done"
-               % self.mounts[0].mountpoint)
-        ret, _, _ = g.run(self.first_client, cmd)
-        self.assertEqual(ret, 0, "IO failed on volume %s"
-                         % self.volname)
+        cmd = (f"cd {self.mountpoint}; for i in {{1..1000}} ; do "
+               "dd if=/dev/urandom of=file$i bs=10M count=1; done")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Start I/O from mount point for volume 2 and wait for it to complete
-        cmd = ("cd %s; for i in {1..1000} ; do "
-               "dd if=/dev/urandom of=file$i bs=10M count=1; done"
-               % self.second_mountpoint)
-        ret, _, _ = g.run(self.first_client, cmd)
-        self.assertEqual(ret, 0, "IO failed on volume %s"
-                         % self.second_vol_name)
+        cmd = (f"cd {self.second_mountpoint}; for i in {{1..1000}} ; do "
+               "dd if=/dev/urandom of=file$i bs=10M count=1; done")
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Collect arequal checksum before rebalance
-        arequal_checksum_before = collect_mounts_arequal(self.mounts[0])
+        arequal_checksum_before = redant.collect_mounts_arequal(self.mounts[0])
 
         # Add bricks to volumes
-        for volume in (self.volname, self.second_vol_name):
-            ret = expand_volume(self.mnode, volume, self.servers,
-                                self.all_servers_info)
-            self.assertTrue(ret, "Failed to add brick on volume %s"
-                            % volume)
+        for volume in (self.vol_name, self.second_vol_name):
+            ret = redant.expand_volume(self.server_list[0],
+                                       volume, self.server_list,
+                                       self.brick_roots, force=True)
+            if not ret:
+                raise Exception("Failed to expand volume")
 
         # Trigger rebalance
-        for volume in (self.volname, self.second_vol_name):
-            ret, _, _ = rebalance_start(self.mnode, volume,
-                                        force=True)
-            self.assertEqual(ret, 0, "Failed to start rebalance on the"
-                             " volume %s" % volume)
+        for volume in (self.vol_name, self.second_vol_name):
+            redant.rebalance_start(volume, self.server_list[0],
+                                   force=True)
 
         # Wait for rebalance to complete
-        for volume in (self.volname, self.second_vol_name):
-            ret = wait_for_rebalance_to_complete(self.mnode, volume,
-                                                 timeout=1200)
-            self.assertTrue(ret, "Rebalance is not yet complete on the volume"
-                            " %s" % volume)
-            g.log.info("Rebalance successfully completed")
+        for volume in (self.vol_name, self.second_vol_name):
+            ret = redant.wait_for_rebalance_to_complete(volume,
+                                                        self.server_list[0],
+                                                        timeout=1200)
+            if not ret:
+                raise Exception("Rebalance is not yet complete on the volume "
+                                f"{volume}")
 
         # Collect arequal checksum after rebalance
-        arequal_checksum_after = collect_mounts_arequal(self.mounts[0])
+        arequal_checksum_after = redant.collect_mounts_arequal(self.mounts[0])
 
         # Check for data loss by comparing arequal before and after rebalance
-        self.assertEqual(arequal_checksum_before, arequal_checksum_after,
-                         "arequal checksum is NOT MATCHNG")
-        g.log.info("arequal checksum is SAME")
+        if arequal_checksum_before != arequal_checksum_after:
+            raise Exception("arequal checksum in NOT equal")
