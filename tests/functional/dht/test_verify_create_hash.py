82761c3a33295f281354b244fe3eb0381cbc7e64
Nikhil Ladha <nladha@redhat.com>
2021-10-04 00:20:34 +0530

    Migrated TC test_verify_create_hash (#1152)
    * glusto/dht: Verify create file operation
    
    test case:
    - Verify that the file is created on the hashed subvol alone
    - Verify that the trusted.glusterfs.pathinfo reflects the file location
    - Verify that the file creation fails if the hashed subvol is down
    
    Change-Id: I951c20f03772a0c5739244ec354f9bbfd6d0ea65
    Signed-off-by: Susant Palai <spalai@redhat.com>
    
    * Add py3 support in 'tests/functional/dht'
    
    Change-Id: I44fe85519c8fd381064670e54dac8736107b0928
    Signed-off-by: Valerii Ponomarov <kiparis.kh@gmail.com>
    
    * Moving test steps from teardown class to teardown
    
    Move cases from teardown class to teardown in dht
    
    Change-Id: Id0cf120c6229715521ae19fd4bb00cad553d701f
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>
    
    * Fix wrong comparion in test_create_file
    
    Problem:
    brickdir.hashrange_contains_hash() returns true
    or False. However it test test_create_file it's
    check it ret == 1 or not
    
    Fix:
    Changing ret == 1 to ret.
    
    Change-Id: I53655794f10fc5d778790bdffbe65563907bef6d
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>
    
    * Migrated TC test_verify_create_hash
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    * Fix brick path
    
    Signed-off-by: nik-redhat <nladha@redhat.com>
    
    Co-authored-by: Susant Palai <spalai@redhat.com>
    Co-authored-by: Valerii Ponomarov <vponomar@redhat.com>
    Co-authored-by: Sri Vignesh <sselvan@redhat.com>
    Co-authored-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ tests/functional/dht/test_verify_create_hash.py
@@ -0,0 +1,127 @@
+"""
+ Copyright (C) 2017-2020 Red Hat, Inc. <http://www.redhat.com>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ Description:
+    Test File creation with hashed and cached subvol down scenarios
+"""
+
+# disruptive;dist,dist-rep,dist-disp
+from tests.d_parent_test import DParentTest
+
+
+class TestCreateFile(DParentTest):
+
+    def run_test(self, redant):
+        '''
+        test case: (file creation)
+        Steps:
+        - Create and start a volume
+        - Mount it on a client
+        - Create a file on mountpoint
+        - Verify that the file is created on the hashed subvol alone
+        - Verify that the trusted.glusterfs.pathinfo reflects the file location
+        - Verify that the file creation fails if the hashed subvol is down
+        '''
+        # hash for file1
+        filehash = redant.calculate_hash(self.server_list[0], 'file1')
+
+        # collect subvol info
+        subvols = redant.get_subvols(self.vol_name, self.server_list[0])
+        if not subvols:
+            raise Exception("Failed to get subvols")
+
+        secondary_bricks = []
+        for subvol in subvols:
+            secondary_bricks.append(subvol[0])
+
+        bricklist = []
+        for item in secondary_bricks:
+            bricklist.append(item)
+
+        file_one = f"{self.mountpoint}/file1"
+
+        # create a file
+        redant.execute_abstract_op_node(f"touch {file_one}",
+                                        self.client_list[0])
+
+        # get pathinfo xattr on the file
+        cmd = f"getfattr -n trusted.glusterfs.pathinfo {file_one}"
+        ret = redant.execute_abstract_op_node(cmd, self.client_list[0])
+        out = "".join(ret['msg'])
+
+        if self.volume_type == "dist":
+            brickhost = (out.split(":"))[3]
+            brickpath = (out.split(":"))[4].split(">")[0]
+        else:
+            brickhost = (out.split(":"))[4]
+            brickpath = (out.split(":")[5]).split(">")[0]
+
+        # make sure the file is present only on the hashed brick
+        count = -1
+        for brickdir in bricklist:
+            count += 1
+            ret = redant.hashrange_contains_hash(brickdir, filehash)
+            host, path = brickdir.split(":")
+            if ret:
+                hash_subvol = subvols[count]
+                redant.execute_abstract_op_node(f"stat {path}/file1", host)
+                continue
+
+            ret = redant.execute_abstract_op_node(f"stat {path}/file1",
+                                                  host, False)
+            if ret['error_code'] == 0:
+                raise Exception("Expected stat to fail")
+
+        # checking if pathinfo xattr has the right value
+        redant.execute_abstract_op_node(f"stat {brickpath}", brickhost)
+
+        # get permission from mount
+        ret = redant.execute_abstract_op_node(f"ls -l {file_one}",
+                                              self.client_list[0])
+        mperm = ret['msg'][0].strip().split(" ")[0]
+        if mperm is None:
+            raise Exception("Unexpected: failed to get file1 permissions")
+
+        # get permission from brick
+        ret = redant.execute_abstract_op_node(f"ls -l {brickpath}",
+                                              brickhost)
+        bperm = ret['msg'][0].strip().split(" ")[0]
+        if bperm is None:
+            raise Exception("Unexpected: failed to get file1 permissions")
+
+        # check if the permission matches
+        if mperm != bperm:
+            raise Exception("Expected permission to match")
+
+        # check that gfid xattr is present on the brick
+        redant.execute_abstract_op_node("getfattr -n trusted.gfid "
+                                        f"{brickpath}", brickhost)
+
+        # delete the file, bring down it's hash, create the file,
+        redant.execute_abstract_op_node(f"rm -f {file_one}",
+                                        self.client_list[0])
+
+        ret = redant.bring_bricks_offline(self.vol_name, hash_subvol)
+        if not ret:
+            raise Exception('Error in bringing down subvolume '
+                            f'{hash_subvol}')
+
+        # check file creation should fail
+        ret = redant.execute_abstract_op_node(f"touch {file_one}",
+                                              self.client_list[0], False)
+        if ret['error_code'] == 0:
+            raise Exception("Unexpected: file creation is successful")
