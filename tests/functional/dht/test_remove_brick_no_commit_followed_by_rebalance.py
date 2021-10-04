6b7586c3690174f13fc304830b769fa66ca5eef6
Sheetal Pamecha <spamecha@redhat.com>
2021-10-04 20:23:11 +0530

    Migrated TC test_remove_brick_no_commit_followed_by_rebalance.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/dht/test_remove_brick_no_commit_followed_by_rebalance.py
+++ tests/functional/dht/test_remove_brick_no_commit_followed_by_rebalance.py
@@ -1,78 +1,34 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description: Tests to check that there is no data loss when
+             remove-brick operation is stopped and then new bricks
+             are added to the volume.
+"""
+
+# disruptive;dist-rep
+# TODOdist,dist-rep,dist-disp,dist-arb
 from time import sleep
-from glusto.core import Glusto as g
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.io.utils import collect_mounts_arequal, validate_io_procs
-from glustolibs.misc.misc_libs import upload_scripts
-from glustolibs.gluster.volume_libs import (form_bricks_list_to_remove_brick,
-                                            expand_volume)
-from glustolibs.gluster.brick_ops import remove_brick
-from glustolibs.gluster.rebalance_ops import (rebalance_start,
-                                              wait_for_rebalance_to_complete)
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed', 'distributed-replicated',
-           'distributed-dispersed', 'distributed-arbiter'],
-          ['glusterfs']])
-class TestRemoveBrickNoCommitFollowedByRebalance(GlusterBaseClass):
-    @classmethod
-    def setUpClass(cls):
-        cls.get_super_method(cls, 'setUpClass')()
-
-        # Upload io scripts for running IO on mounts
-        cls.script_upload_path = ("/usr/share/glustolibs/io/scripts/"
-                                  "file_dir_ops.py")
-        ret = upload_scripts(cls.clients, cls.script_upload_path)
-        if not ret:
-            raise ExecutionError("Failed to upload IO scripts "
-                                 "to clients %s" % cls.clients)
-        g.log.info("Successfully uploaded IO scripts to clients %s",
-                   cls.clients)
-
-    def setUp(self):
+class TestRemoveBrickNoCommitFollowedByRebalance(DParentTest):
+    def run_test(self, redant):
         """
-        Setup and mount volume or raise ExecutionError
-        """
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
-
-        # Setup Volume
-        ret = self.setup_volume_and_mount_volume([self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Failed to Setup and Mount Volume")
-
-    def tearDown(self):
-
-        # Unmount and cleanup original volume
-        ret = self.unmount_volume_and_cleanup_volume(mounts=[self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Failed to umount the vol & cleanup Volume")
-        g.log.info("Successful in umounting the volume and Cleanup")
-
-        # Calling GlusterBaseClass tearDown
-        self.get_super_method(self, 'tearDown')()
-
-    def test_remove_brick_no_commit_followed_by_rebalance(self):
-        """
-        Description: Tests to check that there is no data loss when
-                     remove-brick operation is stopped and then new bricks
-                     are added to the volume.
          Steps :
          1) Create a volume.
          2) Mount the volume using FUSE.
@@ -86,84 +42,69 @@ class TestRemoveBrickNoCommitFollowedByRebalance(GlusterBaseClass):
          9) Calculate the arequal-checksum on the mount-point.
          """
         # Start IO on mounts
-        m_point = self.mounts[0].mountpoint
-        cmd = ("/usr/bin/env python %s create_deep_dirs_with_files "
-               "--dir-length 10 --dir-depth 2 --max-num-of-dirs 1 "
-               "--num-of-files 50 --file-type empty-file %s" % (
-                   self.script_upload_path, m_point))
-        proc = g.run_async(self.mounts[0].client_system,
-                           cmd, user=self.mounts[0].user)
-        g.log.info("IO on %s:%s is started successfully",
-                   self.mounts[0].client_system, m_point)
+        self.mounts = {
+            "client": self.client_list[0],
+            "mountpath": self.mountpoint
+        }
+        file_dir_script = "/usr/share/redant/script/file_dir_ops.py"
+        cmd = (f"python3 {file_dir_script} create_deep_dirs_with_files"
+               " --dir-length 2 --dir-depth 2 --max-num-of-dirs 2 "
+               " --num-of-files 10 --file-type empty-file"
+               f" {self.mountpoint}/")
+        proc = redant.execute_command_async(cmd, self.client_list[0])
 
-        # Validate IO
-        self.assertTrue(
-            validate_io_procs([proc], self.mounts[0]),
-            "IO failed on some of the clients"
-        )
+        # Validate deep dirs and files are created successfully
+        if not redant.validate_io_procs(proc, self.mounts):
+            raise Exception("IO failed on some of the clients")
 
         # Calculate arequal-checksum before starting remove-brick
-        ret, arequal_before = collect_mounts_arequal(self.mounts[0])
-        self.assertTrue(ret, "Collecting arequal-checksum failed")
+        arequal_before = redant.collect_mounts_arequal(self.mounts)
 
         # Form bricks list for volume shrink
-        remove_brick_list = form_bricks_list_to_remove_brick(
-            self.mnode, self.volname, subvol_name=1)
-        self.assertIsNotNone(remove_brick_list, ("Volume %s: Failed to "
-                                                 "form bricks list for "
-                                                 "shrink", self.volname))
-        g.log.info("Volume %s: Formed bricks list for shrink", self.volname)
+        remove_brick_list = (redant.form_bricks_list_to_remove_brick(
+                             self.server_list[0], self.vol_name))
+        if remove_brick_list is None:
+            raise Exception(f"Volume {self.vol_name}: Failed to form bricks"
+                            " list for shrink")
 
         # Shrink volume by removing bricks
-        ret, _, _ = remove_brick(self.mnode, self.volname,
-                                 remove_brick_list, "start")
-        self.assertEqual(ret, 0, ("Volume %s shrink failed ",
-                                  self.volname))
-        g.log.info("Volume %s shrink started ", self.volname)
+        redant.remove_brick(self.server_list[0], self.vol_name,
+                            remove_brick_list, "start")
 
         # Log remove-brick status
-        ret, out, _ = remove_brick(self.mnode, self.volname,
-                                   remove_brick_list, "status")
-        self.assertEqual(ret, 0, ("Remove-brick status failed on %s ",
-                                  self.volname))
-
+        ret = redant.remove_brick(self.server_list[0], self.vol_name,
+                                  remove_brick_list, "status")
         # Check if migration is in progress
-        if r'in progress' in out:
+        if r'in progress' in ret:
             # Stop remove-brick process
-            g.log.info("Stop removing bricks from volume")
-            ret, out, _ = remove_brick(self.mnode, self.volname,
-                                       remove_brick_list, "stop")
-            self.assertEqual(ret, 0, "Failed to stop remove-brick process")
-            g.log.info("Stopped remove-brick process successfully")
+            redant.remove_brick(self.server_list[0], self.vol_name,
+                                remove_brick_list, "stop")
+            redant.logger.info("Stopped remove-brick process successfully")
         else:
-            g.log.error("Migration for remove-brick is complete")
+            redant.logger.error("Migration for remove-brick is complete")
 
         # Sleep for 30 secs so that any running remove-brick process stops
         sleep(30)
 
         # Add bricks to the volume
-        ret = expand_volume(self.mnode, self.volname, self.servers,
-                            self.all_servers_info)
-        self.assertTrue(ret, ("Volume %s: Add-brick failed", self.volname))
-        g.log.info("Volume %s: Add-brick successful", self.volname)
+        ret = redant.expand_volume(self.server_list[0], self.vol_name,
+                                   self.server_list, self.brick_roots,
+                                   distribute_count=3, force=True)
+        if not ret:
+            raise Exception("Failed to add-brick to the volume")
 
         # Tigger rebalance
-        ret, _, _ = rebalance_start(self.mnode, self.volname)
-        self.assertEqual(ret, 0, ("Volume %s: Failed to start rebalance",
-                                  self.volname))
-        g.log.info("Volume %s: Rebalance started ", self.volname)
+        redant.rebalance_start(self.vol_name, self.server_list[0])
 
         # Wait for rebalance to complete
-        ret = wait_for_rebalance_to_complete(self.mnode, self.volname)
-        self.assertTrue(ret, "Rebalance has not completed")
-        g.log.info("Rebalance has completed successfully")
+        ret = redant.wait_for_rebalance_to_complete(self.vol_name,
+                                                    self.server_list[0])
+        if not ret:
+            raise Exception("Rebalace is not yet complete on the volume "
+                            f"{self.vol_name}")
 
         # Calculate arequal-checksum on mount-point
-        ret, arequal_after = collect_mounts_arequal(self.mounts[0])
-        self.assertTrue(ret, "Collecting arequal-checksum failed")
+        arequal_after = self.redant.collect_mounts_arequal(self.mounts)
 
-        # Check if there is any data loss
-        self.assertEqual(set(arequal_before), set(arequal_after),
-                         ("There is data loss"))
-        g.log.info("The checksum before and after rebalance is same."
-                   " There is no data loss.")
+        if arequal_before != arequal_after:
+            raise Exception("There is data loss")
