f832b8a3de405c043b30fcdc2aba6ad5fe8136c6
Sheetal Pamecha <spamecha@redhat.com>
2021-10-10 15:19:56 +0530

    Migrate TC test_memory_leaks_with_files_delete.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/resource_leak/test_memory_leaks_with_files_delete.py
+++ tests/functional/resource_leak/test_memory_leaks_with_files_delete.py
@@ -1,57 +1,46 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.glusterdir import get_dir_contents
-from glustolibs.io.memory_and_cpu_utils import (
-    wait_for_logging_processes_to_stop)
-from glustolibs.gluster.brick_libs import get_all_bricks
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+# disruptive;arb,dist-arb
+import traceback
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['arbiter', 'distributed-arbiter'], ['glusterfs']])
-class TestMemoryLeakWithRm(GlusterBaseClass):
+class TestCase(DParentTest):
 
-    def setUp(self):
+    def terminate(self):
+        """
+        Check if memory logging and cpu usage
+        """
+        try:
+            if self.is_logging:
+                ret = (self.redant.wait_for_logging_processes_to_stop(
+                       self.monitor_proc_dict, cluster=True))
+                if not ret:
+                    raise Exception("ERROR: Failed to stop monitoring "
+                                    "processes")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        self.get_super_method(self, 'setUp')()
-
-        # Set test_id for get gathering
-        self.test_id = self.id()
-
-        # Set I/O flag to false
-        self.is_io_running = False
-
-        # Creating Volume and mounting the volume
-        ret = self.setup_volume_and_mount_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-
-    def tearDown(self):
-
-        # Unmounting and cleaning volume
-        ret = self.unmount_volume_and_cleanup_volume(self.mounts)
-        if not ret:
-            raise ExecutionError("Unable to delete volume %s" % self.volname)
-
-        self.get_super_method(self, 'tearDown')()
-
-    def test_memory_leak_with_rm(self):
+    def run_test(self, redant):
         """
         Test case:
         1. Create a volume, start it and mount it.
@@ -60,54 +49,55 @@ class TestMemoryLeakWithRm(GlusterBaseClass):
         4. Check if the files are deleted from backend
         5. Check if there are any memory leaks and OOM killers.
         """
+        self.is_logging = False
         # Start monitoring resource usage on servers and clients
-        monitor_proc_dict = self.start_memory_and_cpu_usage_logging(
-            self.test_id, count=30)
-        self.assertIsNotNone(monitor_proc_dict,
-                             "Failed to start monitoring on servers and "
-                             "clients")
+        self.test_id = f"{self.test_name}-{self.volume_type}-logusage"
+        self.monitor_proc_dict = (redant.log_memory_and_cpu_usage_on_cluster(
+                                  self.server_list, self.client_list,
+                                  self.test_id, count=30))
+        if not self.monitor_proc_dict:
+            raise Exception("Failed to start monitoring on servers and "
+                            "clients")
+
+        self.is_logging = True
         # Create files on mount point
-        cmd = ('cd %s;for i in {1..10000};'
+        cmd = (f'cd {self.mountpoint};for i in {{1..10000}};'
                'do dd if=/dev/urandom bs=200K count=1 of=file$i;done;'
-               'rm -rf %s/file*'
-               % (self.mounts[0].mountpoint, self.mounts[0].mountpoint))
-        ret, _, _ = g.run(self.mounts[0].client_system, cmd)
-        self.assertEqual(ret, 0, "Failed to create and delete files on"
-                         " mountpoint")
-        g.log.info("Successfully created and removed files on mountpoint")
+               f'rm -rf {self.mountpoint}/file*')
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
         # Delete files from mount point and check if all files
         # are deleted or not from mount point as well as backend bricks.
-        ret, _, _ = g.run(self.clients[0],
-                          "rm -rf {}/*".format(self.mounts[0].mountpoint))
-        self.assertFalse(ret, "rm -rf * failed on mount point")
+        cmd = f"rm -rf {self.mountpoint}/*"
+        redant.execute_abstract_op_node(cmd, self.client_list[0])
 
-        ret = get_dir_contents(self.clients[0],
-                               "{}/".format(self.mounts[0].mountpoint))
-        self.assertEqual(ret, [], "Unexpected: Files and directories still "
-                         "seen from mount point")
+        ret = redant.get_dir_contents(self.mountpoint, self.client_list[0])
+        if ret:
+            raise Exception("Mountpoint is not empty")
 
-        for brick in get_all_bricks(self.mnode, self.volname):
+        for brick in redant.get_all_bricks(self.vol_name, self.server_list[0]):
             node, brick_path = brick.split(":")
-            ret = get_dir_contents(node, "{}/".format(brick_path))
-            self.assertEqual(ret, [], "Unexpected: Files and dirs still seen "
-                             "on brick %s on node %s" % (brick_path, node))
-        g.log.info("rm -rf * on mount point successful")
+            ret = redant.get_dir_contents(brick_path, node)
+            if ret:
+                raise Exception("Bricks are not empty")
 
         # Wait for monitoring processes to complete
-        ret = wait_for_logging_processes_to_stop(monitor_proc_dict,
-                                                 cluster=True)
-        self.assertTrue(ret,
-                        "ERROR: Failed to stop monitoring processes")
+        ret = redant.wait_for_logging_processes_to_stop(self.monitor_proc_dict,
+                                                        cluster=True)
+        if not ret:
+            raise Exception("ERROR: Failed to stop monitoring processes")
+
+        self.is_logging = False
 
         # Check if there are any memory leaks and OOM killers
-        ret = self.check_for_memory_leaks_and_oom_kills_on_servers(
-            self.test_id)
-        self.assertFalse(ret,
-                         "Memory leak and OOM kills check failed on servers")
+        ret = (redant.check_for_memory_leaks_and_oom_kills_on_servers(
+               self.test_id, self.server_list, self.vol_name))
+        if ret:
+            raise Exception("Memory leak and OOM kills check failed on"
+                            " servers")
 
-        ret = self.check_for_memory_leaks_and_oom_kills_on_clients(
-            self.test_id)
-        self.assertFalse(ret,
-                         "Memory leak and OOM kills check failed on clients")
-        g.log.info("No memory leaks or OOM kills found on serves and clients")
+        ret = (redant.check_for_memory_leaks_and_oom_kills_on_clients(
+               self.test_id, self.client_list))
+        if ret:
+            raise Exception("Memory leak and OOM kills check failed on"
+                            " clients")
