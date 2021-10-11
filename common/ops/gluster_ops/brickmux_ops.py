15ac871072bdf6661562ddc54fb032d55c53b16e
Sheetal Pamecha <spamecha@redhat.com>
2021-10-11 13:28:42 +0530

    Migrate TC test_cli_reset_commands_behaviour.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

---
+++ common/ops/gluster_ops/brickmux_ops.py
@@ -0,0 +1,139 @@
+"""
+Module for gluster brick multiplex operations
+"""
+
+from common.ops.abstract_ops import AbstractOps
+
+
+class BrickMuxOps(AbstractOps):
+    """
+    Class which is responsible for methods for brick multiplexing
+    operations.
+    """
+
+    def get_brick_mux_status(self, server: str) -> str:
+        """
+        Gets brick multiplex status
+        Args:
+            server (str): Node on which cmd has to be executed.
+
+        Returns:
+            str : Brick multiplex status. None otherwise
+        """
+        cmd = ("gluster v get all all | grep cluster.brick-multiplex |"
+               "awk '{print $2}'")
+        ret = self.execute_abstract_op_node(cmd, server)
+        out = ret['msg'][0].split()[0].strip()
+        return out
+
+    def is_brick_mux_enabled(self, server: str) -> bool:
+        """
+        Gets brick multiplex status and checks for positive and
+        negative status
+        Args:
+            server (str): Node on which cmd has to be executed.
+
+        Returns:
+            True : Brick multiplex status is one of 'true, on, 1, enable'.
+            False : Brick multiplex status is one of 'false, off, 0, disable'.
+            Exception : otherwise
+        """
+        positive_states = ['true', 'on', '1', 'enable']
+        negative_states = ['off', 'disable', '0', 'false']
+        status = self.get_brick_mux_status(server)
+        if status in positive_states:
+            return True
+        elif status in negative_states:
+            return False
+        else:
+            raise Exception(f"Brick mux status {status} is incorrect")
+
+    def enable_brick_mux(self, server: str) -> bool:
+        """
+        Enables brick multiplex operation on all servers
+
+        Args:
+            server (str): Node on which cmd has to be executed.
+
+        Returns:
+            bool : True if successfully enabled brickmux. False otherwise.
+        """
+        cmd = "gluster v set all cluster.brick-multiplex enable --mode=script"
+        ret = self.execute_abstract_op_node(cmd, server, False)
+        if ret['error_code'] != 0:
+            self.logger.error("Failed to enable brick multiplex")
+            return False
+        return True
+
+    def disable_brick_mux(self, server: str) -> bool:
+        """
+        Disables brick multiplex operation on all servers
+
+        Args:
+            server (str): Node on which cmd has to be executed.
+
+        Returns:
+            bool : True if successfully disabled brickmux. False otherwise.
+        """
+        cmd = "gluster v set all cluster.brick-multiplex disable --mode=script"
+        ret = self.execute_abstract_op_node(cmd, server, False)
+        if ret['error_code'] != 0:
+            self.logger.error("Failed to disable brick multiplex")
+            return False
+        return True
+
+    def check_brick_pid_matches_glusterfsd_pid(self, volname: str,
+                                               node: str) -> bool:
+        """Checks for brick process(es) both volume status
+           and 'ps -eaf | grep glusterfsd' matches for
+           the given volume
+
+        Args:
+            node (str): Node on which cmd has to be executed.
+            volname (str): Name of the volume.
+
+        Returns:
+            bool : True if pid's matches. False otherwise.
+        """
+        _rc = True
+        bricks_list = self.get_all_bricks(volname, node)
+        for brick in bricks_list:
+            brick_node, brick_path = brick.split(":")
+            ret = self.get_volume_status(volname, node)
+            for i in ret[volname]['node']:
+                if i['path'] == brick_path:
+                    brick_pid = i['pid']
+
+            cmd = "pgrep -x glusterfsd"
+            ret = self.execute_abstract_op_node(cmd, brick_node, False)
+            if ret['error_code'] != 0:
+                raise Exception(f"Failed to get glusterfsdpid on {brick_node}")
+                _rc = False
+            else:
+                glusterfsd_pid = ret['msg'][0].split()[0].strip()
+
+            if brick_pid not in glusterfsd_pid:
+                raise Exception(f"Brick pid {brick_pid} doesn't match "
+                                "glusterfsd pid {glusterfsd_pid} of the node")
+                _rc = False
+        return _rc
+
+    def get_brick_processes_count(self, node: str) -> int:
+        """
+        Get the brick process count for a given node.
+
+        Args:
+            node (str): Node on which brick process has to be counted.
+
+        Returns:
+            int: Number of brick processes running on the node.
+            None: If the command fails to execute.
+        """
+        cmd = "pgrep -x glusterfsd"
+        ret = self.execute_abstract_op_node(cmd, node, False)
+        if ret['error_code'] != 0:
+            return None
+        else:
+            list_of_pids = ret['msg'].split("\n")
+            list_of_pids.pop()
+            return len(list_of_pids)
--- tests/functional/multiplex/test_cli_reset_commands_behaviour.py
+++ tests/functional/multiplex/test_cli_reset_commands_behaviour.py
@@ -1,89 +1,32 @@
-#  Copyright (C) 2019  Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2019  Red Hat, Inc. <http://www.redhat.com>
 
-from time import sleep
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import (GlusterBaseClass, runs_on)
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.brickmux_ops import (
-    disable_brick_mux, is_brick_mux_enabled,
-    enable_brick_mux, get_brick_mux_status,
-    check_brick_pid_matches_glusterfsd_pid, get_brick_processes_count)
-from glustolibs.gluster.volume_libs import setup_volume, cleanup_volume
-from glustolibs.gluster.volume_ops import (get_volume_list,
-                                           volume_stop,
-                                           get_volume_options,
-                                           volume_start)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-@runs_on([['replicated'], ['glusterfs']])
-class TestBrickMultiplexingResetCommand(GlusterBaseClass):
-    """
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Description:
     Tests for brick multiplexing reset command
-    """
-    def setUp(self):
-        # Calling GlusterBaseClass setUp
-        self.get_super_method(self, 'setUp')()
+"""
 
-        # Enable brick multiplexing
-        g.log.info("Checking for brick multiplexing status...")
-        if not is_brick_mux_enabled(self.mnode):
-            g.log.info("Enabling brick multiplexing...")
-            if not enable_brick_mux(self.mnode):
-                raise ExecutionError("Failed to enable brick multiplexing")
-            g.log.info("Enabled brick multiplexing successfully")
+# disruptive;rep
+from time import sleep
+from tests.d_parent_test import DParentTest
 
-    def tearDown(self):
-        # Stopping all volumes
-        g.log.info("Starting to Cleanup all Volumes")
-        volume_list = get_volume_list(self.mnode)
-        for volume in volume_list:
-            ret = cleanup_volume(self.mnode, volume)
-            if not ret:
-                raise ExecutionError("Failed to cleanup Volume %s" % volume)
-            g.log.info("Volume: %s cleanup is done", volume)
-        g.log.info("Successfully Cleanedup all Volumes")
 
-        # Disable brick multiplexing
-        g.log.info("Checking for brick multiplexing status...")
-        if is_brick_mux_enabled(self.mnode):
-            g.log.info("Disabling brick multiplexing...")
-            if not disable_brick_mux(self.mnode):
-                raise ExecutionError("Failed to disable brick multiplexing")
-            g.log.info("Disabled brick multiplexing successfully")
+class TestCase(DParentTest):
 
-        # Calling GlusterBaseClass teardown
-        self.get_super_method(self, 'tearDown')()
-
-    def use_config_setup_volumes(self):
-        """
-        A function to setup volumes based on volume_configs.
-        """
-        for volume_config in self.volume_configs:
-            ret = setup_volume(mnode=self.mnode,
-                               all_servers_info=self.all_servers_info,
-                               volume_config=volume_config,
-                               force=False)
-            if not ret:
-                raise ExecutionError("Failed to setup Volume %s"
-                                     % volume_config['name'])
-            g.log.info("Successful in setting volume %s",
-                       volume_config['name'])
-
-    def test_cli_reset_commands_behaviour(self):
+    def run_test(self, redant):
         """
         1. Set cluster.brick-multiplex to enabled.
         2. Create and start 2 volumes of type 1x3 and 2x3.
@@ -96,118 +39,85 @@ class TestBrickMultiplexingResetCommand(GlusterBaseClass):
         9. Check the if pids match and check if more
            than one pids of glusterfsd is present.
         """
-        # pylint: disable=too-many-statements
-        # Setup Volumes
-        self.volume_configs = []
+        # Enable brick multiplexing
+        if not redant.is_brick_mux_enabled(self.server_list[0]):
+            if not redant.enable_brick_mux(self.server_list[0]):
+                raise Exception("Failed to enable brick multiplexing")
 
-        # Define volumes to create
-        # Define replicated volume
-        self.volume['voltype'] = {
-            'type': 'replicated',
-            'replica_count': 3,
-            'transport': 'tcp'}
-
-        volume_config = {'name': '%s' % self.volume['voltype']['type'],
-                         'servers': self.servers,
-                         'voltype': self.volume['voltype']}
-        self.volume_configs.append(volume_config)
-
-        # Define 2x3 distributed-replicated volume
-        self.volume['voltype'] = {
-            'type': 'distributed-replicated',
-            'dist_count': 2,
-            'replica_count': 3,
-            'transport': 'tcp'}
-
-        volume_config = {'name': '%s' % self.volume['voltype']['type'],
-                         'servers': self.servers,
-                         'voltype': self.volume['voltype']}
-        self.volume_configs.append(volume_config)
-
-        # Create volumes using the config.
-        self.use_config_setup_volumes()
+        # Create and start 2 volumes of type 1x3 and 2x3
+        volume_type_config = {
+            'rep': {
+                'dist_count': 1,
+                'replica_count': 2,
+                'transport': 'tcp'},
+            'dist-rep': {
+                'dist_count': 2,
+                'replica_count': 3,
+                'transport': 'tcp'}
+        }
+        for vol_type, conf in volume_type_config.items():
+            vol_name = f"vol-{vol_type}"
+            redant.setup_volume(vol_name, self.server_list[0],
+                                conf, self.server_list,
+                                self.brick_roots, force=True)
 
         # Check if volume option cluster.brick-multiplex is enabled
-        volume_list = get_volume_list(self.mnode)
+        volume_list = redant.get_volume_list(self.server_list[0])
+
         for volume in volume_list:
-            options_dict = get_volume_options(self.mnode, volume)
-            self.assertEqual(options_dict['cluster.brick-multiplex'], 'enable',
-                             'Option brick-multiplex is not enabled')
-            g.log.info('Option brick-multiplex is enabled for volume %s',
-                       volume)
+            option_dict = (redant.get_volume_options(volume,
+                           'cluster.brick-multiplex', self.server_list[0]))
+            if option_dict['cluster.brick-multiplex'] != 'enable':
+                raise Exception("Option brick-multiplex is not enabled")
 
         # Reset cluster
-        g.log.info("Reset cluster...")
-        cmd = 'gluster v reset all'
-        ret, _, _ = g.run(self.mnode, cmd)
-        self.assertFalse(ret, "Failed on reset cluster")
-        g.log.info("Successfully reset cluster")
+        redant.reset_volume_option("all", "all", self.server_list[0])
 
         # Check if brick-multiplex is disabled
-        g.log.info("Checking for brick multiplexing status...")
-        self.assertEqual('disable', get_brick_mux_status(self.mnode),
-                         "Brick multiplexing status is not 'disable'")
-        g.log.info("Brick multiplexing status is 'disable'")
+        if redant.is_brick_mux_enabled(self.server_list[0]):
+            raise Exception("Brick multiplexing status is not disable")
 
         # Create new distributed-replicated volume
         # Define new 2x3 distributed-replicated volume
-        new_vol = 'new_vol'
-        self.volume['voltype'] = {
-            'type': 'distributed-replicated',
+        volume_type_config = {
             'dist_count': 2,
             'replica_count': 3,
-            'transport': 'tcp'}
-
-        volume_config = {'name': '%s' % new_vol,
-                         'servers': self.servers,
-                         'voltype': self.volume['voltype']}
-        self.volume_configs.append(volume_config)
-
-        # Create volumes using the config.
-        self.use_config_setup_volumes()
+            'transport': 'tcp'
+        }
+        vol_name = "new_vol"
+        redant.setup_volume(vol_name, self.server_list[0],
+                            volume_type_config, self.server_list,
+                            self.brick_roots, force=True)
 
         # Resetting brick-mux back to enabled.
-        g.log.info("Enabling brick multiplexing...")
-        if not enable_brick_mux(self.mnode):
-            raise ExecutionError("Failed to enable brick multiplexing")
-        g.log.info("Enabled brick multiplexing successfully")
+        if not redant.enable_brick_mux(self.server_list[0]):
+            raise Exception("Failed to enable brick multiplexing")
 
         # Restart all volumes
-        g.log.info("Restarting all volumes...")
-        volume_list = get_volume_list(self.mnode)
+        volume_list = redant.get_volume_list(self.server_list[0])
         for volume in volume_list:
             # Stop the volume
-            g.log.info("Stopping volume %s...", volume)
-            ret, _, err = volume_stop(self.mnode, volume)
-            self.assertFalse(ret, "Failed on stopping volume %s: %s"
-                             % (volume, err))
-            g.log.info("Stopped %s successfully", volume)
+            redant.volume_stop(volume, self.server_list[0])
 
             # Sleeping for 2 seconds between stop and start.
             sleep(2)
 
             # Start the volume
-            g.log.info("Starting volume %s...", volume)
-            ret, _, err = volume_start(self.mnode, volume)
-            self.assertFalse(ret, "Failed on starting volume %s: %s"
-                             % (volume, err))
-            g.log.info("Started %s successfully", volume)
-        g.log.info("Restarted all volumes successfully")
+            redant.volume_start(volume, self.server_list[0])
 
         # Check if bricks pid don`t match glusterfsd pid
-        g.log.info("Checking if bricks pid don`t match glusterfsd pid...")
         for volume in volume_list:
-            g.log.info("Checking if bricks pid don`t match glusterfsd pid "
-                       "for %s volume...", volume)
-            self.assertTrue(
-                check_brick_pid_matches_glusterfsd_pid(self.mnode, volume),
-                "Bricks pid match glusterfsd pid for %s volume..." % volume)
-            g.log.info("Bricks pid don`t match glusterfsd pid "
-                       "for %s volume...", volume)
+            if not (redant.check_brick_pid_matches_glusterfsd_pid(volume,
+                    self.server_list[0])):
+                raise Exception("Bricks pid match glusterfsd pid for volume")
 
         # Checking if the number of glusterfsd is more than one
-        for server in self.servers:
-            ret = get_brick_processes_count(server)
-            self.assertEqual(ret, 1,
-                             "Number of glusterfsd is more than one.")
-        g.log.info("Only one glusterfsd found on all the nodes.")
+        for server in self.server_list:
+            ret = redant.get_brick_processes_count(server)
+            if ret != 1:
+                raise Exception("Number of glusterfsd is more than one.")
+
+        # Disable brick multiplexing
+        if redant.is_brick_mux_enabled(self.server_list[0]):
+            if not redant.disable_brick_mux(self.server_list[0]):
+                raise Exception("Failed to disable brick multiplexing")
