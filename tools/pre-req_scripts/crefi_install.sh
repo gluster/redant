1187c65d8d5f65055a6d03339ea1c0583d054a84
nik-redhat <nladha@redhat.com>
2021-10-11 19:35:14 +0530

    Add crefi installation script
    Signed-off-by: nik-redhat <nladha@redhat.com>

---
+++ tools/pre-req_scripts/crefi_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pip3 uninstall -y crefi
+mkdir /tmp/crefi_repo
+cd /tmp/crefi_repo
+git clone https://github.com/vijaykumar-koppad/Crefi.git
+cd Crefi
+python3 setup.py install
+cd .
+rm -rf /tmp/crefi_repo
