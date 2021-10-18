3c4195e2d980c7a51631a3478ab5e9d4ba9a3671
Sheetal Pamecha <spamecha@redhat.com>
2021-10-18 17:34:31 +0530

    Migrate TC test_memory_leak_in_shd_with_cache_invalidation_on.py
    Updates: #292
    Signed-off-by: Sheetal Pamecha <spamecha@redhat.com>

--- tests/functional/resource_leak/test_memory_leak_in_shd_with_cache_invalidation_on.py
+++ tests/functional/resource_leak/test_memory_leak_in_shd_with_cache_invalidation_on.py
@@ -1,66 +1,50 @@
-#  Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License along`
-#  with this program; if not, write to the Free Software Foundation, Inc.,
-#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+Copyright (C) 2020 Red Hat, Inc. <http://www.redhat.com>
 
-from glusto.core import Glusto as g
-from glustolibs.gluster.gluster_base_class import GlusterBaseClass, runs_on
-from glustolibs.gluster.exceptions import ExecutionError
-from glustolibs.gluster.heal_ops import trigger_heal
-from glustolibs.gluster.volume_ops import set_volume_options
-from glustolibs.io.utils import (validate_io_procs, wait_for_io_to_complete)
-from glustolibs.io.memory_and_cpu_utils import (
-    wait_for_logging_processes_to_stop)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along`
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+# disruptive;arb,rep,disp,dist-arb,dist-rep,dist-disp
+import traceback
+from tests.d_parent_test import DParentTest
 
 
-@runs_on([['distributed-replicated', 'distributed-arbiter',
-           'distributed-dispersed', 'replicated',
-           'arbiter', 'dispersed'], ['glusterfs']])
-class TestMemoryLeakInShdWithCacheInvalidationOn(GlusterBaseClass):
+class TestMemoryLeakInShdWithCacheInvalidationOn(DParentTest):
 
-    def setUp(self):
+    def terminate(self):
+        """
+        Check if memory logging and cpu usage
+        """
+        try:
+            if self.is_logging:
+                ret = (self.redant.wait_for_logging_processes_to_stop(
+                       self.monitor_proc_dict, cluster=True))
+                if not ret:
+                    raise Exception("ERROR: Failed to stop monitoring "
+                                    "processes")
+            if self.is_io_running:
+                if not self.redant.wait_for_io_to_complete(self.procs_list,
+                                                           self.mounts):
+                    raise Exception("Failed to wait for I/O to complete")
+        except Exception as error:
+            tb = traceback.format_exc()
+            self.redant.logger.error(error)
+            self.redant.logger.error(tb)
+        super().terminate()
 
-        self.get_super_method(self, 'setUp')()
-
-        # Set test_id for get gathering
-        self.test_id = self.id()
-
-        # Set I/O flag to false
-        self.is_io_running = False
-
-        # Creating Volume and mounting the volume
-        ret = self.setup_volume_and_mount_volume([self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Volume creation or mount failed: %s"
-                                 % self.volname)
-
-    def tearDown(self):
-
-        # Wait for I/O to complete
-        if self.is_io_running:
-            if wait_for_io_to_complete(self.list_of_io_processes,
-                                       self.mounts[0]):
-                raise ExecutionError("Failed to wait for I/O to complete")
-
-        # Unmounting and cleaning volume
-        ret = self.unmount_volume_and_cleanup_volume([self.mounts[0]])
-        if not ret:
-            raise ExecutionError("Unable to delete volume %s" % self.volname)
-
-        self.get_super_method(self, 'tearDown')()
-
-    def test_memory_leak_in_shd_with_cache_invalidation_on(self):
+    def run_test(self, redant):
         """
         Test case:
         1. Create a volume, start it and mount it.
@@ -69,49 +53,54 @@ class TestMemoryLeakInShdWithCacheInvalidationOn(GlusterBaseClass):
         4. Run gluster volume heal command in a loop
         5. Check if there are any memory leaks and OOM killers on servers.
         """
+        self.is_io_running = False
+        self.is_logging = False
+        self.mounts = redant.es.get_mnt_pts_dict_in_list(self.vol_name)
+
         # Start monitoring resource usage on servers and clients
-        monitor_proc_dict = self.start_memory_and_cpu_usage_logging(
-            self.test_id, count=10)
-        self.assertIsNotNone(monitor_proc_dict,
-                             "Failed to start monitoring on servers and"
-                             " clients")
+        self.test_id = f"{self.test_name}-{self.volume_type}-logusage"
+        self.monitor_proc_dict = (redant.log_memory_and_cpu_usage_on_cluster(
+                                  self.server_list, self.client_list,
+                                  self.test_id, count=10))
+        if not self.monitor_proc_dict:
+            raise Exception("Failed to start monitoring on servers and "
+                            "clients")
+        self.is_logging = True
 
         # Set features.cache-invalidation to ON
-        ret = set_volume_options(self.mnode, self.volname,
-                                 {'features.cache-invalidation': 'on'})
-        self.assertTrue(ret, "Failed to set features.cache-invalidation to ON")
-        g.log.info("Successfully set features.cache-invalidation to ON")
+        redant.set_volume_options(self.vol_name,
+                                  {'features.cache-invalidation': 'on'},
+                                  self.server_list[0])
 
         # Start multiple I/O from mount points
-        self.list_of_io_processes = []
-        cmd = ("cd {};for i in `seq 1 1000`;do echo 'abc' > myfile;done"
-               .format(self.mounts[0].mountpoint))
-        ret = g.run_async(self.mounts[0].client_system, cmd)
-        self.list_of_io_processes = [ret]
+        self.procs_list = []
+        cmd = (f"cd {self.mountpoint};for i in `seq 1 1000`;"
+               "do echo 'abc' > myfile;done")
+        ret = redant.execute_command_async(cmd, self.client_list[0])
+        self.procs_list.append(ret)
         self.is_io_running = True
 
         # Run gluster volume heal command in a loop for 100 iterations
-        for iteration in range(0, 100):
-            g.log.info("Running gluster volume heal command for %d time",
-                       iteration)
-            ret = trigger_heal(self.mnode, self.volname)
-            self.assertTrue(ret, "Heal command triggered successfully")
-        g.log.info("Ran gluster volume heal command in a loop for "
-                   "100 iterations.")
+        for _ in range(0, 100):
+            if not redant.trigger_heal(self.vol_name, self.server_list[0]):
+                raise Exception("Starting heal failed")
 
         # Wait for I/O to complete and validate I/O on mount points
-        ret = validate_io_procs(self.list_of_io_processes, self.mounts[0])
-        self.assertTrue(ret, "I/O failed on mount point")
+        if not redant.validate_io_procs(self.procs_list, self.mounts):
+            raise Exception("IO didn't complete or failed on client")
         self.is_io_running = False
 
         # Wait for monitoring processes to complete
-        ret = wait_for_logging_processes_to_stop(monitor_proc_dict,
-                                                 cluster=True)
-        self.assertTrue(ret,
-                        "ERROR: Failed to stop monitoring processes")
+        ret = redant.wait_for_logging_processes_to_stop(self.monitor_proc_dict,
+                                                        cluster=True)
+        if not ret:
+            raise Exception("ERROR: Failed to stop monitoring processes")
+
+        self.is_logging = False
 
         # Check if there are any memory leaks and OOM killers
-        ret = self.check_for_memory_leaks_and_oom_kills_on_servers(
-            self.test_id)
-        self.assertFalse(ret,
-                         "Memory leak and OOM kills check failed on servers")
+        ret = (redant.check_for_memory_leaks_and_oom_kills_on_servers(
+               self.test_id, self.server_list, self.vol_name))
+        if ret:
+            raise Exception("Memory leak and OOM kills check failed on"
+                            " servers")
