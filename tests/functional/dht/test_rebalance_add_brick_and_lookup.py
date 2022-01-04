236a7bc1fae8a5df6a4c1774036140dfeed69f3a
nik-redhat <nladha@redhat.com>
2022-01-04 11:27:36 +0530

    check rhgs install for tc
    Signed-off-by: nik-redhat <nladha@redhat.com>

--- tests/functional/dht/test_rebalance_add_brick_and_lookup.py
+++ tests/functional/dht/test_rebalance_add_brick_and_lookup.py
@@ -26,6 +26,25 @@ from tests.d_parent_test import DParentTest
 
 class TestRebalanceWithAddBrickAndLookup(DParentTest):
 
+    @DParentTest.setup_custom_enable
+    def setup_test(self):
+        """
+        Override the volume create, start and mount in parent_run_test
+        """
+        # Run TC only on RHGS installation
+        self.redant.check_rhgs_installation(self.server_list)
+
+        conf_hash = self.vol_type_inf[self.volume_type]
+        self.redant.setup_volume(self.vol_name, self.server_list[0],
+                                 conf_hash, self.server_list,
+                                 self.brick_roots)
+        self.mountpoint = (f"/mnt/{self.vol_name}")
+        self.redant.execute_abstract_op_node(f"mkdir -p "
+                                             f"{self.mountpoint}",
+                                             self.client_list[0])
+        self.redant.volume_mount(self.server_list[0], self.vol_name,
+                                 self.mountpoint, self.client_list[0])
+
     def run_test(self, redant):
         """
         Steps:
